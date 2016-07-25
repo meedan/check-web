d7e6b9fc14f20d5ea7662ef16c4da7156286705d
Ahmed Mostafa <ahmed.nasser.swe@gmail.com>
2016-07-25 20:37:44 +0200

    Adding tabs to user profile page

--- src/app/components/UserProfileViewer.js
+++ src/app/components/UserProfileViewer.js
@@ -2,19 +2,49 @@ import React, { Component, PropTypes } from 'react';
 import UserProfileBasicInfo from './UserProfileBasicInfo'
 import AnnotationsListView from './AnnotationsListView'
 import source from './Data_Source'
+import {Tabs, Tab} from 'material-ui/lib/Tabs';
+ var injectTapEventPlugin = require("react-tap-event-plugin");
+ injectTapEventPlugin();
 
 class UserProfileViewer extends Component {
+  constructor(props) {
+     super(props);
+     this.state = {
+       value: 'a',
+     };
+   }
 
-  render () {
-    return(
+   handleChange = (value) => {
+     this.setState({
+       value: value,
+     });
+   };
 
-    <div>
-      <UserProfileBasicInfo source={source}/>
-      <AnnotationsListView annotations={source.annotations} />
+   render() {
+     return (
+       <div>
+       <UserProfileBasicInfo source={source}/>
 
-    </div>
+         <Tabs
 
-    )
-  }
+         >
+           <Tab label="Annotations"  >
+             <div>
+                <AnnotationsListView annotations={source.annotations} />
+             </div>
+           </Tab>
+           <Tab label="Tab B" >
+             <div>
+                <p>
+                 This is another example of a controllable tab. Remember, if you
+                 use controllable Tabs, you need to give all of your tabs values or else
+                 you wont be able to select them.
+               </p>
+             </div>
+           </Tab>
+         </Tabs>
+        </div>
+     );
+   }
 }
 export default UserProfileViewer;
