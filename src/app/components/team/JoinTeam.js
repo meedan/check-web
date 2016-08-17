e1c39aa77104cb337c3701f3a1806529456a9c27
Zach Alexander <zach@zachalexander.com>
2016-08-17 00:45:16 -0700

    Prototype of request to join team page

--- src/app/components/team/JoinTeam.js
+++ src/app/components/team/JoinTeam.js
@@ -2,22 +2,51 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 class JoinTeam extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRequestSent: false
+    };
+  }
+
+  handleRequestAccess(e) {
+    e.preventDefault();
+    this.setState({isRequestSent: true});
+  }
+
   render() {
+    const team = {
+      displayName: 'Meedan',
+      subdomain: 'meedan'
+    };
+    const teamUrl = 'https://' + team.subdomain + '.checkdesk.org/';
+    var isLoggedIn = true;
+    var isRequestSent = this.state.isRequestSent;
+
     return (
       <div className='join-team'>
-        <h2 className='join-team__heading'>Request to Join</h2>
-
-        Request to join
-
-        > bellingcat.checkdesk.org/join
-
-
-        <h3 className='join-team__join-requests'>Join requests</h3>
-
-        // * John Smith (2 public teams, 8 public reports)  accept / ignore
-
-        <h3 className='join-team__join-requests'>Members</h3>
-
+        <h2 className='join-team__main-heading'>Request to Join</h2>
+        <div className='join-team__blurb'>
+          <p className='join-team__blurb-graf'>To request access to the <a href={teamUrl}>{team.displayName}</a> Checkdesk, {isLoggedIn ? 'click here:' : 'please sign in:'}</p>
+          {(() => {
+            if (isLoggedIn) {
+              return (
+                <div>
+                  <button
+                    className={'join-team__button' + (isRequestSent ? ' join-team__button--submitted' : '')}
+                    onClick={this.handleRequestAccess.bind(this)}
+                    disabled={isRequestSent}
+                  >
+                    {isRequestSent ? 'Request Sent' : 'Request to Join'}
+                  </button>
+                  <p className='join-team__blurb-graf'>Your request {isRequestSent ? 'has been' : 'will be'} sent to the project admins for approval.</p>
+                </div>
+              );
+            } else {
+              return (<Link to='/signin' className='join-team__button-link'><button className='join-team__button'>Sign In »</button></Link>);
+            }
+          })()}
+        </div>
       </div>
     );
   }
