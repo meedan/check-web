60a8418c7eacd2a3a0fdf5196b70f24a4a32e73d
Zach Alexander <zach@zachalexander.com>
2016-08-26 02:13:34 -0700

    Tests & WIP redirecting / to relevant team pages
    I haven't been able to try this at all because my checkdesk
    app is currently broken, so it might be wildly incorrect, but
    hopefully the intent is clear from the code. 
    
    In case not: We want to send users from / to /teams/new if they
    don't have a team yet. If they do, send them to /team/:current.

--- src/app/components/IndexComponent.js
+++ src/app/components/IndexComponent.js
@@ -1,13 +1,43 @@
 import React, { Component, PropTypes } from 'react';
+import Relay from 'react-relay';
+import MeRoute from '../relay/MeRoute';
 
 class IndexComponent extends Component {
   render() {
-    return (
-      <div>
-        <h2 className="main-title">Welcome to Checkdesk</h2>
-      </div>
-    );
+    return null;
   }
 }
 
-export default IndexComponent;
+const IndexContainer = Relay.createContainer(IndexComponent, {
+  fragments: {
+    me: () => Relay.QL`
+      fragment on User {
+        current_team {
+          id
+        }
+      }
+    `
+  }
+});
+
+class Index extends Component {
+  render() {
+    var route = new MeRoute();
+    return (<Relay.RootContainer Component={IndexContainer} route={route} />);
+  }
+
+  componentDidMount() {
+    var redirectTo;
+    const currentTeam = this.props.me ? this.props.me.current_team : null;
+
+    if (currentTeam) {
+      redirectTo = ('/team/' + currentTeam.id);
+    } else {
+      redirectTo = ('/teams/new');
+    }
+
+    this.props.history.push(redirectTo);
+  }
+}
+
+export default Index;
