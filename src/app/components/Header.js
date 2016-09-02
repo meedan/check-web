8358691476aed61d2494dfc651f72fa8d6d308a4
Zach Alexander <zach@zachalexander.com>
2016-09-02 11:01:04 -0700

    Project page and media list markup & styling

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,20 +4,34 @@ import TeamHeader from './team/TeamHeader';
 import ProjectHeader from './project/ProjectHeader';
 
 class Header extends Component {
+  isProjectRoute() {
+    return (
+      this.props.location.pathname &&
+      this.props.location.pathname.match(/project\/[0-9]+/)
+    );
+  }
+
   render() {
-    const { state, logout } = this.props;
+    const { state } = this.props;
 
     if (!state.app.token) {
       return null;
     }
 
-    return (
-      <header className='header'>
-        <div className='header__team'><TeamHeader {...this.props} /></div>
-
-        <ProjectHeader {...this.props} />
-      </header>
-    );
+    if (this.isProjectRoute()) {
+      return (
+        <header className='header header--project'>
+          <div className='header__team'><TeamHeader {...this.props} /></div>
+          <ProjectHeader {...this.props} />
+        </header>
+      );
+    } else {
+      return (
+        <header className='header header--todo'>
+          <img className='header--todo__brand' src='/img/logo/logo-1.svg'/>
+        </header>
+      )
+    }
   }
 }
 
