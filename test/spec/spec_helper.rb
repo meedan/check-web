399a9ebee9b3cd4f4016726cd48f262119b15fe7
Caio SBA <caiosba@gmail.com>
2016-09-29 11:26:00 -0300

    Fixing configuration for rspec-retry

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -4,7 +4,11 @@ require 'rspec/retry'
 RSpec.configure do |config|
   config.verbose_retry = true
   config.default_retry_count = 3
-  config.exceptions_to_retry = [Net::ReadTimeout]
+  config.default_sleep_interval = 3
+
+  config.around :each do |ex|
+    ex.run_with_retry retry: 3
+  end
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
