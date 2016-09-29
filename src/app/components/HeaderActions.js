c5042d264766678c533359f53f1c5dda062b2ba1
Zach Alexander <zach@zachalexander.com>
2016-09-28 23:17:35 -0700

    Clean up HeaderActions component markup & styles

--- src/app/components/HeaderActions.js
+++ src/app/components/HeaderActions.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import UserMenuRelay from '../relay/UserMenuRelay';
 import { logout } from '../actions/actions';
+import FontAwesome from 'react-fontawesome';
 
 class HeaderActions extends Component {
   constructor(props) {
@@ -27,14 +28,14 @@ class HeaderActions extends Component {
     const project = this.props.project;
 
     return (
-      <div className={this.bemClass('project-header__project-settings', this.state.isSettingsMenuOpen, '--active')}>
-        <i className='project-header__project-search-icon fa fa-search'></i>
-        <i className='project-header__project-settings-icon fa fa-gear' onClick={this.toggleSettingsMenu.bind(this)}></i>
-        <div className={this.bemClass('project-header__project-settings-overlay', this.state.isSettingsMenuOpen, '--active')} onClick={this.toggleSettingsMenu.bind(this)}></div>
-        <ul className={this.bemClass('project-header__project-settings-panel', this.state.isSettingsMenuOpen, '--active')}>
-          <li className='TODO project-header__project-setting'><UserMenuRelay {...this.props} /></li>
-          <li className='TODO project-header__project-setting' onClick={this.contactHuman.bind(this)}>Contact a Human</li>
-          <li className='TODO project-header__project-setting project-header__logout' onClick={logout}>Sign Out</li>
+      <div className={this.bemClass('header-actions', this.state.isSettingsMenuOpen, '--active')}>
+        {/*<FontAwesome name='search' className='header-actions__search-icon'/>*/}
+        <FontAwesome name='gear' className='header-actions__settings-icon' onClick={this.toggleSettingsMenu.bind(this)} />
+        <div className={this.bemClass('header-actions__settings-overlay', this.state.isSettingsMenuOpen, '--active')} onClick={this.toggleSettingsMenu.bind(this)}></div>
+        <ul className={this.bemClass('header-actions__settings-panel', this.state.isSettingsMenuOpen, '--active')}>
+          <li className='header-actions__setting'><UserMenuRelay {...this.props} /></li>
+          <li className='header-actions__setting' onClick={this.contactHuman.bind(this)}>Contact a Human</li>
+          <li className='header-actions__setting' onClick={logout}>Sign Out</li>
         </ul>
       </div>
     );
