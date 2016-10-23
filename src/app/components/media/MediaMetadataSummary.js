7e084ea41653908aa0106d70e1a14f7036765a71
Zach Alexander <zach@zachalexander.com>
2016-10-23 12:29:38 -0700

    Hide media metadata when unavailable

--- src/app/components/media/MediaMetadataSummary.js
+++ src/app/components/media/MediaMetadataSummary.js
@@ -7,12 +7,13 @@ class MediaMetadataSummary extends Component {
   render() {
     const { media, data } = this.props;
     const annotationsCount = media.annotations_count; // TODO: filter to count of annotations displayed on timeline
+    const authorUsername = util.authorUsername(media, data);
 
     return (
       <div className='media-metadata-summary'>
         <span className='media-metadata-summary__datum'>{annotationsCount} note{annotationsCount === 1 ? '' : 's'}</span>
-        <span className='media-metadata-summary__datum'>{media.domain}</span>
-        <span className='media-metadata-summary__datum'>{util.authorUsername(media, data)}</span>
+        {media.domain ? <span className='media-metadata-summary__datum'>{media.domain}</span> : null}
+        {authorUsername ? <span className='media-metadata-summary__datum'>{authorUsername}</span> : null}
       </div>
     );
   }
