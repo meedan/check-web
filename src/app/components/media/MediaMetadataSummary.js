1df935c92af70ca0273948e4a9d43a6cdc8a7a63
Zach Alexander <zach@zachalexander.com>
2016-10-22 18:36:19 -0700

    Pluralize media notes count appropriately

--- src/app/components/media/MediaMetadataSummary.js
+++ src/app/components/media/MediaMetadataSummary.js
@@ -5,10 +5,11 @@ class MediaMetadataSummary extends Component {
 
   render() {
     const { media, data } = this.props;
+    const annotationsCount = media.annotations_count; // TODO: filter to count of annotations displayed on timeline
 
     return (
       <div className='media-metadata-summary'>
-        <span className='media-metadata-summary__datum'>{media.annotations_count} notes</span>
+        <span className='media-metadata-summary__datum'>{annotationsCount} note{annotationsCount === 1 ? '' : 's'}</span>
         <span className='media-metadata-summary__datum'>{media.domain}</span>
         <span className='media-metadata-summary__datum'>{data.username ? '@' + data.username : null}</span>
       </div>
