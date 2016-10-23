3e801ee006c2b87895ea8c263ea947808094df11
Zach Alexander <zach@zachalexander.com>
2016-10-23 00:01:12 -0700

    Show claim preview on claim header and other tweaks

--- src/app/components/media/MediaUtil.js
+++ src/app/components/media/MediaUtil.js
@@ -1,3 +1,5 @@
+import truncate from 'lodash.truncate';
+
 const MediaUtil = {
   networkIconName(media) {
     return ({
@@ -13,7 +15,9 @@ const MediaUtil = {
       case 'facebook.com':
         return data.user_name;
       case 'youtube.com':
-        return data.user_name;
+        return data.username;
+      case 'instagram.com':
+        return data.username;
       default:
         return data.user_name || data.username || '';
     }
@@ -24,7 +28,7 @@ const MediaUtil = {
       case 'twitter.com':
         return data.user ? `@${data.user.screen_name}` : ''; // data.username?
       case 'facebook.com':
-        return data.username;
+        return '';
       case 'instagram.com':
         return `@${data.author_name}`;
       case 'youtube.com':
@@ -34,19 +38,31 @@ const MediaUtil = {
     }
   },
 
+  typeLabel(media, data) {
+    return ({
+      'twitter.com': 'Tweet',
+      'facebook.com': 'Facebook post',
+      'instagram.com': 'Instagram',
+      'youtube.com': 'Video'
+    }[media.domain] || (data.quote ? 'Claim' : 'Page'));
+  },
+
   title(media, data) {
-    switch (media.domain) {
-      case 'twitter.com':
-        return `Tweet by ${this.authorUsername(media, data)}`;
-      case 'facebook.com':
-        return `Facebook post by ${this.authorName(media, data)}`;
-      case 'instagram.com':
-        return `Instagram by ${this.authorUsername(media, data)}`;
-      case 'youtube.com':
-        return `Video by ${this.authorUsername(media, data)}`;
-      default:
-        return data.title;
+    const typeLabel = this.typeLabel(media, data);
+    if (typeLabel === 'Page') {
+      return `${typeLabel} on ${media.domain}`;
     }
+    else if (typeLabel === 'Claim') {
+      const text = truncate(data.quote, {length: 50, separator: /,? +/, ellipsis: '…'});
+      return `${typeLabel}${text ? ': ' + text : ''}`;
+    }
+    else {
+      return `${typeLabel} by ${this.authorName(media, data)}`;
+    }
+  },
+
+  notesCount(media, data) {
+    return media.annotations_count; // TODO: filter to visible notes
   }
 }
 
