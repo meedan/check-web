1eaaf2f731f0e801008908ccee6786d04a9d1ca0
Zach Alexander <zach@zachalexander.com>
2016-10-27 13:10:47 -0700

    Add suggested tags data

---
+++ data/suggestedTags.js
@@ -0,0 +1,15 @@
+const elCategories = [
+  "Polling place problems",
+  "Registration problems",
+  "Voter ID problems",
+  "Arrest impacting ability to vote",
+  "Ballots — not enough/provisional",
+  "Equipment problems",
+  "Accessibility — language or disabled",
+  "General information",
+  "Intimidation or challenges"
+];
+
+export default {
+  "electionland-categories": elCategories
+}
