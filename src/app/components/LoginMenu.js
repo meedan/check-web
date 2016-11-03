80eba5d929cd8a8b992b75d6e17095ab78d0568a
Chris Blow <chris@meedan.com>
2016-11-03 15:46:23 -0700

    Add images and remove second slack button

--- src/app/components/LoginMenu.js
+++ src/app/components/LoginMenu.js
@@ -8,24 +8,24 @@ class LoginMenu extends Component {
     const { loginTwitter, loginFacebook, loginSlack, state } = this.props;
     return (
 
-      <div id="login-menu" class="login-menu">
-        <div class="browser-support">
-          <p class="browser-support__message">Best viewed with <i class='fa fa-chrome'></i> <a href="https://www.google.com/chrome/browser/desktop/">Chrome for Desktop</a>.</p>
+      <div id="login-menu" className="login-menu">
+        <div className="browser-support">
+          <p className="browser-support__message">Best viewed with <i className='fa fa-chrome'></i> <a href="https://www.google.com/chrome/browser/desktop/">Chrome for Desktop</a>.</p>
         </div>
         <Message message={state.app.message} />
 
-        <img class="login-menu__icon" src="/images/logo-1.svg">
-        <div class="login-menu__content">
+        <img width="36" className="login-menu__icon" src="/images/logo/logo-1.svg" />
+        <div className="login-menu__content">
           
           <h2>Create an account or sign in</h2>
 
-          <div class="nudge-electionland">
-            <img class="electionland-logo" width="30" src="/images/Electionland_small.svg" alt=" " title=" " />
+          <div className="nudge-electionland">
+            <img className="electionland-logo" width="30" src="/images/Electionland_small.svg" alt=" " title=" " />
             <p>New Electionland members should sign up with Slack!</p>
-            <img class="arrow" src="/images/arrow-left.png" alt=" " title=" " >
+            <img className="arrow" src="/images/arrow-left.png" alt=" " title=" " />
           </div>
 
-          <ul class="login-menu__options">
+          <ul className="login-menu__options">
             <li>
               <button onClick={loginSlack} id="slack-login" className='login-menu__button login-menu__button--slack'>Sign in with Slack</button>
             </li>
@@ -33,12 +33,9 @@ class LoginMenu extends Component {
 
           <p>Reports discovered via social media are meant solely as <em>tips</em>. Always contact the source before sharing publicly.</p>
 
-          <p class="other-method">If you’ve already connected with another method or aren’t on the Electionland team, you have a few more options.</p>
+          <p className="other-method">If you’ve already connected with another method or aren’t on the Electionland team, you have other options.</p>
 
           <ul className="login-menu__options">
-            <li>
-              <button onClick={loginSlack} id="slack-login" className='login-menu__button login-menu__button--slack'>Sign in with Slack</button>
-            </li>
             <li className="item">
               <button onClick={loginTwitter} id="twitter-login" className='login-menu__button login-menu__button--twitter'>Sign in with Twitter</button>
             </li>
@@ -50,9 +47,9 @@ class LoginMenu extends Component {
             </li>
           </ul>
 
-          <p class="nudge-support">Forgot your password? Can’t find your team? If you’re having any trouble, contact a human at <a href="mailto:check@meedan.com">check@meedan.com</a>.</p>
+          <p className='nudge-support'>Forgot your password? Can’t find your team? If you’re having any trouble, contact a human at <a href='mailto:check@meedan.com'>check@meedan.com</a>.</p>
 
-          <p class="login-menu__footer"><span>By signing in, you agree to the Check </span><a class="login-menu__footer-link" href="/tos">Terms of Service</a><span> and </span><a class="login-menu__footer-link" href="/privacy">Privacy&nbsp;Policy</a><span>.</span> </p>
+          <p className='login-menu__footer'>By signing in, you agree to the Check <Link to='/tos' className='login-menu__footer-link'>Terms of Service</Link> and <Link to='/privacy' className='login-menu__footer-link'>Privacy&nbsp;Policy</Link>.</p>
         </div>
       </div>
     );
