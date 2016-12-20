e9f25bfd3c61ffb79703411aabe503e51eace0de
Caio SBA <caiosba@gmail.com>
2016-12-19 23:13:42 -0300

    Ticket #4979: Unit tests structure, documentation and a couple tests for PenderCard component

---
+++ test/unit/pender_card.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'enzyme';
+import { expect } from 'chai';
+
+import PenderCard from '../../src/app/components/PenderCard';
+
+describe('PenderCard />', () => {
+  it('contains an <Spinner /> component if fallback is not provided', function() {
+    const wrapper = render(<PenderCard />);
+    expect(wrapper.find('.react-spinner')).to.have.length(1);
+  });
+
+  it('does not contain an <Spinner /> component if fallback is provided', function() {
+    const wrapper = render(<PenderCard fallback={<div></div>} />);
+    expect(wrapper.find('.react-spinner')).to.have.length(0);
+  });
+});
