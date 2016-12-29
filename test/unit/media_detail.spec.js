ec5097d2e8682f5bc1fa7c2f31f14aed345b570f
Zach Alexander <zach@zachalexander.com>
2016-12-29 14:32:49 -0800

    Add unit test for MediaDetail

---
+++ test/unit/media_detail.spec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'enzyme';
+import { expect } from 'chai';
+
+import MediaDetail from '../../src/app/components/media/MediaDetail';
+
+describe('<MediaDetail />', () => {
+  const media = {
+    jsondata: JSON.stringify({}),
+    last_status: 'verified',
+    verification_statuses: JSON.stringify({statuses: [{label: 'verified'}]}),
+    annotations_count: 0,
+    permissions: JSON.stringify({})
+  };
+
+  it('renders', function() {
+    const mediaDetail = render(<MediaDetail media={media} />);
+    expect(mediaDetail.find('.media-detail')).to.have.length(1);
+  });
+});
