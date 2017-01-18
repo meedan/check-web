69cb85ed00880d17b8d54e49f4f346a6f910ec3b
Caio SBA <caiosba@gmail.com>
2017-01-18 13:50:54 -0300

    Ticket #5616: Auto-refresh team projects when project is created

--- src/app/components/team/TeamHeader.js
+++ src/app/components/team/TeamHeader.js
@@ -1,12 +1,18 @@
 import React, { Component, PropTypes } from 'react';
 import Relay from 'react-relay';
 import { Link } from 'react-router';
+import Pusher from 'pusher-js';
 import TeamRoute from '../../relay/TeamRoute';
 import teamFragment from '../../relay/teamFragment';
 import CheckContext from '../../CheckContext';
 import ProjectList from '../project/ProjectList';
 
 class TeamHeaderComponent extends Component {
+  getPusher() {
+    const context = new CheckContext(this);
+    return context.getContextStore().pusher;
+  }
+
   updateContext() {
     new CheckContext(this).setContextStore({ team: this.props.team });
   }
@@ -17,6 +23,31 @@ class TeamHeaderComponent extends Component {
 
   componentWillUpdate() {
     this.updateContext();
+  }
+
+  subscribe() {
+    const pusher = this.getPusher();
+    if (pusher) {
+      const that = this;
+      pusher.subscribe(this.props.team.pusher_channel).bind('project_created', (data) => {
+        that.props.relay.forceFetch();
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.subscribe();
+  }
+
+  unsubscribe() {
+    const pusher = this.getPusher();
+    if (pusher) {
+      pusher.unsubscribe(this.props.team.pusher_channel);
+    }
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
   }
 
   render() {
