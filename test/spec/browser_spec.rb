177edca506a420d68fe3937ef638755b3ab6c1fe
clarissacastella <clacx@bol.com.br>
2017-01-19 18:47:53 -0200

    On going work

--- test/spec/browser_spec.rb
+++ test/spec/browser_spec.rb
@@ -2,9 +2,9 @@ require 'yaml'
 require_relative './app_spec.rb'
 
 config = YAML.load_file('config.yml')
-webdriver_url = "http://#{config['sauce_username']}:#{config['sauce_access_key']}@ondemand.saucelabs.com:80/wd/hub"
+webdriver_url = "http://clarissaxavier1:TfqXkp3vcowrJWdDuJAK@hub-cloud.browserstack.com/wd/hub" #"http://#{config['sauce_username']}:#{config['sauce_access_key']}@ondemand.saucelabs.com:80/wd/hub"
 
-browser_definitions = {
+browser_definitionsOLD = {
   safari_ios10_ipad: {
     browserName: 'Safari',
     deviceName: 'iPad Simulator',
@@ -30,13 +30,65 @@ browser_definitions = {
   }
 }
 
+browser_definitions = {
+  safari_osx_sierra: {
+		browser: 'Safari',
+		browser_version: '10.0',
+		os: 'OS X',
+		os_version: 'Sierra',
+		resolution: '1024x768',
+		'browserstack.localIdentifier': 'Test123',
+	  'browserstack.debug': 'true',
+		'browserstack.local': 'true' 
+  },
+  chrome54_osx_sierra: {
+		browser: 'Chrome',
+		browser_version: '54.0',
+		os: 'OS X',
+		os_version: 'Sierra',
+		resolution: '1024x768',
+		'browserstack.localIdentifier': 'Test123',
+	  'browserstack.debug': 'true',
+		'browserstack.local': 'true' 
+  },
+  firefox_windows10: {
+		browser: 'Firefox',
+		browser_version: '50.0',
+		os: 'Windows',
+		os_version: '10',
+		resolution: '1280x1024',
+		'browserstack.localIdentifier': 'Test123',
+	  'browserstack.debug': 'true',
+		'browserstack.local': 'true' 
+  },
+  chrome54_windows10: {
+		browser: 'Chrome',
+		browser_version: '54.0',
+		os: 'Windows',
+		os_version: '10',
+		resolution: '1280x1024',
+		'browserstack.localIdentifier': 'Test123',
+	  'browserstack.debug': 'true',
+		'browserstack.local': 'true' 
+  },
+	ios_iphone5:{
+		browserName: 'iPhone',
+		platform: 'MAC',
+		device: 'iPhone 5',
+		'browserstack.localIdentifier': 'Test123',
+	  'browserstack.debug': 'true',
+		'browserstack.local': 'true' 
+	}
+}
+
 def browsers_to_test
   # TODO: put ENV vars in npm scripts; read here; return subset of browser_definitions.keys
   [
-    :safari_ios10_ipad,
-    # :chrome54_macos1012,
-    # :chrome54_windows10,
-    # :chrome48_linux
+		:safari_osx_sierra,
+		:chrome54_osx_sierra,
+		:firefox_windows10,
+		:chrome54_windows10,
+		:ios_iphone5
   ]
 end
 
