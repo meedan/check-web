e3310d61e110592e09029950c6a9147dfcd009c4
clarissacastella <clacx@bol.com.br>
2017-01-28 00:23:12 -0200

    Updating confirm_email to be recorded by jmeter

--- test/spec/pages/login_page.rb
+++ test/spec/pages/login_page.rb
@@ -74,7 +74,6 @@ class LoginPage < Page
   end
 
   def confirm_email(email) # TODO: test real email confirmation flow
-    p "LOAD"
     load_api('/test/confirm_user', { email: email })
   end
 end
