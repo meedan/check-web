bbfee4586ceeaaf3fb4a82d63ee564c3dfc47efe
Caio SBA <caiosba@gmail.com>
2017-02-16 01:33:34 -0300

    Ticket #5447: Do not use a fallback on <PenderCard />

--- src/app/components/media/MediaDetail.js
+++ src/app/components/media/MediaDetail.js
@@ -94,7 +94,7 @@ class MediaDetail extends Component {
     } else if (media.url) {
        embedCard = condensed ?
                    <SocialMediaCard media={media} data={data} condensed={condensed} /> :
-                   <PenderCard url={media.url} penderUrl={config.penderUrl} fallback={<SocialMediaCard media={media} data={data} condensed={condensed} />} />;
+                   <PenderCard url={media.url} penderUrl={config.penderUrl} fallback={null} />;
     }
 
     return (
