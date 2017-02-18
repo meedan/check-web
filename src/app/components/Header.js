a708c7aeaff43982a9abc49b8c1ee2989c9fa485
Zach Alexander <zach@zachalexander.com>
2017-02-17 22:32:45 -0800

    Add Check logo to header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -7,6 +7,7 @@ import ProjectHeader from './project/ProjectHeader';
 import Breadcrumb from './layout/Breadcrumb';
 import HeaderActions from './HeaderActions';
 import Can from './Can';
+import { Link } from 'react-router';
 
 const messages = defineMessages({
   projectLabel: {
@@ -28,11 +29,15 @@ class Header extends Component {
     const { state } = this.props;
     const path = this.props.location ? this.props.location.pathname : null;
     const { formatMessage } = this.props.intl;
+    const showCheckLogo = /^\/(check(\/.*)?)?$/.test(path);
 
     const defaultHeader = (
       <header className="header header--default">
         <div className="header__container">
-          <div className="header__team"><TeamHeader {...this.props} /></div>
+          { showCheckLogo ?
+            (<Link to='/check/teams' className='header__app-link'><img src='/images/logo/check.svg' /></Link>) :
+            (<div className="header__team"><TeamHeader {...this.props} /></div>)
+          }
           <ProjectHeader {...this.props} />
           <HeaderActions {...this.props} />
         </div>
