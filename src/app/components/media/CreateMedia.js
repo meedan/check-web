19434c21de469c429f1d1d9eaa5b3ee0e513212b
Chris Blow <chris@meedan.com>
2017-02-17 18:48:45 -0800

    Refine create-media input and image uploader

--- src/app/components/media/CreateMedia.js
+++ src/app/components/media/CreateMedia.js
@@ -158,11 +158,14 @@ class CreateProjectMedia extends Component {
               })()}
               <MdInsertPhoto id="create-media__switcher" title="Upload an image" className={this.state.fileMode ? 'create-media__file' : ''} onClick={this.switchMode.bind(this)} />
             </div>
-            <div className="create-media__helper">Add a link, quote or image for verification</div>
 
-            <div className="create-media__buttons">
+            <footer>
+              <div className="create-media__helper">Add a link, quote or image for verification</div>
+
+              <div className="create-media__buttons">
               <FlatButton id="create-media-submit" primary onClick={this.handleSubmit.bind(this)} label="Post" className="create-media__button create-media__button--submit" />
-            </div>
+              </div>  
+            </footer>
           </form>
         </ContentColumn>
       </div>
