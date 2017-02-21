c7cab784ec910ef280d921ac05f80e09578da8d7
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2017-02-20 23:11:48 -0300

    Removed injectIntl from component
    Unit tests now reflect current behavior of Header.

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import { defineMessages, injectIntl, intlShape } from 'react-intl';
 import FlatButton from 'material-ui/FlatButton';
 import TeamHeader from './team/TeamHeader';
 import TeamPublicHeader from './team/TeamPublicHeader';
@@ -9,26 +8,10 @@ import HeaderActions from './HeaderActions';
 import Can from './Can';
 import { Link } from 'react-router';
 
-const messages = defineMessages({
-  projectLabel: {
-    id: 'header.projectLabel',
-    defaultMessage: 'Project'
-  },
-  teamLabel: {
-    id: 'header.teamLabel',
-    defaultMessage: 'Team'
-  },
-  teamsLabel: {
-    id: 'header.teamsLabel',
-    defaultMessage: 'Teams'
-  }
-});
-
 class Header extends Component {
   render() {
     const { state } = this.props;
     const path = this.props.location ? this.props.location.pathname : null;
-    const { formatMessage } = this.props.intl;
     const showCheckLogo = /^\/(check(\/.*)?)?$/.test(path);
 
     const defaultHeader = (
@@ -48,8 +31,4 @@ class Header extends Component {
   }
 }
 
-Header.propTypes = {
-  intl: intlShape.isRequired
-};
-
-export default injectIntl(Header);
+export default Header;
--- test/unit/header.spec.js
+++ test/unit/header.spec.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Relay from 'react-relay';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
-import { shallowWithIntl } from './helpers/intl-enzyme-test-helper.js';
 
 import Header from '../../src/app/components/Header';
 import TeamHeader from '../../src/app/components/team/TeamHeader';
@@ -11,14 +10,14 @@ import TeamPublicHeader from '../../src/app/components/team/TeamPublicHeader';
 describe('<Header />', () => {
   it('renders full team header on members page', function() {
     const location = { pathname: '/members' };
-    const header = shallowWithIntl(<Header location={location} params={{}} />);
+    const header = shallow(<Header location={location} params={{}} />);
     expect(header.find(TeamHeader)).to.have.length(1);
     expect(header.find(TeamPublicHeader)).to.have.length(0);
   });
 
   it('renders public team header on team join page', function() {
     const location = { pathname: '/join' };
-    const header = shallowWithIntl(<Header location={location} params={{}} />);
+    const header = shallow(<Header location={location} params={{}} />);
     expect(header.find(TeamHeader)).to.have.length(0);
     expect(header.find(TeamPublicHeader)).to.have.length(1);
   });
