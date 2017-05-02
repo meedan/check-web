a8348d6187cc2eb43d94d2e97cf00fe2486cc2af
Caio SBA <caiosba@gmail.com>
2017-05-01 21:41:28 -0300

    Ticket #5936: Updating .gitattributes

--- test/spec/quicktest_status_spec.rb
+++ test/spec/quicktest_status_spec.rb
@@ -1,19 +1,4 @@
 require_relative './spec_helper.rb'
 
 shared_examples 'quicktest_status' do
-  #Set a verification status to this media.
-  it "should change a media status via the dropdown menu" do
-    media_pg = LoginPage.new(config: @config, driver: @driver).load
-        .login_with_email(email: @e1, password: @password, project: true)
-    @driver.navigate.to team_url('project/' + get_project + '/media/' + $media_id)
-    sleep 3
-    element = @driver.find_element(:css, ".media-status__label")
-    element.click
-    element = @driver.find_element(:css, ".media-status__menu-item--translated")
-    element.click
-    sleep 3
-    # Uncomment after we implement translation status messages on the log
-    # element = @driver.find_element(:css, '.annotation__status--translated')
-    # expect(element.nil?).to be(false)
-  end
 end
