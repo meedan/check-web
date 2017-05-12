217ccd3b2a8eaec1ff8a93d4eddb523ce486fa15
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2017-05-12 14:53:21 -0300

    Removing debug comments #5640

--- src/app/components/MappedMessage.js
+++ src/app/components/MappedMessage.js
@@ -4,17 +4,9 @@ import config from 'config';
 
 class MappedMessage extends Component {
   render() {
-    console.log('--- MappedMessage ---');
-
-    console.log('config.appName');
-    console.log(config.appName);
-
     const appPrefix = config.appName === 'bridge' ? 'bridge_' : '';
     const parent = this.props.msgObj;
     const string_resource = parent[appPrefix + this.props.msgKey];
-
-    console.log('string_resource');
-    console.log(string_resource);
 
     return (
       <span>{this.props.intl.formatMessage(string_resource)}</span>
