4c89d25758c4fa5e9993b32c9bd600b0071378e7
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2017-06-02 19:46:21 -0300

    Human app name #5923

--- src/app/components/LoginContainer.js
+++ src/app/components/LoginContainer.js
@@ -6,9 +6,16 @@ import Message from './Message';
 import Login from './Login';
 import config from 'config';
 import { stringHelper } from '../customHelpers';
-import { capitalize } from '../helpers';
 
 const messages = defineMessages({
+  appNameHuman: {
+    id: 'loginContainer.appNameHuman',
+    defaultMessage: 'Check',
+  },
+  bridge_appNameHuman: {
+    id: 'bridge.loginContainer.appNameHuman',
+    defaultMessage: 'Bridge',
+  },
   agreeTerms: {
     id: 'agree.terms',
     defaultMessage: 'By signing in, you agree to the Check {tosLink} and {ppLink}.',
@@ -39,7 +46,7 @@ class LoginContainer extends Component {
           <FormattedMessage
             id="loginContainer.agreeTerms" defaultMessage={'By signing in, you agree to the {appName} {tosLink} and {ppLink}.'}
             values={{
-              appName: capitalize(config.appName),
+              appName: <MappedMessage msgObj={messages} msgKey="appNameHuman" />,
               tosLink: <a className="login-container__footer-link" target="_blank" rel="noopener noreferrer" href={stringHelper('TOS_URL')}><FormattedMessage id="tos.title" defaultMessage="Terms of Service" /></a>,
               ppLink: <a className="login-container__footer-link" target="_blank" rel="noopener noreferrer" href={stringHelper('PP_URL')}><FormattedMessage id="privacy.policy.title" defaultMessage="Privacy&nbsp;Policy" /></a>
             }}
--- src/app/components/MappedMessage.js
+++ src/app/components/MappedMessage.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import { formatHTMLMessage, injectIntl } from 'react-intl';
 import config from 'config';
 
+function mapMessage(intl, msgObj, msgKey, values) {
+  const appPrefix = config.appName === 'bridge' ? 'bridge_' : '';
+  const parent = msgObj;
+  const string_resource = parent[appPrefix + msgKey];
+
+  return intl.formatHTMLMessage(string_resource, {...values});
+}
+
 class MappedMessage extends Component {
   render() {
-    const appPrefix = config.appName === 'bridge' ? 'bridge_' : '';
-    const parent = this.props.msgObj;
-    const string_resource = parent[appPrefix + this.props.msgKey];
-
     return (
-      <span>{this.props.intl.formatHTMLMessage(string_resource, {...this.props.values})}</span>
+      <span>{mapMessage(this.props.intl, this.props.msgObj, this.props.msgKey, this.props.values)}</span>
     );
   }
 }
 
 export default injectIntl(MappedMessage);
+export {
+  mapMessage,
+};
