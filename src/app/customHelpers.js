3d5e29b643d4b36744c89821c97b35d1260d0580
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2017-06-06 11:30:34 -0300

    Single customHelpers.js for Bridge and Check #5923

--- src/app/customHelpers.js
+++ src/app/customHelpers.js
@@ -1,25 +1,65 @@
 import config from 'config';
 
-function teamStatuses(team) {
-  return team.media_verification_statuses;
-}
+const customHelpers = {
+  bridge: {
+    teamStatuses: function(team) {
+      return team.translation_statuses;
+    },
+    mediaStatuses: function(media) {
+      return media.translation_statuses;
+    },
+    mediaLastStatus: function(media) {
+      return media.field_value || 'pending';
+    },
+  },
+  check: {
+    teamStatuses: function(team){
+      return team.media_verification_statuses;
+    },
+    mediaStatuses: function(media) {
+      return media.verification_statuses;
+    },
+    mediaLastStatus: function(media) {
+      return media.last_status;
+    },
+  },
+};
 
-function mediaStatuses(media) {
-  return media.verification_statuses;
-}
-
-function mediaLastStatus(media) {
-  return media.last_status;
-}
-
-function stringHelper(key) {
-  return {
+const customStrings = {
+  bridge: {
+    ABOUT_URL: 'https://meedan.com/bridge',
+    CONTACT_HUMAN_URL: `mailto:${config.supportEmail}`,
+    PP_URL: 'https://meedan.com/en/bridge/bridge_privacy.html',
+    TOS_URL: 'https://meedan.com/en/bridge/bridge_tos.html',
+    LOGO_URL: '/images/logo/bridge.svg',
+  },
+  check: {
     ABOUT_URL: 'https://meedan.com/check',
     CONTACT_HUMAN_URL: 'https://docs.google.com/forms/d/e/1FAIpQLSdctP7RhxeHjTnevnvRi6AKs4fX3wNnxecVdBFwKe7GRVcchg/viewform',
     PP_URL: 'https://meedan.com/en/check/check_privacy.html',
     TOS_URL: 'https://meedan.com/en/check/check_tos.html',
     LOGO_URL: '/images/logo/check.svg',
-  }[key];
+  },
+};
+
+function resolveHelper(name, args){
+  return customHelpers[config.appName][name].apply(this, args);
+}
+
+function teamStatuses(team) {
+  return resolveHelper('teamStatuses', arguments);
+}
+
+function mediaStatuses(media) {
+  return resolveHelper('mediaStatuses', arguments);
+}
+
+function mediaLastStatus(media) {
+  return resolveHelper('mediaLastStatus', arguments);
+}
+
+function stringHelper(key) {
+  return customStrings[config.appName][key];
 }
 
 export {
