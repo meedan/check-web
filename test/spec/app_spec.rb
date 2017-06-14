55287e7f6bbe7724cad55b0b6cfac91d8dd2b6e3
infojunkie <karim.ratib@gmail.com>
2017-06-13 17:24:45 -0700

    Sleep before screenshot

--- test/spec/app_spec.rb
+++ test/spec/app_spec.rb
@@ -110,10 +110,11 @@ shared_examples 'app' do |webdriver_url, browser_capabilities|
       expect(media_pg.primary_heading.text).to eq('Edited media title')
       project_pg = media_pg.go_to_project
 
+      sleep 15
+
       link = save_screenshot('Debugging media title editing')
       puts "Test \"should edit the title of a media\": Check project screenshot at #{link}"
 
-      sleep 3
       expect(project_pg.elements('.media-detail__heading').map(&:text).include?('Edited media title')).to be(true)
     end
 
