524d8c7a4d3efc4cc1709f01772bc1dd6656b944
Caio SBA <caiosba@gmail.com>
2017-06-16 16:02:42 -0300

    Ticket #5494: :zzz:

--- test/spec/app_spec.rb
+++ test/spec/app_spec.rb
@@ -1040,7 +1040,7 @@ shared_examples 'app' do |webdriver_url, browser_capabilities|
       expect(@driver.page_source.include?('My search result')).to be(true)
 
       @driver.find_element(:xpath, "//span[contains(text(), 'Oldest')]").click
-      sleep 10
+      sleep 20
       expect((@driver.current_url.to_s.match(/DESC/)).nil?).to be(true)
       expect((@driver.current_url.to_s.match(/ASC/)).nil?).to be(false)
       expect(@driver.page_source.include?('My search result')).to be(true)
