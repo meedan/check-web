c75b07ae5c1af809ba4a8e957feb2ccfda5bc7f2
infojunkie <karim.ratib@gmail.com>
2017-06-19 13:37:14 -0700

    Restore backend refresh #5494

--- src/app/components/AuthorPicture.js
+++ src/app/components/AuthorPicture.js
@@ -25,7 +25,7 @@ class AuthorPicture extends Component {
     const onSuccess = (response) => {
       this.setState({ avatarUrl: JSON.parse(response.updateProjectMedia.project_media.embed).author_picture });
     };
-/*
+
     Relay.Store.commitUpdate(
       new UpdateProjectMediaMutation({
         refresh_media: 1,
@@ -33,7 +33,7 @@ class AuthorPicture extends Component {
       }),
       { onSuccess, onFailure },
     );
-*/
+
     return this.defaultAvatar();
   }
 
