bff8268910a11c8f078a50695ee0f7f7a03fa8ea
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2017-06-23 15:35:44 -0300

    Modified CreateMedia component to add sources #6106

---
+++ src/app/relay/CreateProjectSourceMutation.js
@@ -0,0 +1,42 @@
+import React, { Component, PropTypes } from 'react';
+import Relay from 'react-relay';
+
+class CreateProjectSourceMutation extends Relay.Mutation {
+  getMutation() {
+    return Relay.QL`mutation { createProjectSource }`;
+  }
+
+  getFatQuery() {
+    return Relay.QL`fragment on CreateProjectSourcePayload {
+      project_source {
+         id,
+         dbid
+      }
+    }`;
+  }
+
+  getConfigs() {
+    return [
+      {
+        type: 'REQUIRED_CHILDREN',
+        children: [Relay.QL`
+          fragment on CreateProjectSourcePayload {
+            project_source {
+              dbid
+            },
+          }`,
+        ],
+      },
+    ];
+  }
+
+  getVariables() {
+    return {
+      project_id: this.props.project_id,
+      name: this.props.source_name,
+      url: this.props.source_url,
+    };
+  }
+}
+
+export default CreateProjectSourceMutation;
