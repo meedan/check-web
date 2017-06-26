c294d880afcd17693f9318ceb66042be160204c3
Chris Blow <chris@meedan.com>
2017-06-25 22:42:51 -0700

    #6134 Lint. Reconfigure some eslint errors into warnings

--- src/app/components/team/TeamComponent.js
+++ src/app/components/team/TeamComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Relay from 'react-relay';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router';
 import {
   FormattedMessage,
   defineMessages,
