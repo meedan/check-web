79e0cc51d32d9ccb054b1a5e6d00055c838f06b1
Caio SBA <caiosba@gmail.com>
2017-07-17 19:30:09 -0300

    Ticket #6106: Fixing unit test

--- test/unit/annotation_updated_at.spec.js
+++ test/unit/annotation_updated_at.spec.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import { IntlProvider } from 'react-intl';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { expect } from 'chai';
 import { render } from 'enzyme';
 
 import Annotation from '../../src/app/components/source/Annotation';
 
 describe('<Annotations />', () => {
-  let annotation = {
-    annotation_type: "comment",
-    content: JSON.stringify({text: 'testing'}),
-    updated_at: "2017-02-08 17:19:40 UTC",
-    created_at: "2017-02-07 17:19:40 UTC",
-    medias: {edges: []},
-    permissions: JSON.stringify({"read Comment": false, "update Comment": true, "destroy Comment": true}),
-    annotator: {name: '', profile_image: null}
-  }
-  let annotated = {};
+  const annotation = {
+    annotation_type: 'comment',
+    content: JSON.stringify({ text: 'testing' }),
+    updated_at: '2017-02-08 17:19:40 UTC',
+    created_at: '2017-02-07 17:19:40 UTC',
+    medias: { edges: [] },
+    permissions: JSON.stringify({ 'read Comment': false, 'update Comment': true, 'destroy Comment': true }),
+    annotator: { name: '', profile_image: null },
+  };
+  const annotated = {};
 
-  it('should render annotation updated_at time', function() {
-    const wrapper = render(<IntlProvider locale="en"><Annotation annotation={annotation} annotated={annotated} annotatedType={"ProjectMedia"} /></IntlProvider>);
-    const container = wrapper.find('.annotation__timestamp > time')
-    expect(container.length).to.equal(1)
+  it('should render annotation updated_at time', () => {
+    const wrapper = render(<IntlProvider locale="en"><MuiThemeProvider><Annotation annotation={annotation} annotated={annotated} annotatedType={'ProjectMedia'} /></MuiThemeProvider></IntlProvider>);
+    const container = wrapper.find('.annotation__timestamp > time');
+    expect(container.length).to.equal(1);
     expect(container.prop('title')).to.contain('2017-02-08');
   });
-
 });
