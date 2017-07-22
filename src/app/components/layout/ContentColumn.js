7130221e425f1d3ac950f2ad08c72acbdb83ecdc
Chris Blow <chris@meedan.com>
2017-07-21 22:05:16 -0700

    #6184 Fix ContentColumn to ensure it passes in classNames, without duplication

--- src/app/components/layout/ContentColumn.js
+++ src/app/components/layout/ContentColumn.js
@@ -3,15 +3,16 @@ import { bemClass } from '../../helpers';
 
 class ContentColumn extends Component {
   render() {
-    const { flex, wide, narrow } = this.props;
-    const classNames =
-        `${bemClass('content-column', narrow, '--narrow')} ${
-        bemClass('content-column', flex, '--flex')} ${
-        bemClass('content-column', wide, '--wide')} ${
-        this.props.className || ''}`;
+    const { flex, wide, narrow, className } = this.props;
+    const classNames = [];
+    classNames.push('content-column');
+    if (narrow) { classNames.push(bemClass('content-column', narrow, '--narrow')); }
+    if (flex) { classNames.push(bemClass('content-column', flex, '--flex')); }
+    if (wide) { classNames.push(bemClass('content-column', wide, '--wide')); }
+    classNames.push(className || '');
 
     return (
-      <div className={`${classNames} ${this.props.ClassName}`} style={this.props.style}>
+      <div className={classNames.join(' ')} style={this.props.style} id={this.props.id}>
         {this.props.children}
       </div>
     );
