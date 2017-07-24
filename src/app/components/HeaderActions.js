286c309a8fb61ce9e69116b623d9d93e5331b039
Chris Blow <chris@meedan.com>
2017-07-24 10:43:30 -0700

    Lint, prettier, use IconButton instead of Link

--- src/app/components/HeaderActions.js
+++ src/app/components/HeaderActions.js
@@ -5,12 +5,20 @@ import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
 import MappedMessage, { mapGlobalMessage } from './MappedMessage';
 import MdSearch from 'react-icons/lib/md/search';
 import MdMoreVert from 'react-icons/lib/md/more-vert';
+import IconButton from 'material-ui/IconButton';
 import TeamMenuRelay from '../relay/TeamMenuRelay';
 import ProjectMenuRelay from '../relay/ProjectMenuRelay';
 import { bemClass } from '../helpers';
 import { stringHelper } from '../customHelpers';
 import { Link } from 'react-router';
 import config from 'config';
+import { units } from '../styles/js/variables';
+
+const styles = {
+  icon: {
+    fontSize: '24px',
+  },
+};
 
 class HeaderActions extends Component {
   constructor(props) {
@@ -32,37 +40,159 @@ class HeaderActions extends Component {
     const loggedIn = this.props.loggedIn;
 
     return (
-      <div className={bemClass('header-actions', this.state.isMenuOpen, '--active')}>
+      <div
+        className={bemClass(
+          'header-actions',
+          this.state.isMenuOpen,
+          '--active',
+        )}
+      >
         {(() => {
           if (this.props.params && this.props.params.team) {
-            return (<Link to={`/${this.props.params.team}/search`}><MdSearch name="search" className="header-actions__search-icon" /></Link>);
+            return (
+              <IconButton
+                style={styles.icon}
+                href={`/${this.props.params.team}/search`}
+              >
+                <MdSearch
+                  name="search"
+                  className="header-actions__search-icon"
+                />
+              </IconButton>
+            );
           }
         })()}
-        <MdMoreVert className="header-actions__menu-toggle" onClick={this.toggleSettingsMenu.bind(this)} />
-        <div className={bemClass('header-actions__menu-overlay', this.state.isMenuOpen, '--active')} onClick={this.toggleSettingsMenu.bind(this)} />
-        <ul className={bemClass('header-actions__menu', this.state.isMenuOpen, '--active')}>
-          { loggedIn ? <li key="headerActions.userMenu" className="header-actions__menu-item" style={{ cursor: 'default' }}><UserMenuRelay {...this.props} /></li> : null }
-          { loggedIn ? <li key="headerActions.userTeams" className="header-actions__menu-item"><Link to="/check/teams"><FormattedMessage id="headerActions.userTeams" defaultMessage="Your Teams" /></Link></li> : null }
+        <MdMoreVert
+          className="header-actions__menu-toggle"
+          onClick={this.toggleSettingsMenu.bind(this)}
+        />
+        <div
+          className={bemClass(
+            'header-actions__menu-overlay',
+            this.state.isMenuOpen,
+            '--active',
+          )}
+          onClick={this.toggleSettingsMenu.bind(this)}
+        />
+        <ul
+          className={bemClass(
+            'header-actions__menu',
+            this.state.isMenuOpen,
+            '--active',
+          )}
+        >
+          {loggedIn
+            ? <li
+              key="headerActions.userMenu"
+              className="header-actions__menu-item"
+              style={{ cursor: 'default' }}
+            >
+              <UserMenuRelay {...this.props} />
+            </li>
+            : null}
+          {loggedIn
+            ? <li
+              key="headerActions.userTeams"
+              className="header-actions__menu-item"
+            >
+              <Link to="/check/teams">
+                <FormattedMessage
+                  id="headerActions.userTeams"
+                  defaultMessage="Your Teams"
+                />
+              </Link>
+            </li>
+            : null}
           {(() => {
             if (!joinPage) {
-              return ([
-                <ProjectMenuRelay key="headerActions.projectMenu" {...this.props} />,
+              return [
+                <ProjectMenuRelay
+                  key="headerActions.projectMenu"
+                  {...this.props}
+                />,
                 <TeamMenuRelay key="headerActions.teamMenu" {...this.props} />,
-              ]);
+              ];
             }
           })()}
-          { loggedIn ? <li key="headerActions.signIn" className="header-actions__menu-item header-actions__menu-item--logout" onClick={logout}><FormattedMessage id="headerActions.signOut" defaultMessage="Sign Out" /></li> : <li className="header-actions__menu-item header-actions__menu-item--login"><Link to="/"><FormattedMessage id="headerActions.signIn" defaultMessage="Sign In" /></Link></li> }
-          <li key="headerActions.contactHuman" className="header-actions__menu-item">
-            <a className="header-actions__link" target="_blank" rel="noopener noreferrer" href={stringHelper('CONTACT_HUMAN_URL')}><FormattedMessage id="headerActions.contactHuman" defaultMessage="Contact a Human" /></a>
+          {loggedIn
+            ? <li
+              key="headerActions.signIn"
+              className="header-actions__menu-item header-actions__menu-item--logout"
+              onClick={logout}
+            >
+              <FormattedMessage
+                id="headerActions.signOut"
+                defaultMessage="Sign Out"
+              />
+            </li>
+            : <li className="header-actions__menu-item header-actions__menu-item--login">
+              <Link to="/">
+                <FormattedMessage
+                  id="headerActions.signIn"
+                  defaultMessage="Sign In"
+                />
+              </Link>
+            </li>}
+          <li
+            key="headerActions.contactHuman"
+            className="header-actions__menu-item"
+          >
+            <a
+              className="header-actions__link"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={stringHelper('CONTACT_HUMAN_URL')}
+            >
+              <FormattedMessage
+                id="headerActions.contactHuman"
+                defaultMessage="Contact a Human"
+              />
+            </a>
           </li>
           <li key="headerActions.tos" className="header-actions__menu-item">
-            <a className="header-actions__link" target="_blank" rel="noopener noreferrer" href={stringHelper('TOS_URL')}><FormattedMessage id="headerActions.tos" defaultMessage="Terms of Service" /></a>
+            <a
+              className="header-actions__link"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={stringHelper('TOS_URL')}
+            >
+              <FormattedMessage
+                id="headerActions.tos"
+                defaultMessage="Terms of Service"
+              />
+            </a>
           </li>
-          <li key="headerActions.privacyPolicy" className="header-actions__menu-item">
-            <a className="header-actions__link" target="_blank" rel="noopener noreferrer" href={stringHelper('PP_URL')}><FormattedMessage id="headerActions.privacyPolicy" defaultMessage="Privacy Policy" /></a>
+          <li
+            key="headerActions.privacyPolicy"
+            className="header-actions__menu-item"
+          >
+            <a
+              className="header-actions__link"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={stringHelper('PP_URL')}
+            >
+              <FormattedMessage
+                id="headerActions.privacyPolicy"
+                defaultMessage="Privacy Policy"
+              />
+            </a>
           </li>
           <li key="headerActions.about" className="header-actions__menu-item">
-            <a className="header-actions__link" target="_blank" rel="noopener noreferrer" href={stringHelper('ABOUT_URL')}><FormattedMessage id="headerActions.about" defaultMessage="About {appName}" values={{ appName: mapGlobalMessage(this.props.intl, 'appNameHuman') }} /></a>
+            <a
+              className="header-actions__link"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={stringHelper('ABOUT_URL')}
+            >
+              <FormattedMessage
+                id="headerActions.about"
+                defaultMessage="About {appName}"
+                values={{
+                  appName: mapGlobalMessage(this.props.intl, 'appNameHuman'),
+                }}
+              />
+            </a>
           </li>
         </ul>
       </div>
