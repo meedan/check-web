b2805174c96bfc679509f4f70527f53efd5ffae8
Chris Blow <chris@meedan.com>
2017-07-25 10:11:57 -0700

    #6183 Fix test, remove stylesheet

--- test/unit/header.spec.js
+++ test/unit/header.spec.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
 import Header from '../../src/app/components/Header';
 import TeamHeader from '../../src/app/components/team/TeamHeader';
+import { mountWithIntl } from './helpers/intl-test';
 
 describe('<Header />', () => {
   it('renders full team header on members page', () => {
     const location = { pathname: '/team/members' };
-    const header = shallow(<Header location={location} params={{}} />);
+    const header = mountWithIntl(<Header location={location} params={{}} />);
     expect(header.find(TeamHeader)).to.have.length(1);
   });
 });
 
-// TODO — We used to have a component "TeamPublicHeader" which this test exercised, but it didn't add a lot of value so it's been removed. Now this test does not do much. We should improve it to better exercise header logic, as we need it. CGB 2017-7-23
+// TODO: We used to have a component "TeamPublicHeader" which this test exercised, but it didn't add a lot of value so it's been removed. Now this test does not do much. We should improve it to better exercise header logic, as we need it. CGB 2017-7-23
