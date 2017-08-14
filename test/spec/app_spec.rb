25c49f7f17c0976d042e9851bda7099a59b92a49
Caio SBA <caiosba@gmail.com>
2017-08-14 12:14:57 -0700

    Ticket #6219: Another attempt to use custom cached bundler path

--- test/spec/app_spec.rb
+++ test/spec/app_spec.rb
@@ -110,7 +110,7 @@ shared_examples 'app' do |webdriver_url, browser_capabilities|
         .logout_and_close
     end
 
-    it "should redirect to access denied page bli" do
+    it "should redirect to access denied page" do
       user = api_register_and_login_with_email
       api_logout
       api_register_and_login_with_email
