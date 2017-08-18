c83ae159f2ece1997bc92f50bee9da8be4572488
Chris Blow <chris@meedan.com>
2017-08-17 18:11:10 -0700

    #6168 Lint PenderCard

--- src/app/components/PenderCard.js
+++ src/app/components/PenderCard.js
@@ -1,10 +1,30 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 
 class PenderCard extends Component {
+
+  componentDidMount() {
+    this.addTag();
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return nextProps.url !== this.props.url || nextProps.mediaVersion !== this.props.mediaVersion;
+  }
+
+  componentDidUpdate() {
+    this.removeTag();
+    this.addTag();
+  }
+
+  componentWillUnmount() {
+    this.removeTag();
+  }
+
   addTag() {
     const script = document.createElement('script');
     const version = this.props.mediaVersion || 0;
-    script.src = `${this.props.penderUrl}/api/medias.js?version=${version}&url=${encodeURIComponent(this.props.url)}`;
+    script.src = `${this.props.penderUrl}/api/medias.js?version=${version}&url=${encodeURIComponent(
+      this.props.url,
+    )}`;
     script.async = true;
     script.type = 'text/javascript';
     const card = document.getElementById('pender-card');
@@ -21,33 +41,33 @@ class PenderCard extends Component {
     }
   }
 
-  componentDidMount() {
-    this.addTag();
-  }
-
-  componentDidUpdate() {
-    this.removeTag();
-    this.addTag();
-  }
-
-  componentWillUnmount() {
-    this.removeTag();
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return ((nextProps.url != this.props.url) || (nextProps.mediaVersion != this.props.mediaVersion));
-  }
-
   render() {
     return (
       <div id="pender-card" className="pender-card">
         <div id="pender-card-loader" className="pender-card__loader">
           {(() => {
             if (this.props.fallback) {
-              return (this.props.fallback);
+              return this.props.fallback;
             }
-            return (<svg className="spinner" width="40px" height="40px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg"><circle className="spinner-path" fill="none" strokeWidth="2" strokeLinecap="round" cx="33" cy="33" r="30" />
-            </svg>);
+            return (
+              <svg
+                className="spinner"
+                width="40px"
+                height="40px"
+                viewBox="0 0 66 66"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  className="spinner-path"
+                  fill="none"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  cx="33"
+                  cy="33"
+                  r="30"
+                />
+              </svg>
+            );
           })()}
         </div>
       </div>
