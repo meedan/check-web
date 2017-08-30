e5e9cf540115476fca2523a6d846a726c18ee2b8
infojunkie <karim.ratib@gmail.com>
2017-08-29 17:11:53 -0700

    Revert ":zzz:"
    This reverts commit 6eb758f9954ae82125073c38793f2ba0be48c2ab.

--- test/spec/api_helpers.rb
+++ test/spec/api_helpers.rb
@@ -53,7 +53,7 @@ module ApiHelpers
     data = api_create_team_and_project
     claim = request_api 'claim', { quote: quote, email: data[:user].email, team_id: data[:team].dbid, project_id: data[:project].dbid }
     @driver.quit if quit
-    claim
+    claim 
   end
 
   def api_create_team_project_and_link(url = @media_url)
@@ -64,7 +64,7 @@ module ApiHelpers
   def api_create_team_project_and_link_and_redirect_to_media_page(url = @media_url)
     media = api_create_team_project_and_link url
     @driver.navigate.to media.full_url
-    sleep 5
+    sleep 2
     MediaPage.new(config: @config, driver: @driver)
   end
 
@@ -74,7 +74,7 @@ module ApiHelpers
     sleep 2
     MediaPage.new(config: @config, driver: @driver)
   end
-
+  
   def api_create_claim_and_go_to_search_page
     media = api_create_team_project_and_claim(false, 'My search result')
     @driver.navigate.to media.full_url
