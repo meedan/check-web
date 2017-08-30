9839595cd3672f9d5023cd013583cdf59a880f74
Caio SBA <caiosba@gmail.com>
2017-08-30 12:33:33 -0700

    Ticket #6257: :zzz:

--- test/spec/app_spec.rb
+++ test/spec/app_spec.rb
@@ -146,7 +146,7 @@ shared_examples 'app' do |webdriver_url, browser_capabilities|
 
       # Try to add duplicate
       page.add_tag(new_tag)
-      sleep 5
+      sleep 10
 
       # Verify that tag is not added and that error message is displayed
       expect(page.tags.count(new_tag)).to be(1)
