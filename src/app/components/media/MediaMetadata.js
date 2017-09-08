353c4f22e0bbc07599b994678b16b4cacb7540fb
Chris Blow <chris@meedan.com>
2017-09-07 22:33:40 -0700

    Add sourceUrl WIP #6109

--- src/app/components/media/MediaMetadata.js
+++ src/app/components/media/MediaMetadata.js
@@ -7,6 +7,7 @@ import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
 import TextField from 'material-ui/TextField';
 import styled from 'styled-components';
 import rtlDetect from 'rtl-detect';
+import { Link } from 'react-router';
 import MediaTags from './MediaTags';
 import MediaActions from './MediaActions';
 import MediaUtil from './MediaUtil';
@@ -232,12 +233,17 @@ class MediaMetadata extends Component {
 
   render() {
     const { media, mediaUrl } = this.props;
+    const data = JSON.parse(media.embed);
     const context = this.getContext();
     const currentProject = this.currentProject();
     const destinationProjects = this.destinationProjects();
     const locale = this.props.intl.locale;
     const isRtl = rtlDetect.isRtlLang(locale);
     const fromDirection = isRtl ? 'right' : 'left';
+    const sourceUrl = media.team && media.project && media.project_source ? `/${media.team.slug}/project/${media.project.dbid}/source/${media.project_source.dbid}` : null;
+    const authorName = MediaUtil.authorName(media, data);
+    const authorUsername = MediaUtil.authorUsername(media, data);
+    const authorUrl = MediaUtil.authorUrl(media, data);
 
     const byUser = media.user &&
       media.user.source &&
@@ -317,6 +323,19 @@ class MediaMetadata extends Component {
           </span>
           : null}
         {media.tags ? <MediaTags media={media} tags={media.tags.edges} isEditing={false} /> : null}
+
+        { sourceUrl
+          ? <Link to={sourceUrl}>{authorName || authorUsername}</Link>
+          : <a
+            href={sourceUrl || authorUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-media-card__name"
+          >
+            {authorName || authorUsername}
+          </a>
+        }
+
         {this.props.readonly || this.state.isEditing
           ? null
           : <MediaActions
