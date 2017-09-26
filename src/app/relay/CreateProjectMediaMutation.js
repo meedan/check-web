5a4c9450887000350dbf2175515ba4a6c1147374
Sawy <melsawy.be7eri@gmail.com>
2017-09-26 23:35:00 +0200

    Ticket #6301 - fix test

--- src/app/relay/CreateProjectMediaMutation.js
+++ src/app/relay/CreateProjectMediaMutation.js
@@ -24,7 +24,7 @@ class CreateProjectMediaMutation extends Relay.Mutation {
     return {
       url: this.props.url,
       quote: this.props.quote,
-      quote_attributions: this.props.quote_attributions
+      quote_attributions: this.props.quote_attributions,
       project_id: this.props.project.dbid,
     };
   }
