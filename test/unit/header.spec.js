50b6654ba5cc592aef531b111825b6ab2a6acdd7
Chris Blow <chris@meedan.com>
2017-09-29 22:20:36 -0700

    Remove a test for removed conditional header logic #6388

--- test/unit/header.spec.js
+++ test/unit/header.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { expect } from 'chai';
-import IconMenu from 'material-ui/IconMenu';
 import IconArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
 import Header from '../../src/app/components/Header';
 import TeamHeader from '../../src/app/components/team/TeamHeader';
@@ -22,12 +21,5 @@ describe('<Header />', () => {
     expect(header.find(TeamHeader)).to.have.length(0);
     expect(header.find(TeamPublicHeader)).to.have.length(1);
     expect(header.find(IconArrowBack)).to.have.length(0);
-  });
-
-  it('does not show the avatar or team icon on teams page', () => {
-    const location = { pathname: '/check/teams' };
-    const header = mountWithIntl(<Header location={location} loggedIn params={{}} />);
-    expect(header.find(TeamHeader)).to.have.length(0);
-    expect(header.find(IconMenu)).to.have.length(0);
   });
 });
