d773d36604863405c4c081bd2ec9f79f50283e54
Caio Almeida <caiosba@gmail.com>
2017-09-29 23:04:21 -0300

    Ticket #6323: Removing fields that Check does not need, from the GraphQL query

---
+++ src/app/relay/bridgeSearchResultFragment.js
@@ -0,0 +1,136 @@
+import Relay from 'react-relay';
+
+const bridgeSearchResultFragment = Relay.QL`
+  fragment on CheckSearch {
+    id,
+    pusher_channel,
+    medias(first: $pageSize) {
+      edges {
+        node {
+          id,
+          dbid,
+          url,
+          quote,
+          published,
+          updated_at,
+          embed,
+          archived,
+          log_count,
+          verification_statuses,
+          translation_statuses,
+          overridden,
+          project_id,
+          pusher_channel,
+          language,
+          language_code,
+          domain,
+          permissions,
+          last_status,
+          field_value(annotation_type_field_name: "translation_status:translation_status_status"),
+          translation_status: annotation(annotation_type: "translation_status") {
+            id
+            dbid
+          }
+          last_status_obj {
+            id,
+            dbid
+          }
+          project {
+            id,
+            dbid,
+            search_id,
+            title
+          },
+          project_source {
+            dbid,
+            project_id,
+            source {
+              name
+            }
+          },
+          media {
+            url,
+            quote,
+            embed_path,
+            thumbnail_path
+          }
+          user {
+            dbid,
+            name,
+            source {
+              dbid,
+              accounts(first: 10000) {
+                edges {
+                  node {
+                    url
+                  }
+                }
+              }
+            }
+          }
+          team {
+            slug
+            search_id
+          }
+          tags(first: 10000) {
+            edges {
+              node {
+                tag,
+                id
+              }
+            }
+          }
+        }
+      }
+    },
+    sources(first: $pageSize) {
+      edges {
+        node {
+          id,
+          dbid,
+          team {
+            dbid,
+            slug
+          },
+          project_id,
+          updated_at,
+          source_id,
+          source {
+            id,
+            dbid,
+            name,
+            description,
+            image,
+            accounts(first: 10000) {
+              edges {
+                node {
+                  id,
+                  data,
+                  embed,
+                  provider,
+                  url
+                }
+              }
+            }
+          },
+          user {
+            name,
+            source {
+              dbid,
+              accounts(first: 10000) {
+                edges {
+                  node {
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    number_of_results
+  }
+`;
+
+module.exports = bridgeSearchResultFragment;
