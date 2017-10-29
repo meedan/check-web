13080bb4d7bc03f575dfcbcb869d353e1defd958
Chris Blow <chris@meedan.com>
2017-10-29 16:21:10 -0700

    Use material card and button, update copywriting #6404

--- src/app/components/UserConfirmed.js
+++ src/app/components/UserConfirmed.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router';
+import RaisedButton from 'material-ui/RaisedButton';
+import { Card, CardTitle, CardText, CardActions } from 'material-ui/Card';
 import { ContentColumn } from '../styles/js/shared';
 
 class UserConfirmed extends Component {
   render() {
     return (
-      <ContentColumn className="card">
-        <h2 className="main-title"><FormattedMessage id="userConfirmed.heading" defaultMessage="Account Confirmed" /></h2>
-        <p>
-          <FormattedMessage
-            id="userConfirmed.message"
-            defaultMessage={'Thanks for confirming your e-mail address! Now you can {login}.'}
-            values={{ login: <Link to="/"><FormattedMessage id="userConfirmed.login" defaultMessage="login" /></Link> }}
+      <ContentColumn>
+        <Card>
+          <CardTitle
+            title={<FormattedMessage id="userConfirmed.heading" defaultMessage="Account Confirmed" />}
           />
-        </p>
+          <CardText>
+            <p>
+              <FormattedMessage
+                id="userConfirmed.message"
+                defaultMessage={'Thanks for confirming your e-mail address! Now you can sign in.'}
+              />
+            </p>
+
+          </CardText>
+          <CardActions>
+            <Link to="/">
+              <RaisedButton label={<FormattedMessage defaultMessage="Sign In" id="headerActions.signIn" />} primary />
+            </Link>
+          </CardActions>
+        </Card>
       </ContentColumn>
     );
   }
