4e04b1f87df0dffbd50c589924c0d3af6f543146
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2017-11-15 12:53:49 -0300

    #6339 Refactor TaskUpdate, submit null description on edit

--- src/app/components/annotations/TaskUpdate.js
+++ src/app/components/annotations/TaskUpdate.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
+let from = null;
+let to = null;
+let editedTitle = false;
+let editedNote = false;
+let createdNote = false;
+
 function shouldLogChange(activity) {
   const changes = JSON.parse(activity.object_changes_json);
   if (changes.data) {
-    const from = changes.data[0];
-    const to = changes.data[1];
-    let editedTitle = false;
-    let editedNote = false;
-    let createdNote = false;
+    from = changes.data[0];
+    to = changes.data[1];
+
     if (from.label && to.label && from.label != to.label) {
       editedTitle = true;
     }
@@ -29,36 +33,40 @@ function shouldLogChange(activity) {
 class TaskUpdate extends React.Component {
   render() {
     const author = this.props.authorName;
-    return (
-      <span>
-        <span className="annotation__update-task" />
-        {editedTitle
+
+    if (shouldLogChange(this.props.activity)) {
+      return (
+        <span>
+          <span className="annotation__update-task" />
+          {editedTitle
             ? <FormattedMessage
-              id="annotation.taskLabelUpdated"
-              defaultMessage={'Task "{from}" edited to "{to}" by {author}'}
-              values={{ from: from.label, to: to.label, author }}
+            id="annotation.taskLabelUpdated"
+            defaultMessage={'Task "{from}" edited to "{to}" by {author}'}
+            values={{ from: from.label, to: to.label, author }}
             />
-            : null}
-        { editedTitle && editedNote && <br />}
-        {editedNote
+          : null}
+          { editedTitle && editedNote && <br />}
+          { editedNote
             ? <FormattedMessage
-              id="annotation.taskNoteUpdated"
-              defaultMessage={
-                  'Task "{title}" has note edited from "{from}" to "{to}" by {author}'
-                }
-              values={{ title: to.label, from: from.description, to: to.description, author }}
+            id="annotation.taskNoteUpdated"
+            defaultMessage={
+              'Task "{title}" has note edited from "{from}" to "{to}" by {author}'
+            }
+            values={{ title: to.label, from: from.description, to: to.description, author }}
             />
-            : null}
-        { editedTitle && createdNote && <br />}
-        {createdNote
+          : null}
+          { editedTitle && createdNote && <br />}
+          { createdNote
             ? <FormattedMessage
-              id="annotation.taskNoteCreated"
-              defaultMessage={'Task "{title}" has new note "{note}" by {author}'}
-              values={{ title: to.label, note: to.description, author }}
+            id="annotation.taskNoteCreated"
+            defaultMessage={'Task "{title}" has new note "{note}" by {author}'}
+            values={{ title: to.label, note: to.description, author }}
             />
-            : null}
-      </span>
-    );
+          : null}
+        </span>
+      );
+    }
+    return null;
   }
 }
 
