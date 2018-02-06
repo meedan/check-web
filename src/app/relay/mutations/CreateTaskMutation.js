0818e2aff5e16d89e265157b732d679614fd8bce
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2018-02-06 17:44:17 -0300

    #6607 update project_media.verification_statuses on create/update task

--- src/app/relay/mutations/CreateTaskMutation.js
+++ src/app/relay/mutations/CreateTaskMutation.js
@@ -8,7 +8,7 @@ class CreateTaskMutation extends Relay.Mutation {
   }
 
   getFatQuery() {
-    return Relay.QL`fragment on CreateTaskPayload { taskEdge, project_media { log, log_count } }`;
+    return Relay.QL`fragment on CreateTaskPayload { taskEdge, project_media { verification_statuses, translation_statuses, log, log_count } }`;
   }
 
   getVariables() {
