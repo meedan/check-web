d1d63f60d49c4b36b81411295c34e5bf24ab44eb
Caio Almeida <caiosba@gmail.com>
2018-03-13 10:58:49 -0300

    Attempt to fix stalled Travis job

--- src/app/components/media/DestinationProjects.js
+++ src/app/components/media/DestinationProjects.js
@@ -3,6 +3,7 @@ import Relay from 'react-relay';
 import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
 import { units } from '../../styles/js/shared';
 import TeamRoute from '../../relay/TeamRoute';
+import RelayContainer from '../../relay/RelayContainer';
 
 const destinationProjects = (team, projectId) => {
   if (team.projects) {
@@ -61,7 +62,7 @@ const DestinationProjects = (props) => {
   console.log(teamSlug);
 
   return (
-    <Relay.RootContainer
+    <RelayContainer
       Component={DestinationProjectsContainer}
       route={route}
       renderFetched={data => <DestinationProjectsContainer {...props} {...data} />}
