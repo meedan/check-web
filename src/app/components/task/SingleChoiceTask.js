a31d1a87ad0b26ccc70134430bd2f2c3a4d1b1d7
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2018-03-31 01:50:27 -0300

    Use CreateOptionsTask to create MultiSelect tasks

--- src/app/components/task/SingleChoiceTask.js
+++ src/app/components/task/SingleChoiceTask.js
@@ -1,77 +1,20 @@
 import React, { Component } from 'react';
-import Checkbox from 'material-ui/Checkbox';
 import Radio, { RadioGroup } from 'material-ui-next/Radio';
 import { FormGroup, FormControlLabel } from 'material-ui-next/Form';
 import FlatButton from 'material-ui/FlatButton';
-import Dialog from 'material-ui/Dialog';
-import TextField from 'material-ui/TextField';
-import { FormattedMessage, defineMessages, injectIntl, intlShape } from 'react-intl';
-import MdCancel from 'react-icons/lib/md/cancel';
-import MdRadioButtonUnchecked from 'react-icons/lib/md/radio-button-unchecked';
-import Message from '../Message';
-import Attribution from './Attribution';
-import ConfirmRequired from './ConfirmRequired';
-import { safelyParseJSON, getStatus } from '../../helpers';
-import { mediaStatuses, mediaLastStatus } from '../../customHelpers';
-import { units, StyledIconButton, StyledSmallTextField, StyledTaskDescription } from '../../styles/js/shared';
-
-const messages = defineMessages({
-  addValue: {
-    id: 'singleChoiceTask.addValue',
-    defaultMessage: 'Add Option',
-  },
-  value: {
-    id: 'singleChoiceTask.value',
-    defaultMessage: 'Value',
-  },
-  addOther: {
-    id: 'singleChoiceTask.addOther',
-    defaultMessage: 'Add "Other"',
-  },
-  other: {
-    id: 'singleChoiceTask.other',
-    defaultMessage: 'Other',
-  },
-  newTask: {
-    id: 'singleChoiceTask.newTask',
-    defaultMessage: 'New task',
-  },
-});
+import { FormattedMessage } from 'react-intl';
+import CreateOptionsTask from './CreateOptionsTask';
+import { safelyParseJSON } from '../../helpers';
+import { StyledSmallTextField } from '../../styles/js/shared';
 
 class SingleChoiceTask extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      label: null,
-      description: null,
-      message: null,
-      options: [{ label: '' }, { label: '' }],
       responseOther: null,
-      submitDisabled: true,
       taskAnswerDisabled: true,
-      showAssignmentField: false,
-      required: false,
-      confirmRequired: false,
     };
-  }
-
-  handleSubmitTask() {
-    const jsonoptions = JSON.stringify(this.state.options.filter(item => item.label !== ''));
-
-    if (!this.state.submitDisabled) {
-      this.props.onSubmit(
-        this.state.label,
-        this.state.description,
-        this.state.required,
-        jsonoptions,
-      );
-      this.setState({ submitDisabled: true });
-    }
-  }
-
-  toggleAssignmentField() {
-    this.setState({ showAssignmentField: !this.state.showAssignmentField });
   }
 
   handleSubmitResponse() {
@@ -95,71 +38,6 @@ class SingleChoiceTask extends Component {
     this.setState({ note: e.target.value }, this.canSubmit);
   }
 
-  handleLabelChange(e) {
-    this.setState({ label: e.target.value });
-    this.validateSingleChoice(e.target.value, this.state.options);
-  }
-
-  handleDescriptionChange(e) {
-    this.setState({ description: e.target.value });
-  }
-
-  handleAddValue() {
-    const options = Array.isArray(this.state.options) ? this.state.options.slice(0) : [];
-    if (this.state.hasOther) {
-      options.splice(-1, 0, { label: '' });
-    } else {
-      options.push({ label: '' });
-    }
-    this.setState({ options });
-
-    this.validateSingleChoice(this.state.label, options);
-  }
-
-  handleAddOther() {
-    const options = Array.isArray(this.state.options) ? this.state.options.slice(0) : [];
-    const other = true;
-    let label = '';
-
-    if (!this.state.hasOther) {
-      label = this.props.intl.formatMessage(messages.other);
-      options.push({ label, other });
-      this.setState({ options, hasOther: true });
-    }
-
-    this.validateSingleChoice(this.state.label, options);
-  }
-
-  handleEditOption(e) {
-    const options = this.state.options.slice(0);
-    options[parseInt(e.target.id, 10)].label = e.target.value;
-    this.setState({ options });
-
-    this.validateSingleChoice(this.state.label, options);
-  }
-
-  handleRemoveOption(index) {
-    const options = this.state.options.slice(0);
-    let hasOther = null;
-
-    if (this.state.hasOther) {
-      hasOther = index !== options.length - 1;
-    } else {
-      hasOther = false;
-    }
-
-    options.splice(index, 1);
-    this.setState({ options, hasOther });
-
-    this.validateSingleChoice(this.state.label, options);
-  }
-
-  validateSingleChoice(label, options) {
-    const valid = !!(label && label.trim()) && options.filter(item => item.label !== '').length > 1;
-    this.setState({ submitDisabled: !valid });
-    return valid;
-  }
-
   handleCancelResponse() {
     this.setState({
       response: null,
@@ -168,6 +46,7 @@ class SingleChoiceTask extends Component {
       note: '',
       focus: false,
     }, this.canSubmit);
+
     if (this.props.onDismiss) {
       this.props.onDismiss();
     }
@@ -186,6 +65,7 @@ class SingleChoiceTask extends Component {
   handleSelectRadioOther() {
     // TODO Use React ref
     const input = document.querySelector('.task__option_other_text_input input');
+
     if (input) {
       input.focus();
     }
@@ -209,17 +89,6 @@ class SingleChoiceTask extends Component {
     }, this.canSubmit);
   }
 
-  handleSelectRequired(e, inputChecked) {
-    const { media } = this.props;
-    const status = getStatus(mediaStatuses(media), mediaLastStatus(media));
-
-    if (status.completed && inputChecked) {
-      this.setState({ required: inputChecked, confirmRequired: true, status });
-    } else {
-      this.setState({ required: inputChecked });
-    }
-  }
-
   handleKeyPress(e) {
     if (e.key === 'Enter' && !e.shiftKey) {
       if (this.canSubmit()) {
@@ -228,147 +97,6 @@ class SingleChoiceTask extends Component {
       }
       e.preventDefault();
     }
-  }
-
-  renderCreateDialog() {
-    const canRemove = this.state.options.length > 2;
-    const { formatMessage } = this.props.intl;
-    const actions = [
-      <FlatButton
-        key="create-task__dialog-cancel-button"
-        label={<FormattedMessage id="tasks.cancelAdd" defaultMessage="Cancel" />}
-        onClick={this.props.onDismiss.bind(this)}
-      />,
-      <FlatButton
-        key="create-task__dialog-submit-button"
-        className="create-task__dialog-submit-button"
-        label={<FormattedMessage id="tasks.add" defaultMessage="Create Task" />}
-        primary
-        keyboardFocused
-        onClick={this.handleSubmitTask.bind(this)}
-        disabled={this.state.submitDisabled}
-      />,
-    ];
-
-    return (
-      <div>
-        <Dialog
-          title={this.props.intl.formatMessage(messages.newTask)}
-          className="create-task__dialog"
-          actions={actions}
-          modal={false}
-          open={this.props.mode === 'create'}
-          onRequestClose={this.props.onDismiss}
-          autoScrollBodyContent
-        >
-          <Message message={this.state.message} />
-
-          <TextField
-            id="task-label-input"
-            className="tasks__task-label-input"
-            floatingLabelText={<FormattedMessage id="tasks.taskPrompt" defaultMessage="Prompt" />}
-            onChange={this.handleLabelChange.bind(this)}
-            multiLine
-            fullWidth
-          />
-
-          <Checkbox
-            label={
-              <FormattedMessage
-                id="tasks.requiredCheckbox"
-                defaultMessage="Required"
-              />
-            }
-            checked={this.state.required}
-            onCheck={this.handleSelectRequired.bind(this)}
-          />
-          <ConfirmRequired
-            open={this.state.confirmRequired}
-            status={this.state.status}
-            handleCancel={() => { this.setState({ required: false, confirmRequired: false }); }}
-            handleConfirm={() => { this.setState({ confirmRequired: false }); }}
-          />
-
-
-          <div style={{ marginTop: units(2) }}>
-            {this.state.options.map((item, index) => (
-              <div key={`create-task__add-options-radiobutton-${index.toString()}`}>
-                <StyledIconButton>
-                  <MdRadioButtonUnchecked
-                    key="create-task__md-icon"
-                    className="create-task__md-icon"
-                  />
-                </StyledIconButton>
-                <TextField
-                  key="create-task__add-option-input"
-                  className="create-task__add-option-input"
-                  id={index.toString()}
-                  onChange={this.handleEditOption.bind(this)}
-                  placeholder={`${formatMessage(messages.value)} ${index + 1}`}
-                  value={item.label}
-                  disabled={item.other}
-                  style={{ padding: `${units(0.5)} ${units(1)}`, width: '75%' }}
-                />
-                {canRemove ?
-                  <StyledIconButton>
-                    <MdCancel
-                      key="create-task__remove-option-button"
-                      className="create-task__remove-option-button create-task__md-icon"
-                      onClick={this.handleRemoveOption.bind(this, index)}
-                    />
-                  </StyledIconButton>
-                  : null}
-              </div>
-            ))}
-            <div style={{ marginTop: units(1) }}>
-              <FlatButton
-                label={this.props.intl.formatMessage(messages.addValue)}
-                onClick={this.handleAddValue.bind(this)}
-              />
-              <FlatButton
-                label={this.props.intl.formatMessage(messages.addOther)}
-                onClick={this.handleAddOther.bind(this)}
-                disabled={this.state.hasOther}
-              />
-            </div>
-          </div>
-          <StyledTaskDescription>
-            <input
-              className="create-task__add-task-description"
-              id="create-task__add-task-description"
-              type="checkbox"
-            />
-            <TextField
-              id="task-description-input"
-              className="create-task__task-description-input"
-              floatingLabelText={
-                <FormattedMessage id="tasks.description" defaultMessage="Description" />
-              }
-              onChange={this.handleDescriptionChange.bind(this)}
-              multiLine
-            />
-            <label
-              className="create-task__add-task-description-label"
-              htmlFor="create-task__add-task-description"
-            >
-              <span className="create-task__add-task-description-icon">+</span>{' '}
-              <FormattedMessage id="tasks.addDescription" defaultMessage="Add a description" />
-            </label>
-
-            { this.state.showAssignmentField ?
-              <Attribution multi={false} selectedUsers={[]} id="new" /> :
-              <button
-                className="create-task__add-assignment-button"
-                onClick={this.toggleAssignmentField.bind(this)}
-              >
-                {'+ '}
-                <FormattedMessage id="tasks.assign" defaultMessage="Assign" />
-              </button>
-            }
-          </StyledTaskDescription>
-        </Dialog>
-      </div>
-    );
   }
 
   renderOptions(response, note, jsonoptions) {
@@ -471,7 +199,7 @@ class SingleChoiceTask extends Component {
             </div>
 
             {editable ?
-              <TextField
+              <StyledSmallTextField
                 className="task__response-note-input"
                 hintText={
                   <FormattedMessage
@@ -498,11 +226,24 @@ class SingleChoiceTask extends Component {
   }
 
   render() {
-    const { response, note, jsonoptions } = this.props;
+    const {
+      media,
+      response,
+      note,
+      jsonoptions,
+    } = this.props;
 
     return (
       <div>
-        {this.props.mode === 'create' ? this.renderCreateDialog() : null}
+        {this.props.mode === 'create' ?
+          <CreateOptionsTask
+            taskType="single_choice"
+            media={media}
+            onDismiss={this.props.onDismiss.bind(this)}
+            onSubmit={this.props.onSubmit.bind(this)}
+          />
+          : null
+        }
         {this.props.mode === 'respond' ? this.renderOptions(response, note, jsonoptions) : null}
         {this.props.mode === 'show_response'
           ? this.renderOptions(response, note, jsonoptions)
@@ -515,10 +256,4 @@ class SingleChoiceTask extends Component {
   }
 }
 
-SingleChoiceTask.propTypes = {
-  // https://github.com/yannickcr/eslint-plugin-react/issues/1389
-  // eslint-disable-next-line react/no-typos
-  intl: intlShape.isRequired,
-};
-
-export default injectIntl(SingleChoiceTask);
+export default SingleChoiceTask;
