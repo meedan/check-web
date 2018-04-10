603674365258c3c38655f7192d5763c615a2a8b1
infojunkie <karim.ratib@gmail.com>
2018-04-10 14:11:10 -0700

    Better target submit button #6678

--- test/spec/app_spec.rb
+++ test/spec/app_spec.rb
@@ -1731,9 +1731,9 @@ shared_examples 'app' do |webdriver_url, browser_capabilities|
       #media
       media_pg = api_create_team_project_and_claim_and_redirect_to_media_page
       expect(@driver.page_source.include?('Your comment was added!')).to be(false)
-      old = wait_for_selector_list("annotation__default-content",:class).length
+      old = wait_for_selector_list("annotation__default-content", :class).length
       fill_field('textarea[name="cmd"]', 'Test')
-      el = wait_for_selector("//span[contains(text(), 'Submit')]", :xpath)
+      el = wait_for_selector(".add-annotation button[type=submit]", :class)
       el.click
       old = wait_for_size_change(old, "annotation__default-content", :class)
       expect(@driver.page_source.include?('Your comment was added!')).to be(true)
