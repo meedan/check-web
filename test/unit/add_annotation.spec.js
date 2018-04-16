f9ac189f7e748e7ae5ec86b5aac3ec36e7412710
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2018-04-16 17:49:57 -0300

    #6751 AddAnnotation unit test

---
+++ test/unit/add_annotation.spec.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mountWithIntl } from './helpers/intl-test';
+
+import AddAnnotation from '../../src/app/components/annotations/AddAnnotation';
+
+describe('<AddAnnotation />', () => {
+  const annotated = { archived: true };
+  const annotatedType = 'ProjectMedia';
+
+  it('Hides when media is archived (Trash)', () => {
+    const addAnnotation = mountWithIntl(<AddAnnotation annotated={annotated} />);
+    expect(addAnnotation.html()).to.equal(null);
+  });
+});
