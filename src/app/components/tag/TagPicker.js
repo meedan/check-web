26260f8fdcce607b74c5438c2b63245bf1af2e54
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2018-06-27 16:12:46 -0300

    #6891 Apply QA feedback

--- src/app/components/tag/TagPicker.js
+++ src/app/components/tag/TagPicker.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Relay from 'react-relay';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import isEqual from 'lodash.isequal';
 import { FormGroup, FormControlLabel } from 'material-ui-next/Form';
 import CheckboxNext from 'material-ui-next/Checkbox';
 import styled from 'styled-components';
@@ -78,27 +79,60 @@ class TagPickerComponent extends React.Component {
     }
   }
 
+  renderNotFound(shownTagsCount, totalTagsCount) {
+    if (shownTagsCount > 0) {
+      return null;
+    }
+
+    if (totalTagsCount === 0) {
+      return (
+        <StyledNotFound>
+          <FormattedMessage
+            id="tagPicker.emptyTags"
+            defaultMessage="You currently have no tags added"
+          />
+        </StyledNotFound>
+      );
+    }
+
+    return (
+      <StyledNotFound>
+        <FormattedMessage
+          id="tagPicker.tagNotFound"
+          defaultMessage='Tag "{tag}" not found'
+          values={{ tag: this.props.value }}
+        />
+      </StyledNotFound>
+    );
+  }
+
   render() {
     const { media, tags, value } = this.props;
 
-    const plainMediaTags = tags.map(tag => tag.node.tag);
+    const compareString = (tag, val) => tag.toLowerCase().includes(val.toLowerCase());
+
+    // eslint-disable-next-line no-underscore-dangle
+    const plainMediaTags = tags.filter(tag => !tag.__mutationStatus__).map(tag => tag.node.tag);
 
     const suggestedTags = media.team && media.team.get_suggested_tags
-      ? media.team.get_suggested_tags.split(',').filter(tag => tag.includes(value))
+      ? media.team.get_suggested_tags.split(',').filter(tag => compareString(tag, value))
       : [];
 
     const nonRepeatedUsedTags =
-      difference(media.team.used_tags, suggestedTags).filter(tag => tag.includes(value));
+      difference(media.team.used_tags, suggestedTags).filter(tag => compareString(tag, value));
 
     const checkedSuggestedTags = intersection(suggestedTags, plainMediaTags);
     const uncheckedSuggestedTags = difference(suggestedTags, plainMediaTags);
 
     const checkedUsedTags =
-      difference(plainMediaTags, suggestedTags).filter(tag => tag.includes(value));
+      difference(plainMediaTags, suggestedTags).filter(tag => compareString(tag, value));
     const uncheckedUsedTags = difference(nonRepeatedUsedTags, plainMediaTags);
 
     const shownSuggestedCount = checkedSuggestedTags.length + uncheckedSuggestedTags.length;
     const shownUsedCount = checkedUsedTags.length + uncheckedUsedTags.length;
+
+    const shownTagsCount = shownSuggestedCount + shownUsedCount;
+    const totalTagsCount = suggestedTags.length + tags.length + media.team.used_tags.length;
 
     return (
       <StyledTagPickerArea>
@@ -144,16 +178,7 @@ class TagPickerComponent extends React.Component {
             />
           ))}
         </FormGroup>
-        { shownSuggestedCount + shownUsedCount > 0 ?
-          null :
-          <StyledNotFound>
-            <FormattedMessage
-              id="tagPicker.tagNotFound"
-              defaultMessage='Tag "{tag}" not found'
-              values={{ tag: this.props.value }}
-            />
-          </StyledNotFound>
-        }
+        { this.renderNotFound(shownTagsCount, totalTagsCount) }
       </StyledTagPickerArea>
     );
   }
@@ -186,17 +211,29 @@ const TagPickerContainer = Relay.createContainer(TagPickerComponent, {
   },
 });
 
-const TagPicker = (props) => {
-  const ids = `${props.media.dbid},${props.media.project_id}`;
-  const route = new MediaRoute({ ids });
+// eslint-disable-next-line react/no-multi-comp
+class TagPicker extends React.Component {
+  // eslint-disable-next-line class-methods-use-this
+  shouldComponentUpdate(nextProps, nextState) {
+    if (isEqual(this.props, nextProps) && isEqual(this.state, nextState)) {
+      return false;
+    }
+    return true;
+  }
 
-  return (
-    <RelayContainer
-      Component={TagPickerContainer}
-      route={route}
-      renderFetched={data => <TagPickerContainer {...props} {...data} />}
-    />
-  );
-};
+  render() {
+    const ids = `${this.props.media.dbid},${this.props.media.project_id}`;
+    const route = new MediaRoute({ ids });
+
+    return (
+      <RelayContainer
+        Component={TagPickerContainer}
+        route={route}
+        renderFetched={data => <TagPickerContainer {...this.props} {...data} />}
+        forceFetch
+      />
+    );
+  }
+}
 
 export default TagPicker;
