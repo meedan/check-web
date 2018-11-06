9515252dd9b014b5f39849c3239938f169760d37
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2018-11-06 15:14:37 -0300

    #6847 Prevent actions from unnecessary re-rendering

--- src/app/components/layout/CardHeaderOutside.js
+++ src/app/components/layout/CardHeaderOutside.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Row } from '../../styles/js/shared';
 
 const CardHeaderOutside = (props) => {
-  const AlignOpposite = styled.div`
-    margin-${props.direction.from}: auto;
-  `;
+  const style = {};
+  style[`margin-${props.direction.from}`] = 'auto';
 
   return (
     <Row>
       <h2>{props.title}</h2>
-      <AlignOpposite>{props.actions}</AlignOpposite>
+      <div style={style}>{props.actions}</div>
     </Row>
   );
 };
