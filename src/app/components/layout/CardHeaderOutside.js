42797652f4859cccaba871337326801bf72e3d0f
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2018-11-06 15:23:28 -0300

    #6847 Prevent unnecessary renders of action buttons

--- src/app/components/layout/CardHeaderOutside.js
+++ src/app/components/layout/CardHeaderOutside.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Row } from '../../styles/js/shared';
 
 const CardHeaderOutside = (props) => {
-  const style = {};
-  style[`margin-${props.direction.from}`] = 'auto';
+  const style = {
+    marginLeft: props.direction.from === 'left' ? 'auto' : undefined,
+    marginRight: props.direction.from === 'right' ? 'auto' : undefined,
+  };
 
   return (
     <Row>
