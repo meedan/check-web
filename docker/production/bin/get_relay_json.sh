77dd93805e8007a8bec7d5454383f7ac20980aaf
infojunkie <karim.ratib@gmail.com>
2019-02-05 14:23:52 -0800

    Fix relay.json location 2

--- docker/production/bin/get_relay_json.sh
+++ docker/production/bin/get_relay_json.sh
@@ -3,6 +3,8 @@
 cd ${DEPLOYDIR}/new
 
 BRANCH=${VERSION}
+echo $BRANCH
+
 STATUS=$(curl -s -o /dev/null -w "%{http_code}" https://raw.githubusercontent.com/meedan/check-api/${BRANCH}/public/relay.json)
 
 # if it's 404, then grab the one from `develop` branch
