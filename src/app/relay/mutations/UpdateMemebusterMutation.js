da0fabf1c06c487cd4d02547dc5d3af4c9e0d8ac
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2019-02-14 15:32:18 -0300

    #7262 Use createDynamicAnnotationMemebuster

---
+++ src/app/relay/mutations/UpdateMemebusterMutation.js
@@ -0,0 +1,42 @@
+import Relay from 'react-relay/classic';
+
+class UpdateMemebusterMutation extends Relay.Mutation {
+  getMutation() {
+    return Relay.QL`mutation {
+      updateDynamicAnnotationMemebuster
+    }`;
+  }
+
+  getFatQuery() {
+    return Relay.QL`fragment on UpdateDynamicAnnotationMemebusterPayload {
+      dynamicEdge,
+      project_media {
+        annotations(first: 1, annotation_type: "memebuster")
+      }
+    }`;
+  }
+
+  getFiles() {
+    if (this.props.image) {
+      return { 'file[]': this.props.image };
+    }
+    return {};
+  }
+
+  getVariables() {
+    const dynamic = this.props.annotation;
+    return {
+      id: this.props.id,
+      set_fields: JSON.stringify(dynamic.fields),
+      annotation_type: dynamic.annotation_type,
+      annotated_id: `${dynamic.annotated_id}`,
+      annotated_type: dynamic.annotated_type,
+    };
+  }
+
+  getConfigs() {
+    return [];
+  }
+}
+
+export default UpdateMemebusterMutation;
