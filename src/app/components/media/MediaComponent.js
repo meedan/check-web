ba34735678e711f56177a0955aa39b0c976dc1ac
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2019-05-16 22:33:09 -0300

    #7584 Fix error message and pusher update

--- src/app/components/media/MediaComponent.js
+++ src/app/components/media/MediaComponent.js
@@ -101,7 +101,8 @@ class MediaComponent extends Component {
         const relationship = JSON.parse(data.message);
         if (
           (!relationship.id || this.getContext().clientSessionId !== data.actor_session_id) &&
-          (relationship.source_id === this.props.media.dbid)
+          (relationship.source_id === this.props.media.dbid ||
+          relationship.target_id === this.props.media.dbid)
         ) {
           if (run) {
             this.props.relay.forceFetch();
