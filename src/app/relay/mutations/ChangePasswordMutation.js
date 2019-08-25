93ac80a599f52b0a5f9a4c7b84a34a8d666a2309
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2019-08-25 21:05:31 +0200

    Feature/7652 enable 2 fa for non password users (#454)
    * Ticket #7652 - add change password section
    
    * Ticket #7652 - handle change password using new component
    
    * Ticket #7652 - remove un-needed lines
    
    * Ticket #7652 - reuse ChangePasswordComponent in both  reset and update password screens
    
    * Ticket #7657 - style the error msg

--- src/app/relay/mutations/ChangePasswordMutation.js
+++ src/app/relay/mutations/ChangePasswordMutation.js
@@ -18,6 +18,8 @@ class ChangePasswordMutation extends Relay.Mutation {
       reset_password_token: this.props.reset_password_token,
       password: this.props.password,
       password_confirmation: this.props.password_confirmation,
+      current_password: this.props.current_password,
+      id: this.props.id,
     };
   }
 }
