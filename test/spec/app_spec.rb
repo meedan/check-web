83f07d300c3c0f87e016e1f071ed3d7017f9ccb8
danielevalverde <danierzevalverde@gmail.com>
2019-10-07 16:33:59 -0300

    fix test: should search and change sort order

--- test/spec/app_spec.rb
+++ test/spec/app_spec.rb
@@ -1747,18 +1747,18 @@ shared_examples 'app' do |webdriver_url, browser_capabilities|
       api_create_claim_and_go_to_search_page
       expect((@driver.current_url.to_s.match(/ASC|DESC/)).nil?).to be(true)
 
-      wait_for_selector("search__open-dialog-button", :id).click
+      wait_for_selector("#search__open-dialog-button").click
       @driver.find_element(:xpath, "//span[contains(text(), 'Newest')]").click
-      wait_for_selector("search-query__submit-button", :id).click
-      sleep 10
+      wait_for_selector("#search-query__submit-button").click
+      wait_for_selector_none("#search-query__reset-button")
       expect((@driver.current_url.to_s.match(/DESC/)).nil?).to be(false)
       expect((@driver.current_url.to_s.match(/ASC/)).nil?).to be(true)
       expect(@driver.page_source.include?('My search result')).to be(true)
 
-      wait_for_selector("search__open-dialog-button", :id).click
+      wait_for_selector("#search__open-dialog-button").click
       @driver.find_element(:xpath, "//span[contains(text(), 'Oldest')]").click
-      wait_for_selector("search-query__submit-button", :id).click
-      sleep 20
+      wait_for_selector("#search-query__submit-button").click
+      wait_for_selector_none("#search-query__reset-button")
       expect((@driver.current_url.to_s.match(/DESC/)).nil?).to be(true)
       expect((@driver.current_url.to_s.match(/ASC/)).nil?).to be(false)
       expect(@driver.page_source.include?('My search result')).to be(true)
