06d57cd9cbd4fe83e86052d3dc5e44df9625d0bc
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2019-12-17 22:50:46 -0300

    #7859 Fix margins

--- src/app/components/project/Project.js
+++ src/app/components/project/Project.js
@@ -90,7 +90,7 @@ class ProjectComponent extends Component {
               <ProjectActions project={project} />
             </Row>
             {project.description && project.description.trim().length ?
-              <div style={{ margin: `0 ${units(1)} ${units(1)}` }} className="project__description">
+              <div className="project__description">
                 <ParsedText text={project.description} />
               </div>
               : null}
