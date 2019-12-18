500ee8087c8bfbaa92721ddbeb50304f33cf2496
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2019-12-18 08:51:42 -0300

    Feature/7859 layout update (#506)
    * #7859 Sidebar update init
    
    * #7859 More sidebar
    
    * #7859 RTL, Edit team info
    
    * updating testes
    
    * #7859 Restore edit source button
    
    * #7859 Prevent breaking sidebar
    
    * #7859 Keep header for media page only
    
    * #7859 Remove header test obsolete cases
    
    * #7859 Project menu
    
    * #7859 Updating tests
    
    * #7859 Fix edit project tests
    
    * #7859 Fix edit source language test
    
    * #7859 Update team context from media page
    
    * Revert "#7859 Update team context from media page"
    
    This reverts commit 4b91e10a7753acd300b20bab0adafd2849a40811.
    
    * 7859 Update team context from DrawerNavigationComponent
    
    * Revert "Revert "#7859 Update team context from media page""
    
    This reverts commit 161b4eb7bc7dcbabc52d022256dc4d329e8894d7.
    
    * Revert "Revert "Revert "#7859 Update team context from media page"""
    
    This reverts commit ce1bc2a9b9ca2f9189d8a9588b13021d7f0903b7.
    
    * #7859 Fix DrawerNavigation and test
    
    * #7859 Fix DrawerNavigationComponent unit test
    
    * #7859 Some feedback fixes
    
    * #7859 Updating some tests about search page
    
    *  #7859 updating tests
    
    * #7859 Modify some selectors in tests
    
    * #7859 Add user menu entries
    
    * #7859 Fix projects drawer div sizing
    
    * #7859 Fix margins
    
    * #7859 Hide CreateProject on TextField blur

--- src/app/components/drawer/Projects.js
+++ src/app/components/drawer/Projects.js
@@ -84,6 +84,7 @@ class DrawerProjectsComponent extends Component {
           className="project-list__input"
           team={props.team}
           onCreate={this.handleToggleDrawer}
+          onBlur={props.handleAddProj}
           autofocus
         />
       </div>
@@ -91,7 +92,7 @@ class DrawerProjectsComponent extends Component {
 
     const styles = {
       projectsList: {
-        height: 'calc(100vh - 412px)',
+        maxHeight: 'calc(100vh - 310px)',
         overflow: 'auto',
       },
     };
