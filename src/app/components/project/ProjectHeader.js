89fe2f38ce2331fa12ba77af47b98aa36438a563
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2020-01-15 09:55:58 -0300

    Fix back button label

--- src/app/components/project/ProjectHeader.js
+++ src/app/components/project/ProjectHeader.js
@@ -61,7 +61,22 @@ class ProjectHeaderComponent extends React.PureComponent {
       return `${path.match(regexTeam)[1]}/all-items`;
     };
 
+    const backLabel = () => {
+      const allItems = <FormattedMessage id="projectHeader.allItems" defaultMessage="All items" />;
+      if (mediaQuery) {
+        switch (mediaQuery.referer) {
+        case 'search':
+          return allItems;
+        case 'trash':
+          return <FormattedMessage id="projectHeader.trash" defaultMessage="Trash" />;
+        default:
+        }
+      }
+      return currentProject ? currentProject.title : '';
+    };
+
     const url = backUrl();
+    const label = backLabel();
 
     return (
       <div style={{ display: 'flex', alignItems: 'center', overflow: 'hidden' }}>
@@ -79,7 +94,7 @@ class ProjectHeaderComponent extends React.PureComponent {
             </IconButton>
             <HeaderTitle className="project-header__title" style={{ maxWidth: '100%' }}>
               <Text ellipsis>
-                {currentProject ? currentProject.title : <FormattedMessage id="projectHeader.allItems" defaultMessage="All items" />}
+                {label}
               </Text>
             </HeaderTitle>
           </Row>
