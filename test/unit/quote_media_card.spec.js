c86aebd1de72eecfba040135201779b9978db803
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2020-01-20 16:18:59 -0300

    #7901 Fix quote_media_card.spec

--- test/unit/quote_media_card.spec.js
+++ test/unit/quote_media_card.spec.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import { IntlProvider } from 'react-intl';
 import { expect } from 'chai';
 import QuoteMediaCard from '../../src/app/components/media/QuoteMediaCard.js';
 import ParsedText from '../../src/app/components/ParsedText.js';
-import { mountWithIntl } from './helpers/intl-test';
+import { render } from 'enzyme';
 
 describe('<QuoteMediaCard />', () => {
   it('renders arabic quotes in RTL form', () => {
-    const quote = mountWithIntl(<QuoteMediaCard quote="الدخول ممنوع" languageCode="ar" />);
-    expect(quote.find('.quote__text').at(0).hasClass('translation__rtl')).to.equal(true);
-    expect(quote.find('.quote__text').at(0).hasClass('translation__ltr')).to.equal(false);
+    const quote = render(<IntlProvider locale="en"><QuoteMediaCard quote="الدخول ممنوع" languageCode="ar" /></IntlProvider>);
+    expect(quote.html()).to.contain('translation__rtl');
+    expect(quote.html()).to.not.contain('translation__ltr');
   });
 
   it('renders english quotes in LTR form', () => {
-    const quote = mountWithIntl(<QuoteMediaCard quote="Access Denied" languageCode="en" />);
-    expect(quote.find('.quote__text').at(0).hasClass('translation__ltr')).to.equal(true);
-    expect(quote.find('.quote__text').at(0).hasClass('translation__rtl')).to.equal(false);
+    const quote = render(<IntlProvider locale="en"><QuoteMediaCard quote="Access Denied" languageCode="en" /></IntlProvider>);
+    expect(quote.html()).to.contain('translation__ltr');
+    expect(quote.html()).to.not.contain('translation__rtl');
   });
 
   it('renders quotes wrapped in ParsedText', () => {
-    const quote = mountWithIntl(<QuoteMediaCard quote="Access Denied" languageCode="en" />);
+    const quote = render(<IntlProvider locale="en"><QuoteMediaCard quote="Access Denied" languageCode="en" /></IntlProvider>);
     expect(quote.find(ParsedText));
   });
 });
