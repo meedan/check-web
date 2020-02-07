c9bdcabceaf0c38aac7037853f8103bedc786f95
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2020-02-07 19:51:32 -0300

    #8017 Fix media tags unit test

--- test/unit/media_tags.spec.js
+++ test/unit/media_tags.spec.js
@@ -13,15 +13,8 @@ describe('<MediaTags />', () => {
 
   const tags = [{ node: { tag: 123, id: '123', tag_text: 'tag1' } }, { node: { tag: 234, tag_text: 'suggestedTag3', id: '234' } }];
 
-  it('renders correctly when editing', () => {
-    const mediaTags = mountWithIntl(<MediaTags media={media} tags={tags} isEditing />);
+  it('renders correctly', () => {
+    const mediaTags = mountWithIntl(<MediaTags media={media} tags={tags} />);
     expect(mediaTags.find('.media-tags')).to.have.length(1);
-    expect(mediaTags.find('.media-tags--editing')).to.have.length(1);
-  });
-
-  it('renders correctly when not editing', () => {
-    const mediaTags = mountWithIntl(<MediaTags media={media} tags={tags} isEditing={false} />);
-    expect(mediaTags.find('.media-tags')).to.have.length(1);
-    expect(mediaTags.find('.media-tags--editing')).to.have.length(0);
   });
 });
