df854e7b3b57fbaa66d34752e7007f4a70d5931f
danielevalverde <danierzevalverde@gmail.com>
2020-02-27 18:19:05 -0300

    add test

--- test/spec/smoke_spec.rb
+++ test/spec/smoke_spec.rb
@@ -107,13 +107,25 @@ shared_examples 'smoke' do
     expect(@driver.page_source.include?('#wEDnesday')).to be(true)
   end
 
-  it "should register and create a claim", bin4: true do
+  it "should register, create a claim and assign it", bin4: true do
     page = LoginPage.new(config: @config, driver: @driver).load
     page = page.register_and_login_with_email(email: "sysops+#{Time.now.to_i}#{rand(1000)}@meedan.com", password: @password)
     page
       .create_team
       .create_project
       .create_media(input: 'Claim')
+    expect(@driver.page_source.include?('Assigments updated successfully!')).to be(false)
+    wait_for_selector('.media-actions__icon').click
+    wait_for_selector(".media-actions__assign").click
+    wait_for_selector(".Select-input input").send_keys("user")
+    @driver.action.send_keys(:enter).perform
+    wait_for_selector("//span[contains(text(), 'Done')]", :xpath).click
+    wait_for_selector_none("//span[contains(text(), 'Done')]", :xpath)
+    wait_for_selector(".message")
+    expect(@driver.page_source.include?('Assignments updated successfully!')).to be(true)
+    wait_for_selector(".media-tab__activity").click
+    wait_for_selector(".annotation__timestamp")
+    expect(@driver.page_source.include?('Item assigned to')).to be(true)
   end
 
   it "should lock and unlock status", bin2: true do
@@ -659,45 +671,43 @@ shared_examples 'smoke' do
   end
 #Embed section end
 
-#Meme Generator section start
-  # it "should go to meme generator change the title and description save and publish and generate a embed", bin1: true do
-  #   create_team_project_and_image_and_redirect_to_media_page
-  #   wait_for_selector(".media__annotations-column")
-  #   generate_a_embed_and_copy_embed_code
-  #   expect(@driver.page_source.include?('Meme')).to be(false)
-  #   @driver.navigate.back
-  #   wait_for_selector(".media__annotations-column")
-  #   wait_for_selector('.media-actions__icon').click
-  #   wait_for_selector('.media-actions__edit')
-  #   el = wait_for_selector('.media-actions__memebuster')
-  #   el.location_once_scrolled_into_view
-  #   el.click
-  #   expect(@driver.page_source.include?('Last saved')).to be(false)
-  #   wait_for_selector(".without-file")
-  #   fill_field('input[name="headline"]', 'Meme')
-  #   save_button = wait_for_selector(".memebuster__viewport-column > div > div button + button")
-  #   expect(save_button.attribute('tabindex')== "-1" ).to be(true) #if save_button is not enable
-  #   fill_field('textarea[name="description"]', 'description')
-  #   expect(save_button.attribute('tabindex')== "0" ).to be(true) #if save_button is enable
-  #   save_button.click
-  #   wait_for_selector(".memebuster__viewport-column > div > div >  span")
-  #   expect(@driver.page_source.include?('Last saved')).to be(true)
-  #   publish_button = wait_for_selector(".memebuster__viewport-column > div > div button + button + button")
-  #   publish_button.click
-  #   wait_for_selector(".memebuster__viewport-column > div > div > div")
-  #   expect(@driver.page_source.include?('Publishing')).to be(true)
-  #   wait_for_selector(".memebuster__viewport-column > div > div > div > span")
-  #   wait_for_selector(".memebuster__viewport-column > div > div > div > span > time")
-  #   expect(@driver.page_source.include?('Publishing')).to be(false)
-  #   expect(@driver.page_source.include?('Last published')).to be(true)
-  #   @driver.navigate.back
-  #   wait_for_selector_none(".without-file")
-  #   wait_for_selector(".media__annotations-column")
-  #   generate_a_embed_and_copy_embed_code
-  #   wait_for_selector(".oembed__meme")
-  #   expect(@driver.page_source.include?('Meme')).to be(true)
-  # end
-#Meme Generator section end
+# # Meme Generator section start
+#   it "should go to mem  e generator change the title and description publish and generate a embed", bin1: true do
+#     create_team_project_and_image_and_redirect_to_media_page
+#     wait_for_selector(".media__annotations-column")
+#     wait_for_selector('.media-actions__icon').click
+#     wait_for_selector('.media-actions__edit')
+#     sleep 5
+#     el = wait_for_selector('.media-actions__memebuster')
+#     el.location_once_scrolled_into_view
+#     el.click
+#     sleep 2
+#     expect(@driver.page_source.include?('Last saved')).to be(false)
+#     wait_for_selector(".without-file")
+#     fill_field('input[name="headline"]', 'Meme')
+#     save_button = wait_for_selector(".memebuster__viewport-column > div > div button + button")
+#     expect(save_button.attribute('tabindex')== "-1" ).to be(true) #if save_button is not enable
+#     fill_field('textarea[name="description"]', 'description')
+#     expect(save_button.attribute('tabindex')== "0" ).to be(true) #if save_button is enable
+#     save_button.click
+#     wait_for_selector(".memebuster__viewport-column > div > div >  span")
+#     expect(@driver.page_source.include?('Last saved')).to be(true)
+#     publish_button = wait_for_selector(".memebuster__viewport-column > div > div button + button + button")
+#     publish_button.click
+#     wait_for_selector(".memebuster__viewport-column > div > div > div")
+#     expect(@driver.page_source.include?('Publishing')).to be(true)
+#     wait_for_selector(".memebuster__viewport-column > div > div > div > span")
+#     wait_for_selector(".memebuster__viewport-column > div > div > div > span > time")
+#     expect(@driver.page_source.include?('Publishing')).to be(false)
+#     expect(@driver.page_source.include?('Last published')).to be(true)
+#     @driver.navigate.back
+#     wait_for_selector_none(".without-file")
+#     wait_for_selector(".media__annotations-column")
+#     generate_a_embed_and_copy_embed_code
+#     wait_for_selector(".oembed__meme")
+#     expect(@driver.page_source.include?('Meme')).to be(true)
+#   end
+# Meme Generator section end
 
 #Bulk Actions section start
   it "should move media to another project", bin2: true do
