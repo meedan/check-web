c73237a47307c95389d2f55e10bdaedd1458354c
Aaron Huslage <aaron@meedan.com>
2020-03-04 19:40:01 -0500

    Move to Passenger Standalone, Upgrade Node, Cleanup Dockerfile

--- production/bin/start.sh
+++ production/bin/start.sh
@@ -2,20 +2,6 @@
 # start.sh
 # the Dockerfile CMD
 
-function config_replace() {
-  # sed -i "s/ddRAILS_ENVdd/${RAILS_ENV}/g" /etc/nginx/sites-available/${APP}
-  VAR=$1
-  VAL=$2
-  FILE=$3
-  #    echo evaluating $VAR $VAL $FILE;
-  if grep --quiet "dd${VAR}dd" $FILE; then
-    echo "setting $VAR to $VAL in $FILE";
-	CMD="s/dd${VAR}dd/${VAL}/g";
-	sed -i -e ${CMD} ${FILE};
-  fi
-}
-
-
 if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
 	echo "GITHUB_TOKEN, DEPLOY_ENV, and APP must be in the environment. Exiting."
 	exit 1
@@ -26,12 +12,5 @@ d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "
 
 #Put config into place
 cp ${DEPLOYDIR}/latest/config.js ${DEPLOYDIR}/latest/build/web/js/config.js
- 
-# sed in environmental variables
-for ENV in $( env | cut -d= -f1); do
-  config_replace "$ENV" "${!ENV}" /etc/nginx/sites-enabled/${APP};
-done
 
-echo "starting nginx";
-echo "--STARTUP FINISHED--";
-nginx;
+passenger start
