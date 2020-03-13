c348a867369a023b3eb199f14fe56318846ec3ff
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2020-03-13 13:01:22 -0300

    #8119 Add MetadataCell.js to repo

---
+++ src/app/components/media/MetadataCell.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router';
+import styled from 'styled-components';
+import { black87 } from '../../styles/js/shared';
+
+const Content = styled.div`
+  width: 100%;
+  color: ${black87};
+`;
+
+const MetadataCell = (props) => {
+  const { url, query } = props.data;
+
+  return (
+    <Link to={url} state={{ query }}>
+      <Content>
+        { props.value }
+      </Content>
+    </Link>
+  );
+};
+
+
+export default MetadataCell;
