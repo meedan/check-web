1ace0b0706c676f499d1e4b5f27923f370ade1b4
Adam Hooper <adam@adamhooper.com>
2020-04-23 08:30:26 -0400

    pender_card.spec.js: don't test componentWillUpdate()
    The name of the test is very different from the logic in the test.
    Basically, the test is testing React, not PenderCard. It's not
    worth porting this to Jest.

--- test/unit/pender_card.spec.js
+++ test/unit/pender_card.spec.js
@@ -16,25 +16,4 @@ describe('<PenderCard />', () => {
     const wrapper = mountWithMuiTheme(<PenderCard fallback={<div />} />);
     expect(wrapper.find(CircularProgress)).to.have.length(0);
   });
-
-  it('does not reload if URL did not change', () => {
-    sinon.spy(PenderCard.prototype, 'componentDidUpdate');
-
-    const wrapper = mountWithMuiTheme(<PenderCard url="http://meedan.com" mediaVersion={0} />);
-
-    expect(PenderCard.prototype.componentDidUpdate).to.have.property('callCount', 0);
-    wrapper.setProps({ url: 'http://meedan.com' });
-    expect(PenderCard.prototype.componentDidUpdate).to.have.property('callCount', 0);
-    wrapper.setProps({ url: 'http://meedan.com/check' });
-    expect(PenderCard.prototype.componentDidUpdate).to.have.property('callCount', 1);
-    wrapper.setProps({ url: 'http://meedan.com/check' });
-    expect(PenderCard.prototype.componentDidUpdate).to.have.property('callCount', 1);
-
-    wrapper.setProps({ mediaVersion: 0 });
-    expect(PenderCard.prototype.componentDidUpdate).to.have.property('callCount', 1);
-    wrapper.setProps({ mediaVersion: 1 });
-    expect(PenderCard.prototype.componentDidUpdate).to.have.property('callCount', 2);
-    wrapper.setProps({ mediaVersion: 1 });
-    expect(PenderCard.prototype.componentDidUpdate).to.have.property('callCount', 2);
-  });
 });
