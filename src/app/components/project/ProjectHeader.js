7ccb0066d26b8207a1b48942de64eb28dc6af0b5
Adam Hooper <adam@adamhooper.com>
2020-05-20 16:43:38 -0400

    Nix mediaQuery, and fix <ProjectHeader>
    Also, Relay-modernize it [#6211]

--- src/app/components/project/ProjectHeader.js
+++ src/app/components/project/ProjectHeader.js
@@ -1,146 +1,86 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { QueryRenderer, createFragmentContainer, graphql } from 'react-relay/compat';
 import Relay from 'react-relay/classic';
-import { browserHistory, Link } from 'react-router';
-import IconArrowBack from '@material-ui/icons/ArrowBack';
+import { Link } from 'react-router';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import IconButton from '@material-ui/core/IconButton';
 import { FormattedMessage } from 'react-intl';
-import ProjectRoute from '../../relay/ProjectRoute';
-import { urlFromSearchQuery } from '../search/Search';
 import { getListUrlQueryAndIndex } from '../../urlHelpers';
-import { safelyParseJSON } from '../../helpers';
-import { Row, Text, HeaderTitle, FadeIn, SlideIn } from '../../styles/js/shared';
+import { Row, Text, HeaderTitle } from '../../styles/js/shared';
 
 class ProjectHeaderComponent extends React.PureComponent {
   render() {
-    const { props } = this;
-    const currentProject = props.project;
-    const path = props.location ? props.location.pathname : window.location.pathname;
-    const regexProject = /^(\/[^/]+\/project\/[0-9]+)/;
-    const regexTeam = /^(\/[^/]+)/;
-    const regexMedia = /project\/[0-9]+\/media\/[0-9]/;
-    const regexSource = /\/source\/[0-9]/;
-    let mediaQuery = null;
-    const loc = browserHistory.getCurrentLocation(); // TODO use props
-    if (loc && loc.query) {
-      mediaQuery = safelyParseJSON(loc.query, null);
+    const { params, project, location } = this.props;
+    const { listUrl } = getListUrlQueryAndIndex(params, location.query);
+
+    let pageTitle;
+    if (/\/trash(\/|$)/.test(listUrl)) {
+      pageTitle = <FormattedMessage id="projectHeader.trash" defaultMessage="Trash" />;
+    } else if (project) {
+      pageTitle = project.title;
+    } else {
+      pageTitle = <FormattedMessage id="projectHeader.allItems" defaultMessage="All items" />;
     }
-    const isProjectSubpage = regexMedia.test(path) || regexSource.test(path);
-
-    const backUrl = () => {
-      // New-style: read using getListUrlQueryAndIndex()
-      //
-      // TODO: nix this logic _entirely_ and pass it as props from a parent
-      // that understands URL. Move `getListUrlQueryAndIndex()` to
-      // `<MediaPage>`.
-      const { listUrl, listQuery } = getListUrlQueryAndIndex(
-        props.params,
-        (loc && loc.query) ? loc.query : {},
-      );
-      if (listQuery !== null) {
-        return listUrl;
-      }
-
-      if (mediaQuery) {
-        const query = Object.assign({}, mediaQuery);
-        let basePath = '';
-        switch (query.referer) {
-        case 'search':
-          basePath = `${path.match(regexTeam)[1]}/all-items`;
-          break;
-        case 'trash':
-          basePath = `${path.match(regexTeam)[1]}/trash`;
-          break;
-        default:
-          basePath = `${path.match(regexTeam)[1]}/all-items`;
-          if (regexProject.test(path)) {
-            basePath = `${path.match(regexProject)[1]}`;
-          }
-          break;
-        }
-        const baseQuery = query.original || query;
-        baseQuery.esoffset = Math.floor(query.esoffset / 20) * 20;
-        if (baseQuery.esoffset === 0) {
-          delete baseQuery.esoffset;
-        }
-        return urlFromSearchQuery(baseQuery, basePath);
-      } else if (isProjectSubpage) {
-        return path.match(regexProject)[1];
-      }
-      return `${path.match(regexTeam)[1]}/all-items`;
-    };
-
-    const backLabel = () => {
-      const allItems = <FormattedMessage id="projectHeader.allItems" defaultMessage="All items" />;
-      if (mediaQuery) {
-        switch (mediaQuery.referer) {
-        case 'search':
-          return allItems;
-        case 'trash':
-          return <FormattedMessage id="projectHeader.trash" defaultMessage="Trash" />;
-        default:
-        }
-      }
-      return currentProject ? currentProject.title : '';
-    };
-
-    const url = backUrl();
-    const label = backLabel();
 
     return (
       <div style={{ display: 'flex', alignItems: 'center', overflow: 'hidden' }}>
-        {url ?
-          <Row>
-            <Link to={url}>
-              <IconButton className="project-header__back-button">
-                <FadeIn>
-                  <SlideIn>
-                    <IconArrowBack />
-                  </SlideIn>
-                </FadeIn>
-              </IconButton>
-            </Link>
-            <HeaderTitle className="project-header__title" style={{ maxWidth: '100%' }}>
-              <Text ellipsis>
-                {label}
-              </Text>
-            </HeaderTitle>
-          </Row>
-          : null}
+        <Row>
+          <IconButton component={Link} to={listUrl} className="project-header__back-button">
+            <ArrowBackIcon />
+          </IconButton>
+          <HeaderTitle className="project-header__title" style={{ maxWidth: '100%' }}>
+            <Text ellipsis>
+              {pageTitle}
+            </Text>
+          </HeaderTitle>
+        </Row>
       </div>
     );
   }
 }
-
-ProjectHeaderComponent.contextTypes = {
-  router: PropTypes.object,
+ProjectHeaderComponent.defaultProps = {
+  project: null,
+};
+ProjectHeaderComponent.propTypes = {
+  params: PropTypes.object.isRequired,
+  location: PropTypes.shape({ query: PropTypes.object.isRequired }).isRequired,
+  project: PropTypes.shape({ title: PropTypes.string.isRequired }), // or null
 };
 
-const ProjectHeaderContainer = Relay.createContainer(ProjectHeaderComponent, {
-  fragments: {
-    project: () => Relay.QL`
-      fragment on Project {
-        title,
-        description
-      }
-    `,
-  },
-});
-
-const ProjectHeader = (props) => {
-  if (props.params && props.params.projectId) {
-    const route = new ProjectRoute({ projectId: props.params.projectId });
-    return (<Relay.RootContainer
-      Component={ProjectHeaderContainer}
-      route={route}
-      renderFetched={data => <ProjectHeaderComponent {...data} {...props} />}
-    />);
-  } else if (props.params && props.params.mediaId) {
-    return (<ProjectHeaderComponent
-      {...props}
-    />);
+const ProjectHeaderContainer = createFragmentContainer(ProjectHeaderComponent, graphql`
+  fragment ProjectHeaderContainer_project on Project {
+    title
   }
-  return null;
+`);
+
+const ProjectPlaceholder = { title: '' };
+
+const ProjectHeader = ({ location, params }) => {
+  const commonProps = { location, params };
+
+  if (params.projectId) {
+    return (
+      <QueryRenderer
+        environment={Relay.Store}
+        query={graphql`
+          query ProjectHeaderQuery($projectId: String!) {
+            project(id: $projectId) {
+              ...ProjectHeaderContainer_project
+            }
+          }
+        `}
+        variables={{ projectId: params.projectId }}
+        render={({ props }) => (
+          props
+            ? <ProjectHeaderContainer {...commonProps} {...props} />
+            : <ProjectHeaderComponent {...commonProps} project={ProjectPlaceholder} />
+        )}
+      />
+    );
+  }
+
+  return <ProjectHeaderComponent {...commonProps} />;
 };
 
 export default ProjectHeader;
