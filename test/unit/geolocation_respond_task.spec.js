42c7a9f3bd79efefaef9fa5ab9d3775b0048d2be
Adam Hooper <adam@adamhooper.com>
2020-05-21 16:59:57 -0400

    GeolocationRespondTask: nix warnings [#8179]
    Also: nix injectIntl() [#8347]. To make things more legible, I used
    async/await. This helped avoid throwing an Error with a React component
    embedded.

--- test/unit/geolocation_respond_task.spec.js
+++ test/unit/geolocation_respond_task.spec.js
@@ -1,5 +1,6 @@
+/* global describe, expect, it */
 import React from 'react';
-import { mountWithIntl } from './helpers/intl-test';
+import { mountWithIntlProvider } from './helpers/intl-test';
 
 import GeolocationRespondTask from '../../src/app/components/task/GeolocationRespondTask';
 
@@ -7,7 +8,7 @@ describe('<GeolocationRespondTask />', () => {
   const response = '{"type":"Feature","geometry":{"type":"Point","coordinates":[0,0]},"properties":{"name":"A Place"}}';
 
   it('Renders inputs', () => {
-    const wrapper = mountWithIntl(<GeolocationRespondTask response={response} />);
+    const wrapper = mountWithIntlProvider(<GeolocationRespondTask response={response} />);
 
     expect(wrapper.find('input#geolocationsearch')).toHaveLength(1);
     expect(wrapper.find('textarea#task__response-geolocation-name')).toHaveLength(1);
@@ -15,7 +16,7 @@ describe('<GeolocationRespondTask />', () => {
   });
 
   it('Renders error for invalid coordinates', () => {
-    const wrapper = mountWithIntl(<GeolocationRespondTask />);
+    const wrapper = mountWithIntlProvider(<GeolocationRespondTask />);
 
     wrapper.find('#task__response-geolocation-coordinates')
       .hostNodes().simulate('change', { target: { name: 'coordinates', value: 'This is not a valid gps position' } });
@@ -25,7 +26,7 @@ describe('<GeolocationRespondTask />', () => {
   });
 
   it('Does not render error for valid coordinates', () => {
-    const wrapper = mountWithIntl(<GeolocationRespondTask />);
+    const wrapper = mountWithIntlProvider(<GeolocationRespondTask />);
 
     wrapper.find('#task__response-geolocation-coordinates')
       .hostNodes().simulate('change', { target: { name: 'coordinates', value: '-13, -38' } });
