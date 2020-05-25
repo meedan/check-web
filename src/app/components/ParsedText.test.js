d4d77d71c5c5c75999d1e84f1b9b6a64a7192f61
Adam Hooper <adam@adamhooper.com>
2020-05-25 16:00:02 -0400

    ParsedText: fix unit-test warnings [#8179]

---
+++ src/app/components/ParsedText.test.js
@@ -0,0 +1,23 @@
+/* global describe, expect, it */
+import React from 'react';
+import { mount } from 'enzyme';
+
+import ParsedText from './ParsedText';
+
+describe('<ParsedText />', () => {
+  it('converts new lines to <br />s', () => {
+    const text = 'Foo\nBar';
+    const wrapper = mount(<ParsedText text={text} />);
+    expect(wrapper.html()).toMatch('Foo<br>Bar');
+  });
+
+  it('converts links', () => {
+    const wrapper = mount(<ParsedText text="Please visit http://meedan.com" />);
+    expect(wrapper.html()).toMatch('<a href');
+  });
+
+  it('converts emojis', () => {
+    const wrapper = mount(<ParsedText text="I :heart: U" />);
+    expect(wrapper.html()).toMatch('<img');
+  });
+});
