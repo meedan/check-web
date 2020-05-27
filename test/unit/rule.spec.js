c32081600704aea7d513212c296c50e36726e52a
Adam Hooper <adam@adamhooper.com>
2020-05-27 13:10:47 -0400

    rule.spec.js: eslint

--- test/unit/rule.spec.js
+++ test/unit/rule.spec.js
@@ -1,6 +1,6 @@
+/* global describe, expect, it */
 import React from 'react';
-import { IntlProvider } from 'react-intl';
-import { mountWithIntl } from './helpers/intl-test';
+import { mountWithIntlProvider } from './helpers/intl-test';
 import Rule from './../../src/app/components/team/Rules/Rule';
 import schema from './mocks/rules-schema';
 import rules from './mocks/rules';
@@ -13,18 +13,14 @@ const props = {
 };
 
 describe('<Rule />', () => {
-  it('should render fields for one rule', function() {
-    const wrapper = mountWithIntl(
-      <Rule {...props} index={0} />
-    );
+  it('should render fields for one rule', () => {
+    const wrapper = mountWithIntlProvider(<Rule {...props} index={0} />);
     expect(wrapper.find('input').hostNodes()).toHaveLength(9);
     expect(wrapper.find('input[value="Rule 1"]').hostNodes()).toHaveLength(1);
   });
 
-  it('should render fields for another rule', function() {
-    const wrapper = mountWithIntl(
-      <Rule {...props} index={1} />
-    );
+  it('should render fields for another rule', () => {
+    const wrapper = mountWithIntlProvider(<Rule {...props} index={1} />);
     expect(wrapper.find('input').hostNodes()).toHaveLength(8);
     expect(wrapper.find('input[value="Rule 2"]').hostNodes()).toHaveLength(1);
   });
