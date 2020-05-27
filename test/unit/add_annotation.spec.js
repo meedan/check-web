c58ef076432ee22254617b54b0170c0dd0f10f17
danielevalverde <danierzevalverde@gmail.com>
2020-05-27 19:46:29 -0300

    ticket#8296 - fix unit test

--- test/unit/add_annotation.spec.js
+++ test/unit/add_annotation.spec.js
@@ -4,8 +4,7 @@ import { mountWithIntl } from './helpers/intl-test';
 import AddAnnotation from '../../src/app/components/annotations/AddAnnotation';
 
 describe('<AddAnnotation />', () => {
-  const annotated = { archived: true };
-  const annotatedType = 'ProjectMedia';
+  const annotated = { archived: false , annotatedType:'Task' };
 
   it('Hides when media is archived (Trash)', () => {
     const addAnnotation = mountWithIntl(<AddAnnotation annotated={annotated} />);
