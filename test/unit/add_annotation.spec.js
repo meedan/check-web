387949a0716f67051b562b1387033e4fc1ab70f1
danielevalverde <danierzevalverde@gmail.com>
2020-05-27 21:36:47 -0300

    ticket#8296 - fix and adding unit tests to create task

--- test/unit/add_annotation.spec.js
+++ test/unit/add_annotation.spec.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { mountWithIntl } from './helpers/intl-test';
-
 import AddAnnotation from '../../src/app/components/annotations/AddAnnotation';
 
 describe('<AddAnnotation />', () => {
-  const annotated = { archived: false , annotatedType:'Task' };
+  const annotated_archived = { archived: true };
+  const annotated_not_archived = { archived: false };
 
   it('Hides when media is archived (Trash)', () => {
-    const addAnnotation = mountWithIntl(<AddAnnotation annotated={annotated} />);
+    const addAnnotation = mountWithIntl(<AddAnnotation annotated={annotated_archived} />);
     expect(addAnnotation.html()).toEqual('');
+  });
+
+  it('Render component when media is not archived (not in the trash)', () => {
+    const addAnnotation = mountWithIntl(<AddAnnotation annotated={annotated_not_archived} />);
+    expect(addAnnotation.html()).not.toEqual('');
   });
 });
