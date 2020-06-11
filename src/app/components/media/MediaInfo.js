b928b502877934830c1f392959d591c2b585ee48
Laurian Gridinoc <laurian@gridinoc.name>
2020-06-11 15:09:56 +0300

    bare map

--- src/app/components/media/MediaInfo.js
+++ src/app/components/media/MediaInfo.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 import Grid from '@material-ui/core/Grid';
+import { Map } from '@meedan/check-ui';
 
 class MediaInfo extends Component {
   render() {
+    const { places = [], index = -1 } = this.props;
+
     return (
       <Grid container>
-        <Grid item>Hello Metadata Tab</Grid>
+        <Grid item>
+          <div style={{
+            width: 560,
+            height: 315
+          }}>
+            <Map places={places} index={index} />
+          </div>
+        </Grid>
       </Grid>
     )
   }
