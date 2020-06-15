77e2f5068a977d78e107abcf898f853aefb2138a
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2020-06-15 19:19:53 -0300

    #8332 Remove video.js player

--- src/app/components/media/VideoMediaCard.js
+++ src/app/components/media/VideoMediaCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Player } from '@meedan/check-ui';
 import AspectRatio from '../layout/AspectRatio';
-import '../../styles/css/player.css';
 
 const VideoMediaCard = props => (
   <article className="video-media-card">
