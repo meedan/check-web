936c3d1d665aecdb04c0957e5d291f8d54ed93ba
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2020-06-15 19:01:38 -0300

    #8332 Use AspectRatio wrapper

--- src/app/components/media/VideoMediaCard.js
+++ src/app/components/media/VideoMediaCard.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Player } from '@meedan/check-ui';
+import AspectRatio from '../layout/AspectRatio';
 import '../../styles/css/player.css';
 
 const VideoMediaCard = props => (
   <article className="video-media-card">
-    <Player
-      url={props.videoPath}
-      className="video-media-player"
-    />
+    <AspectRatio>
+      <Player
+        url={props.videoPath}
+        className="video-media-player"
+      />
+    </AspectRatio>
   </article>);
 
 export default VideoMediaCard;
