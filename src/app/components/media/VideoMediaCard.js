b0780023f6a5f2ef54809bb5957234be5faa3d35
Laurian Gridinoc <laurian@gridinoc.name>
2020-06-22 23:44:25 +0300

    player props fix

--- src/app/components/media/VideoMediaCard.js
+++ src/app/components/media/VideoMediaCard.js
@@ -8,6 +8,19 @@ const VideoMediaCard = props => (
       <Player
         url={props.videoPath}
         className="video-media-player"
+        key={`${props.videoPath}#t=${props.start},${props.end}`}
+        onDuration={d => props.setPlayerState({ duration: d })}
+        onPause={() => props.setPlayerState({ playing: false })}
+        onPlay={() => props.setPlayerState({ playing: true })}
+        onProgress={p => props.setPlayerState({ progress: p })}
+        onReady={props.onPlayerReady}
+        onTimeUpdate={t => props.setPlayerState({ time: t })}
+        playing={props.playing}
+        start={props.start}
+        end={props.end}
+        gaps={props.gaps}
+        scrubTo={props.scrubTo}
+        seekTo={props.seekTo}
       />
     </AspectRatio>
   </article>);
