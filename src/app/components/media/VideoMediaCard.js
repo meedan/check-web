5ca46c88a9bb8c3ab1c35f2b7491e21f44f4218e
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2020-06-25 13:39:57 -0300

    #8431 Add playback rate select control

--- src/app/components/media/VideoMediaCard.js
+++ src/app/components/media/VideoMediaCard.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import { Player } from '@meedan/check-ui';
+import FormControl from '@material-ui/core/FormControl';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import styled from 'styled-components';
 import AspectRatio from '../layout/AspectRatio';
+import { units } from '../../styles/js/shared';
 
-const VideoMediaCard = props => (
-  <article className="video-media-card">
-    <AspectRatio>
-      <Player
-        url={props.videoPath}
-        className="video-media-player"
-      />
-    </AspectRatio>
-  </article>);
+const StyledPlaybackRate = styled.div`
+  margin-top: ${units(1)};
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const VideoMediaCard = (props) => {
+  const [playbackRate, setplaybackRate] = React.useState(1);
+
+  return (
+    <article className="video-media-card">
+      <AspectRatio>
+        <Player
+          url={props.videoPath}
+          className="video-media-player"
+          playbackRate={playbackRate}
+        />
+      </AspectRatio>
+      <StyledPlaybackRate>
+        <FormControl variant="outlined">
+          <Select
+            id="video-media-card__playback-rate"
+            value={playbackRate}
+            onChange={e => setplaybackRate(e.target.value)}
+          >
+            <MenuItem value={1}>1x</MenuItem>
+            <MenuItem value={1.5}>1.5x</MenuItem>
+            <MenuItem value={2}>2x</MenuItem>
+          </Select>
+        </FormControl>
+      </StyledPlaybackRate>
+    </article>
+  );
+};
 
 export default VideoMediaCard;
