fceb23f5c24de94c7b54189316d1df7790375c62
Adam Hooper <adam@adamhooper.com>
2020-06-26 11:17:04 -0400

    <AddTeamPage>: nix injectIntl()

--- src/app/components/team/AddTeamPage.js
+++ src/app/components/team/AddTeamPage.js
@@ -1,37 +1,22 @@
 import React from 'react';
 import Relay from 'react-relay/classic';
-import {
-  defineMessages,
-  injectIntl,
-  intlShape,
-} from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 import CreateTeamCard from './CreateTeamCard';
 import FindTeamCard from './FindTeamCard';
 import PageTitle from '../PageTitle';
-import {
-  ContentColumn,
-} from '../../styles/js/shared';
+import { ContentColumn } from '../../styles/js/shared';
 import FindPublicTeamRoute from '../../relay/FindPublicTeamRoute';
 import RelayContainer from '../../relay/RelayContainer';
-
-const messages = defineMessages({
-  titleCreate: {
-    id: 'addTeamPage.titleCreate',
-    defaultMessage: 'Create a workspace',
-  },
-  titleFind: {
-    id: 'addTeamPage.titleFind',
-    defaultMessage: 'Find an existing workspace',
-  },
-});
 
 const AddTeamComponent = (props) => {
   const mode = props.route.path === 'check/teams/find(/:slug)' ? 'find' : 'create';
 
-  const title = mode === 'find' ? messages.titleFind : messages.titleCreate;
-
   return (
-    <PageTitle prefix={props.intl.formatMessage(title)}>
+    <PageTitle prefix={mode === 'find'
+      ? <FormattedMessage id="addTeamPage.titleFind" defaultMessage="Find an existing workspace" />
+      : <FormattedMessage id="addTeamPage.titleCreate" defaultMessage="Create a workspace" />
+    }
+    >
       <main className="create-team">
         <ContentColumn narrow>
           { mode === 'find' ?
@@ -43,13 +28,7 @@ const AddTeamComponent = (props) => {
   );
 };
 
-AddTeamComponent.propTypes = {
-  // https://github.com/yannickcr/eslint-plugin-react/issues/1389
-  // eslint-disable-next-line react/no-typos
-  intl: intlShape.isRequired,
-};
-
-const AddTeamContainer = Relay.createContainer(injectIntl(AddTeamComponent), {
+const AddTeamContainer = Relay.createContainer(AddTeamComponent, {
   fragments: {
     team: () => Relay.QL`
       fragment on PublicTeam {
