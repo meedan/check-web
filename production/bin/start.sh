d63d33bbbef091237230cb905fdd27b36bcf94e2
infojunkie <karim.ratib@gmail.com>
2020-06-26 15:35:06 -0700

    Always overwrite configs in production

--- production/bin/start.sh
+++ production/bin/start.sh
@@ -7,10 +7,10 @@ if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
 	exit 1
 fi
 
-if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ${DEPLOYDIR}/latest/configurator; fi
+git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ${DEPLOYDIR}/latest/configurator
 d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
 
-#Put config into place
+# Put config into place
 cp ${DEPLOYDIR}/latest/config.js ${DEPLOYDIR}/latest/build/web/js/config.js
 
 passenger start
