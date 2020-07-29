2457dd4315ee75037e1c568ad6c5ac3dda6ec703
Adam Hooper <adam@adamhooper.com>
2020-07-29 09:56:20 -0400

    <SearchQuery>: FragmentContainer, not RootContainer [#6211] (#660)
    I was getting an exception on dev involving a null team. Dunno why,
    but deleting the query deletes the possibility of unexpected results.
    It's also faster and part of #6211.

--- src/app/components/search/SearchQuery.js
+++ src/app/components/search/SearchQuery.js
@@ -1,85 +1,36 @@
-import React from 'react';
 import Relay from 'react-relay/classic';
-import isEqual from 'lodash.isequal';
 import SearchQueryComponent from './SearchQueryComponent';
-import TeamNodeRoute from '../../relay/TeamNodeRoute';
 
-const queryWithoutProjects = Relay.QL`
-  fragment on Team {
-    id,
-    dbid,
-    verification_statuses,
-    tag_texts(first: 10000) {
-      edges {
-        node {
-          text
+export default Relay.createContainer(SearchQueryComponent, {
+  fragments: {
+    team: () => Relay.QL`
+      fragment on Team {
+        id
+        dbid
+        verification_statuses
+        tag_texts(first: 10000) {
+          edges {
+            node {
+              text
+            }
+          }
+        }
+        pusher_channel
+        dynamic_search_fields_json_schema
+        rules_search_fields_json_schema
+        name
+        slug
+        projects(first: 10000) {
+          edges {
+            node {
+              title
+              dbid
+              id
+              description
+            }
+          }
         }
       }
-    },
-    pusher_channel,
-    dynamic_search_fields_json_schema,
-    rules_search_fields_json_schema,
-    name,
-    slug,
-  }
-`;
-
-const queryWithProjects = Relay.QL`
-  fragment on Team {
-    id,
-    dbid,
-    verification_statuses,
-    tag_texts(first: 10000) {
-      edges {
-        node {
-          text
-        }
-      }
-    },
-    pusher_channel,
-    dynamic_search_fields_json_schema,
-    rules_search_fields_json_schema,
-    name,
-    slug,
-    projects(first: 10000) {
-      edges {
-        node {
-          title,
-          dbid,
-          id,
-          description
-        }
-      }
-    }
-  }
-`;
-
-class SearchQuery extends React.Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return !isEqual(this.state, nextState) ||
-           !isEqual(this.props, nextProps);
-  }
-
-  render() {
-    const gqlquery = this.props.project ? queryWithoutProjects : queryWithProjects;
-
-    const SearchQueryContainer = Relay.createContainer(SearchQueryComponent, {
-      fragments: {
-        team: () => gqlquery,
-      },
-    });
-
-    // eslint-disable-next-line no-underscore-dangle
-    const queryRoute = new TeamNodeRoute({ id: this.props.team.__dataID__ });
-
-    return (
-      <Relay.RootContainer
-        Component={SearchQueryContainer}
-        route={queryRoute}
-        renderFetched={data => <SearchQueryContainer {...this.props} {...data} />}
-      />
-    );
-  }
-}
-
-export default SearchQuery;
+    `,
+  },
+});
