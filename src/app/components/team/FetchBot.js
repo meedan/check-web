34cbf5a7270a646e08371318dedbce6cc4b3096b
Caio Almeida <caiosba@gmail.com>
2020-07-30 21:06:39 -0300

    Ticket #8229: Fetch Bot

---
+++ src/app/components/team/FetchBot.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import Button from '@material-ui/core/Button';
+
+const FetchBot = () => {
+  const handleOpenForm = () => {
+    window.open('https://airtable.com/shrRtHDE2J18vJ9zR');
+  };
+
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      onClick={handleOpenForm}
+    >
+      <FormattedMessage
+        id="fetchBot.contactUs"
+        defaultMessage="Contact us to setup"
+      />
+    </Button>
+  );
+};
+
+export default FetchBot;
