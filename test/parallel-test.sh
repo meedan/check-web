5d1cdce567437f7600a4cee85055d3fbf91d2734
Adam Hooper <adam@adamhooper.com>
2020-08-14 11:21:34 -0400

    Test with modern chrome [#8469] (#677)
    * `.travis.yml`: upgrade to Bionic (for new Chromium+chromedriver)
    * `.travis.yml`: run chromedriver once, not six times
    * `.travis.yml`: don't run a copy of Chrome in the background
    * `parallel-test.sh`: use a single Chromedriver
    * `parallel-test.sh`, `app_spec.rb`: only overwrite `config.js` when we aren't integration-testing. (There was a race....)
    * `app_spec.rb`: put test prep in a `before(:each)`, not a `before(:all)`.
    * `app_spec.rb`: revamp `new_driver()` so it's easier to call and works with modern Chrome
    * `spec_helper.rb`: turn on verbose retry, and nix doubled retry logic
    * `spec/*`: nix unused code
    * `spec/*`: handle the failures with modern Chrome
    
    H/T @danielevalverde

--- test/parallel-test.sh
+++ test/parallel-test.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+!/bin/bash
 
 echo 'Starting tests...'
 
 FAIL=0
 
-CHROMEDRIVER_URL=http://localhost:9517 bundle exec rspec --tag bin6 spec/integration_spec.rb &
-CHROMEDRIVER_URL=http://localhost:9516 bundle exec rspec --tag bin1 spec/integration_spec.rb &
-CHROMEDRIVER_URL=http://localhost:9518 bundle exec rspec --tag bin2 spec/integration_spec.rb &
-CHROMEDRIVER_URL=http://localhost:9519 bundle exec rspec --tag bin5 spec/integration_spec.rb &
-CHROMEDRIVER_URL=http://localhost:9520 bundle exec rspec --tag bin3 spec/integration_spec.rb &
-CHROMEDRIVER_URL=http://localhost:9521 bundle exec rspec --tag bin4 spec/integration_spec.rb &
+SKIP_CONFIG_JS_OVERWRITE=1 bundle exec rspec --tag bin6 spec/integration_spec.rb &
+SKIP_CONFIG_JS_OVERWRITE=1 bundle exec rspec --tag bin1 spec/integration_spec.rb &
+SKIP_CONFIG_JS_OVERWRITE=1 bundle exec rspec --tag bin2 spec/integration_spec.rb &
+SKIP_CONFIG_JS_OVERWRITE=1 bundle exec rspec --tag bin5 spec/integration_spec.rb &
+SKIP_CONFIG_JS_OVERWRITE=1 bundle exec rspec --tag bin3 spec/integration_spec.rb &
+SKIP_CONFIG_JS_OVERWRITE=1 bundle exec rspec --tag bin4 spec/integration_spec.rb &
 
 for job in `jobs -p`
 do
