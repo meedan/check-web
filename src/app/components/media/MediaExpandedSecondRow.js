953d55f31326c52b6e89964c5dbd35cd03b98450
Caio Almeida <caiosba@gmail.com>
2020-10-12 19:48:14 -0300

    Ticket #8754: New copy, style and position for the button and dialog

--- src/app/components/media/MediaExpandedSecondRow.js
+++ src/app/components/media/MediaExpandedSecondRow.js
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components';
-import Box from '@material-ui/core/Box';
 import MediaTypeDisplayName from './MediaTypeDisplayName';
-import BlankMediaButton from './BlankMediaButton';
 import { isBotInstalled, parseStringUnixTimestamp } from '../../helpers';
 import TimeBefore from '../TimeBefore';
 import {
@@ -24,40 +22,30 @@ const MediaExpandedSecondRow = ({ projectMedia }) => (
   <div>
     <StyledHeaderTextSecondary>
       <Row flexWrap style={{ fontWeight: '500' }}>
-        <Box display="flex" alignItems="center" justifyContent="space-between" width="100%" flexWrap="wrap">
-          <Box>
-            <span><MediaTypeDisplayName mediaType={projectMedia.media.type} /></span>
+        <span><MediaTypeDisplayName mediaType={projectMedia.media.type} /></span>
+        <span style={{ margin: `0 ${units(1)}` }}> - </span>
+        <span>
+          <FormattedMessage id="mediaExpanded.firstSeen" defaultMessage="First seen: " />
+          <TimeBefore date={parseStringUnixTimestamp(projectMedia.created_at)} />
+        </span>
+        { isBotInstalled(projectMedia.team, 'smooch') ?
+          <span>
             <span style={{ margin: `0 ${units(1)}` }}> - </span>
             <span>
-              <FormattedMessage id="mediaExpanded.firstSeen" defaultMessage="First seen: " />
-              <TimeBefore date={parseStringUnixTimestamp(projectMedia.created_at)} />
+              <FormattedMessage id="mediaExpanded.lastSeen" defaultMessage="Last seen: " />
+              <TimeBefore date={parseStringUnixTimestamp(projectMedia.last_seen)} />
             </span>
-            { isBotInstalled(projectMedia.team, 'smooch') ?
-              <span>
-                <span style={{ margin: `0 ${units(1)}` }}> - </span>
-                <span>
-                  <FormattedMessage id="mediaExpanded.lastSeen" defaultMessage="Last seen: " />
-                  <TimeBefore date={parseStringUnixTimestamp(projectMedia.last_seen)} />
-                </span>
-                <span style={{ margin: `0 ${units(1)}` }}> - </span>
-                <span>
-                  <FormattedMessage
-                    id="mediaExpanded.requests"
-                    defaultMessage="{count} requests"
-                    values={{
-                      count: projectMedia.requests_count,
-                    }}
-                  />
-                </span>
-              </span> : null }
-          </Box>
-          { isBotInstalled(projectMedia.team, 'fetch') ?
-            <BlankMediaButton
-              projectMediaId={projectMedia.id}
-              team={projectMedia.team}
-              reverse
-            /> : null }
-        </Box>
+            <span style={{ margin: `0 ${units(1)}` }}> - </span>
+            <span>
+              <FormattedMessage
+                id="mediaExpanded.requests"
+                defaultMessage="{count} requests"
+                values={{
+                  count: projectMedia.requests_count,
+                }}
+              />
+            </span>
+          </span> : null }
       </Row>
     </StyledHeaderTextSecondary>
   </div>
