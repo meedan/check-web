7dbda2de9960f44e743f43e142f59148dd34fe0f
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2020-10-28 20:23:40 -0300

    #8776 Add close dialog button

--- src/app/components/media/ItemHistoryDialog.js
+++ src/app/components/media/ItemHistoryDialog.js
@@ -1,32 +1,50 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import CloseIcon from '@material-ui/icons/Close';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
 import MediaLog from './MediaLog';
+
+const useStyles = makeStyles(theme => ({
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+  },
+}));
 
 const ItemHistoryDialog = ({
   open,
   projectMedia,
   team,
   onClose,
-}) => (
-  <Dialog
-    open={open}
-    onClose={onClose}
-    maxWidth="sm"
-    fullWidth
-  >
-    <DialogTitle>
-      <FormattedMessage id="ItemHistoryDialog.title" defaultMessage="Item history" />
-    </DialogTitle>
-    <DialogContent>
-      <MediaLog
-        media={projectMedia}
-        team={team}
-      />
-    </DialogContent>
-  </Dialog>
-);
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      maxWidth="sm"
+      fullWidth
+    >
+      <DialogTitle>
+        <FormattedMessage id="ItemHistoryDialog.title" defaultMessage="Item history" />
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent>
+        <MediaLog
+          media={projectMedia}
+          team={team}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default ItemHistoryDialog;
