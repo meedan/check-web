b1b6dbd0ba7646dca18c198ed1bec17de65c846a
danielevalverde <danierzevalverde@gmail.com>
2020-10-30 22:20:41 -0300

    Ticket #8635: run all tests again

--- test/parallel-test.sh
+++ test/parallel-test.sh
@@ -1,2 +1,27 @@
-#!/bin/bash
-CHROMEDRIVER_URL=http://localhost:9515 bundle exec rspec --example "flaky-test" spec/integration_spec.rb
+!/bin/bash
+
+echo 'Starting tests...'
+
+FAIL=0
+
+SKIP_CONFIG_JS_OVERWRITE=1 bundle exec rspec --tag bin6 spec/integration_spec.rb &
+SKIP_CONFIG_JS_OVERWRITE=1 bundle exec rspec --tag bin1 spec/integration_spec.rb &
+SKIP_CONFIG_JS_OVERWRITE=1 bundle exec rspec --tag bin2 spec/integration_spec.rb &
+SKIP_CONFIG_JS_OVERWRITE=1 bundle exec rspec --tag bin5 spec/integration_spec.rb &
+SKIP_CONFIG_JS_OVERWRITE=1 bundle exec rspec --tag bin3 spec/integration_spec.rb &
+SKIP_CONFIG_JS_OVERWRITE=1 bundle exec rspec --tag bin4 spec/integration_spec.rb &
+
+for job in `jobs -p`
+do
+  echo $job
+  wait $job || let "FAIL+=1"
+done
+
+echo $FAIL
+
+if [ "$FAIL" == "0" ];
+then
+  exit 0
+else
+  exit 1
+fi
