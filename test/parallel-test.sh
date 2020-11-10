e75c84f129e98d7626244e1bf9813545d76669b8
danielevalverde <danierzevalverde@gmail.com>
2020-11-10 10:49:44 -0300

    debugging test on Travis: fix test name

--- test/parallel-test.sh
+++ test/parallel-test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CHROMEDRIVER_URL=http://localhost:9515 bundle exec rspec --example "nome do teste" spec/integration_spec.rb
+CHROMEDRIVER_URL=http://localhost:9515 bundle exec rspec --example "should promote related item to main item" spec/integration_spec.rb
