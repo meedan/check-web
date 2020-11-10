8a6a0b6da71e67a4ccabb89903ba10d424c4dd40
Daniele Valverde <34126648+danielevalverde@users.noreply.github.com>
2020-11-10 15:05:07 -0300

    Tests/fix flaky tests (#757)
    * Fix some flaky tests and refactor tests files to separate feature by file

--- test/spec/app_spec.rb
+++ test/spec/app_spec.rb
@@ -3,14 +3,26 @@ require 'yaml'
 require_relative './spec_helper.rb'
 require_relative './app_spec_helpers.rb'
 require_relative './api_helpers.rb'
-require_relative './smoke_spec.rb'
-require_relative './media_spec.rb'
-require_relative './task_spec.rb'
-require_relative './metadata_spec.rb'
-require_relative './login_spec.rb'
-require_relative './task_spec_helpers.rb'
-require_relative './login_spec_helpers.rb'
 require_relative './flaky_tests_spec.rb'
+require_relative './language_spec.rb'
+require_relative './login_spec.rb'
+require_relative './login_spec_helpers.rb'
+require_relative './media_actions_spec.rb'
+require_relative './media_spec.rb'
+require_relative './metadata_spec.rb'
+require_relative './project_spec.rb'
+require_relative './report_spec.rb'
+require_relative './rules_spec.rb'
+require_relative './search_spec.rb'
+require_relative './secondary_items_spec.rb'
+require_relative './status_spec.rb'
+require_relative './tag_spec.rb'
+require_relative './tag_spec_helpers.rb'
+require_relative './team_spec.rb'
+require_relative './team_spec_helpers.rb'
+require_relative './task_spec.rb'
+require_relative './task_spec_helpers.rb'
+require_relative './video_timeline_spec.rb'
 # require_relative './source_spec.rb'
 
 CONFIG = YAML.load_file('config.yml')
@@ -21,6 +33,8 @@ shared_examples 'app' do |webdriver_url|
   include AppSpecHelpers
   include ApiHelpers
   include TaskSpecHelpers
+  include TagSpecHelpers
+  include TeamSpecHelpers
   include LoginSpecHelpers
   include FlakyTests
 
@@ -96,10 +110,20 @@ shared_examples 'app' do |webdriver_url|
   # The tests themselves start here
   context "web" do
 
-    include_examples "smoke"
-    include_examples "task"
-    include_examples "metadata"
+    include_examples "language"
     include_examples "login"
+    include_examples "media actions"
+    include_examples "metadata"
+    include_examples "project"
+    include_examples "report"
+    include_examples "rules"
+    include_examples "search"
+    include_examples "secondary items"
+    include_examples "status"
+    include_examples "task"
+    include_examples "tag"
+    include_examples "team"
+    include_examples "videotimeline"
     it_behaves_like "media", 'BELONGS_TO_ONE_PROJECT'
     it_behaves_like "media", 'DOES_NOT_BELONG_TO_ANY_PROJECT'
 
@@ -175,23 +199,6 @@ shared_examples 'app' do |webdriver_url|
       expect((@driver.current_url.to_s =~ /\/terms-of-service$/).nil?).to be(false)
     end
 
-    it "should not create duplicated media", bin4: true do
-      url = 'https://twitter.com/meedan/status/1262644257996898305'
-      api_create_team_project_and_link_and_redirect_to_media_page url
-      id1 = @driver.current_url.to_s.gsub(/^.*\/media\//, '').to_i
-      expect(id1 > 0).to be(true)
-      wait_for_selector("#media-actions-bar__add-to")
-      wait_for_selector(".project-header__back-button").click
-      wait_for_selector(".medias__item")
-      wait_for_selector("#create-media__add-item").click
-      wait_for_selector("#create-media__link")
-      fill_field('#create-media-input', url)
-      wait_for_selector('#create-media-dialog__submit-button').click
-      wait_for_selector(".create-related-media__add-button")
-      id2 = @driver.current_url.to_s.gsub(/^.*\/media\//, '').to_i
-      expect(id1 == id2).to be(true)
-    end
-
     it "should redirect to 404 page if id does not exist", bin4: true do
       api_create_team_and_project
       @driver.navigate.to @config['self_url']
@@ -201,64 +208,6 @@ shared_examples 'app' do |webdriver_url|
       title = wait_for_selector('.main-title')
       expect(title.text == 'Not Found').to be(true)
       expect((@driver.current_url.to_s =~ /\/not-found$/).nil?).to be(false)
-    end
-
-    it "should show: edit project (link only to users with update project permission)", bin3: true do
-      utp = api_create_team_project_and_two_users
-      @driver.navigate.to (@config['api_path'] + '/test/session?email='+utp[:user1]["email"])
-      @driver.navigate.to (@config['self_url'] + '/'+utp[:team]["slug"]+'/project/'+utp[:project]["dbid"].to_s)
-      wait_for_selector("#search-form")
-      expect(wait_for_selector_list('.project-actions').length == 1).to be(true)
-      @driver.navigate.to (@config['api_path'] + '/test/session?email='+utp[:user2]["email"])
-      @driver.navigate.to (@config['self_url'] + '/'+utp[:team]["slug"]+'/project/'+utp[:project]["dbid"].to_s)
-      wait_for_selector("#search-form")
-      wait_for_selector_none('.project-actions')
-      expect(@driver.find_elements(:class, "project-actions").length == 0).to be(true)
-    end
-
-    it "should autorefresh project when media is created", bin1: true do
-      api_create_team_and_project
-      @driver.navigate.to @config['self_url']
-      url = @driver.current_url
-      wait_for_selector('#search__open-dialog-button')
-      expect(@driver.page_source.include?('Auto-Refresh')).to be(false)
-      current_window = @driver.window_handles.last
-      @driver.execute_script("window.open('#{url}')")
-      wait_for_selector("#search-input")
-      @driver.switch_to.window(@driver.window_handles.last)
-      wait_for_selector('.avatar')
-      wait_for_selector("#create-media__add-item").click
-      wait_for_selector("#create-media-input").click
-      fill_field('#create-media-input', 'Auto-Refresh')
-      wait_for_selector('#create-media-dialog__submit-button').click
-      wait_for_selector('.medias__item')
-      @driver.execute_script('window.close()')
-      @driver.switch_to.window(current_window)
-      wait_for_selector("#create-media__add-item")
-      wait_for_selector_list_size(".medias__item", 1, :css , 80)
-      @wait.until { (@driver.page_source.include?('Auto-Refresh')) }
-      result = @driver.find_elements(:css, '.media__heading')
-      expect(result.size == 1).to be(true)
-      expect(@driver.page_source.include?('Auto-Refresh')).to be(true)
-    end
-
-    it "should autorefresh media when annotation is created", bin3: true do
-      api_create_team_project_and_claim_and_redirect_to_media_page
-      url = @driver.current_url
-      wait_for_selector(".media-detail")
-      expect(@driver.page_source.include?('Auto-Refresh')).to be(false)
-      current_window = @driver.window_handles.last
-      @driver.execute_script("window.open('#{url}')")
-      @driver.switch_to.window(@driver.window_handles.last)
-      wait_for_selector(".media-tab__comments").click
-      fill_field('#cmd-input', 'Auto-Refresh')
-      @driver.action.send_keys(:enter).perform
-      wait_for_selector(".annotations__list-item")
-      @driver.execute_script('window.close()')
-      @driver.switch_to.window(current_window)
-      wait_for_selector(".media-tab__comments").click
-      wait_for_selector('.annotation__card-activity-create-comment')
-      expect(@driver.page_source.include?('Auto-Refresh')).to be(true)
     end
 
     it "should give 404 when trying to access a media that is not related to the project on the URL", bin1: true do
@@ -272,22 +221,33 @@ shared_examples 'app' do |webdriver_url|
       expect(title.text == 'Not Found').to be(true)
     end
 
-    it "should cancel request through switch teams", bin1: true do
-      user = api_register_and_login_with_email
-      t1 = api_create_team(user: user)
-      t2 = api_create_team(user: user)
-      @driver.navigate.to(@config['self_url'] + '/check/me')
-      wait_for_selector(".source__primary-info")
-      select_team(name: t1.name)
-      wait_for_selector(".team-menu__edit-team-button")
-      team_name = wait_for_selector('.team__name').text
-      expect(team_name).to eq(t1.name)
-      @driver.navigate.to(@config['self_url'] + '/check/me')
-      wait_for_selector(".source__primary-info")
-      select_team(name: t2.name)
-      wait_for_selector(".team-menu__edit-team-button")
-      team_name = wait_for_selector('.team__name').text
-      expect(team_name).to eq(t2.name)
+    it "should go back to the right url from the item page", bin3: true do
+      #item created in a project
+      api_create_team_project_and_claim_and_redirect_to_media_page
+      wait_for_selector(".card")
+      wait_for_selector(".project-header__back-button").click
+      wait_for_selector("#create-media__add-item")
+      expect(@driver.current_url.to_s.match(/\/project\/[0-9]+$/).nil?).to be(false) #project page
+      # send this item to trash go to the item page and go back to trash page
+      wait_for_selector("input[type=checkbox]").click
+      wait_for_selector(".media-bulk-actions__delete-icon").click
+      wait_for_selector("//span[contains(text(), 'Add a link or text')]", :xpath)
+      wait_for_selector(".project-list__item-trash").click #Go to the trash page
+      wait_for_selector("//span[contains(text(), 'Trash')]", :xpath)
+      wait_for_selector(".medias__item")
+      wait_for_selector(".media__heading").click
+      wait_for_selector(".media-actions__icon")
+      wait_for_selector(".project-header__back-button").click
+      wait_for_selector("#media-bulk-actions")
+      expect(@driver.current_url.to_s.match(/trash/).nil?).to be(false) # trash page
+      #item created from "all items" page
+      wait_for_selector('a[href$="/all-items"]').click
+      create_media("claim 2")
+      wait_for_selector(".media__heading").click
+      wait_for_selector("#media-detail__report-designer")
+      wait_for_selector(".project-header__back-button").click
+      wait_for_selector("#create-media__add-item")
+      expect(@driver.current_url.to_s.match(/all-items/).nil?).to be(false) # all items page
     end
 
     it "should linkify URLs on comments", bin1: true do
@@ -302,19 +262,6 @@ shared_examples 'app' do |webdriver_url|
       expect(wait_for_selector_list("//a[contains(text(), 'https://meedan.com/en/')]", :xpath).length == 1).to be(true)
     end
 
-    it "should refresh media", bin1: true do
-      api_create_team_project_and_link_and_redirect_to_media_page 'http://ca.ios.ba/files/meedan/random.php'
-      wait_for_selector(".media-detail")
-      title1 = wait_for_selector('.media-expanded__title span').text
-      expect((title1 =~ /Random/).nil?).to be(false)
-      wait_for_selector('.media-actions__icon').click
-      wait_for_selector('.media-actions__refresh').click
-      wait_for_text_change(title1, '.media-expanded__title span', :css)
-      title2 = wait_for_selector('.media-expanded__title span').text
-      expect((title2 =~ /Random/).nil?).to be(false)
-      expect(title1 != title2).to be(true)
-    end
-
     it "should set metatags", bin5: true do
       api_create_team_project_and_link_and_redirect_to_media_page 'https://twitter.com/marcouza/status/875424957613920256'
       request_api('make_team_public', { slug: get_team })
@@ -327,155 +274,7 @@ shared_examples 'app' do |webdriver_url|
       twitter_title = @driver.find_element(:css, 'meta[name="twitter\\:title"]').attribute('content')
       expect(twitter_title == 'This is a test').to be(true)
     end
-
-    it "should paginate project page", bin4: true do
-      api_create_team_project_claims_sources_and_redirect_to_project_page 21, 0
-      wait_for_selector(".search__results-heading")
-      wait_for_selector('.media__heading')
-      wait_for_selector("//span[contains(text(), '1 - 20 / 21')]", :xpath)
-      expect(@driver.page_source.include?('1 - 20 / 21')).to be(true)
-      wait_for_selector(".search__next-page").click
-      wait_for_selector(".search__results-heading")
-      wait_for_selector('.media__heading')
-      wait_for_selector("//span[contains(text(), '21 - 21 / 21')]", :xpath)
-      expect(@driver.page_source.include?('21 - 21 / 21')).to be(true)
-    end
-
-    it "should show teams at /check/teams", bin1: true do
-      api_create_team
-      @driver.navigate.to @config['self_url'] + '/check/teams'
-      wait_for_selector("teams", :class)
-      expect(@driver.find_elements(:css, '.teams').empty?).to be(false)
-    end
-
-    it "should upload image when registering", bin3: true do
-      @driver.navigate.to @config['self_url']
-      wait_for_selector(".login__form")
-      wait_for_selector("#register").click
-      wait_for_selector(".without-file")
-      fill_field('.login__name input', 'User With Email')
-      fill_field('.login__email input', @email)
-      fill_field('.login__password input', '12345678')
-      fill_field('.login__password-confirmation input', '12345678')
-      wait_for_selector('input[type=file]').send_keys(File.join(File.dirname(__FILE__), 'test.png'))
-      wait_for_selector(".with-file")
-      expect(wait_for_selector(".with-file div").text.include?('test.png')).to be(true)
-      agree_to_tos(false)
-      press_button('#submit-register-or-login')
-      wait_for_selector(".message")
-      expect(@driver.page_source.include?('Please check your email to verify your account')).to be(true)
-    end
-
-    it "should redirect to last visited project", bin3: true do
-      user = api_register_and_login_with_email
-      api_create_team_and_project(user: user)
-      api_create_team_and_project(user: user)
-
-      @driver.navigate.to(@config['self_url'] + '/check/me')
-      wait_for_selector('#teams-tab').click
-      wait_for_selector(".switch-teams__joined-team")
-      wait_for_selector_list('.teams a').first.click
-      wait_for_selector(".project__title")
-      wait_for_selector(".project-list__link-trash")
-      wait_for_selector(".project__title")
-      wait_for_selector(".team-header__drawer-team-link").click
-      wait_for_selector('.project-list__link').click
-      wait_for_selector_none(".team-members__edit-button", :css, 10)
-
-      @driver.navigate.to(@config['self_url'] + '/check/me')
-      wait_for_selector('#teams-tab').click
-      wait_for_selector(".switch-teams__joined-team")
-      wait_for_selector_list('.teams a').last.click
-      wait_for_selector(".project__title")
-      wait_for_selector(".project-list__link-trash")
-      wait_for_selector(".team-header__drawer-team-link").click
-
-      @driver.navigate.to(@config['self_url'])
-      wait_for_selector('.project__title')
-      notfound = @config['self_url'] + '/check/404'
-      expect(@driver.current_url.to_s == notfound).to be(false)
-    end
-
-    it "should be able to find a team after signing up", bin5: true do
-      user = api_register_and_login_with_email
-      @driver.navigate.to @config['self_url']
-      wait_for_selector('.find-team-card')
-      expect(@driver.page_source.include?('Find an existing workspace')).to be(true)
-
-      # return error for non existing team
-      fill_field('#team-slug-container', 'non-existing-slug')
-      wait_for_selector('.find-team__submit-button').click
-      wait_for_selector('.find-team-card')
-      expect(@driver.page_source.include?('Workspace not found!')).to be(true)
-
-      # redirect to /team-slug/join if team exists
-      # /team-slug/join in turn redirects to team page because already member
-      @driver.navigate.to @config['self_url']  + "/check/teams/new"
-      wait_for_selector('.create-team__submit-button')
-      team = "existing-team-#{Time.now.to_i}"
-      api_create_team(team: team)
-      @driver.navigate.to @config['self_url'] + '/check/teams/find'
-      wait_for_selector(".find-team__form")
-      fill_field('#team-slug-container', team )
-      wait_for_selector('.find-team__submit-button').click
-      wait_for_selector(".team__primary-info")
-      expect(@driver.page_source.include?(team)).to be(true)
-    end
-
-    it "should go back to previous team", bin1: true do
-      user = api_register_and_login_with_email
-      t1 = api_create_team(user: user)
-      t2 = api_create_team(user: user)
-
-      # Go to first team
-      @driver.navigate.to @config['self_url'] + '/' + t1.slug + '/all-items'
-      wait_for_selector(".team-header__drawer-team-link[href=\"/#{t1.slug}/\"]")
-
-      # Navigate to second team
-      wait_for_selector('.header__user-menu').click
-      wait_for_selector('a[href="/check/me"]').click
-      wait_for_selector('#teams-tab').click
-      wait_for_selector("#switch-teams__link-to-#{t2.slug}").click
-      wait_for_selector(".team-header__drawer-team-link[href=\"/#{t2.slug}/\"]")
-
-      # Navigate back to first team
-      wait_for_selector('.header__user-menu').click
-      wait_for_selector('a[href="/check/me"]').click
-      wait_for_selector('#teams-tab').click
-      wait_for_selector("#switch-teams__link-to-#{t1.slug}").click
-      wait_for_selector(".team-header__drawer-team-link[href=\"/#{t1.slug}/\"]")
-    end
-
-    it "should go back to primary item", bin1: true do
-      # Go to primary item
-      api_create_team_project_and_claim_and_redirect_to_media_page
-      expect((@driver.title =~ /Claim/).nil?).to be(false)
-      expect((@driver.title =~ /Secondary/).nil?).to be(true)
-
-      # Create related item
-      wait_for_selector('.media-detail')
-      wait_for_selector('.create-related-media__add-button')
-      press_button('.create-related-media__add-button')
-      wait_for_selector('#create-media-dialog__tab-new').click
-      wait_for_selector('#create-media__quote').click
-      wait_for_selector('#create-media-quote-input')
-      fill_field('#create-media-quote-input', 'Secondary Item')
-      press_button('#create-media-dialog__submit-button')
-      wait_for_selector_none('#create-media-quote-input')
-
-      # Go to related item
-      wait_for_selector('.media-condensed__title').click
-      wait_for_selector('#media-related__primary-item')
-      expect((@driver.title =~ /Claim/).nil?).to be(true)
-      expect((@driver.title =~ /Secondary/).nil?).to be(false)
-
-      # Go back to primary item
-      wait_for_selector('.media-condensed__title').click
-      wait_for_selector('.media-related__secondary-item')
-      expect((@driver.title =~ /Claim/).nil?).to be(false)
-      expect((@driver.title =~ /Secondary/).nil?).to be(true)
-    end
-
+  
     it "should show current team content on sidebar when viewing profile", bin3: true do
       user = api_register_and_login_with_email
       api_create_team_and_project(user: user)
@@ -483,22 +282,5 @@ shared_examples 'app' do |webdriver_url|
       wait_for_selector('#teams-tab')
       wait_for_selector('.projects__list a[href$="/all-items"]')
     end
-
-    def edit_project(options)
-      wait_for_selector('.project-actions').click
-      wait_for_selector('.project-actions__edit').click
-      if (options[:title])
-        update_field('#project-title-field', options[:title])
-      end
-      if (options[:description] != nil)
-        wait_for_selector('#project-description-field').send_keys(:control, 'a', :delete)
-        @driver.action.send_keys(" \b").perform
-        sleep 1
-        update_field('#project-description-field', options[:description])
-      end
-      wait_for_selector('button.project-edit__editing-button--save').click
-      self
-    end
-
   end
 end
