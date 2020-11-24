e4c0dc9e2f3b7c4f0b57cc2a1e968d8404684eca
danielevalverde <danierzevalverde@gmail.com>
2020-11-24 17:15:33 -0300

    Ticket #8861: Makes the test fail to check the screenshot

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'rspec/retry'
 RSpec.configure do |config|
   config.verbose_retry = true
   config.display_try_failure_messages = true
-  config.default_retry_count = 3
+  config.default_retry_count = 0
   config.default_sleep_interval = 1
 
   config.expect_with :rspec do |expectations|
