a3cd7227968a7b407cc75edd9115f79130d034bf
Martin Peck <martin@meedan.com>
2021-02-06 11:25:13 -0700

    Set AWS_DEFAULT_REGION for SSM access. Always create configs during startup.

--- production/bin/start.sh
+++ production/bin/start.sh
@@ -7,14 +7,13 @@ if [[ -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
 	exit 1
 fi
 
-# Put config into place if not yet created
-if [ ! -f ${DEPLOYDIR}/latest/config.js ]; then
-	/opt/bin/create_configs.sh
-	if (( $? != 0 )); then
-		echo "Error creating configuration files. Exiting."
-		exit 1
-	fi
-fi	
+# Always put config into place when starting service. This is because
+# build stage includes the QA configs which Live must replace.
+/opt/bin/create_configs.sh
+if (( $? != 0 )); then
+	echo "Error creating configuration files. Exiting."
+	exit 1
+fi
 
 cp ${DEPLOYDIR}/latest/config.js ${DEPLOYDIR}/latest/build/web/js/config.js
 
