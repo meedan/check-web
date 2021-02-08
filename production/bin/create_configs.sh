d35ede98fd3c813e955ec890f70716955ee883e8
Martin Peck <51542678+sonoransun@users.noreply.github.com>
2021-02-08 09:10:38 -0700

    Support check web configuration via SSM parameter store (#801)
    * Initial support for SSM based configuration.
    
    Co-authored-by: Martin Peck <martin@meedan.com>

---
+++ production/bin/create_configs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This script generates json configuration files for check-web using values
+# from the SSM parameter store.
+
+# The following environment variables must be set:
+if [[ -z ${DEPLOY_ENV+x} || -z ${DEPLOYDIR+x} || -z ${AWS_DEFAULT_REGION+x} ]]; then
+  echo "DEPLOY_ENV, DEPLOYDIR, and AWS_DEFAULT_REGION must be in the environment. Exiting."
+  exit 1
+fi
+
+SSM_PREFIX="/${DEPLOY_ENV}/check-web"
+WORKTMP=$(mktemp)
+
+# Create user config.js from SSM parameter value:
+DESTFILE="${DEPLOYDIR}/latest/config.js"
+aws ssm get-parameters --region $AWS_DEFAULT_REGION --name "${SSM_PREFIX}/config" | jq .Parameters[].Value|sed 's/["]//g' | python -m base64 -d > $WORKTMP
+if (( $? != 0 )); then
+  echo "Error retrieving SSM parameter ${SSM_PREFIX}/config. Exiting."
+  exit 1
+fi
+mv $WORKTMP $DESTFILE
+
+# Create config-server.js from SSM parameter value:
+DESTFILE="${DEPLOYDIR}/latest/config-server.js"
+aws ssm get-parameters --region $AWS_DEFAULT_REGION --name "${SSM_PREFIX}/config-server" | jq .Parameters[].Value|sed 's/["]//g' | python -m base64 -d > $WORKTMP
+if (( $? != 0 )); then
+  echo "Error retrieving SSM parameter ${SSM_PREFIX}/config-server. Exiting."
+  exit 1
+fi
+mv $WORKTMP $DESTFILE
+
+
+echo "Configuration for env $DEPLOY_ENV complete."
+exit 0
--- production/bin/start.sh
+++ production/bin/start.sh
@@ -2,15 +2,19 @@
 # start.sh
 # the Dockerfile CMD
 
-if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
-	echo "GITHUB_TOKEN, DEPLOY_ENV, and APP must be in the environment. Exiting."
+if [[ -z ${DEPLOY_ENV+x} || -z ${APP+x} ]]; then
+	echo "DEPLOY_ENV, and APP must be in the environment. Exiting."
 	exit 1
 fi
 
-rm -rf ${DEPLOYDIR}/latest/configurator && git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ${DEPLOYDIR}/latest/configurator
-d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
+# Always put config into place when starting service. This is because the
+# build stage includes the sample configs which real deployments must replace.
+/opt/bin/create_configs.sh
+if (( $? != 0 )); then
+	echo "Error creating configuration files. Exiting."
+	exit 1
+fi
 
-# Put config into place
 cp ${DEPLOYDIR}/latest/config.js ${DEPLOYDIR}/latest/build/web/js/config.js
 
 passenger start
