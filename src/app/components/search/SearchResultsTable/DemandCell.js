982b6be75d991cc9dbb16273570f77de8440a26f
Caio Almeida <caiosba@gmail.com>
2021-03-15 16:48:07 -0300

    Ticket CHECK-354: Include similar items when search query comes from "all items" page

--- src/app/components/search/SearchResultsTable/DemandCell.js
+++ src/app/components/search/SearchResultsTable/DemandCell.js
@@ -4,10 +4,16 @@ import NumberCell from './NumberCell';
 
 export default function DemandCell({ projectMedia }) {
   const { demand } = projectMedia.list_columns_values;
-  return <NumberCell value={demand} />;
+  let value = demand;
+  if (projectMedia.is_secondary) {
+    value = projectMedia.requests_count;
+  }
+  return <NumberCell value={value} />;
 }
 DemandCell.propTypes = {
   projectMedia: PropTypes.shape({
+    is_secondary: PropTypes.bool.isRequired,
+    requests_count: PropTypes.number.isRequired,
     list_columns_values: PropTypes.shape({
       demand: PropTypes.number, // or null/undefined
     }).isRequired,
