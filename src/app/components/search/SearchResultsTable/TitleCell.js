810cba53bd380b59f8ae3cabe32348a786a9c1ff
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2021-03-16 18:05:13 -0300

    Fix/check 59 confirm pages (#819)
    * Ticket CHECK-59: apply new design for user confirm page
    
    * Ticket CHECK-59: hide sidebar and align confirmation text
    
    * Ticket CHECK-59: add CHECK terms text(footer)
    
    * Ticket CHECK-59: fix tests
    
    * Ticket CHECK-59: fix tests
    
    * CHECK-59: Undo global gray styling for links and text
    
    * CHECK-59: Fix item description color and verify your account message background
    
    Co-authored-by: Sawy <melsawy.be7eri@gmail.com>

--- src/app/components/search/SearchResultsTable/TitleCell.js
+++ src/app/components/search/SearchResultsTable/TitleCell.js
@@ -5,7 +5,7 @@ import TableCell from '@material-ui/core/TableCell';
 import LayersIcon from '@material-ui/icons/Layers';
 import { Link } from 'react-router';
 import { makeStyles } from '@material-ui/core/styles';
-import { units, black87, checkBlue } from '../../../styles/js/shared';
+import { units, black87, checkBlue, opaqueBlack54 } from '../../../styles/js/shared';
 
 const useStyles = makeStyles({
   root: {
@@ -47,6 +47,7 @@ const useStyles = makeStyles({
   }),
   description: {
     maxHeight: units(5),
+    color: opaqueBlack54,
     overflow: 'hidden',
   },
   similarityIcon: {
