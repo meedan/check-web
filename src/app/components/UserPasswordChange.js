35a0ad6905442c6a1cb3a102e72160487094cd17
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2021-03-22 22:04:59 +0200

    Refactor/check 358 reset change password pages (#822)
    * Ticket CHECK-358: reset/change password Modernization
    
    * CHECK-358: add best view link and check terms text to anonymous pages
    
    * CHECK-358: theme password page
    
    * CHECK-358: apply PR comments
    
    * CHECK-358: fix tests
    
    * CHECK-358: Get mutation response data; Use outlined textfields
    
    Co-authored-by: Alexandre Amoedo Amorim <amoedoamorim@gmail.com>

--- src/app/components/UserPasswordChange.js
+++ src/app/components/UserPasswordChange.js
@@ -1,81 +1,106 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
+import React from 'react';
+import { FormattedHTMLMessage, FormattedMessage } from 'react-intl';
 import { browserHistory } from 'react-router';
-import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import PageTitle from './PageTitle';
 import ChangePasswordComponent from './ChangePasswordComponent';
+import { FormattedGlobalMessage } from './MappedMessage';
+import CheckAgreeTerms from './CheckAgreeTerms';
+import globalStrings from '../globalStrings';
 import { stringHelper } from '../customHelpers';
-import { StyledPasswordChange } from '../styles/js/shared';
+import {
+  ContentColumn,
+  StyledSubHeader,
+  StyledCard,
+} from '../styles/js/shared';
 
-function handleSignIn() {
-  browserHistory.push('/');
-}
+const useStyles = makeStyles({
+  logo: {
+    margin: '0 auto',
+    display: 'block',
+  },
+});
 
-class UserPasswordChange extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showConfirmDialog: false,
-    };
-  }
+function UserPasswordChange() {
+  const [showConfirmDialog, setShowConfirmDialog] = React.useState(false);
 
-  static getQueryStringValue(key) {
-    return decodeURIComponent(window.location.search.replace(new RegExp(`^(?:.*[&\\?]${encodeURIComponent(key).replace(/[.+*]/g, '\\$&')}(?:\\=([^&]*))?)?.*$`, 'i'), '$1'));
-  }
+  const handleSignIn = () => {
+    browserHistory.push('/');
+  };
 
-  showConfirm() {
-    this.setState({ showConfirmDialog: true });
-  }
+  const getQueryStringValue = (key) => {
+    const value = window.location.search.replace(new RegExp(`^(?:.*[&\\?]${encodeURIComponent(key).replace(/[.+*]/g, '\\$&')}(?:\\=([^&]*))?)?.*$`, 'i'), '$1');
+    return decodeURIComponent(value);
+  };
 
-  render() {
-    const token = UserPasswordChange.getQueryStringValue('reset_password_token');
-    return (
-      <FormattedMessage id="passwordChange.title" defaultMessage="Change password">
-        { label => (
-          <PageTitle prefix={label}>
-            <StyledPasswordChange>
-              { this.state.showConfirmDialog ?
-                <Card className="user-password-change__confirm-card">
-                  <CardHeader title={<FormattedMessage id="passwordChange.successTitle" defaultMessage="Password updated" />} />
-                  <CardContent>
-                    <FormattedMessage
-                      id="passwordChange.successMsg"
-                      defaultMessage="You're all set. Now you can log in with your new password."
-                    />
-                  </CardContent>
-                  <CardActions className="user-password-change__actions">
-                    <Button color="primary" onClick={handleSignIn}>
-                      <FormattedMessage id="passwordChange.signIn" defaultMessage="Got it" />
-                    </Button>
-                  </CardActions>
-                </Card> :
-                <Card className="user-password-change__card">
-                  <CardContent>
-                    <img alt="" src={stringHelper('LOGO_URL')} className="user-password-change__logo" />
+  const showConfirm = () => {
+    setShowConfirmDialog(true);
+  };
 
-                    <span className="user-password-change__title">
-                      <FormattedMessage id="passwordChange.title" defaultMessage="Change password" />
-                    </span>
+  const token = getQueryStringValue('reset_password_token');
 
-                    <ChangePasswordComponent
-                      type="reset-password"
-                      show_current_password={false}
-                      token={token}
-                      show_confirm={this.showConfirm.bind(this)}
-                    />
-                  </CardContent>
-                </Card>
-              }
-            </StyledPasswordChange>
-          </PageTitle>
-        )}
-      </FormattedMessage>
-    );
-  }
+  const classes = useStyles();
+
+  return (
+    <PageTitle>
+      <Box m={2} align="center">
+        <FormattedHTMLMessage {...globalStrings.bestViewed} />
+      </Box>
+      <ContentColumn center className="user-password-reset__component">
+        <StyledCard>
+          <FormattedGlobalMessage messageKey="appNameHuman">
+            {appNameHuman => (
+              <img
+                className={classes.logo}
+                alt={appNameHuman}
+                width="120"
+                src={stringHelper('LOGO_URL')}
+              />
+            )}
+          </FormattedGlobalMessage>
+          <StyledSubHeader className="reset-password__heading">
+            { showConfirmDialog ?
+              <FormattedMessage id="passwordChange.successTitle" defaultMessage="Password updated" />
+              : <FormattedMessage id="passwordChange.title" defaultMessage="Change password" />
+            }
+          </StyledSubHeader>
+
+          { showConfirmDialog ?
+            <React.Fragment>
+              <CardContent>
+                <FormattedMessage
+                  id="passwordChange.successMsg"
+                  defaultMessage="You're all set. Now you can log in with your new password."
+                />
+              </CardContent>
+              <CardActions className="user-password-change__actions">
+                <Button color="primary" onClick={handleSignIn}>
+                  <FormattedMessage id="passwordChange.signIn" defaultMessage="Got it" />
+                </Button>
+              </CardActions>
+            </React.Fragment> :
+            <div className="user-password-change__card">
+              <CardContent>
+                <ChangePasswordComponent
+                  type="reset-password"
+                  showCurrentPassword={false}
+                  token={token}
+                  showConfirm={showConfirm}
+                />
+              </CardContent>
+            </div>
+          }
+        </StyledCard>
+        <Box my={4}>
+          <CheckAgreeTerms />
+        </Box>
+      </ContentColumn>
+    </PageTitle>
+  );
 }
 
 export default UserPasswordChange;
