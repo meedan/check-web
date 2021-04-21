5c09d539a24051576f80d4e7f1d622873a8b893b
Caio Almeida <caiosba@gmail.com>
2021-04-21 16:23:57 -0300

    Ticket CHECK-477: Rules table should be sortable

--- src/app/components/team/Rules/RulesTableHead.js
+++ src/app/components/team/Rules/RulesTableHead.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 
 const headCells = [
   { id: 'name', label: <FormattedMessage id="rulesTableHead.name" defaultMessage="Name" description="Table header for rule names" /> },
   { id: 'updated_at', label: <FormattedMessage id="rulesTableHead.updated" defaultMessage="Updated" description="Table header for rule update dates" /> },
 ];
 
-const RulesTableHead = () => (
+const RulesTableHead = ({ orderBy, order, onSort }) => (
   <TableHead className="rulesTableHead">
     <TableRow>
       <TableCell padding="checkbox" />
@@ -18,14 +20,25 @@ const RulesTableHead = () => (
           key={headCell.id}
           align="left"
           padding="default"
+          sortDirection={orderBy === headCell.id ? order : false}
         >
-          {headCell.label}
+          <TableSortLabel
+            active={orderBy === headCell.id}
+            direction={orderBy === headCell.id ? order : 'asc'}
+            onClick={() => { onSort(headCell.id); }}
+          >
+            {headCell.label}
+          </TableSortLabel>
         </TableCell>
       ))}
     </TableRow>
   </TableHead>
 );
 
-RulesTableHead.propTypes = {};
+RulesTableHead.propTypes = {
+  orderBy: PropTypes.string.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  onSort: PropTypes.func.isRequired,
+};
 
 export default RulesTableHead;
