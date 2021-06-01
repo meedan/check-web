cb45d36fe0355170ae70d6290ebe5d2a2ee6b063
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2021-06-01 16:19:05 -0300

    Perf/remove unused fields and files (#862)
    * Remove unused EmptyTrashButton.js
    
    * CHECK-599: Add eslint and webpack unused file checkers
    
    * CHECK-599: Halt on linter failure
    
    * CHECK-599: Fix eslint detected unused exports
    
    * CHECK-599: Remove unused stuff reported by eslint
    
    * CHECK-599: Try bail: true on travis
    
    * CHECK-599: --abort-on-container-exit &
    
    * CHECK-599: Limit curl to 60 reps
    
    * CHECK-599: Webpack build bail on error if MODE==test
    
    * CHECK-599: Signaling reason of build termination
    
    * CHECK-599: Give limit to check-web curl requests only
    
    * CHECK-599: Do not abort on container exit
    
    * CHECK-599: Ignore tests, remove unused from UnusedFilesWebpackPlugin
    
    * CHECK-599: Test travis build failure for eslint unused export checking
    
    * Revert "CHECK-599: Test travis build failure for eslint unused export checking"
    
    This reverts commit d1c6f5ad863a6fadbc90c6f32ca5311db7b33306.
    
    * CHECK-599: Fail travis build with UnusedFilesWebpackPlugin
    
    * Revert "CHECK-599: Fail travis build with UnusedFilesWebpackPlugin"
    
    This reverts commit 361f162aa71df22ca942ba8bfe9fbc62891027db.

--- src/app/components/Header.test.js
+++ src/app/components/Header.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { HeaderComponent } from './Header';
-import TeamHeader from './team/TeamHeader';
 import TeamPublicHeader from './team/TeamPublicHeader';
 import { mountWithIntl } from '../../../test/unit/helpers/intl-test';
 
 describe('<HeaderComponent />', () => {
-  it.skip('renders the Check logo and not TeamHeader or TeamPublicHeader on 404 page', () => {
+  it.skip('renders the Check logo and not TeamPublicHeader on 404 page', () => {
     const location = { pathname: '/check/not-found' };
     const params = { team: 'team' };
     const header = mountWithIntl(<HeaderComponent
@@ -15,7 +14,6 @@ describe('<HeaderComponent />', () => {
       params={params}
     />);
     expect(header.find('[src="/images/logo/check.svg"]')).toHaveLength(1);
-    expect(header.find(TeamHeader)).toHaveLength(0);
     expect(header.find(TeamPublicHeader)).toHaveLength(0);
   });
 });
