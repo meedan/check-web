17bcd168781a6dfbcb05d742b5f8859943b27d08
danielevalverde <danierzevalverde@gmail.com>
2021-06-07 19:40:16 -0300

    debug test

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-npm run test:unit || exit 1
-./../scripts/uncovered-files
-bundle install
-./parallel-test.sh
+# npm run test:unit || exit 1
+# ./../scripts/uncovered-files
+# bundle install
+# ./parallel-test.sh
+rspec --example 'should check, edit and remove source info' spec/integration_spec.rb
--- test/spec/source_spec.rb
+++ test/spec/source_spec.rb
@@ -22,12 +22,17 @@ shared_examples 'source' do
     wait_for_selector('#source__link-item0')
     # remove main link
     expect(@driver.find_elements(:css, '.source__remove-link-button').length == 2).to be(true)
+    puts "remove main link"
     wait_for_selector_list('.source__remove-link-button')[0].click
+    puts "wait main link change"
     wait_for_text_change('https://edition.cnn.com', '#main_source__link', :css)
     # check that the main link was changed
     expect(wait_for_selector('#main_source__link').attribute('value') == 'https://www.bbc.com/news/uk').to be(true)
+    puts "after change link"
     expect(@driver.find_elements(:css, '.source__remove-link-button').length == 1).to be(true)
+    puts "before try add the same source"
     wait_for_selector('.source__add-link-button').click
+    puts "after find button"
     # try add the same source again
     wait_for_selector('#source__link-input-new').send_keys('https://www.bbc.com/news/uk')
     @driver.action.send_keys(:enter).perform
