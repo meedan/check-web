15f5db5bf6668a7a465c088d9e3511bef67c4090
danielevalverde <danierzevalverde@gmail.com>
2021-06-07 21:09:40 -0300

    run all tests again

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# npm run test:unit || exit 1
-# ./../scripts/uncovered-files
-# bundle install
-# ./parallel-test.sh
-rspec --example 'should check, edit and remove source info' spec/integration_spec.rb
+npm run test:unit || exit 1
+./../scripts/uncovered-files
+bundle install
+./parallel-test.sh
