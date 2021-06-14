861471100e2d6b1c9fd3f1b220134a2694927252
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2021-06-14 19:21:28 -0300

    Feature/51 show extracted text (#869)
    * CHECK-51: Show extracted text
    
    * CHECK-51: Fix copy and MoreLess height
    
    * CHECK-51: Always expanded text items; Add margin between extracted text and image

--- src/app/components/media/QuoteMediaCard.js
+++ src/app/components/media/QuoteMediaCard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import rtlDetect from 'rtl-detect';
 import styled from 'styled-components';
-import MoreLess from '../layout/MoreLess';
 import ParsedText from '../ParsedText';
 import { breakWordStyles } from '../../styles/js/shared';
 
@@ -15,9 +14,7 @@ const StyledQuoteText = styled.div`
 const QuoteMediaCard = ({ quote, languageCode }) => (
   <div>
     <StyledQuoteText dir={rtlDetect.isRtlLang(languageCode) ? 'rtl' : 'ltr'} lang={languageCode}>
-      <MoreLess key={quote /* reset when changing quote */}>
-        <ParsedText text={quote} />
-      </MoreLess>
+      <ParsedText text={quote} />
     </StyledQuoteText>
   </div>
 );
