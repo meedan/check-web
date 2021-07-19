58f1d02c870612b9684941d95b94ac0788b16765
Caio Almeida <caiosba@gmail.com>
2021-07-18 23:36:12 -0300

    Ticket CHECK-702: Filter assignments on frontend

--- src/app/components/source/UserAssignments.js
+++ src/app/components/source/UserAssignments.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Relay from 'react-relay/classic';
 import { injectIntl, FormattedMessage, defineMessages } from 'react-intl';
 import Box from '@material-ui/core/Box';
@@ -24,7 +23,6 @@ import BlankState from '../layout/BlankState';
 import FilterPopup from '../layout/FilterPopup';
 import TeamSelect from '../team/TeamSelect';
 import UserRoute from '../../relay/UserRoute';
-import CheckContext from '../../CheckContext';
 import { AlignOpposite } from '../../styles/js/shared';
 import NumberIcon from '../../icons/NumberIcon';
 
@@ -53,58 +51,17 @@ class UserAssignmentsComponent extends Component {
     super(props);
 
     this.state = {
-      teamId: null,
+      teamId: 0, // Zero means all teams
     };
   }
 
-  componentWillMount() {
-    let teamId = null;
-    const { currentUser } = this.getContext().getContextStore();
-    const { user } = this.props;
-    user.teams.edges.forEach((team) => {
-      const tid = parseInt(team.node.dbid, 10);
-      if (tid === parseInt(currentUser.current_team_id, 10)) {
-        teamId = tid;
-      }
-    });
-    if (!teamId) {
-      const team = user.teams.edges[0];
-      if (team) {
-        teamId = team.node.dbid;
-      }
-    }
-    this.setState({ teamId });
-  }
-
-  componentDidMount() {
-    this.refresh();
-  }
-
-  componentDidUpdate() {
-    this.refresh();
-  }
-
-  getContext() {
-    return new CheckContext(this);
-  }
-
-  refresh() {
-    if (this.props.relay.variables.teamId !== this.state.teamId) {
-      this.props.relay.setVariables({ teamId: this.state.teamId, userId: this.props.user.dbid });
-      this.props.relay.forceFetch();
-    }
-  }
-
   handleSelect(e) {
-    this.setState({ teamId: e.target.value });
+    this.setState({ teamId: parseInt(e.target.value, 10) });
   }
 
   render() {
     const { user } = this.props;
-
-    if (this.props.relay.variables.teamId === null) {
-      return null;
-    }
+    const { teamId } = this.state;
 
     if (!user.assignments) {
       return null;
@@ -113,23 +70,27 @@ class UserAssignmentsComponent extends Component {
     const assignments = {};
     const assignmentsWithoutProject = [];
     const projectPaths = {};
-    const hasAssignment = user.assignments.edges.length > 0;
+    let hasAssignment = false;
 
     user.assignments.edges.forEach((assignment) => {
       const a = assignment.node;
-      if (a.project) {
-        const project = a.project.title;
-        if (!assignments[project]) {
-          assignments[project] = [];
+      if (!teamId || teamId === a.team.dbid) {
+        if (a.project) {
+          const project = a.project.title;
+          if (!assignments[project]) {
+            assignments[project] = [];
+          }
+          if (!projectPaths[project]) {
+            projectPaths[project] = `/${a.team.slug}/project/${a.project_id}`;
+          }
+          a.path = `/${a.team.slug}/project/${a.project_id}/media/${a.dbid}`;
+          assignments[project].push(a);
+          hasAssignment = true;
+        } else {
+          a.path = `/${a.team.slug}/media/${a.dbid}`;
+          assignmentsWithoutProject.push(a);
+          hasAssignment = true;
         }
-        if (!projectPaths[project]) {
-          projectPaths[project] = `/${a.team.slug}/project/${a.project_id}`;
-        }
-        a.path = `/${a.team.slug}/project/${a.project_id}/media/${a.dbid}`;
-        assignments[project].push(a);
-      } else {
-        a.path = `/${a.team.slug}/media/${a.dbid}`;
-        assignmentsWithoutProject.push(a);
       }
     });
 
@@ -193,14 +154,10 @@ class UserAssignmentsComponent extends Component {
   }
 }
 
-UserAssignmentsComponent.contextTypes = {
-  store: PropTypes.object,
-};
-
 const UserAssignmentsContainer = Relay.createContainer(injectIntl(UserAssignmentsComponent), {
   initialVariables: {
     userId: null,
-    teamId: null,
+    teamId: 0,
   },
   fragments: {
     user: () => Relay.QL`
