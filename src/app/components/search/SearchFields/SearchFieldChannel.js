92e71bb43ab03302e48af42e5cf778a7e65dd841
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2021-08-02 18:10:52 -0300

    CHECK-577: Localize channel names

--- src/app/components/search/SearchFields/SearchFieldChannel.js
+++ src/app/components/search/SearchFields/SearchFieldChannel.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { defineMessages, injectIntl, FormattedMessage } from 'react-intl';
 import { QueryRenderer, graphql } from 'react-relay/compat';
 import Relay from 'react-relay/classic';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import ForwardIcon from '@material-ui/icons/Forward';
 import MultiSelectFilter from '../MultiSelectFilter';
+
+const messages = defineMessages({
+  manual: {
+    id: 'searchFieldChannel.manual',
+    defaultMessage: 'Manual',
+    description: 'Filter option that refers to items created manually',
+  },
+  browserExtension: {
+    id: 'searchFieldChannel.browserExtension',
+    defaultMessage: 'Browser extension',
+    description: 'Filter option that refers to items created via the browser extension',
+  },
+  api: {
+    id: 'searchFieldChannel.api',
+    defaultMessage: 'API',
+    description: 'Filter option that refers to items created via the API',
+  },
+  anyTipline: {
+    id: 'searchFieldChannel.anyTipline',
+    defaultMessage: 'Any tipline',
+    description: 'Filter option that refers to items created via a tipline',
+  },
+});
 
 const SearchFieldChannelComponent = ({
   selected,
@@ -13,12 +36,32 @@ const SearchFieldChannelComponent = ({
   onRemove,
   about,
   readOnly,
+  intl,
 }) => {
   const { channels } = about;
-  let options = Object.keys(channels).map(key => ({ label: key, value: `${channels[key]}` })).filter(c => c.label !== 'TIPLINE');
-  options = options.concat([{ label: 'Any tipline', value: 'any_tipline', hasChildren: true }, { label: '', value: '' }]);
 
-  const tiplines = Object.keys(channels.TIPLINE).map(key => ({ label: key, value: `${channels.TIPLINE[key]}`, parent: 'any_tipline' }));
+  const optionLabels = {
+    MANUAL: intl.formatMessage(messages.manual),
+    FETCH: 'Fetch',
+    BROWSER_EXTENSION: intl.formatMessage(messages.browserExtension),
+    API: intl.formatMessage(messages.api),
+    ZAPIER: 'Zapier',
+    WHATSAPP: 'WhatsApp',
+    MESSENGER: 'Messenger',
+    TWITTER: 'Twitter',
+    TELEGRAM: 'Telegram',
+    VIBER: 'Viber',
+    LINE: 'Line',
+  };
+
+  let options = Object.keys(channels).filter(key => key !== 'TIPLINE').map(key => ({ label: optionLabels[key], value: `${channels[key]}` }));
+
+  options = options.concat([
+    { label: intl.formatMessage(messages.anyTipline), value: 'any_tipline', hasChildren: true },
+    { label: '', value: '' },
+  ]);
+
+  const tiplines = Object.keys(channels.TIPLINE).map(key => ({ label: optionLabels[key], value: `${channels.TIPLINE[key]}`, parent: 'any_tipline' }));
   options = options.concat(tiplines);
 
   const handleChange = (channelIds) => {
@@ -78,4 +121,4 @@ SearchFieldChannel.propTypes = {
   onRemove: PropTypes.func.isRequired,
 };
 
-export default SearchFieldChannel;
+export default injectIntl(SearchFieldChannel);
