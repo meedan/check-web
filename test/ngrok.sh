dd076b4ed46975cb6aa251793121275cc1bebb79
danielevalverde <danierzevalverde@gmail.com>
2021-08-30 15:55:37 -0300

    update ngrok script

--- test/ngrok.sh
+++ test/ngrok.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+./ngrok http 9000 >/dev/null &
 curl localhost:4040/api/tunnels > ngrok.json
 export NGROK_URL=$(grep -Po '"public_url": *\K"[^"]*"' ngrok.json | tail -n1 | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/')
 sed -i '/storage_public_endpoint/ i \  \storage_asset_host:\ '"'$NGROK_URL/check-api-test'"'' config/config.yml
