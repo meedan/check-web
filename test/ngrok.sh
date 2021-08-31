a1209bf5ae1ee3076a52ea53d4503e157126335d
danielevalverde <danierzevalverde@gmail.com>
2021-08-31 13:41:09 -0300

    fix file path

--- test/ngrok.sh
+++ test/ngrok.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-./ngrok http 9000 >/dev/null &
 curl localhost:4040/api/tunnels > ngrok.json
 export NGROK_URL=$(grep -Po '"public_url": *\K"[^"]*"' ngrok.json | tail -n1 | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/')
-sed -i '/storage_public_endpoint/ i \  \storage_asset_host:\ '"'$NGROK_URL/check-api-test'"'' config/config.yml
-sed -i '/storage_public_endpoint/ i \  \storage_endpoint:\ '"'$NGROK_URL'"'' config/config.yml
+sed -i '/storage_public_endpoint/ i \  \storage_asset_host:\ '"'$NGROK_URL/check-api-test'"'' check-api/config/config.yml
+sed -i '/storage_public_endpoint/ i \  \storage_endpoint:\ '"'$NGROK_URL'"'' check-api/config/config.yml
 docker-compose restart api
 until curl --silent -I -f --fail http://localhost:3000; do printf .; sleep 1; done
 docker-compose exec web bash -c "cd test && rspec --example 'should extract text from a image' spec/integration_spec.rb"
