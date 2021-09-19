0251e4d83b22735f51ad7477b0359112728f1c1f
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2021-09-20 00:59:56 +0200

    Feature/check 897 amendments to tipline inbox (#912)
    * CHECK-897: allow user to filter by read/unread and none projects items
    
    * CHECK-897: show default filters for tipline page

--- src/app/components/team/TiplineInbox.js
+++ src/app/components/team/TiplineInbox.js
@@ -1,33 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { QueryRenderer, graphql } from 'react-relay/compat';
+import Relay from 'react-relay/classic';
 import { FormattedMessage } from 'react-intl';
 import ForumIcon from '@material-ui/icons/Forum';
 import { safelyParseJSON } from '../../helpers';
 import Search from '../search/Search';
 import CheckChannels from '../../CheckChannels';
 
-export default function TiplineInbox({ routeParams }) {
-  const query = {
-    ...safelyParseJSON(routeParams.query, {}),
-    channels: [CheckChannels.ANYTIPLINE],
-  };
+const TiplineInbox = ({ routeParams }) => (
+  <QueryRenderer
+    environment={Relay.Store}
+    query={graphql`
+      query TiplineInboxQuery($slug: String!) {
+        team(slug: $slug) {
+          verification_statuses
+        }
+      }
+    `}
+    variables={{
+      slug: routeParams.team,
+    }}
+    render={({ error, props }) => {
+      if (!error && props) {
+        const defaultStatusId = props.team.verification_statuses.default;
+        console.log('routeParams', safelyParseJSON(routeParams.query, {})); // eslint-disable-line no-console
+        const query = {
+          read: ['0'],
+          projects: ['-1'],
+          verification_status: [defaultStatusId],
+          ...safelyParseJSON(routeParams.query, {}),
+          channels: [CheckChannels.ANYTIPLINE],
+        };
 
-  return (
-    <Search
-      searchUrlPrefix={`/${routeParams.team}/tipline-inbox`}
-      mediaUrlPrefix={`/${routeParams.team}/media`}
-      title={<FormattedMessage id="tiplineInbox.title" defaultMessage="Tipline inbox" />}
-      icon={<ForumIcon />}
-      teamSlug={routeParams.team}
-      query={query}
-      hideFields={['channels']}
-      page="tipline-inbox"
-    />
-  );
-}
+        return (
+          <Search
+            searchUrlPrefix={`/${routeParams.team}/tipline-inbox`}
+            mediaUrlPrefix={`/${routeParams.team}/media`}
+            title={<FormattedMessage id="tiplineInbox.title" defaultMessage="Tipline inbox" />}
+            icon={<ForumIcon />}
+            teamSlug={routeParams.team}
+            query={query}
+            hideFields={['channels']}
+            page="tipline-inbox"
+          />
+        );
+      }
+      return null;
+    }}
+  />
+);
+
 TiplineInbox.propTypes = {
   routeParams: PropTypes.shape({
     team: PropTypes.string.isRequired,
     query: PropTypes.string, // JSON-encoded value; can be empty/null/invalid
   }).isRequired,
 };
+
+export default TiplineInbox;
