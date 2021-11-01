ff710558f735a905312a2cab2355cc77aedaed9f
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2021-11-01 03:58:58 +0200

    Feature/check 1022 note edit option and UI updates (#935)
    * CHECK-1022: add edit to item notes
    
    * CHECK-1022: handle edit note
    
    * CHECK-1022: handle edit note
    
    * CHECK-1022: Styling upgrades
    
    * CHECK-1022: More styling, fix tooltip
    
    * CHECK-1022: more CSS, allow user to attach all file types and handle success for edit mode
    
    * CHECK-1022: allow user to upload file for comments without text
    
    * CHECK-1022: fix tests
    
    Co-authored-by: Alexandre Amoedo Amorim <amoedoamorim@gmail.com>

---
+++ src/app/components/annotations/Comment.js
@@ -0,0 +1,319 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
+import Relay from 'react-relay/classic';
+import RCTooltip from 'rc-tooltip';
+import styled from 'styled-components';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
+import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton';
+import MoreHoriz from '@material-ui/icons/MoreHoriz';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
+import AddAnnotation from './AddAnnotation';
+import { can } from '../Can';
+import { withSetFlashMessage } from '../FlashMessage';
+import ParsedText from '../ParsedText';
+import TimeBefore from '../TimeBefore';
+import SourcePicture from '../source/SourcePicture';
+import ProfileLink from '../layout/ProfileLink';
+import UserTooltip from '../user/UserTooltip';
+import DeleteAnnotationMutation from '../../relay/mutations/DeleteAnnotationMutation';
+import {
+  getErrorMessage,
+  parseStringUnixTimestamp,
+} from '../../helpers';
+import globalStrings from '../../globalStrings';
+import { stringHelper } from '../../customHelpers';
+import {
+  units,
+  black38,
+  black54,
+  black87,
+  caption,
+  breakWordStyles,
+  separationGray,
+  Row,
+} from '../../styles/js/shared';
+
+const StyledAnnotationCardWrapper = styled.div`
+  width: 100%;
+  z-index: initial !important;
+
+  img {
+    cursor: pointer;
+  }
+`;
+
+const StyledAvatarColumn = styled.div`
+  margin-${props => (props.theme.dir === 'rtl' ? 'left' : 'right')}: ${units(3)};
+`;
+
+const StyledPrimaryColumn = styled.div`
+  flex: 1;
+
+  .annotation__card-content {
+    ${breakWordStyles}
+    display: flex;
+    width: 100%;
+
+    & > span:first-child {
+      flex: 1;
+    }
+  }
+
+  .annotation__card-thumbnail {
+    padding: ${units(1)};
+  }
+`;
+
+const StyledAnnotationWrapper = styled.section`
+  .annotation__card-text {
+    display: flex;
+  }
+
+  .annotation__timestamp {
+    color: ${black38};
+    display: inline;
+    flex: 1;
+    white-space: pre;
+    margin-${props => (props.theme.dir === 'rtl' ? 'left' : 'right')}: ${units(1)};
+  }
+`;
+
+const StyledAnnotationMetadata = styled(Row)`
+  color: ${black54};
+  flex-flow: wrap row;
+  font: ${caption};
+
+  .annotation__card-author {
+    color: ${black87};
+    padding-${props => (props.theme.dir === 'rtl' ? 'left' : 'right')}: ${units(1)};
+  }
+`;
+
+const StyledAnnotationActionsWrapper = styled.div`
+  margin-${props => (props.theme.dir === 'rtl' ? 'right' : 'left')}: auto;
+`;
+
+// TODO Fix a11y issues
+/* eslint jsx-a11y/click-events-have-key-events: 0 */
+class Comment extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      zoomedCommentImage: false,
+      editMode: false,
+    };
+  }
+
+  handleCloseCommentImage() {
+    this.setState({ zoomedCommentImage: false });
+  }
+
+  handleOpenCommentImage(image) {
+    this.setState({ zoomedCommentImage: image });
+  }
+
+  handleOpenMenu = (e) => {
+    e.stopPropagation();
+    this.setState({ anchorEl: e.currentTarget });
+  };
+
+  handleCloseMenu = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  handleEdit = () => {
+    this.setState({ editMode: true });
+    this.handleCloseMenu();
+  };
+
+  handleCloseEdit = () => {
+    this.setState({ editMode: false });
+  };
+
+  handleDelete(id) {
+    const onSuccess = () => {};
+    Relay.Store.commitUpdate(
+      new DeleteAnnotationMutation({
+        parent_type: this.props.annotatedType.replace(/([a-z])([A-Z])/, '$1_$2').toLowerCase(),
+        annotated: this.props.annotated,
+        id,
+      }),
+      { onSuccess, onFailure: this.fail },
+    );
+  }
+
+  fail = (transaction) => {
+    const message = getErrorMessage(
+      transaction,
+      (
+        <FormattedMessage
+          {...globalStrings.unknownError}
+          values={{ supportEmail: stringHelper('SUPPORT_EMAIL') }}
+        />
+      ),
+    );
+    this.props.setFlashMessage(message, 'error');
+  };
+
+  render() {
+    const { annotation, annotated } = this.props;
+    let annotationActions = null;
+    if (annotation && annotation.annotation_type) {
+      const canUpdate = can(annotation.permissions, 'update Comment');
+      const canDestroy = can(annotation.permissions, 'destroy Comment');
+      const canDoAnnotationActions = canDestroy || canUpdate;
+      annotationActions = canDoAnnotationActions ? (
+        <div>
+          <IconButton
+            className="menu-button"
+            onClick={this.handleOpenMenu}
+          >
+            <MoreHoriz />
+          </IconButton>
+          <Menu
+            id="customized-menu"
+            anchorEl={this.state.anchorEl}
+            keepMounted
+            open={Boolean(this.state.anchorEl)}
+            onClose={this.handleCloseMenu}
+          >
+            {canUpdate ? (
+              <MenuItem
+                className="annotation__update"
+                onClick={this.handleEdit}
+              >
+                <FormattedMessage id="annotation.editButton" defaultMessage="Edit" />
+              </MenuItem>
+            ) : null}
+            {canDestroy ? (
+              <MenuItem
+                className="annotation__delete"
+                onClick={this.handleDelete.bind(this, annotation.id)}
+              >
+                <FormattedMessage id="annotation.deleteButton" defaultMessage="Delete" />
+              </MenuItem>
+            ) : null}
+            <MenuItem>
+              <a
+                href={`#annotation-${annotation.dbid}`}
+                style={{ textDecoration: 'none', color: black87 }}
+              >
+                <FormattedMessage
+                  id="annotation.permalink"
+                  defaultMessage="Permalink"
+                />
+              </a>
+            </MenuItem>
+          </Menu>
+        </div>)
+        : null;
+    }
+
+    const createdAt = parseStringUnixTimestamp(annotation.created_at);
+
+    const { user } = annotation.annotator;
+    const authorName = user
+      ? <ProfileLink className="annotation__author-name" teamUser={user.team_user} /> : null;
+
+    const commentText = annotation.text;
+    const commentContent = JSON.parse(annotation.content);
+    const contentTemplate = (
+      <div>
+        <div className="annotation__card-content">
+          { this.state.editMode ?
+            <AddAnnotation
+              cmdText={commentText}
+              editMode={this.state.editMode}
+              handleCloseEdit={this.handleCloseEdit}
+              annotated={annotated}
+              annotation={annotation}
+              annotatedType="ProjectMedia"
+              types={['comment']}
+            />
+            : <ParsedText text={commentText} />
+          }
+          {/* thumbnail */}
+          {commentContent.original ?
+            <div onClick={this.handleOpenCommentImage.bind(this, commentContent.original)}>
+              <img
+                src={commentContent.thumbnail}
+                className="annotation__card-thumbnail"
+                alt=""
+              />
+            </div> : null}
+        </div>
+
+        {/* lightbox */}
+        {commentContent.original && !!this.state.zoomedCommentImage
+          ? <Lightbox
+            onCloseRequest={this.handleCloseCommentImage.bind(this)}
+            mainSrc={this.state.zoomedCommentImage}
+          />
+          : null}
+      </div>
+    );
+
+    return (
+      <StyledAnnotationWrapper
+        className="annotation annotation--card annotation--comment"
+        id={`annotation-${annotation.dbid}`}
+      >
+        <StyledAnnotationCardWrapper>
+          <Box
+            py={2}
+            borderBottom={`1px ${separationGray} solid`}
+            className="annotation__card-text annotation__card-activity-comment"
+          >
+            { authorName ?
+              <RCTooltip placement="top" overlay={<UserTooltip teamUser={user.team_user} />}>
+                <StyledAvatarColumn className="annotation__avatar-col">
+                  <SourcePicture
+                    className="avatar"
+                    type="user"
+                    object={user.source}
+                  />
+                </StyledAvatarColumn>
+              </RCTooltip> : null }
+
+            <StyledPrimaryColumn>
+              <StyledAnnotationMetadata>
+                <span className="annotation__card-footer">
+                  { authorName ?
+                    <ProfileLink
+                      className="annotation__card-author"
+                      teamUser={user.team_user}
+                    /> : null }
+                  <span>
+                    <span className="annotation__timestamp"><TimeBefore date={createdAt} /></span>
+                  </span>
+                </span>
+
+                <StyledAnnotationActionsWrapper>
+                  {annotationActions}
+                </StyledAnnotationActionsWrapper>
+              </StyledAnnotationMetadata>
+              <Typography variant="body1" component="div">
+                {contentTemplate}
+              </Typography>
+            </StyledPrimaryColumn>
+          </Box>
+        </StyledAnnotationCardWrapper>
+      </StyledAnnotationWrapper>
+    );
+  }
+}
+
+Comment.propTypes = {
+  // https://github.com/yannickcr/eslint-plugin-react/issues/1389
+  // eslint-disable-next-line react/no-typos
+  setFlashMessage: PropTypes.func.isRequired,
+  intl: intlShape.isRequired,
+};
+
+export default withSetFlashMessage(injectIntl(Comment));
--- test/spec/media_actions_spec.rb
+++ test/spec/media_actions_spec.rb
@@ -57,7 +57,7 @@ shared_examples 'media actions' do
     @driver.execute_script('window.close()')
     @driver.switch_to.window(current_window)
     wait_for_selector('.media-tab__comments').click
-    wait_for_selector('.annotation__card-activity-create-comment')
+    wait_for_selector('.annotation__card-activity-comment')
     expect(@driver.page_source.include?('Auto-Refresh')).to be(true)
   end
 
@@ -91,7 +91,7 @@ shared_examples 'media actions' do
     fill_field('#cmd-input', 'This is my comment with image')
     wait_for_selector('.add-annotation__insert-photo').click
     wait_for_selector('input[type=file]').send_keys(File.join(File.dirname(__FILE__), 'test.png'))
-    wait_for_selector('.add-annotation__buttons button').click
+    wait_for_selector('#add-annotation_submit').click
     wait_for_selector_none('.with-file')
     wait_for_selector('.media-actions__icon').click
     wait_for_selector('.media-actions__history').click
