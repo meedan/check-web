36ee06f90d09ea1cfc36836e66fd156d11455470
Daniele Valverde <34126648+danielevalverde@users.noreply.github.com>
2022-01-14 11:05:04 -0300

    Fix flaky test (#996)
    * fix flaky test

--- test/spec/app_spec.rb
+++ test/spec/app_spec.rb
@@ -209,19 +209,17 @@ shared_examples 'app' do |webdriver_url|
     it 'should go back to the right url from the item page', bin3: true do
       # item created in a project
       api_create_team_project_and_claim_and_redirect_to_media_page
-      api_install_bot 'smooch'
-      wait_for_selector('.media')
+      wait_for_selector('.media__annotations-tabs')
       wait_for_selector('.project-header__back-button').click
+      wait_for_selector('#search-input')
       wait_for_selector_list_size('.medias__item', 1, :css, 30)
-      wait_for_selector('#create-media__add-item')
       expect(@driver.current_url.to_s.match(%r{/project/[0-9]+$}).nil?).to be(false) # project page
-      expect(@driver.page_source.include?('There are no items')).to be(false)
       # send this item to trash go to the item page and go back to trash page
       wait_for_selector('table input[type=checkbox]').click
       wait_for_selector('#media-bulk-actions')
       wait_for_selector('.media-bulk-actions__delete-icon').click
-      wait_for_selector("//span[contains(text(), 'There are no items')]", :xpath)
-      expect(@driver.page_source.include?('There are no items')).to be(true)
+      wait_for_selector_none('.medias__item')
+      expect(@driver.find_elements(:css, '.media__heading').length.zero?).to be(true)
       @driver.navigate.to "#{@config['self_url']}/#{get_team}/trash"
       wait_for_selector("//span[contains(text(), 'Trash')]", :xpath)
       wait_for_selector('.medias__item', :css, 20, true)
@@ -234,7 +232,6 @@ shared_examples 'app' do |webdriver_url|
       all.click
       create_media('claim 2', false)
       wait_for_selector('.media__heading', :css, 20, true).click
-      wait_for_selector('#media-detail__report-designer')
       wait_for_selector('.project-header__back-button').click
       wait_for_selector_list_size('.medias__item', 1, :css, 30)
       wait_for_selector('#create-media__add-item')
