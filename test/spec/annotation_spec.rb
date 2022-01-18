8bc719212f3a36d89b01aaed4fe049c7548e628a
Daniele Valverde <34126648+danielevalverde@users.noreply.github.com>
2022-01-18 15:21:46 -0300

    Fix flake tests (#1000)
    * fix flake tests

--- test/spec/annotation_spec.rb
+++ test/spec/annotation_spec.rb
@@ -51,6 +51,7 @@ shared_examples 'annotation' do
     wait_for_selector('.task__response-inputs')
     # answer the annotation
     wait_for_selector('.form-edit').click
+    wait_for_selector('.clear-button')
     wait_for_selector('#metadata-input').send_keys('answer')
     wait_for_selector('.form-save').click
     wait_for_selector('.form-edit')
