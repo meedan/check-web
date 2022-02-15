613a2a310c849d8256f89f225ec2cd82905f0c07
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2022-02-15 19:09:01 -0300

    Fix/1187 swallow click on numbercell link (#1023)
    * CHECK-1187: Swallow click on NumberCell link to avoid going to item page instead of similar/suggestions page
    
    * CHECK-1187: Bigger click area highlighted on hover

--- src/app/components/search/SearchResultsTable/NumberCell.js
+++ src/app/components/search/SearchResultsTable/NumberCell.js
@@ -2,7 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedNumber } from 'react-intl';
 import { Link } from 'react-router';
+import { makeStyles } from '@material-ui/core/styles';
 import TableCell from '@material-ui/core/TableCell';
+import { opaqueBlack10 } from '../../../styles/js/shared';
+
+const useStyles = makeStyles({
+  link: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 'auto',
+    maxWidth: '40px',
+    minHeight: '40px',
+    borderRadius: '50%',
+    '&:hover': {
+      backgroundColor: opaqueBlack10,
+    },
+  },
+});
 
 const swallowClick = (ev) => {
   // prevent <TableRow onClick> from firing when we click the link
@@ -10,10 +27,16 @@ const swallowClick = (ev) => {
 };
 
 export default function NumberCell({ value, linkTo }) {
+  const classes = useStyles();
   const formattedValue = value === null ? null : <FormattedNumber value={value} />;
   return (
     <TableCell align="center">
-      {linkTo ? <Link onClick={swallowClick} to={linkTo}>{formattedValue}</Link> : formattedValue}
+      {linkTo ?
+        <Link onClick={swallowClick} to={linkTo}>
+          <div className={classes.link}>
+            {formattedValue}
+          </div>
+        </Link> : formattedValue}
     </TableCell>
   );
 }
