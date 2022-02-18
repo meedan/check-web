a85920377f6752b3ec1bc424c1496d790164546b
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2022-02-18 19:03:15 -0300

    CHECK-1120: Refactor and reuse ValueListCell component

---
+++ src/app/components/search/SearchResultsTable/ValueListCell.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import TableCell from '@material-ui/core/TableCell';
+import { makeStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+import { checkBlue, inProgressYellow } from '../../../styles/js/shared';
+
+const useStyles = makeStyles({
+  names: {
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+  chip: {
+    color: 'white',
+    fontSize: 12,
+  },
+  noFactCheck: {
+    background: inProgressYellow,
+  },
+  more: {
+    background: checkBlue,
+  },
+});
+
+export default function ValueListCell({
+  values,
+  noValueLabel,
+  onClick,
+  randomizeOrder,
+  renderNoValueAsChip,
+}) {
+  const classes = useStyles();
+  const [showMore, setShowMore] = React.useState(false);
+
+  function handleUrlChipClick(e) {
+    e.stopPropagation();
+    setShowMore(true);
+  }
+
+  const handleClick = (e) => {
+    e.stopPropagation();
+    if (onClick) onClick();
+  };
+
+  if (!values) return (<TableCell align="left"> {noValueLabel || '-'}</TableCell>);
+  if (values.length === 0) {
+    return (
+      <TableCell onClick={handleClick} className={classes.names}>
+        <div className={renderNoValueAsChip ? [classes.chip, classes.noFactCheck].join(' ') : null}>
+          {noValueLabel}
+        </div>
+      </TableCell>
+    );
+  }
+  const labels = [];
+  let more = 0;
+  if (randomizeOrder) {
+    values.sort(() => Math.random() - 0.5);
+  }
+  values.forEach((name, i) => {
+    if (showMore || i < 2) {
+      labels.push(<div key={name}>{name}</div>);
+    } else {
+      more += 1;
+    }
+  });
+  if (!showMore && more > 0) {
+    labels.push(
+      <Chip
+        size="small"
+        className={[classes.chip, classes.more].join(' ')}
+        onClick={handleUrlChipClick}
+        label={
+          <FormattedMessage
+            id="valueListCell.more"
+            defaultMessage="+{number} more"
+            values={{ number: more }}
+            description="Button to maximize list display (tags, urls, checked options) with all values within a table cell"
+          />
+        }
+      />,
+    );
+  }
+  return <TableCell onClick={handleClick} className={classes.names}>{labels}</TableCell>;
+}
+
+ValueListCell.defaultProps = {
+  renderNoValueAsChip: false,
+};
+ValueListCell.propTypes = {
+  values: PropTypes.arrayOf(PropTypes.node.isRequired).isRequired,
+  noValueLabel: PropTypes.object.isRequired,
+  renderNoValueAsChip: PropTypes.bool,
+};
