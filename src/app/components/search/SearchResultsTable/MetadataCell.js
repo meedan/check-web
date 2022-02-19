4a97fc15ec61d9d7af44c040db00e6dc6c1e4925
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2022-02-19 00:55:41 -0300

    CHECK-1120: Refactor and reuse ValueListCell component (#1026)

--- src/app/components/search/SearchResultsTable/MetadataCell.js
+++ src/app/components/search/SearchResultsTable/MetadataCell.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TableCell from '@material-ui/core/TableCell';
-import Chip from '@material-ui/core/Chip';
 import { makeStyles } from '@material-ui/core/styles';
+import ValueListCell from './ValueListCell';
 
 function truncate(str, length) {
   const dots = str.length > length ? '...' : '';
   return `${str.substring(0, length)}${dots}`;
 }
 
-function formatMultipleChoice(value) {
-  const choices = value.split(', ');
-  const newValue = choices.slice(0, 3);
-  if (choices.length > 3) {
-    newValue[2] = `${newValue[2]} + ${choices.length - 3}`;
-  }
-  return newValue;
-}
-
 const useStyles = makeStyles({
-  multiChoice: {
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-  },
   number: {
     textAlign: 'right',
   },
@@ -42,12 +28,6 @@ export default function MetadataCell({ projectMedia, field, type }) {
   const classes = useStyles();
   let value = projectMedia.list_columns_values[field];
   let urls;
-  const [showMore, setShowMore] = React.useState(false);
-
-  function handleUrlChipClick(e) {
-    e.stopPropagation();
-    setShowMore(true);
-  }
 
   if (value) {
     switch (type) {
@@ -61,75 +41,36 @@ export default function MetadataCell({ projectMedia, field, type }) {
       value = <b>{value}</b>;
       break;
     case 'multiple_choice':
-      value = formatMultipleChoice(value).map(v => <div className={classes.multiChoice}>{v}</div>);
+      value = value.split(',');
       break;
     case 'datetime':
       value = value.replace('at 00:00 notime', '').replace(/\(.*\)/, '');
       break;
     case 'url':
-      urls = JSON.parse(value);
-      if (urls.length < 4) {
-        value = (
-          <ul>
-            { urls.map(item => (
-              <li key={item.url}>
-                <a
-                  className={classes.link}
-                  href={item.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  onClick={e => e.stopPropagation()}
-                >
-                  {truncate(item.title || item.url, 22)}
-                </a>
-              </li>
-            ))}
-          </ul>
-        );
-      } else {
-        value = (
-          <ul>
-            { urls.slice(0, 2).map(item => (
-              <li key={item.url}>
-                <a
-                  className={classes.link}
-                  href={item.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  onClick={e => e.stopPropagation()}
-                >
-                  {truncate(item.title || item.url, 25)}
-                </a>
-              </li>
-            ))}
-            {
-              showMore ? urls.slice(2).map(item => (
-                <li key={item.url}>
-                  <a
-                    className={classes.link}
-                    href={item.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    onClick={e => e.stopPropagation()}
-                  >
-                    {truncate(item.title || item.url, 25)}
-                  </a>
-                </li>
-              )) : (
-                <Chip
-                  className={classes.urlChip}
-                  label={`+${urls.length - 2} more`}
-                  size="small"
-                  onClick={handleUrlChipClick}
-                />
-              )}
-          </ul>
-        );
-      }
+      urls = JSON.parse(value).map(item => (
+        <a
+          className={classes.link}
+          href={item.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={e => e.stopPropagation()}
+        >
+          {truncate(item.title || item.url, 22)}
+        </a>
+      ));
       break;
     default:
       break;
     }
+  }
+
+  if (type === 'url' || type === 'multiple_choice') {
+    return (
+      <ValueListCell
+        values={urls || value}
+        noValueLabel="-"
+      />
+    );
   }
 
   return (
