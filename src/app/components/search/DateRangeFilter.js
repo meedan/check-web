meedan/check-web

commit 7ce75c8928703115257a4c2939648bb192d8b4a6

CHECK-1302: Putting back in the first remove button

src/app/components/search/DateRangeFilter.js
@@ -11,6 +11,7 @@ import Select from '@material-ui/core/Select';
 import { withStyles } from '@material-ui/core/styles';
 import DateRangeIcon from '@material-ui/icons/DateRange';
 import CloseIcon from '@material-ui/icons/Close';
+import RemoveableWrapper from './RemoveableWrapper';
 import { FlexRow, units, opaqueBlack07, checkBlue } from '../../styles/js/shared';
 import globalStrings from '../../globalStrings';
 
@@ -201,6 +202,7 @@ const DateRangeFilter = ({
   hide,
   value,
   onChange,
+  onRemove,
 }) => {
   const getValueType = () => {
     if (value && value.updated_at) return 'updated_at';
@@ -367,11 +369,7 @@ const DateRangeFilter = ({
             onChange={handleChangeType}
             value={getValueType()}
             input={
-              <StyledInputBaseDropdown
-                startAdornment={
-                  <DateRangeIcon />
-                }
-              />
+              <RemoveableWrapper icon={<DateRangeIcon />} onRemove={onRemove} boxProps={{ pr: 1 }} />
             }
           >
             <MenuItem value="created_at"> { label.created_at } </MenuItem>
@@ -405,6 +403,7 @@ const DateRangeFilter = ({
               handleChangeRelativeQuantity,
               handleChangeRelativeRange,
               label,
+              onRemove,
               relativeQuantity,
               relativeRange,
             }}
@@ -479,6 +478,7 @@ DateRangeFilter.propTypes = {
     }),
   ]),
   onChange: PropTypes.func.isRequired,
+  onRemove: PropTypes.func.isRequired,
 };
 
 export default withStyles(Styles)(DateRangeFilter);

src/app/components/search/SearchFields/index.js
@@ -357,6 +357,7 @@ class SearchFields extends React.Component {
           <DateRangeFilter
             onChange={this.handleDateChange}
             value={this.props.query.range}
+            onRemove={() => this.handleRemoveField('range')}
           />
         </Box>
       ),

commit 738b020e4b4f2400186e9b8c9a2786519631d150

Hotfix for missing field in relative date range
Fixing regression introduced in 7ce75c8928703115257a4c2939648bb192d8b4a6.

src/app/components/search/DateRangeFilter.js
@@ -369,7 +369,11 @@ const DateRangeFilter = ({
             onChange={handleChangeType}
             value={getValueType()}
             input={
-              <RemoveableWrapper icon={<DateRangeIcon />} onRemove={onRemove} boxProps={{ pr: 1 }} />
+              <StyledInputBaseDropdown
+                startAdornment={
+                  <RemoveableWrapper icon={<DateRangeIcon />} onRemove={onRemove} boxProps={{ pr: 1 }} />
+                }
+              />
             }
           >
             <MenuItem value="created_at"> { label.created_at } </MenuItem>