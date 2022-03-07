6cb32bb22968e8f588719bba3cdf8e4dffc1ca66
danielevalverde <danierzevalverde@gmail.com>
2022-03-07 17:16:07 -0300

    do not wait for alegre being up on integration tests build

--- build.sh
+++ build.sh
@@ -5,5 +5,9 @@ docker-compose build web api api-background pender pender-background
 docker-compose -f docker-compose.yml -f docker-test.yml up -d web api api-background pender pender-background chromedriver
 else
 docker-compose build
-docker-compose -f docker-compose.yml -f docker-test.yml up
+docker-compose -f docker-compose.yml -f docker-test.yml up -d
+until curl --silent -I -f --fail http://localhost:5000; do printf .; sleep 1; done
 fi
+
+until curl --silent -I -f --fail http://localhost:3200; do printf .; sleep 1; done
+until curl --silent -I -f --fail http://localhost:3000; do printf .; sleep 1; done
