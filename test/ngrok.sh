f4cba9609b4e2da8bb40ac8ed4f9ffe65f5493fb
danielevalverde <danierzevalverde@gmail.com>
2022-03-07 20:02:22 -0300

    Ticket CHECK-1417: wait until ngrok tunnel is available

--- test/ngrok.sh
+++ test/ngrok.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+until curl --silent -I -f --fail http://localhost:4040; do printf .; sleep 1; done
 curl localhost:4040/api/tunnels > ngrok.json
 export NGROK_URL=$(grep -Po '"public_url": *\K"[^"]*"' ngrok.json | tail -n1 | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/')
 sed -i '/storage_public_endpoint/ i \  \storage_asset_host:\ '"'$NGROK_URL/check-api-test'"'' check-api/config/config.yml
