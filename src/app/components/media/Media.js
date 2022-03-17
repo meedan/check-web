7ea50376aa5ca262218d13471988502884d17eba
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2022-03-17 17:30:15 -0300

    CHECK-1609: Remove unused field

--- src/app/components/media/Media.js
+++ src/app/components/media/Media.js
@@ -33,7 +33,6 @@ const MediaContainer = Relay.createContainer(MediaComponent, {
         user_id
         channel
         suggested_main_item # used by MediaClaim, MediaFactCheck
-        confirmed_main_item # used by MediaClaim, MediaFactCheck
         is_confirmed_similar_to_another_item
         is_secondary
         claim_description {
