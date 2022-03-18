a1d703206aad1af4dca3b14664c78785f0e24dea
danielevalverde <danierzevalverde@gmail.com>
2022-03-18 15:13:47 -0300

    debug

--- build.sh
+++ build.sh
@@ -6,10 +6,14 @@ then
 else
   ./ngrok http 9000 >/dev/null &
   until curl --silent -I -f --fail http://localhost:4040; do printf .; sleep 1; done
+  curl -I -v http://localhost:4040
   curl localhost:4040/api/tunnels > ngrok.json
+  cat ngrok.json
   export NGROK_URL=$(grep -Po '"public_url": *\K"[^"]*"' ngrok.json | tail -n1 | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/')
   sed -i '/storage_public_endpoint/ i \  \storage_asset_host:\ '"'$NGROK_URL/check-api-test'"'' check-api/config/config.yml
   sed -i '/storage_public_endpoint/ i \  \storage_endpoint:\ '"'$NGROK_URL'"'' check-api/config/config.yml
+  echo "NGROK_URL"
+  echo $NGROK_URL
   docker-compose build
   docker-compose -f docker-compose.yml -f docker-test.yml up -d
   docker-compose logs -t -f &
