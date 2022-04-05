7db72800298efefda63d4978311d607f281a1df5
danielevalverde <danierzevalverde@gmail.com>
2022-04-05 10:10:22 -0300

    add parallel files

---
+++ test/parallel-integration-test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo 'Starting tests...'
+
+FAIL=0
+
+bundle exec rspec --tag bin6 spec/integration_spec.rb &
+bundle exec rspec --tag bin1 spec/integration_spec.rb &
+bundle exec rspec --tag bin2 spec/integration_spec.rb &
+bundle exec rspec --tag bin5 spec/integration_spec.rb &
+bundle exec rspec --tag bin3 spec/integration_spec.rb &
+bundle exec rspec --tag bin4 spec/integration_spec.rb &
+
+for job in `jobs -p`
+do
+  echo $job
+  wait $job || let "FAIL+=1"
+done
+
+echo $FAIL
+
+if [ "$FAIL" == "0" ];
+then
+  exit 0
+else
+  exit 1
+fi
