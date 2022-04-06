8ebf8e419c812d03d31ed8e658277a4d45de54c2
Caio Almeida <caiosba@gmail.com>
2022-04-06 19:36:36 -0300

    Ticket CHECK-1675: New UI for WhatsApp tipline settings, with QR code display, download, entry point and embed code

--- src/app/components/team/SmoochBot/SmoochBotIntegrations.js
+++ src/app/components/team/SmoochBot/SmoochBotIntegrations.js
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
+import Tooltip from '@material-ui/core/Tooltip';
 import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import WhatsAppIcon from '@material-ui/icons/WhatsApp';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import TelegramIcon from '@material-ui/icons/Telegram';
-import QRCode from 'qrcode.react';
+import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import { QRCodeCanvas } from 'qrcode.react';
 import ViberIcon from '../../../icons/ViberIcon';
 import LineIcon from '../../../icons/LineIcon';
 import SettingsHeader from '../SettingsHeader';
@@ -27,10 +32,20 @@ const useStyles = makeStyles(() => ({
 
 const SmoochBotIntegrations = ({ settings, enabledIntegrations, installationId }) => {
   const classes = useStyles();
+  const [copied, setCopied] = React.useState(null);
 
   const isSmoochSet = settings.smooch_app_id && settings.smooch_secret_key_key_id && settings.smooch_secret_key_secret && settings.smooch_webhook_secret;
 
   const isOnline = name => Object.keys(enabledIntegrations).indexOf(name) > -1;
+
+  const handleDownloadWhatsAppQrCode = () => {
+    const canvas = document.getElementById('whatsapp-qr-code-canvas');
+    const image = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream'); // The "replace" avoids a DOM 18 exception
+    const link = document.createElement('a');
+    link.download = 'whatsapp-qr-code.png';
+    link.href = image;
+    link.click();
+  };
 
   return (
     <React.Fragment>
@@ -71,21 +86,121 @@ const SmoochBotIntegrations = ({ settings, enabledIntegrations, installationId }
                 <Box mt={2} mb={1}>
                   <strong>
                     <FormattedMessage
+                      id="smoochBotIntegrations.entryPointTitle"
+                      defaultMessage="Entry point"
+                      description="Title displayed on WhatsApp tipline settings window, regarding the entry point for WhatsApp"
+                    />
+                  </strong>
+                  <Box mt={1}>
+                    <FormattedMessage
+                      id="smoochBotIntegrations.entryPointDescription"
+                      defaultMessage="Use this address anywhere online to open WhatsApp and start a tipline conversation:"
+                      description="Description displayed on WhatsApp tipline settings window, regarding the entry point for WhatsApp"
+                    />
+                  </Box>
+                  <Box display="flex" alignItems="center">
+                    <TextField
+                      variant="outlined"
+                      margin="normal"
+                      defaultValue={`https://wa.me/${enabledIntegrations.whatsapp.phoneNumber.replace(/[^0-9]/g, '')}`}
+                      InputProps={{
+                        readOnly: true,
+                      }}
+                      fullWidth
+                    />
+                    <Tooltip
+                      PopperProps={{
+                        disablePortal: true,
+                      }}
+                      open={copied === 'entrypoint'}
+                      disableFocusListener
+                      disableHoverListener
+                      disableTouchListener
+                      title={
+                        <FormattedMessage
+                          id="smoochBotIntegrations.copied"
+                          defaultMessage="Copied"
+                          description="Tooltip displayed on tipline settings when a code is copied to clipboard"
+                        />
+                      }
+                    >
+                      <CopyToClipboard
+                        text={`https://wa.me/${enabledIntegrations.whatsapp.phoneNumber.replace(/[^0-9]/g, '')}`}
+                        onCopy={() => {
+                          setCopied('entrypoint');
+                          setTimeout(() => { setCopied(null); }, 1000);
+                        }}
+                      >
+                        <IconButton>
+                          <FileCopyOutlinedIcon />
+                        </IconButton>
+                      </CopyToClipboard>
+                    </Tooltip>
+                  </Box>
+                </Box>
+                <Box mt={2} mb={1}>
+                  <strong>
+                    <FormattedMessage
                       id="smoochBotIntegrations.qrCodeTitle"
-                      defaultMessage="Quickstart QR code"
+                      defaultMessage="QR code"
                       description="Title displayed on WhatsApp tipline settings window, regarding the QR code for WhatsApp"
                     />
                   </strong>
-                </Box>
-                <Box display="flex" justifyContent="space-between">
-                  <Box flex="1">
+                  <Box mt={1} mb={1}>
                     <FormattedMessage
                       id="smoochBotIntegrations.qrCodeDescription"
-                      defaultMessage="Scan this QR code to quickly try out your WhatsApp integration and to share and promote it."
+                      defaultMessage="Use this code or download the image to display the QR code on online or offline promotion. Scanning the QR code opens WhatsApp and starts a tipline conversation."
                       description="Description displayed on WhatsApp tipline settings window, regarding the QR code for WhatsApp"
                     />
                   </Box>
-                  <QRCode value={`https://wa.me/${enabledIntegrations.whatsapp.phoneNumber.replace(/[^0-9]/g, '')}`} renderAs="canvas" />
+                  <Box display="flex" alignItems="flex-start" justifyContent="space-between">
+                    <Box display="flex" alignItems="flex-start">
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        defaultValue={`<img src="https://chart.googleapis.com/chart?chs=150x150&amp;cht=qr&amp;chl=https://wa.me/${enabledIntegrations.whatsapp.phoneNumber.replace(/[^0-9]/g, '')}" />`}
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        multiline
+                        rows={4}
+                      />
+                      <Tooltip
+                        PopperProps={{
+                          disablePortal: true,
+                        }}
+                        open={copied === 'embedcode'}
+                        disableFocusListener
+                        disableHoverListener
+                        disableTouchListener
+                        title={
+                          <FormattedMessage
+                            id="smoochBotIntegrations.copied"
+                            defaultMessage="Copied"
+                            description="Tooltip displayed on tipline settings when a code is copied to clipboard"
+                          />
+                        }
+                      >
+                        <CopyToClipboard
+                          text={`<img src="https://chart.googleapis.com/chart?chs=150x150&amp;cht=qr&amp;chl=https://wa.me/${enabledIntegrations.whatsapp.phoneNumber.replace(/[^0-9]/g, '')}" />`}
+                          onCopy={() => {
+                            setCopied('embedcode');
+                            setTimeout(() => { setCopied(null); }, 1000);
+                          }}
+                        >
+                          <IconButton>
+                            <FileCopyOutlinedIcon />
+                          </IconButton>
+                        </CopyToClipboard>
+                      </Tooltip>
+                    </Box>
+                    <Box ml={4} display="flex" alignItems="flex-start">
+                      <QRCodeCanvas value={`https://wa.me/${enabledIntegrations.whatsapp.phoneNumber.replace(/[^0-9]/g, '')}`} id="whatsapp-qr-code-canvas" />
+                      <IconButton onClick={handleDownloadWhatsAppQrCode}>
+                        <GetAppIcon />
+                      </IconButton>
+                    </Box>
+                  </Box>
                 </Box>
               </Box> : null
           }
