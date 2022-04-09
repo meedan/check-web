aa2f8bac4d3010360635ca10f9630603392aab0e
Daniele Valverde <34126648+danielevalverde@users.noreply.github.com>
2022-04-08 22:07:23 -0300

    Ticket CHECK-1417: add audio and video tests and parallel new tests (#1099)
    * Ticket CHECK-1417: add audio and video tests and parallel new tests

---
+++ test/parallel-similarity-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo 'Starting tests...'
+
+FAIL=0
+
+bundle exec rspec --tag bin7 spec/integration_spec.rb &
+bundle exec rspec --tag bin9 spec/integration_spec.rb &
+bundle exec rspec --tag bin8 spec/integration_spec.rb &
+
+for job in `jobs -p`
+do
+  echo $job
+  wait $job || let "FAIL+=1"
+done
+
+echo $FAIL
+
+if [ "$FAIL" == "0" ];
+then
+  exit 0
+else
+  exit 1
+fi
