2f8e2f7639063137466551f76b5db2c1bc06dac9
Martin Peck <martin@meedan.com>
2022-04-20 20:34:22 -0700

    Use a helper script to clear cache.

---
+++ scripts/clear-cf-cache.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ "$#" -ne 1 ] ; then
+    echo "Missing required environment argument. Must be qa or live."
+    exit 0
+fi
+
+if [[ "$1" == "qa" ]]; then
+    echo "Clearing QA check-web cache at CloudFlare..."
+    curl --fail --output "/dev/null" --silent --show-error -X POST "https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}/purge_cache" \
+        -H "Authorization: Bearer ${CF_CACHE_TOKEN}" -H "Content-Type: application/json" \
+        --data '{"files":["https://qa.checkmedia.org/","https://qa.checkmedia.org/js/config.js"]}'
+elif [[ "$1" == "qa" ]]; then
+    echo "Clearing Live check-web cache at CloudFlare..."
+    curl --fail --output "/dev/null" --silent --show-error -X POST "https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}/purge_cache" \
+        -H "Authorization: Bearer ${CF_CACHE_TOKEN}" -H "Content-Type: application/json" \
+        --data '{"files":["https://checkmedia.org/","https://checkmedia.org/js/config.js"]}'
+else
+    echo "Invalid environment given. Must be qa or live."
+fi
