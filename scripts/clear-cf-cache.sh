612fdbf40a8d97beff31b536ebc29feda6e3fdf8
Martin Peck <martin@meedan.com>
2022-04-20 20:37:33 -0700

    Fix check for live argument.

--- scripts/clear-cf-cache.sh
+++ scripts/clear-cf-cache.sh
@@ -9,7 +9,7 @@ if [[ "$1" == "qa" ]]; then
     curl --fail --output "/dev/null" --silent --show-error -X POST "https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}/purge_cache" \
         -H "Authorization: Bearer ${CF_CACHE_TOKEN}" -H "Content-Type: application/json" \
         --data '{"files":["https://qa.checkmedia.org/","https://qa.checkmedia.org/js/config.js"]}'
-elif [[ "$1" == "qa" ]]; then
+elif [[ "$1" == "live" ]]; then
     echo "Clearing Live check-web cache at CloudFlare..."
     curl --fail --output "/dev/null" --silent --show-error -X POST "https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}/purge_cache" \
         -H "Authorization: Bearer ${CF_CACHE_TOKEN}" -H "Content-Type: application/json" \
