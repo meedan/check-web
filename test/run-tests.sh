6ecd033d0dc649574295a171639dd8a269369bfe
danielevalverde <danierzevalverde@gmail.com>
2022-04-26 20:07:12 -0300

    Remove dependson api from docker config files for check web and run unit tests
    Remove depends on api from docker config files  because for unit tests we only need check web container
    
    References: CHECK-1765

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -2,10 +2,14 @@
 
 if [[ $TRAVIS_JOB_NAME == 'integration-and-unit-tests' ]]
 then
-  # npm run test:unit || exit 1
-  # ./../scripts/uncovered-files
+  npm run test:unit || exit 1
+  ./../scripts/uncovered-files
   bundle install
   ./parallel-integration-test.sh
+elif [[ $TRAVIS_JOB_NAME == 'unit-tests' ]]
+then
+  npm run test:unit || exit 1
+  ./../scripts/uncovered-files
 else
   ./parallel-similarity-test.sh
 fi
