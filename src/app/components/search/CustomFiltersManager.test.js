32351f71befcd081a2d8796f2828f9152496e8c0
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2022-05-31 11:58:32 -0300

    Add unit test for CustomFiltersManager

---
+++ src/app/components/search/CustomFiltersManager.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { mountWithIntl } from '../../../../test/unit/helpers/intl-test';
+import { CustomFiltersManagerTest } from './CustomFiltersManager';
+import MultiSelectFilter from './MultiSelectFilter';
+
+describe('<CustomFiltersManager />', () => {
+  const team = {
+    team_tasks: {
+      edges: [{
+        node: {
+          dbid: 123,
+          type: 'free_text',
+        },
+      }, {
+        node: {
+          dbid: 456,
+          type: 'single_choice',
+        },
+      }],
+    },
+  };
+
+  it('should render a MultiSelectFilter for each team task', () => {
+    const query = {
+      team_tasks: [
+        { id: '123', task_type: 'free_text' },
+        { id: '456', task_type: 'single_choice' },
+      ],
+    };
+
+    const wrapper = mountWithIntl(<CustomFiltersManagerTest
+      onFilterChange={() => {}}
+      team={team}
+      query={query}
+    />);
+
+    expect(wrapper.find('#free_text-123')).toHaveLength(1);
+    expect(wrapper.find('#single_choice-456')).toHaveLength(1);
+  });
+
+  it('should not error if team task not found', () => {
+    const query = {
+      team_tasks: [
+        { id: '0', task_type: 'invalid_task' },
+      ],
+    };
+
+    const wrapper = mountWithIntl(<CustomFiltersManagerTest
+      onFilterChange={() => {}}
+      team={team}
+      query={query}
+    />);
+
+    expect(wrapper.find('#invalid_task-0')).toHaveLength(0);
+  });
+});
