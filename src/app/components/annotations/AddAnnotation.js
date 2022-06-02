b4da65d88375b5a570623d237e0625e1a9d7525a
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2022-06-02 13:47:00 -0300

    Intentionally introduce an error on QA. Will revert shortly.

--- src/app/components/annotations/AddAnnotation.js
+++ src/app/components/annotations/AddAnnotation.js
@@ -382,7 +382,7 @@ class AddAnnotation extends Component {
             id="cmd-input"
             multiline
             fullWidth
-            onKeyPress={this.handleKeyPress.bind(this)}
+            onKeyPress={this.blo.handleKeyPress.bind(this)}
             onKeyUp={this.handleKeyUp.bind(this)}
             value={this.state.cmd}
             onChange={this.handleChange.bind(this)}
