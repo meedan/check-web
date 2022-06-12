874da7fe89300f2b044229022c9a41cce33708f0
Caio Almeida <caiosba@gmail.com>
2022-06-11 22:05:29 -0300

    Adding more filters

--- src/app/components/team/TeamData/TeamDataComponent.js
+++ src/app/components/team/TeamData/TeamDataComponent.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import { FormattedMessage } from 'react-intl';
+import Box from '@material-ui/core/Box';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -12,6 +16,7 @@ import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import SettingsHeader from '../SettingsHeader';
+import LanguageSwitcher from '../../LanguageSwitcher';
 import { ContentColumn } from '../../../styles/js/shared';
 
 const useStyles = makeStyles({
@@ -20,78 +25,114 @@ const useStyles = makeStyles({
   },
 });
 
-const TeamDataComponent = ({ data }) => {
+const TeamDataComponent = ({
+  data,
+  defaultLanguage,
+  languages,
+}) => {
   const classes = useStyles();
+  const [currentLanguage, setCurrentLanguage] = React.useState(defaultLanguage);
 
-  const headers = data ? Object.keys(data[0]).filter(header => !['ID', 'Org'].includes(header)) : null;
+  const headers = data ? Object.keys(data[0]).filter(header => !['ID', 'Org', 'Language', 'Platform'].includes(header)) : null;
+
+  const platforms = [];
+  if (data) {
+    data.forEach((row) => {
+      if (platforms.indexOf(row.Platform) === -1) {
+        platforms.push(row.Platform);
+      }
+    });
+  }
+  const [currentPlatform, setCurrentPlatform] = React.useState(platforms[0]);
 
   return (
-    <ContentColumn large>
-      <SettingsHeader
-        title={
-          <FormattedMessage
-            id="teamDataComponent.title"
-            defaultMessage="Workspace data"
-            description="Header for the stored data page of the current team"
-          />
-        }
-        subtitle={
-          <FormattedMessage
-            id="teamDataComponent.subtitle"
-            defaultMessage="Get insight and analysis about workspace and tipline usage."
-          />
-        }
-        helpUrl="https://help.checkmedia.org/en/articles/4511362"
+    <Box display="flex" justifyContent="left" className="team-report-component">
+      <LanguageSwitcher
+        orientation="vertical"
+        primaryLanguage={defaultLanguage}
+        currentLanguage={currentLanguage}
+        languages={JSON.parse(languages)}
+        onChange={setCurrentLanguage}
       />
-      <Card>
-        <CardContent>
-          { data ?
-            <TableContainer className={classes.container}>
-              <Table stickyHeader>
-                <TableHead>
-                  <TableRow>
-                    {headers.map(header => <TableCell key={header}>{header}</TableCell>)}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data.map(row => (
+      <ContentColumn large>
+        <SettingsHeader
+          title={
+            <FormattedMessage
+              id="teamDataComponent.title"
+              defaultMessage="Tipline engagement data"
+              description="Header for the stored data page of the current team"
+            />
+          }
+          extra={
+            <FormControl variant="outlined">
+              <Select value={currentPlatform} onChange={(e) => { setCurrentPlatform(e.target.value); }}>
+                {platforms.map(platform => (
+                  <MenuItem value={platform} key={platform}>{platform}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          }
+          helpUrl="https://help.checkmedia.org/en/articles/4511362"
+        />
+        <Card>
+          <CardContent>
+            { data ?
+              <TableContainer className={classes.container}>
+                <Table stickyHeader>
+                  <TableHead>
                     <TableRow>
-                      {headers.map(header => <TableCell key={row.ID}>{row[header]}</TableCell>)}
+                      {headers.map(header => <TableCell key={header}>{header}</TableCell>)}
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer> :
-            <React.Fragment>
-              <Typography variant="body1" component="p" paragraph>
-                <FormattedMessage
-                  id="teamDataComponent.set1"
-                  defaultMessage="Fill {thisShortForm} to request access to your data report."
-                  values={{
-                    thisShortForm: (
-                      <a href="https://airtable.com/shrWpaztZ2SzD5TrA" target="_blank" rel="noopener noreferrer">
-                        <FormattedMessage id="teamDataComponent.formLinkText" defaultMessage="this short form" />
-                      </a>
-                    ),
-                  }}
-                />
-              </Typography>
-              <Typography variant="body1" component="p" paragraph>
-                <FormattedMessage id="teamDataComponent.set2" defaultMessage="Your data report will be enabled within one business day." />
-              </Typography>
-            </React.Fragment> }
-        </CardContent>
-      </Card>
-    </ContentColumn>
+                  </TableHead>
+                  <TableBody>
+                    {data.map((row) => {
+                      if (row.Language === currentLanguage && row.Platform === currentPlatform) {
+                        return (
+                          <TableRow>
+                            {headers.map(header => <TableCell key={`${row.ID}-${header}`}>{row[header]}</TableCell>)}
+                          </TableRow>
+                        );
+                      }
+                      return null;
+                    })}
+                  </TableBody>
+                </Table>
+              </TableContainer> :
+              <React.Fragment>
+                <Typography variant="body1" component="p" paragraph>
+                  <FormattedMessage
+                    id="teamDataComponent.set1"
+                    defaultMessage="Fill {thisShortForm} to request access to your data report."
+                    values={{
+                      thisShortForm: (
+                        <a href="https://airtable.com/shrWpaztZ2SzD5TrA" target="_blank" rel="noopener noreferrer">
+                          <FormattedMessage id="teamDataComponent.formLinkText" defaultMessage="this short form" />
+                        </a>
+                      ),
+                    }}
+                  />
+                </Typography>
+                <Typography variant="body1" component="p" paragraph>
+                  <FormattedMessage id="teamDataComponent.set2" defaultMessage="Your data report will be enabled within one business day." />
+                </Typography>
+              </React.Fragment> }
+          </CardContent>
+        </Card>
+      </ContentColumn>
+    </Box>
   );
 };
 
 TeamDataComponent.defaultProps = {
   data: null,
+  defaultLanguage: null,
+  languages: [],
 };
 
 TeamDataComponent.propTypes = {
   data: PropTypes.object, // or null
+  defaultLanguage: PropTypes.string, // or null
+  languages: PropTypes.arrayOf(PropTypes.string), // or null
 };
 
 export default TeamDataComponent;
--- src/app/components/team/TeamData/index.js
+++ src/app/components/team/TeamData/index.js
@@ -6,7 +6,14 @@ import TeamDataComponent from './TeamDataComponent';
 
 const renderQuery = ({ error, props }) => {
   if (!error && props) {
-    return <TeamDataComponent data={props.team.data_report} />;
+    const { team } = props;
+    return (
+      <TeamDataComponent
+        data={team.data_report}
+        defaultLanguage={team.get_language}
+        languages={team.get_languages}
+      />
+    );
   }
 
   // TODO: We need a better error handling in the future, standardized with other components
@@ -20,6 +27,8 @@ const TeamData = props => (
       query TeamDataQuery($teamSlug: String!) {
         team(slug: $teamSlug) {
           id
+          get_language
+          get_languages
           data_report
         }
       }
