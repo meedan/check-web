e5fb0e7131526062124cdd2576c7c9134e2f4de5
Darius Kazemi <darius@meedan.com>
2022-06-14 13:41:41 -0700

    Fix graphql query

--- src/app/components/team/TeamIntegrations.js
+++ src/app/components/team/TeamIntegrations.js
@@ -10,107 +10,99 @@ import SettingsHeader from './SettingsHeader';
 import TeamBots from './TeamBots';
 import { ContentColumn } from '../../styles/js/shared';
 
-const TeamIntegrations = () => {
-  const teamSlug = window.location.pathname.match(/^\/([^/]+)/)[1];
-
-  return (
-    <QueryRenderer
-      environment={Relay.Store}
-      query={graphql`
-        query TeamIntegrationsQuery($teamSlug: String!) {
-          root {
-            current_team {
-              id
-              dbid
-              team_bot_installations(first: 10000) {
-                edges {
-                  node {
-                    id
-                    json_settings
-                    bot_user {
-                      id
-                    }
-                    team {
-                      id
-                      dbid
-                    }
-                    team_bot: bot_user {
-                      id
-                      dbid
-                    }
-                  }
+const TeamIntegrations = () => (<QueryRenderer
+  environment={Relay.Store}
+  query={graphql`
+    query TeamIntegrationsQuery {
+      root {
+        current_team {
+          id
+          dbid
+          team_bot_installations(first: 10000) {
+            edges {
+              node {
+                id
+                json_settings
+                bot_user {
+                  id
                 }
-              }
-              ...SlackConfig_team
-            }
-            team_bots_approved(first: 10000) {
-              edges {
-                node {
+                team {
                   id
                   dbid
-                  avatar
-                  name
-                  default
-                  identifier
-                  description: get_description
-                  team_author {
-                    name
-                    slug
-                  }
-                  installation {
-                    id
-                    json_settings
-                    team {
-                      id
-                      dbid
-                    }
-                    team_bot: bot_user {
-                      id
-                      dbid
-                    }
-                  }
+                }
+                team_bot: bot_user {
+                  id
+                  dbid
+                }
+              }
+            }
+          }
+          ...SlackConfig_team
+        }
+        team_bots_approved(first: 10000) {
+          edges {
+            node {
+              id
+              dbid
+              avatar
+              name
+              default
+              identifier
+              description: get_description
+              team_author {
+                name
+                slug
+              }
+              installation {
+                id
+                json_settings
+                team {
+                  id
+                  dbid
+                }
+                team_bot: bot_user {
+                  id
+                  dbid
                 }
               }
             }
           }
         }
-      `}
-      variables={{
-        teamSlug,
-      }}
-      render={({ props }) => {
-        if (props) {
-          return (
-            <Box className="team-integrations">
-              <ContentColumn>
-                <SettingsHeader
-                  title={
-                    <FormattedMessage
-                      id="teamIntegrations.title"
-                      defaultMessage="Integrations"
-                    />
-                  }
-                  subtitle={
-                    <FormattedMessage
-                      id="teamIntegrations.subtitle"
-                      defaultMessage="Third party tools to enhance productivity."
-                    />
-                  }
-                  helpUrl="https://help.checkmedia.org/en/articles/4841437-integrations"
+      }
+    }
+  `}
+  variables={{}}
+  render={({ props }) => {
+    if (props) {
+      return (
+        <Box className="team-integrations">
+          <ContentColumn>
+            <SettingsHeader
+              title={
+                <FormattedMessage
+                  id="teamIntegrations.title"
+                  defaultMessage="Integrations"
                 />
-                <Box className="team-integrations__integrations">
-                  <TeamBots {...props} />
-                  <SlackConfig team={props.root.current_team} />
-                </Box>
-              </ContentColumn>
+              }
+              subtitle={
+                <FormattedMessage
+                  id="teamIntegrations.subtitle"
+                  defaultMessage="Third party tools to enhance productivity."
+                />
+              }
+              helpUrl="https://help.checkmedia.org/en/articles/4841437-integrations"
+            />
+            <Box className="team-integrations__integrations">
+              <TeamBots {...props} />
+              <SlackConfig team={props.root.current_team} />
             </Box>
-          );
-        }
-        return null;
-      }}
-    />
-  );
-};
+          </ContentColumn>
+        </Box>
+      );
+    }
+    return null;
+  }}
+/>);
 
 TeamIntegrations.propTypes = {
   // FIXME: Specify the required fields of "team"
