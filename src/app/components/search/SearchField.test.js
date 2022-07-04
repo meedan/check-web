530578863e60b3e9ccd5f64c541d874a14ef3dd5
danielevalverde <danierzevalverde@gmail.com>
2022-07-04 09:59:50 -0300

    Add verification before try to call onBlur function and add unit test
    Reference: CHECK-2033

---
+++ src/app/components/search/SearchField.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { mountWithIntl } from '../../../../test/unit/helpers/intl-test';
+import SearchField from './SearchField';
+
+describe('<SearchResults />', () => {
+  const node = <div />;
+
+  it('should check that onBlur is called when press "enter" on input field', () => {
+    const onBlur = jest.fn();
+    const wrapper = mountWithIntl(<SearchField
+      isActive
+      inputBaseProps={{
+        onBlur: onBlur
+      }}
+      endAdorment = {node}
+      showExpand
+      setParentSearchText={() => {}}
+    />);
+    wrapper.find('#search-input').hostNodes().simulate('click');
+    wrapper.find('#search-input').hostNodes().simulate('keypress', {key: 'Enter'})
+    expect(onBlur).toHaveBeenCalled();
+  });
+
+});
