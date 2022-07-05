832d95266d9e8255398145f36288096ac33ae6ee
Caio <caiosba@gmail.com>
2022-07-05 09:46:43 -0300

    Updating privacy policy URL and terms of service URL

--- src/app/customHelpers.js
+++ src/app/customHelpers.js
@@ -5,9 +5,9 @@ const customStrings = {
     ABOUT_URL: 'https://meedan.com/check',
     ADMIN_EMAIL: 'check@meedan.com',
     CONTACT_HUMAN_URL: 'https://meedan.typeform.com/to/Tvf0b8',
-    PP_URL: 'https://meedan.com/en/check/check_privacy.html',
+    PP_URL: 'https://meedan.com/legal/privacy-policy',
     SUPPORT_EMAIL: 'check@meedan.com',
-    TOS_URL: 'https://meedan.com/en/check/check_tos.html',
+    TOS_URL: 'https://meedan.com/legal/terms-of-service',
     LOGO_URL: '/images/logo/check.svg',
   },
 };
