1a556bbb11ad4b968fc7bbd73e996e04abdb730f
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2022-07-20 16:39:21 -0300

    Fix crash in LimitedTextFieldWithCounter when value is null

--- src/app/components/layout/LimitedTextFieldWithCounter.test.js
+++ src/app/components/layout/LimitedTextFieldWithCounter.test.js
@@ -7,4 +7,9 @@ describe('<LimitedTextFieldWithCounter />', () => {
     const wrapper = mountWithIntl(<LimitedTextFieldWithCounter limit={10} value="Test" onUpdate={() => {}} />);
     expect(wrapper.html()).toMatch('6 characters left');
   });
+
+  it('should NOT crash if value is null', () => {
+    const wrapper = mountWithIntl(<LimitedTextFieldWithCounter limit={10} value={null} onUpdate={() => {}} />);
+    expect(wrapper.html()).toMatch('10 characters left');
+  });
 });
