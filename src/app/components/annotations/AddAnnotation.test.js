a2d36971b873345a640a0103e536c4bd17acdd60
danielevalverde <danierzevalverde@gmail.com>
2022-07-20 21:35:37 -0300

    remove debug

--- src/app/components/annotations/AddAnnotation.test.js
+++ src/app/components/annotations/AddAnnotation.test.js
@@ -10,12 +10,10 @@ describe('<AddAnnotation />', () => {
   it('Should hides when media is archived (Trash)', () => {
     const addAnnotation = mountWithIntl(<AddAnnotation annotated={annotated_archived} />);
     expect(addAnnotation.html()).toEqual(null);
-    console.log(addAnnotation.debug());
   });
 
   it('Should render component when media is not archived (not in the trash)', () => {
     const addAnnotation = mountWithIntl(<AddAnnotation annotated={annotated_not_archived} />);
-    console.log(addAnnotation.debug());
     expect(addAnnotation.html()).not.toEqual('');
   });
 });
