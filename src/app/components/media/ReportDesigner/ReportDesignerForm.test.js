fc73a677a7518f7a8753adbd5c6c6acdbbe78145
Daniele Valverde <34126648+danielevalverde@users.noreply.github.com>
2022-07-25 10:30:08 -0300

    Do not add 'https' prefix in empty string in report URL field (#1196)
    and add unit tests to Report Designer Component
    
    Reference: CHECK-2143

---
+++ src/app/components/media/ReportDesigner/ReportDesignerForm.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mountWithIntl } from '../../../../../test/unit/helpers/intl-test';
+import ReportDesignerForm from './ReportDesignerForm';
+
+describe('<ReportDesignerForm />', () => {
+  const data = {
+    use_text_message: true,
+    use_introduction: false,
+    introduction: '',
+    image: '',
+    title: 'title',
+    headline: 'title',
+    text: 'sumary',
+    description: 'desc',
+    published_article_url: '',
+  };
+  const media = {
+    title: 'title',
+  };
+
+  it('should render report form', () => {
+    const wrapper = mountWithIntl(<ReportDesignerForm
+      state="published"
+      media={media}
+      data={data}
+      onUpdate={() => {}}
+      pending
+      disabled={false}
+    />);
+    expect(wrapper.find('[data-testid="report-designer__introduction"]')).toHaveLength(1);
+    expect(wrapper.find('[data-testid="report-designer__text-url"]')).toHaveLength(1);
+  });
+});
