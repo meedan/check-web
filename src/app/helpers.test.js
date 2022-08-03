42a5cdfe94fe0cc30486383a31c8975fba70ca38
Daniele Valverde <34126648+danielevalverde@users.noreply.github.com>
2022-08-03 18:33:19 -0300

    Fix/2181-2187-2076 Errbit erros (#1200)
    * Add safe navigation when trying to access properties to avoid trying to read properties of null and add unit tests
    
    Reference: CHECK-2181, CHECK-2187, CHECK-2076
    
    Co-authored-by: Alexandre Amoedo Amorim <amoedoamorim@gmail.com>

--- src/app/helpers.test.js
+++ src/app/helpers.test.js
@@ -1,17 +1,7 @@
-/* eslint-disable @calm/react-intl/missing-attribute */
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
 import { getErrorMessage } from './helpers';
-import { stringHelper } from './customHelpers';
 
 describe('Helpers', () => {
-  const fallbackMessage = (
-    <FormattedMessage
-      id="addAnnotation.error"
-      defaultMessage="Sorry, an error occurred while updating the item. Please try again and contact {supportEmail} if the condition persists."
-      values={{ supportEmail: stringHelper('SUPPORT_EMAIL') }}
-    />
-  );
+  const fallbackMessage = { defaultMessage: 'Sorry, an error occurred while updating the item. Please try again and contact {supportEmail} if the condition persists.' };
 
   it('Should return the transaction error message)', () => {
     const transaction = { source: JSON.stringify({ errors: [{ message: 'This is not a default error message' }] }) };
@@ -22,6 +12,6 @@ describe('Helpers', () => {
   it('Should return the default error message)', () => {
     const transaction = { source: 'error' };
     const message = getErrorMessage(transaction, fallbackMessage);
-    expect(message.props.defaultMessage).toMatch('Sorry, an error occurred while updating the item. Please try again');
+    expect(message.defaultMessage).toMatch('Sorry, an error occurred while updating the item. Please try again');
   });
 });
