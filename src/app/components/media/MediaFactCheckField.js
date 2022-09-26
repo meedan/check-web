0db1c48347efe648543890922a4d4359a6ff3b18
Caio Almeida <caiosba@gmail.com>
2022-09-26 06:50:04 -0300

    Fact-check limits
    No more limits for fact-check title and URL.
    
    Now only fact-check summary has a limit.
    
    Reference: CHECK-2468.

--- src/app/components/media/MediaFactCheckField.js
+++ src/app/components/media/MediaFactCheckField.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { defineMessages, injectIntl, intlShape } from 'react-intl';
 import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
 import LimitedTextFieldWithCounter from '../layout/LimitedTextFieldWithCounter';
 
 const messages = defineMessages({
@@ -31,22 +32,37 @@ const MediaFactCheckField = ({
     defaultValue = value || undefined;
   }
 
+  const textFieldProps = {
+    required,
+    rows,
+    id: `media-fact-check__${name}`,
+    className: `media-fact-check__${name}`,
+    disabled: (!hasPermission || disabled),
+    key: `media-fact-check__${name}-${hasClaimDescription ? '-with-claim' : '-no-claim'}`,
+  };
+
   return (
     <Box my={2}>
-      <LimitedTextFieldWithCounter
-        limit={limit}
-        label={label}
-        onUpdate={(newValue) => { onBlur(newValue); }}
-        rows={rows}
-        value={defaultValue}
-        textFieldProps={{
-          required,
-          id: `media-fact-check__${name}`,
-          className: `media-fact-check__${name}`,
-          disabled: (!hasPermission || disabled),
-          key: `media-fact-check__${name}-${hasClaimDescription ? '-with-claim' : '-no-claim'}`,
-        }}
-      />
+      { limit !== null ?
+        <LimitedTextFieldWithCounter
+          label={label}
+          limit={limit}
+          value={defaultValue}
+          onUpdate={(newValue) => { onBlur(newValue); }}
+          textFieldProps={textFieldProps}
+        /> :
+        <TextField
+          label={label}
+          inputProps={{ style: { maxHeight: 266, overflow: 'auto' } }}
+          onBlur={(e) => { onBlur(e.target.value.trim()); }}
+          defaultValue={value}
+          multiline
+          rowsMax={Infinity}
+          variant="outlined"
+          fullWidth
+          {...textFieldProps}
+        />
+      }
     </Box>
   );
 };
@@ -55,10 +71,11 @@ MediaFactCheckField.defaultProps = {
   disabled: false,
   rows: 3,
   required: false,
+  limit: null,
 };
 
 MediaFactCheckField.propTypes = {
-  limit: PropTypes.number.isRequired,
+  limit: PropTypes.number, // or null (no limit)
   hasClaimDescription: PropTypes.bool.isRequired,
   hasPermission: PropTypes.bool.isRequired,
   label: PropTypes.object.isRequired,
