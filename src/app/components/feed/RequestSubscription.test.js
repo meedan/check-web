55707042721e2c771b3e7586e03658f61c434a09
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2022-10-19 18:45:21 -0300

    Fix/2605 request card report sent icon (#1273)
    * Display Report Sent date info on Request Card
    
    Makes use of RequestSubscription component to display report sent icon on Request card details row;
    Adds unit tests for TiplineRequest.js and RequestSubscription.js
    
    Fixes CHECK-2605
    
    * Make eslint happy

---
+++ src/app/components/feed/RequestSubscription.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CheckIcon from '@material-ui/icons/Check';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import RequestSubscription from './RequestSubscription';
+import { mountWithIntl } from '../../../../test/unit/helpers/intl-test';
+
+describe('<RequestSubscription />', () => {
+  it('should render check mark icon and date', () => {
+    const wrapper = mountWithIntl(<RequestSubscription lastCalledAt="2022-10-19T20:53:04.500Z" />);
+    expect(wrapper.find(CheckIcon).length).toEqual(1);
+    expect(wrapper.html()).toMatch('Oct 19, 2022');
+  });
+
+  it('should render bell icon', () => {
+    const wrapper = mountWithIntl(<RequestSubscription subscribed />);
+    expect(wrapper.find(NotificationsNoneIcon).length).toEqual(1);
+  });
+});
