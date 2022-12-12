a303a052b2d2dadf20355c6bdf267c49b0fa49a4
Caio <caiosba@gmail.com>
2022-12-11 22:33:57 -0300

    Adding unit test

---
+++ src/app/components/cds/settings-pages/TiplineContentTranslation.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { mountWithIntl } from '../../../../../test/unit/helpers/intl-test';
+import TiplineContentTranslation from './TiplineContentTranslation';
+
+describe('<TiplineContentTranslation />', () => {
+  it('should render TiplineContentTranslation component', () => {
+    const wrapper = mountWithIntl(<TiplineContentTranslation
+      identifier="test"
+      title={<span>Foo</span>}
+      description={<span>Bar</span>}
+      defaultValue="Test"
+      onUpdate={(newValue) => {}}
+    />);
+    expect(wrapper.find('textarea').hostNodes()).toHaveLength(2);
+  });
+});
