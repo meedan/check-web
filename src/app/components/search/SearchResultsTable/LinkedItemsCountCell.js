fbf6d210f4cf8ce8b4677bd2d43e211f9fda5f05
Daniele Valverde <34126648+danielevalverde@users.noreply.github.com>
2023-02-02 13:28:37 -0300

    Replace similarity strings on filter and update the count for media on item page and on list page (#1338)
    Reference: CV2-2540

--- src/app/components/search/SearchResultsTable/LinkedItemsCountCell.js
+++ src/app/components/search/SearchResultsTable/LinkedItemsCountCell.js
@@ -5,7 +5,8 @@ import { getPathnameAndSearch } from '../../../urlHelpers';
 
 export default function LinkedItemsCountCell({ projectMedia, projectMediaUrl }) {
   const { pathname, search } = getPathnameAndSearch(projectMediaUrl);
-  const linkedItemsCount = projectMedia.list_columns_values.linked_items_count;
+  // we need to count the "main" media itself on the list view, so we need to add 1 in the count.
+  const linkedItemsCount = projectMedia.list_columns_values.linked_items_count + 1;
   const linkedItemsUrl = `${pathname}/similar-media${search}`;
   return <NumberCell value={linkedItemsCount} linkTo={linkedItemsCount ? linkedItemsUrl : null} />;
 }
