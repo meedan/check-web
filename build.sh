951b83c263d2c4fb9bf536b5cf9a80831f9cdc00
Caio <117518+caiosba@users.noreply.github.com>
2023-02-09 14:32:09 -0300

    Replace Ngrok by Localtunnel

--- build.sh
+++ build.sh
@@ -1,43 +1,28 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH != 'develop' && $TRAVIS_BRANCH != 'master' ]]
-then
-  docker-compose build web
-  docker-compose -f docker-compose.yml -f docker-test.yml up -d web
-  until curl --silent -I -f --fail http://localhost:3333; do printf .; sleep 1; done
-else
+# if [[ $TRAVIS_BRANCH != 'develop' && $TRAVIS_BRANCH != 'master' ]]
+# then
+#   docker-compose build web
+#   docker-compose -f docker-compose.yml -f docker-test.yml up -d web
+#   until curl --silent -I -f --fail http://localhost:3333; do printf .; sleep 1; done
+# else
   if [[ $TRAVIS_JOB_NAME == 'integration-and-unit-tests' ]]
   then
     docker-compose build web api api-background pender pender-background
     docker-compose -f docker-compose.yml -f docker-test.yml up -d web api api-background pender pender-background chromedriver
   else
-    i=0
-    NGROK_URL=""
-    while [ -z "$NGROK_URL" -a $i -lt 5 ]; do
-      i=$(($i + 1))
-      ./ngrok http 9000 >/dev/null &
-      until curl --silent -I -f --fail http://localhost:4040; do printf .; sleep 1; done
-      curl -I -v http://localhost:4040
-      curl localhost:4040/api/tunnels > ngrok.json
-      cat ngrok.json
-      NGROK_URL=$(grep -Po '"public_url": *\K"[^"]*"' ngrok.json | tail -n1 | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/')
-      if [ -z $NGROK_URL ]
-      then
-        kill -9 $(pgrep ngrok)
-      fi
-      sleep 5
-    done
-    if [ -z $NGROK_URL ]
-    then
-      echo "Not able to connect a Ngrok Tunnel. Please try again"
-      exit 1
-    fi
-    sed -i '/storage_public_endpoint/ i \  \storage_asset_host:\ '"'$NGROK_URL/check-api-test'"'' check-api/config/config.yml
-    sed -i '/storage_public_endpoint/ i \  \storage_endpoint:\ '"'$NGROK_URL'"'' check-api/config/config.yml
+    echo 'Installing localtunnel...'
+    sudo npm install -g localtunnel
+    rand=$(echo $RANDOM | md5sum | head -c 8)
+    subdomain="minio$rand"
+    lt --port 9000 --subdomain $subdomain &
+    tunnel="https:\/\/$subdomain.loca.lt"
+    sleep 10
+    sed -i "s/https\?:\/\/\(localhost\|minio\):9000/$tunnel/g" check-api/config/config.yml
     docker-compose build
     docker-compose -f docker-compose.yml -f docker-test.yml up -d
     until curl --silent -I -f --fail http://localhost:3100; do printf .; sleep 1; done
   fi
   until curl --silent -I -f --fail http://localhost:3200; do printf .; sleep 1; done
   until curl --silent -I -f --fail http://localhost:3000; do printf .; sleep 1; done
-fi
+# fi
