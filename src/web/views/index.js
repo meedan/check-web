9d591919864fbba44c727a3bba549044f644af10
Brian Fleming <brianfleming@gmail.com>
2023-03-22 15:06:56 -0400

    avoid loading analytics or uptime locally and remove unused font weight call

--- src/web/views/index.js
+++ src/web/views/index.js
@@ -1,3 +1,5 @@
+const NODE_ENV = process.env.NODE_ENV || 'production';
+
 const capitalize = (str) => {
   if (!str) {
     return '';
@@ -34,7 +36,7 @@ const facebookTags = (metadata, config, url) => {
 };
 
 const heapAnalytics = (config) => {
-  return config.heapAppId ?
+  return config.heapAppId && NODE_ENV === 'production' ?
     `<script type="text/javascript">
         window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
           heap.load("${config.heapAppId}");
@@ -42,7 +44,7 @@ const heapAnalytics = (config) => {
 };
 
 const uptimeMonitoring = (config) => {
-  return config.uptimeId ?
+  return config.uptimeId && NODE_ENV === 'production' ?
   `<script>(function(w,d,s){w._uptime_rum2={};w._uptime_rum2.errors=[];w._uptime_rum2.uuid='${config.uptimeId}';w._uptime_rum2.url='https://rumcollector.uptime.com';s=document.createElement('script');s.async=1;s.src='https://rum.uptime.com/static/rum/compiled/v2/rum.js';d.getElementsByTagName('head')[0].appendChild(s);w.addEventListener('error',function(e){w._uptime_rum2.errors.push({t:new Date(),err:e})});})(window,document);</script>`
     : '';
 };
@@ -82,7 +84,7 @@ module.exports = ({ config, metadata, url }) => {
           <script src="/js/config.js" defer="defer"></script>
           <script src="/js/vendor.bundle${BUNDLE_PREFIX}.js" defer="defer"></script>
           <script src="https://js.pusher.com/4.4/pusher.min.js"></script>
-          <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Mono" rel="stylesheet" type="text/css">
+          <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500|Roboto+Mono" rel="stylesheet" type="text/css">
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/leaflet.css" />
           ${heapAnalytics(config)}
         </head>
