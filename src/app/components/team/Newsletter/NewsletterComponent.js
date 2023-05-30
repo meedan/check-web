bf2309dfdd5b95c45d84324f3c132fa381ea5624
Darius Kazemi <darius@meedan.com>
2023-05-30 12:09:26 +0100

    Disable newsletter scheduler for past datetimes
    For static type newsletters we can't let users schedule in the past. This code takes the date, time, and time zone of the scheduler and calculates the unix epoch for that exact moment. Then it compares that epoch time to the current, in browser epoch time. If the scheduled time is in the past then we disable saving.

--- src/app/components/team/Newsletter/NewsletterComponent.js
+++ src/app/components/team/Newsletter/NewsletterComponent.js
@@ -74,6 +74,7 @@ const NewsletterComponent = ({
   const [timezone, setTimezone] = React.useState(send_timezone || '');
   const [time, setTime] = React.useState(send_time || '09:00');
   const [scheduled, setScheduled] = React.useState(enabled || false);
+  const [datetimeIsPast, setDatetimeIsPast] = React.useState(false);
 
   const numberOfArticles = (contentType === 'rss' && articleNum === 0) ? 1 : articleNum;
 
@@ -83,6 +84,22 @@ const NewsletterComponent = ({
       setFileName(file.name);
     }
   }, [file]);
+
+  // This triggers when time or scheduled date changes. if it's a static newsletter, then we check to see if the date is in the past and set the datetimeIsPast to enable or disable scheduling.
+  React.useEffect(() => {
+    if (contentType === 'static') {
+      // We have to do `new Date` twice here -- the `Date.parse` gives us a date object with no timezone associated. We wrap that in `new Date` to turn it from a string to a Date object. That object then uses `toLocaleString` to localize it to a string with the correct time derived from our `timezone` which is either `'Region/Zone'` or `'Region/Zone (GMT+xx:xx)'`, which we extract via regex. This gives us a second string, which we then convert back to Date object so we can compare it to the current unix epoch time.
+      const scheduledDateTime = new Date(new Date(Date.parse(`${sendOn} ${time}`)).toLocaleString(undefined, { timeZone: timezone && timezone.match(/^\w*\/\w*/) && timezone.match(/^\w*\/\w*/)[1] }));
+      const currentDateTime = new Date();
+      if (scheduledDateTime < currentDateTime) {
+        setDatetimeIsPast(true);
+      } else {
+        setDatetimeIsPast(false);
+      }
+    } else {
+      setDatetimeIsPast(false);
+    }
+  }, [sendOn, time, timezone]);
 
   // This triggers when the sticky scheduler is fully visible on the screen
   const [containerRef, isVisible] = useElementOnScreen({
@@ -376,7 +393,7 @@ const NewsletterComponent = ({
         }
         actionButton={
           <div>
-            <Button className="save-button" variant="contained" color="primary" onClick={handleSave} disabled={scheduled || saving || !can(team.permissions, 'create TiplineNewsletter')}>
+            <Button className="save-button" variant="contained" color="primary" onClick={handleSave} disabled={scheduled || saving || datetimeIsPast || !can(team.permissions, 'create TiplineNewsletter')}>
               <FormattedMessage id="newsletterComponent.save" defaultMessage="Save" description="Label for a button to save settings for the newsletter" />
             </Button>
           </div>
@@ -495,7 +512,7 @@ const NewsletterComponent = ({
               time={time}
               parentErrors={errors}
               scheduled={scheduled}
-              disabled={saving}
+              disabled={saving || datetimeIsPast}
               subscribersCount={subscribers_count}
               lastSentAt={last_sent_at}
               lastScheduledAt={last_scheduled_at}
