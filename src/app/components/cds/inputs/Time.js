08c133652aaf987d3f96a5b5c3cffcd5f3ca3460
Brian Fleming <brianfleming@gmail.com>
2023-06-13 10:23:56 -0400

    add missing error message and help content reference to Time component

--- src/app/components/cds/inputs/Time.js
+++ src/app/components/cds/inputs/Time.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ErrorIcon from '../../../icons/error.svg';
 import inputStyles from '../../../styles/css/inputs.module.css';
 import styles from './Time.module.css';
 
 const Time = ({
   className,
   variant,
+  error,
+  helpContent,
   ...inputProps
 }) => (
   <div className={className}>
@@ -16,6 +19,12 @@ const Time = ({
         {...inputProps}
       />
     </div>
+    { helpContent && (
+      <div className={`${inputStyles['help-container']} ${error && inputStyles['error-label']}`}>
+        { error && <ErrorIcon className={inputStyles['error-icon']} />}
+        {helpContent}
+      </div>
+    )}
   </div>
 );
 
