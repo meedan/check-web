19cf17321c80ea56aa13c4c7f9ebc725c8d380a2
Scott Hale <computermacgyver@users.noreply.github.com>
2023-06-16 08:06:01 -0600

    CV2-3062: Add settings for Tagger bot (#1425)
    The autotagger bot looks at the k nearest neighbours by vector similarity and applies the most common tag among those neighbors.

---
+++ src/app/components/team/TaggerBot.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { injectIntl } from 'react-intl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+
+const TaggerBot = ({
+  intl,
+  onChange,
+  value,
+}) => {
+  function handleChange(field, val) {
+    const data = { ...value };
+    if (field === 'ignore_autotags') {
+      data[field] = !data[field];
+    } else {
+      data[field] = parseInt(val, 10);
+    }
+    onChange(data);
+  }
+
+  return (
+    <>
+      <FormGroup>
+        <FormControlLabel
+          control={<TextField
+            value={value.threshold}
+            name="threshold"
+            onChange={e => handleChange('threshold', e.target.value)}
+            type="number"
+          />}
+          label={intl.formatMessage({
+            id: 'taggerBot.threshold',
+            defaultMessage: 'Threshold (0-100, default of 70)',
+            description: 'Search similarity threshold (0-100)',
+          })}
+        />
+        <FormControlLabel
+          control={<TextField
+            value={value.minimum_count}
+            name="minimum_count"
+            onChange={e => handleChange('minimum_count', e.target.value)}
+            type="number"
+          />}
+          label={intl.formatMessage({
+            id: 'taggerBot.minimum_count',
+            defaultMessage: 'Minimum count',
+            description: 'Minimum number of times a tag must appear to be applied',
+          })}
+        />
+        <FormControlLabel
+          control={<Checkbox
+            checked={value.ignore_autotags}
+            onChange={e => handleChange('ignore_autotags', e.target.value)}
+            name="ignore_autotags"
+          />}
+          label={intl.formatMessage({
+            id: 'taggerBot.ignore_autotags',
+            defaultMessage: 'Ignore auto-tags?',
+            description: 'If enabled, autotags will not be considered in finding the most common tag',
+          })}
+          minimum_count
+        />
+      </FormGroup>
+    </>
+  );
+};
+
+export default injectIntl(TaggerBot);
