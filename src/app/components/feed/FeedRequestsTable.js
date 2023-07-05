38358fd2b893d404a9ddfd47496ccf581c81ecfe
Alexandre Amorim <amoedoamorim@gmail.com>
2023-07-05 16:42:26 -0300

    Errbit: Prevent crashing on feed requests tab

--- src/app/components/feed/FeedRequestsTable.js
+++ src/app/components/feed/FeedRequestsTable.js
@@ -190,7 +190,7 @@ const FeedRequestsTable = ({
             />
           </Box>
         </Box>
-        { tabs({}) }
+        { typeof tabs === 'function' && tabs({}) }
       </Box>
       <Box>
         <FeedFilters onSubmit={onChangeFilters} currentFilters={filters} feedTeam={feedTeam} />
@@ -477,12 +477,13 @@ const FeedRequestsTableQuery = ({
 
 FeedRequestsTableQuery.defaultProps = {
   filters: {},
+  tabs: () => {},
 };
 
 FeedRequestsTableQuery.propTypes = {
   teamSlug: PropTypes.string.isRequired,
   feedId: PropTypes.number.isRequired,
-  tabs: PropTypes.func.isRequired,
+  tabs: PropTypes.func,
   searchUrlPrefix: PropTypes.string.isRequired,
   filters: PropTypes.object,
   feedTeam: PropTypes.shape({
