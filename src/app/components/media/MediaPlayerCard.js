74f41b346fb2b3b4794c64adff2f589a5e643a86
danielevalverde <danierzevalverde@gmail.com>
2023-08-25 16:05:53 -0300

    Add Alert with error message and button to dowload video if video cannot be played
    Reference: CV2-3486

--- src/app/components/media/MediaPlayerCard.js
+++ src/app/components/media/MediaPlayerCard.js
@@ -1,9 +1,11 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
 import { makeStyles } from '@material-ui/core/styles';
 import AspectRatio from '../layout/AspectRatio';
 import MediaControls from '../cds/media-cards/MediaControls.js';
+import Alert from '../cds/alerts-and-prompts/Alert';
 
 const useStyles = makeStyles(() => ({
   video: {
@@ -31,9 +33,46 @@ const MediaPlayerCard = ({
 }) => {
   const classes = useStyles();
   const videoRef = React.useRef();
+  const [errorAlert, setErrorAlert] = React.useState(false);
+
+  const handleVideoError = () => {
+    setErrorAlert(true);
+  };
+
+  const handleDownloadButtonClick = () => {
+    const downloadLink = document.createElement('a');
+    downloadLink.href = filePath;
+    downloadLink.download = filePath;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
 
   return (
     <article className="video-media-card" style={{ position: 'relative' }}>
+      { errorAlert &&
+      <Alert
+        banner
+        variant="error"
+        buttonLabel="Download video"
+        onButtonClick={handleDownloadButtonClick}
+        title={
+          <FormattedMessage
+            id="mediaPlayer.errorTitle"
+            defaultMessage="A Playback Error Occurred."
+            description="Text displayed in an error box on media page when the video cannot be played"
+          />
+        }
+        content={
+          <FormattedMessage
+            id="mediaPlayer.errorContent"
+            defaultMessage="The video cannot be played because of an issue with the video file. Download the file to play locally."
+            description="Text displayed in an error box on media page when the video cannot be played"
+          />
+        }
+        onClose={() => setErrorAlert(false)}
+      />
+      }
       <AspectRatio
         key={contentWarning}
         contentWarning={contentWarning}
@@ -71,6 +110,7 @@ const MediaPlayerCard = ({
                 src={filePath}
                 className={classes.video}
                 poster={isAudio ? poster : ''}
+                onError={handleVideoError}
               />
               <MediaControls videoRef={videoRef} />
             </>
