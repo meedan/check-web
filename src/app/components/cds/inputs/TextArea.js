0ed371bb5e2f89bea4f28d5aa8d372f07fd08649
danielevalverde <danierzevalverde@gmail.com>
2023-08-29 09:16:36 -0300

    Text areas should auto resize and not scroll
    - Hide the browser resizable icon when it auto-grows and hide scroll

--- src/app/components/cds/inputs/TextArea.js
+++ src/app/components/cds/inputs/TextArea.js
@@ -1,12 +1,17 @@
 // DESIGNS: https://www.figma.com/file/rnSPSHDgFncxjXsZQuEVKd/Design-System?type=design&node-id=3606-26274&mode=design&t=ZVq51pKdIKdWZicO-4
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from './TextField';
 
 const TextArea = React.forwardRef(({
   ...inputProps
-}, ref) => (
-  <TextField textArea ref={ref} {...inputProps} />
-));
+}, ref) => {
+  const [height, setHeight] = useState('auto');
 
-// eslint-disable-next-line import/no-unused-modules
+  const handleChange = (event) => {
+    setHeight(`${event.target.scrollHeight}px`);
+  };
+
+  return <TextField textArea ref={ref} {...inputProps} style={{ height }} onChange={handleChange} />;
+});
+
 export default TextArea;
