0f1f193c80fe36f1abf57fadb57cc8399f465bec
danielevalverde <danierzevalverde@gmail.com>
2023-08-29 19:13:02 -0300

    add min and max height on textArea Component - fix: do not apply 'overflowy: hidden' and 'resize: none' properties on textArea if autogrow is false - and add prototypes
    Reference: CV2-3180

--- src/app/components/cds/inputs/TextArea.js
+++ src/app/components/cds/inputs/TextArea.js
@@ -1,20 +1,36 @@
 // DESIGNS: https://www.figma.com/file/rnSPSHDgFncxjXsZQuEVKd/Design-System?type=design&node-id=3606-26274&mode=design&t=ZVq51pKdIKdWZicO-4
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import TextField from './TextField';
 
 const TextArea = React.forwardRef(({
-  autoGrow = true,
+  autoGrow,
+  maxHeight,
+  minHeight,
   ...inputProps
 }, ref) => {
   const [height, setHeight] = useState('auto');
 
   const handleChange = (event) => {
-    if (autoGrow) {
-      setHeight(`${event.target.scrollHeight}px`);
+    const newHeight = `${event.target.scrollHeight}px`;
+    if (autoGrow && event.target.scrollHeight >= minHeight && (!maxHeight || event.target.scrollHeight <= maxHeight)) {
+      setHeight(newHeight);
     }
   };
 
-  return <TextField textArea ref={ref} {...inputProps} style={{ height }} onChange={handleChange} />;
+  return <TextField textArea autoGrow ref={ref} {...inputProps} style={{ height }} onChange={handleChange} />;
 });
+
+TextArea.defaultProps = {
+  autoGrow: true,
+  maxHeight: null,
+  minHeight: 65,
+};
+
+TextArea.propTypes = {
+  autoGrow: PropTypes.bool,
+  maxHeight: PropTypes.number,
+  minHeight: PropTypes.number,
+};
 
 export default TextArea;
