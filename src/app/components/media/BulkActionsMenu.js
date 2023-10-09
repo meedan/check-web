350e7edf66c03ed14f954364c4cb41484b67da40
danielevalverde <danierzevalverde@gmail.com>
2023-10-09 10:49:51 -0300

    Refactoring: consolidate actions on list page
    Remove Spam and Trash Icon buttons and move those actions to the Actions button flyout menu
    Add option to mark items as read and unread
    add mutation bulkProjectMediaMarkRead mutation
    
    Reference: CV2-2847

--- src/app/components/media/BulkActionsMenu.js
+++ src/app/components/media/BulkActionsMenu.js
@@ -1,7 +1,7 @@
 /* eslint-disable relay/unused-fields */
 import React from 'react';
 import Relay from 'react-relay/classic';
-import { QueryRenderer, graphql } from 'react-relay/compat';
+import { QueryRenderer, graphql, commitMutation } from 'react-relay/compat';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import Button from '@material-ui/core/Button';
@@ -14,11 +14,19 @@ import BulkActionsAssign from './BulkActionsAssign';
 import BulkActionsStatus from './BulkActionsStatus';
 import BulkActionsTag from './BulkActionsTag';
 import BulkActionsRemoveTag from './BulkActionsRemoveTag';
+import BulkArchiveProjectMediaMutation from '../../relay/mutations/BulkArchiveProjectMediaMutation';
+import BulkRestoreProjectMediaMutation from '../../relay/mutations/BulkRestoreProjectMediaMutation';
+import GenericUnknownErrorMessage from '../GenericUnknownErrorMessage';
+import { getErrorMessage } from '../../helpers';
+import CheckArchivedFlags from '../../CheckArchivedFlags';
 
 const BulkActionsMenu = ({
   selectedMedia,
   selectedProjectMedia,
   team,
+  page,
+  setFlashMessage,
+  onUnselectAll,
 }) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mode, setMode] = React.useState('menu');
@@ -32,7 +40,135 @@ const BulkActionsMenu = ({
   const handleMenuAssign = () => setMode('assign');
   const handleMenuChangeStatus = () => setMode('status');
 
-  const menuContent = {
+  const fail = (transaction) => {
+    const message = getErrorMessage(transaction, <GenericUnknownErrorMessage />);
+    this.props.setFlashMessage(message, 'error');
+  };
+
+  const handleRestoreOrConfirm = (params) => {
+    const onSuccess = () => {
+      const message = page === 'trash' ? (
+        <FormattedMessage
+          id="bulkActions.movedRestoreSuccessfully"
+          defaultMessage="{count, plural, one {# item} other {# items}} moved from Trash to 'All'"
+          values={{
+            count: selectedMedia?.length,
+          }}
+          description="Banner displayed after items are moved successfully. 'All' here is the name of the default view in the workspace, which is localized under the id projectsComponent.allItems"
+        />
+      ) : (
+        <FormattedMessage
+          id="bulkActions.movedFromSpamSuccessfully"
+          defaultMessage="{count, plural, one {# item} other {# items}} moved from Spam to 'All'"
+          values={{
+            count: selectedMedia?.length,
+          }}
+          description="Banner displayed after items are moved successfully. 'All' here is the name of the default view in the workspace, which is localized under the id projectsComponent.allItems"
+        />
+      );
+      setFlashMessage(message, 'success');
+      onUnselectAll();
+    };
+
+    if (selectedMedia.length) {
+      Relay.Store.commitUpdate(
+        new BulkRestoreProjectMediaMutation({
+          ids: selectedMedia,
+          team,
+          archived_was: params.archived_was,
+        }),
+        { onSuccess, onFailure: fail },
+      );
+    }
+  };
+
+  const handleArchive = (archived) => {
+    const onSuccess = () => {
+      const message = archived === CheckArchivedFlags.TRASHED ? (
+        <FormattedMessage
+          id="bulkActions.moveToTrashSuccessfully"
+          defaultMessage="{count, plural, one {# item} other {# items}} moved to Trash."
+          values={{
+            count: selectedMedia?.length,
+          }}
+          description="Message that appears when one or more items have been selected and moved to the trash."
+        />
+      ) : (
+        <FormattedMessage
+          id="bulkActions.moveToSpamSuccessfully"
+          defaultMessage="{count, plural, one {# item} other {# items}} moved to Spam."
+          values={{
+            count: selectedMedia?.length,
+          }}
+          description="Message that appears when one or more items have been selected and moved to spam."
+        />
+      );
+      setFlashMessage(message, 'success');
+      onUnselectAll();
+    };
+
+    if (selectedMedia.length) {
+      const mutation = new BulkArchiveProjectMediaMutation({
+        ids: selectedMedia,
+        team,
+        archived,
+      });
+      Relay.Store.commitUpdate(mutation, { onSuccess });
+    }
+  };
+
+  const handleMarkAsRead = (read) => {
+    const onSuccess = () => {
+      const message = read ? (
+        <FormattedMessage
+          id="bulkActions.markAsReadSuccess"
+          defaultMessage="{count, plural, one {# item} other {# items}} marked as read successfully."
+          values={{ count: selectedMedia?.length }}
+          description="Message that appears when one or more items have been marked as read."
+        />
+      ) : (
+        <FormattedMessage
+          id="bulkActions.markAsUnreadSuccess"
+          defaultMessage="{count, plural, one {# item} other {# items}} marked as unread successfully."
+          values={{ count: selectedMedia?.length }}
+          description="Message that appears when one or more items have been marked as unread."
+        />
+      );
+      setFlashMessage(message, 'success');
+      onUnselectAll();
+    };
+
+    if (selectedMedia.length) {
+      commitMutation(Relay.Store, {
+        mutation: graphql`
+          mutation BulkActionsMenuMarkAsReadMutation($input: BulkProjectMediaMarkReadInput!) {
+            bulkProjectMediaMarkRead(input: $input) {
+              updated_objects {
+                id
+                is_read
+              }
+            }
+          }
+        `,
+        variables: {
+          input: {
+            ids: selectedMedia,
+            read,
+          },
+        },
+        onCompleted: ({ response, error }) => {
+          if (error) {
+            return fail(error);
+          }
+          return onSuccess(response);
+        },
+        onError: fail,
+      });
+    }
+  };
+
+
+  const menuContent = page !== 'trash' && page !== 'spam' ? {
     menu: (
       <React.Fragment>
         <MenuItem className="bulk-actions-menu__add-tag" onClick={handleMenuTag}>
@@ -62,6 +198,34 @@ const BulkActionsMenu = ({
             id="bulkActionsMenu.changeStatus"
             defaultMessage="Change status"
             description="Menu option for bulk changing statuses of selected items"
+          />
+        </MenuItem>
+        <MenuItem className="bulk-actions-menu__mark-read" onClick={() => handleMarkAsRead(true)}>
+          <FormattedMessage
+            id="bulkActions.markAsRead"
+            defaultMessage="Mark as Read"
+            description="Menu option for bulk marking selected items as read"
+          />
+        </MenuItem>
+        <MenuItem className="bulk-actions-menu__mark-not-read" onClick={() => handleMarkAsRead(false)}>
+          <FormattedMessage
+            id="bulkActions.markAsNotRead"
+            defaultMessage="Mark as Unread"
+            description="Menu option for bulk marking selected items as unread"
+          />
+        </MenuItem>
+        <MenuItem className="bulk-actions-menu__archive" onClick={() => handleArchive(CheckArchivedFlags.TRASHED)}>
+          <FormattedMessage
+            id="bulkActions.sendItemsToTrash"
+            defaultMessage="Send to Trash"
+            description="Label for button that sends one or more items to Trash"
+          />
+        </MenuItem>
+        <MenuItem className="bulk-actions-menu__archive" onClick={() => handleArchive(CheckArchivedFlags.SPAM)}>
+          <FormattedMessage
+            id="bulkActions.sendItemsToSpam"
+            defaultMessage="Mark as spam"
+            description="Label for button that sends one or more items to Spam"
           />
         </MenuItem>
       </React.Fragment>
@@ -95,6 +259,20 @@ const BulkActionsMenu = ({
         team={team}
       />
     ),
+  } : {
+    menu: page === 'spam' ? (
+      <React.Fragment>
+        <MenuItem className="bulk-actions-menu__restore" onClick={() => handleRestoreOrConfirm({ archived_was: CheckArchivedFlags.SPAM })}>
+          <FormattedMessage id="bulkActions.notSpam" defaultMessage="Not Spam" description="Label for button that removes one or more items from Spam and puts them back in the normal workspace" />
+        </MenuItem>
+      </React.Fragment>
+    ) : (
+      <React.Fragment>
+        <MenuItem className="bulk-actions-menu__restore" onClick={() => handleRestoreOrConfirm({ archived_was: CheckArchivedFlags.TRASHED })}>
+          <FormattedMessage id="bulkActions.restore" defaultMessage="Restore from Trash" description="Label for button that removes one or more items from Trash and puts it back in the normal workspace" />
+        </MenuItem>
+      </React.Fragment>
+    ),
   };
 
   return (
@@ -125,8 +303,11 @@ const BulkActionsMenu = ({
 
 BulkActionsMenu.propTypes = {
   team: PropTypes.object.isRequired,
+  page: PropTypes.oneOf(['all-items', 'tipline-inbox', 'imported-fact-checks', 'suggested-matches', 'unmatched-media', 'published', 'list', 'feed', 'spam', 'trash']).isRequired, // FIXME Define listing types as a global constant
   selectedMedia: PropTypes.arrayOf(PropTypes.string).isRequired,
+  setFlashMessage: PropTypes.func.isRequired,
   selectedProjectMedia: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onUnselectAll: PropTypes.func.isRequired,
 };
 
 const BulkActionsMenuRenderer = (parentProps) => {
@@ -157,6 +338,20 @@ const BulkActionsMenuRenderer = (parentProps) => {
             ...BulkActionsStatus_team
             ...BulkActionsTag_team
             ...BulkActionsRemoveTag_team
+            check_search_trash {
+              id
+              number_of_results
+            }
+            check_search_spam {
+              id
+              number_of_results
+            }
+            public_team {
+              id
+              trash_count
+              spam_count
+            }
+            search_id
           }
         }
       `}
