2d6bf061848a8e59a171d2f1581a16a0efc693fe
Brian Fleming <brianfleming@gmail.com>
2023-10-13 09:26:43 -0400

    fix testing typo

--- src/app/components/team/CreateTeamTask.test.js
+++ src/app/components/team/CreateTeamTask.test.js
@@ -4,7 +4,7 @@ import CreateTeamTask from './CreateTeamTask';
 
 describe('<CreateTeamTask />', () => {
   it('should render create task button', () => {
-    const wrapper = mountWithIntl(<CreateTeamTask team={{}} associatedType='ProjectMedia' />);
+    const wrapper = mountWithIntl(<CreateTeamTask team={{}} associatedType="ProjectMedia" />);
     expect(wrapper.find('.create-task__add-button').hostNodes()).toHaveLength(1);
   });
 });
