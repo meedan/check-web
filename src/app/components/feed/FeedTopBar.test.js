83d38d8f8dd460c07da881c76c34cc0d828cb04d
Darius Kazemi <darius@meedan.com>
2023-10-30 13:44:22 -0700

    Filter shared feeds by organization
    This implements a series of filters available at the top of a Shared Feed view where the user can toggle on/off fact-checks from different organizations in the shared feed. There is also a Quick Filter that lets you pick your own org, or all orgs that are not your own. I'll point out the `toHaveBeenCalledWith` in the unit test -- it's a really handy function for testing whether a mock function was called with certain arguments.
    
    #1707, CV2-3611

--- src/app/components/feed/FeedTopBar.test.js
+++ src/app/components/feed/FeedTopBar.test.js
@@ -9,42 +9,55 @@ const feed = {
     dbid: 1,
     title: 'Test',
   },
+  teams: {
+    edges: [
+      {
+        node: {
+          dbid: 1,
+          slug: 'test',
+          avatar: 'https://example.com',
+        },
+      },
+    ],
+  },
 };
 
 const team = {
   slug: 'test',
 };
 
+const teamFiltersAll = [1];
+
 describe('<FeedTopBar />', () => {
   it('should render FeedTopBar component', () => {
-    const feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={feed} team={team} />);
+    const feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={feed} team={team} setTeamFilters={() => {}} teamFilters={teamFiltersAll} />);
     const feedTopBar = feedTopBarComponent.find('.feed-top-bar');
     expect(feedTopBar).toHaveLength(1);
   });
 
   it('should not render anything if feed is not published', () => {
-    const feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={{ ...feed, published: false }} team={team} />);
+    const feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={{ ...feed, published: false }} team={team} setTeamFilters={() => {}} teamFilters={teamFiltersAll} />);
     const feedTopBar = feedTopBarComponent.find('.feed-top-bar');
     expect(feedTopBar).toHaveLength(0);
   });
 
   it('should render list name if there is a list', () => {
-    let feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={feed} team={team} />);
+    let feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={feed} team={team} setTeamFilters={() => {}} teamFilters={teamFiltersAll} />);
     let feedTopBar = feedTopBarComponent.find('.feed-top-bar-list');
     expect(feedTopBar).toHaveLength(1);
 
-    feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={{ ...feed, saved_search: null }} team={team} />);
+    feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={{ ...feed, saved_search: null }} team={team} setTeamFilters={() => {}} teamFilters={teamFiltersAll} />);
     feedTopBar = feedTopBarComponent.find('.feed-top-bar-list');
     expect(feedTopBar).toHaveLength(0);
   });
 
   it('should render settings icon if there is permission', () => {
-    let feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={feed} team={team} />);
-    let feedTopBar = feedTopBarComponent.find('svg');
+    let feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={feed} team={team} setTeamFilters={() => {}} teamFilters={teamFiltersAll} />);
+    let feedTopBar = feedTopBarComponent.find('button.int-feed-top-bar__icon-button--settings');
     expect(feedTopBar).toHaveLength(1);
 
-    feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={{ ...feed, permissions: '{}' }} team={team} />);
-    feedTopBar = feedTopBarComponent.find('svg');
+    feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={{ ...feed, permissions: '{}' }} team={team} setTeamFilters={() => {}} teamFilters={teamFiltersAll} />);
+    feedTopBar = feedTopBarComponent.find('button.int-feed-top-bar__icon-button--settings');
     expect(feedTopBar).toHaveLength(0);
   });
 });
