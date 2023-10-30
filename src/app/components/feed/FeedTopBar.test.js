b999d5a8eb1a0284f52fa01567994750f90bba99
Darius Kazemi <darius@meedan.com>
2023-10-30 13:33:50 -0700

    Fix linter errors

--- src/app/components/feed/FeedTopBar.test.js
+++ src/app/components/feed/FeedTopBar.test.js
@@ -30,33 +30,33 @@ const teamFiltersAll = [1];
 
 describe('<FeedTopBar />', () => {
   it('should render FeedTopBar component', () => {
-    const feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={feed} team={team} setTeamFilters={ ()=>{} } teamFilters={teamFiltersAll} />);
+    const feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={feed} team={team} setTeamFilters={() => {}} teamFilters={teamFiltersAll} />);
     const feedTopBar = feedTopBarComponent.find('.feed-top-bar');
     expect(feedTopBar).toHaveLength(1);
   });
 
   it('should not render anything if feed is not published', () => {
-    const feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={{ ...feed, published: false }} team={team} setTeamFilters={ ()=>{} } teamFilters={teamFiltersAll} />);
+    const feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={{ ...feed, published: false }} team={team} setTeamFilters={() => {}} teamFilters={teamFiltersAll} />);
     const feedTopBar = feedTopBarComponent.find('.feed-top-bar');
     expect(feedTopBar).toHaveLength(0);
   });
 
   it('should render list name if there is a list', () => {
-    let feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={feed} team={team} setTeamFilters={ ()=>{} } teamFilters={teamFiltersAll} />);
+    let feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={feed} team={team} setTeamFilters={() => {}} teamFilters={teamFiltersAll} />);
     let feedTopBar = feedTopBarComponent.find('.feed-top-bar-list');
     expect(feedTopBar).toHaveLength(1);
 
-    feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={{ ...feed, saved_search: null }} team={team} setTeamFilters={ ()=>{} } teamFilters={teamFiltersAll} />);
+    feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={{ ...feed, saved_search: null }} team={team} setTeamFilters={() => {}} teamFilters={teamFiltersAll} />);
     feedTopBar = feedTopBarComponent.find('.feed-top-bar-list');
     expect(feedTopBar).toHaveLength(0);
   });
 
   it('should render settings icon if there is permission', () => {
-    let feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={feed} team={team} setTeamFilters={ ()=>{} } teamFilters={teamFiltersAll} />);
+    let feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={feed} team={team} setTeamFilters={() => {}} teamFilters={teamFiltersAll} />);
     let feedTopBar = feedTopBarComponent.find('button.int-feed-top-bar__icon-button--settings');
     expect(feedTopBar).toHaveLength(1);
 
-    feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={{ ...feed, permissions: '{}' }} team={team} setTeamFilters={ ()=>{} } teamFilters={teamFiltersAll} />);
+    feedTopBarComponent = mountWithIntlProvider(<FeedTopBar feed={{ ...feed, permissions: '{}' }} team={team} setTeamFilters={() => {}} teamFilters={teamFiltersAll} />);
     feedTopBar = feedTopBarComponent.find('button.int-feed-top-bar__icon-button--settings');
     expect(feedTopBar).toHaveLength(0);
   });
