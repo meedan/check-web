f7149935515d339489eb3e12dd2d8ac817b3f301
Brian Fleming <brianfleming@gmail.com>
2023-11-08 13:39:57 -0500

    update language on the link shortening controls of the settings area CV2-3879

--- src/app/components/team/TeamDetails.js
+++ src/app/components/team/TeamDetails.js
@@ -17,6 +17,7 @@ import SwitchComponent from '../cds/inputs/SwitchComponent';
 import Alert from '../cds/alerts-and-prompts/Alert';
 import UploadFile from '../UploadFile';
 import UpdateTeamMutation from '../../relay/mutations/UpdateTeamMutation';
+import inputStyles from '../../styles/css/inputs.module.css';
 import settingsStyles from './Settings.module.css';
 import styles from './TeamDetails.module.css';
 
@@ -192,54 +193,64 @@ const TeamDetails = ({
             <div className={settingsStyles['setting-content-container-title']}>
               <FormattedMessage id="teamDetails.linkManagement" defaultMessage="Link management" description="Title of the link management section in team details page" />
             </div>
-            <SwitchComponent
-              label={<FormattedMessage
-                id="teamDetails.linkManagementSwitcher"
-                defaultMessage="Enable link shortening and analytics"
-                description="Label for a switch where the user toggles link management in team details page"
-              />}
-              checked={shortenOutgoingUrls || hasRssNewsletters}
-              disabled={hasRssNewsletters || hasScheduledNewsletters}
-              helperContent={
-                hasRssNewsletters ?
-                  <FormattedMessage id="teamDetails.linkManagementRss" defaultMessage="The link service cannot be disabled while RSS newsletters are configured in this workspace" description="Helper text for link management switcher when workspace has RSS newsletters configured" /> :
-                  null
-              }
-              onChange={setShortenOutgoingUrls}
-            />
-            { shortenOutgoingUrls ?
-              <TextField
-                id="team-details__utm-code"
-                defaultValue={utmCode}
-                fullWidth
-                label={
-                  <FormattedMessage
-                    id="teamDetails.utmCode"
-                    defaultMessage="UTM code (optional)"
-                    description="Label for 'UTM code' field"
-                  />
-                }
-                margin="normal"
-                onChange={e => setUtmCode(e.target.value)}
-                helpContent={<FormattedMessage id="teamDetails.utmCodeHelp" defaultMessage="Customize the UTM code appended to links. Leave blank to disable UTM codes." description="Helper text for UTM code field" />}
-                variant="outlined"
-                disabled={hasScheduledNewsletters}
-              /> : null
-            }
-            { shortenOutgoingUrls ?
-              <Alert
-                variant="warning"
-                title={<FormattedMessage id="teamDetails.warnTitle" defaultMessage="All links sent via Check will be rewritten." description="Text displayed in the title of a warning box on team details page when link shortening is on" />}
-                content={
+            <div className={inputStyles['form-fieldset']}>
+              <SwitchComponent
+                className={inputStyles['form-fieldset-field']}
+                label={<FormattedMessage
+                  id="teamDetails.linkManagementSwitcher"
+                  defaultMessage="Enable link shortening and analytics"
+                  description="Label for a switch where the user toggles link management in team details page"
+                />}
+                checked={shortenOutgoingUrls || hasRssNewsletters}
+                disabled={hasRssNewsletters || hasScheduledNewsletters}
+                helperContent={
                   <FormattedHTMLMessage
-                    id="teamDetails.warnContent"
-                    defaultMessage="The rewritten links will also include the UTM code if provided. The UTM code does not change the destination URL.<br /><br /><strong>BEFORE:</strong> https://www.example.com/your-link<br /><strong>AFTER:</strong> https://chck.media/x1y2z3w4/{code}"
-                    values={{ code: utmCode ? `?utm_source=${utmCode}` : '' }}
-                    description="Text displayed in the content of a warning box on team details page when link shortening is on"
+                    id="teamDetails.linkManagementRss"
+                    defaultMessage='Link shortening makes URLs more readable and a predictable length. If you’re using an RSS feed, the link service cannot be disabled. <a href="{helpLink}" target="_blank" title="Learn more">Learn more about link shortening</a>.'
+                    description="Helper text for link management switcher when workspace has RSS newsletters configured"
+                    values={{ helpLink: 'https://help.checkmedia.org/en/articles/5540430-tipline-newsletters' }}
                   />
                 }
-              /> : null
-            }
+                onChange={setShortenOutgoingUrls}
+              />
+              { shortenOutgoingUrls ?
+                <>
+                  <TextField
+                    className={inputStyles['form-fieldset-field']}
+                    id="team-details__utm-code"
+                    defaultValue={utmCode}
+                    fullWidth
+                    label={
+                      <FormattedMessage
+                        id="teamDetails.utmCode"
+                        defaultMessage="UTM code (optional)"
+                        description="Label for 'UTM code' field"
+                      />
+                    }
+                    margin="normal"
+                    onChange={e => setUtmCode(e.target.value)}
+                    helpContent={<FormattedMessage id="teamDetails.utmCodeHelp" defaultMessage="Customize the UTM code appended to links. Leave blank to disable UTM codes." description="Helper text for UTM code field" />}
+                    variant="outlined"
+                    disabled={hasScheduledNewsletters}
+                  />
+                  <Alert
+                    className={inputStyles['form-fieldset-field']}
+                    variant="warning"
+                    contained
+                    title={<FormattedMessage id="teamDetails.warnTitle" defaultMessage="All links sent via Check will be rewritten." description="Text displayed in the title of a warning box on team details page when link shortening is on" />}
+                    content={
+                      <FormattedHTMLMessage
+                        id="teamDetails.warnContent"
+                        defaultMessage="The rewritten links will also include the UTM code if provided. The UTM code does not change the destination URL.<br /><br /><strong>BEFORE:</strong> https://www.example.com/your-link<br /><strong>AFTER:</strong> https://chck.media/x1y2z3w4/{code}"
+                        values={{ code: utmCode ? `?utm_source=${utmCode}` : '' }}
+                        description="Text displayed in the content of a warning box on team details page when link shortening is on"
+                      />
+                    }
+                  />
+                </>
+                : null
+              }
+            </div>
           </div>
         </div>
 
