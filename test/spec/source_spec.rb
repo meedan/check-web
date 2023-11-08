a17d3fffe11a30fb193330a52db27ae17167f5da
Brian Fleming <brianfleming@gmail.com>
2023-11-08 10:05:41 -0500

    do not add autogrow class to textfileds, only text area. Add testing interaction class to textfield error messages [full ci]

--- test/spec/source_spec.rb
+++ test/spec/source_spec.rb
@@ -15,24 +15,24 @@ shared_examples 'source' do
     expect(@driver.page_source.include?('G1- Edited')).to be(true)
     # check main link
     expect(wait_for_selector('#main_source__link').attribute('value') == 'https://g1.globo.com/').to be(true)
-    expect(@driver.find_elements(:css, '.source__remove-link-button').length == 1).to be(true)
+    expect(@driver.find_elements(:css, '.int-clear-input__button--textfield').length == 1).to be(true)
     # add a secondary link
     wait_for_selector('.source__add-link-button').click
     wait_for_selector('#source__link-input-new').send_keys('https://www.bbc.com/news/uk')
     @driver.action.send_keys(:enter).perform
-    wait_for_selector_list_size('.source__remove-link-button', 2)
+    wait_for_selector_list_size('.int-clear-input__button--textfield', 2)
     # remove main link
-    expect(@driver.find_elements(:css, '.source__remove-link-button').length == 2).to be(true)
-    wait_for_selector_list('.source__remove-link-button')[0].click
+    expect(@driver.find_elements(:css, '.int-clear-input__button--textfield').length == 2).to be(true)
+    wait_for_selector_list('.int-clear-input__button--textfield')[0].click
     wait_for_selector_none('input[value="https://g1.globo.com/"]', :css, 30)
     # check that the main link was changed
     expect(wait_for_selector('#main_source__link').attribute('value') == 'https://www.bbc.com/news/uk').to be(true)
-    expect(@driver.find_elements(:css, '.source__remove-link-button').length == 1).to be(true)
+    expect(@driver.find_elements(:css, '.int-clear-input__button--textfield').length == 1).to be(true)
     wait_for_selector('.source__add-link-button').click
     # try to add the same source again
     wait_for_selector('#source__link-input-new').send_keys('https://www.bbc.com/news/uk')
     @driver.action.send_keys(:enter).perform
-    wait_for_selector('#source__link-input-new-helper-text')
+    wait_for_selector('.int-error__message--textfield')
     expect(@driver.page_source.include?('Account has already been taken')).to be(true)
   end
 
