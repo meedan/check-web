b5562eb067f74d3b3b31182c6f480d5eeaf7687a
Alexandre Amorim <amoedoamorim@gmail.com>
2023-12-08 17:22:39 -0300

    Apply content warning for embeds aka PenderCard

--- src/app/components/layout/AspectRatio.js
+++ src/app/components/layout/AspectRatio.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage, FormattedHTMLMessage, injectIntl, defineMessages } from 'react-intl';
+import { FormattedMessage, FormattedHTMLMessage, defineMessages, injectIntl, intlShape } from 'react-intl';
 import { graphql, createFragmentContainer } from 'react-relay/compat';
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
@@ -196,6 +196,18 @@ const AspectRatio = ({
 
   const warningCategory = warningType;
 
+  const isPenderCard = children.type.name === 'PenderCard';
+  const showStraightPenderCard = !maskContent && !superAdminMask && isPenderCard;
+
+  if (showStraightPenderCard) {
+    return (
+      <div style={{ position: 'relative' }}>
+        <ButtonsContainer />
+        {children}
+      </div>
+    );
+  }
+
   return (
     <div className={`${uniqueClassName} ${classes.container}`}>
       { expandedContent ?
@@ -288,12 +300,21 @@ const AspectRatio = ({
 AspectRatio.propTypes = {
   children: PropTypes.node.isRequired,
   downloadUrl: PropTypes.string,
+  expandedImage: PropTypes.string,
   isVideoFile: PropTypes.bool,
   superAdminMask: PropTypes.bool,
+  currentUserRole: PropTypes.string.isRequired,
+  projectMedia: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    show_warning_cover: PropTypes.bool.isRequired,
+    dynamic_annotation_flag: PropTypes.object.isRequired,
+  }).isRequired,
+  intl: intlShape.isRequired,
 };
 
 AspectRatio.defaultProps = {
   downloadUrl: '',
+  expandedImage: '',
   isVideoFile: false,
   superAdminMask: false,
 };
