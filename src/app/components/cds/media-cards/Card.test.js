de0d947ededb4d406e4c07246ad402e74024964d
Caio <117518+caiosba@users.noreply.github.com>
2023-12-15 22:48:25 -0300

    Fixing unit tests

--- src/app/components/cds/media-cards/Card.test.js
+++ src/app/components/cds/media-cards/Card.test.js
@@ -10,13 +10,13 @@ describe('<Card />', () => {
   });
 
   it('should render clickable card', () => {
-    const cardComponent = mount(<Card title="Test" url="https://meedan.com" />);
+    const cardComponent = mount(<Card title="Test" factCheckUrl="https://meedan.com" />);
     const card = cardComponent.find('.card a');
     expect(card).toHaveLength(1);
   });
 
   it('should render non-clickable card', () => {
-    const cardComponent = mount(<Card title="Test" url={null} />);
+    const cardComponent = mount(<Card title="Test" factCheckUrl={null} />);
     const card = cardComponent.find('.card a');
     expect(card).toHaveLength(0);
   });
