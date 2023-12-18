ad1d0da8431936b0588bdf0fc62a480d8cf87890
Caio <117518+caiosba@users.noreply.github.com>
2023-12-18 14:11:25 -0300

    Fixing test

--- src/app/components/cds/media-cards/Card.test.js
+++ src/app/components/cds/media-cards/Card.test.js
@@ -9,14 +9,14 @@ describe('<Card />', () => {
     expect(card).toHaveLength(1);
   });
 
-  it('should render clickable card', () => {
-    const cardComponent = mount(<Card title="Test" factCheckUrl="https://meedan.com" />);
+  it('should render clickable fact-check', () => {
+    const cardComponent = mount(<Card title="Test" description="Test" factCheckUrl="https://meedan.com" />);
     const card = cardComponent.find('.card a');
     expect(card).toHaveLength(1);
   });
 
-  it('should render non-clickable card', () => {
-    const cardComponent = mount(<Card title="Test" factCheckUrl={null} />);
+  it('should render non-clickable fact-check', () => {
+    const cardComponent = mount(<Card title="Test" description="Test" factCheckUrl={null} />);
     const card = cardComponent.find('.card a');
     expect(card).toHaveLength(0);
   });
