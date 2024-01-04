cdc9895036348db27b0607dc758eeb3bd4c9ac4b
Caio <117518+caiosba@users.noreply.github.com>
2024-01-04 11:37:55 -0300

    Trying to fix integration test, take 1 [full ci]

--- test/spec/similarity_spec.rb
+++ test/spec/similarity_spec.rb
@@ -76,15 +76,16 @@ shared_examples 'similarity' do
   end
 
   it 'should extract text from a image', bin7: true do
+    sleep 120
     api_create_team_and_bot(bot: 'alegre', score: {})
     @driver.navigate.to @config['self_url']
-    sleep 240
+    sleep 360
     wait_for_selector('.team-settings__workspace-tab')
     wait_for_selector('#side-navigation__toggle').click
     wait_for_selector('.projects-list')
     wait_for_selector('.projects-list__all-items').click
     create_image('files/test.png')
-    sleep 120 # wait for the text extraction
+    sleep 360 # Wait for the text extraction
     wait_for_selector('.medias__item')
     wait_for_selector('.media__heading').click
     wait_for_selector('.image-media-card')
