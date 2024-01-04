7f475a88f5d40c7994e58c31b10d3fafbba98576
Caio <117518+caiosba@users.noreply.github.com>
2024-01-03 21:16:23 -0300

    :zzz:

--- test/spec/similarity_spec.rb
+++ test/spec/similarity_spec.rb
@@ -78,13 +78,13 @@ shared_examples 'similarity' do
   it 'should extract text from a image', bin7: true do
     api_create_team_and_bot(bot: 'alegre', score: {})
     @driver.navigate.to @config['self_url']
-    sleep 120
+    sleep 240
     wait_for_selector('.team-settings__workspace-tab')
     wait_for_selector('#side-navigation__toggle').click
     wait_for_selector('.projects-list')
     wait_for_selector('.projects-list__all-items').click
     create_image('files/test.png')
-    sleep 60 # wait for the text extraction
+    sleep 120 # wait for the text extraction
     wait_for_selector('.medias__item')
     wait_for_selector('.media__heading').click
     wait_for_selector('.image-media-card')
