70e33705e61b735c0b7466418a6a005066c0df97
Caio <117518+caiosba@users.noreply.github.com>
2024-02-21 14:20:06 -0300

    Fixing styles

--- src/app/components/feed/FeedClusters.js
+++ src/app/components/feed/FeedClusters.js
@@ -159,7 +159,7 @@ const FeedClustersComponent = ({
           const channels = cluster.channels.filter(channel => Object.values(CheckChannels.TIPLINE).includes(channel.toString()));
 
           return (
-            <div key={cluster.id} className="feed-clusters__card">
+            <div key={cluster.id} className={cx('feed-clusters__card', styles.feedClusterCard)}>
               <SharedItemCard
                 title={cluster.center.title}
                 description={cluster.center.description}
