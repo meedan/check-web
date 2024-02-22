0489009e8e19e8cd6c6ec8602f8c452473c95788
Caio <117518+caiosba@users.noreply.github.com>
2024-02-22 12:43:14 -0300

    Adding video

--- build.sh
+++ build.sh
@@ -52,4 +52,5 @@ else
   # docker-compose logs -f presto-server &
   # docker-compose logs -f presto-image &
   # docker-compose logs -f presto-audio &
+  # docker-compose logs -f presto-video &
 fi
