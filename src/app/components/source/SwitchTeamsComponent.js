aca2792f8f77b30b7d7103115a5c9fd4b4f620e7
Brian Fleming <brianfleming@gmail.com>
2024-02-23 16:35:59 -0500

    use shared blank state template on workspaces list in user settings area

--- src/app/components/source/SwitchTeamsComponent.js
+++ src/app/components/source/SwitchTeamsComponent.js
@@ -3,15 +3,14 @@ import PropTypes from 'prop-types';
 import Relay from 'react-relay/classic';
 import { FormattedMessage } from 'react-intl';
 import { browserHistory, Link } from 'react-router';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
-import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import cx from 'classnames/bind';
+import BlankState from '../layout/BlankState';
 import SettingsHeader from '../team/SettingsHeader';
 import ButtonMain from '../cds/buttons-checkboxes-chips/ButtonMain';
 import CreateTeamDialog from '../team/CreateTeamDialog';
@@ -136,9 +135,9 @@ class SwitchTeamsComponent extends Component {
           }
         />
         <div className={styles['user-setting-details-wrapper']}>
-          <Card>
-            { (joinedTeams.length + pendingTeams.length) ?
-              <List className="teams">
+          { (joinedTeams.length + pendingTeams.length) ?
+            <div className={styles['user-setting-content-container']}>
+              <ul className={cx('teams', styles['user-setting-content-list'])}>
                 {joinedTeams.sort((a, b) => a.name.localeCompare(b.name, undefined, { numeric: true, sensitivity: 'base' })).map(team => (
                   <ListItem
                     key={team.slug}
@@ -205,16 +204,18 @@ class SwitchTeamsComponent extends Component {
                     </ListItemSecondaryAction>
                   </ListItem>
                 ))}
-              </List> :
-              <CardContent>
+              </ul>
+            </div> :
+            <div className={styles['user-setting-content-container']}>
+              <BlankState>
                 <FormattedMessage id="switchTeams.noTeams" defaultMessage="Not a member of any workspace." description="Empty message when the user is not a member of a workspace" />
-              </CardContent>
-            }
-            { this.state.showCreateTeamDialog ?
-              <CreateTeamDialog onDismiss={this.handleCancelCreateTeam.bind(this)} /> :
-              null
-            }
-          </Card>
+              </BlankState>
+            </div>
+          }
+          { this.state.showCreateTeamDialog ?
+            <CreateTeamDialog onDismiss={this.handleCancelCreateTeam.bind(this)} /> :
+            null
+          }
         </div>
       </>
     );
