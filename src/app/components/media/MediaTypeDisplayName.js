e633ccba00d0a0d0b8fd73d3720bb3126470464b
danielevalverde <danierzevalverde@gmail.com>
2024-03-07 14:31:03 -0300

    uses the same pattern that we use in search filters for media types

--- src/app/components/media/MediaTypeDisplayName.js
+++ src/app/components/media/MediaTypeDisplayName.js
@@ -17,17 +17,17 @@ export default function MediaTypeDisplayName({ mediaType }) {
   case 'Blank':
     return <FormattedMessage id="media.typeBlank" defaultMessage="Imported fact-check" description="Label to show that the type of media is was imported into the application" />;
   case 'Facebook':
-    return <FormattedMessage id="media.typeFacebook" defaultMessage="Facebook" description="Label to show that the type of media is a Facebook post" />;
+    return <FormattedMessage id="media.typeFacebook" defaultMessage="Facebook Post" description="Label to show that the type of media is a Facebook post" />;
   case 'Instagram':
-    return <FormattedMessage id="media.typeInstagram" defaultMessage="Instagram" description="Label to show that the type of media is an Instagram post" />;
+    return <FormattedMessage id="media.typeInstagram" defaultMessage="Instagram Post" description="Label to show that the type of media is an Instagram post" />;
   case 'Telegram':
     return <FormattedMessage id="media.typeTelegram" defaultMessage="Telegram" description="Label to show that the type of media is a Telegram message" />;
   case 'Tiktok':
-    return <FormattedMessage id="media.typeTiktok" defaultMessage="Tiktok" description="Label to show that the type of media is a Tiktok video" />;
+    return <FormattedMessage id="media.typeTiktok" defaultMessage="Tiktok Post" description="Label to show that the type of media is a Tiktok video" />;
   case 'Twitter':
-    return <FormattedMessage id="media.typeTwitter" defaultMessage="Twitter" description="Label to show that the type of media is a Twitter tweet" />;
+    return <FormattedMessage id="media.typeTwitter" defaultMessage="X (Twitter) Post" description="Label to show that the type of media is a Twitter tweet" />;
   case 'Youtube':
-    return <FormattedMessage id="media.typeYoutube" defaultMessage="Youtube" description="Label to show that the type of media is a Youtube video" />;
+    return <FormattedMessage id="media.typeYoutube" defaultMessage="Youtube Video" description="Label to show that the type of media is a Youtube video" />;
   case '-':
   default:
     return <React.Fragment>-</React.Fragment>;
