848618df6f946752ff5ddeffa451831dceb06995
Brian Fleming <brianfleming@users.noreply.github.com>
2024-03-06 22:26:19 -0500

    CDS and Beta (#1843)
    * update bulk add tag action button to CDS
    
    * replace super admin screen buttons with CDS [CV2-3360]
    
    * update some items to CDS. Add Beta wording to shared feeds nav items, use tooltips in side drawer rail for clarity
    
    * replace some material tooltips with CDS tooltips
    
    * [full ci] Run all integration tests for this branch
    
    * update some tests based on side nav tooltip changes [full ci]
    
    * remove unnecessary non CDS attributes on a textfield
    
    * update more integration test for changes [full ci]
    
    * update tag test for correct id [full ci]
    
    * add back in id to see if it fixes test [full ci]
    
    * use old testing rules and dom structure [full ci]

--- src/app/components/media/MediaSlug.js
+++ src/app/components/media/MediaSlug.js
@@ -1,36 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
 import MediaTypeDisplayIcon from './MediaTypeDisplayIcon';
 import BulletSeparator from '../layout/BulletSeparator';
+import styles from './media.module.css';
 
 const MediaSlug = ({
   mediaType,
   slug,
   details,
 }) => (
-  <>
-    <div>
-      <Box display="flex" alignItems="center" mt={0.5} mb={0.5}>
-        <Box mr={0.5} display="flex" alignItems="center">
-          <MediaTypeDisplayIcon mediaType={mediaType} />
-        </Box>
-        <Typography
-          variant="subtitle2"
-          style={{
-            whiteSpace: 'nowrap',
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            minWidth: 0, /* https://css-tricks.com/flexbox-truncated-text/#aa-the-solution-is-min-width-0-on-the-flex-child */
-          }}
-        >
-          {slug}
-        </Typography>
-      </Box>
-      <BulletSeparator compact details={details} />
+  <div className={styles['media-slug']}>
+    <div className={styles['media-slug-title']}>
+      <MediaTypeDisplayIcon mediaType={mediaType} />
+      <h6>{slug}</h6>
     </div>
-  </>
+    <BulletSeparator compact details={details} />
+  </div>
 );
 
 MediaSlug.propTypes = {
