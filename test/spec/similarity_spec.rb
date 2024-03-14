d92a72407a7620f41f2dbd0645d59dce34fe2590
Devin Gaffney <itsme@devingaffney.com>
2024-03-14 15:23:55 -0700

    Revert "Adding video (#1830)"
    This reverts commit 93b6dcce67ec2b522b9021ad1d1579d9e2bfc33d.

--- test/spec/similarity_spec.rb
+++ test/spec/similarity_spec.rb
@@ -105,11 +105,11 @@ shared_examples 'similarity' do
     api_create_team_and_bot(bot: 'alegre')
     @driver.navigate.to "#{@config['self_url']}/#{@slug}/settings/workspace"
     create_image('files/ocr.png')
-    verbose_wait 8
+    verbose_wait 5
     wait_for_selector('.medias__item')
     wait_for_selector('.media__heading').click
     wait_for_selector('.image-media-card')
-    verbose_wait 4
+    verbose_wait 5
     expect(@driver.page_source.include?('Extracted text')).to be(true)
     expect(@driver.page_source.include?('Test')).to be(true)
   end
