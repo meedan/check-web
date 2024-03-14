3137f42f072199574fb09cdb32e41deddd808263
Darius Kazemi <darius@meedan.com>
2024-03-14 11:03:25 -0700

    Checkboxes for WorkspaceItemCard
    If the component is provided `onCheckboxChange`, then a checkbox is rendered.

--- src/app/components/search/SearchResultsCards/WorkspaceItemCard.js
+++ src/app/components/search/SearchResultsCards/WorkspaceItemCard.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import cx from 'classnames/bind';
 import Card, { CardHoverContext } from '../../cds/media-cards/Card';
+import Checkbox from '../../cds/buttons-checkboxes-chips/Checkbox';
 import ButtonMain from '../../cds/buttons-checkboxes-chips/ButtonMain';
 import MediaCount from '../../cds/media-cards/MediaCount';
 import SuggestionsCount from '../../cds/media-cards/SuggestionsCount';
@@ -22,84 +23,96 @@ const WorkspaceItemCard = ({
   date,
   description,
   factCheckUrl,
+  isChecked,
   isUnread,
   isPublished,
   lastRequestDate,
   mediaCount,
   mediaThumbnail,
   mediaType,
+  onCheckboxChange,
   rating,
   ratingColor,
   requestsCount,
   suggestionsCount,
   title,
-}) => {
-  // eslint-disable-next-line
-  console.log('~~~',isUnread);
-
-  return (
-    <div className={cx(styles.itemCard, 'workspace-item--card')}>
-      <Card className={cx({ [styles.listItemUnread]: isUnread })}>
-        <div className={styles.sharedItemCardLeft}>
-          <ItemThumbnail picture={mediaThumbnail?.media?.picture} maskContent={mediaThumbnail?.show_warning_cover} type={mediaThumbnail?.media?.type} url={mediaThumbnail?.media?.url} />
-        </div>
-        <div className={styles.sharedItemCardMiddle}>
-          <CardHoverContext.Consumer>
-            { isHovered => (
-              <ItemDescription title={title} description={description} factCheckUrl={factCheckUrl} showCollapseButton={isHovered} />
-            )}
-          </CardHoverContext.Consumer>
-          <div>
-            <BulletSeparator
-              className={styles.bulletSeparator}
-              compact
-              details={[
-                mediaCount && (
-                  <MediaCount
-                    mediaCount={mediaCount}
-                    mediaType={mediaType}
-                  />
-                ),
-                suggestionsCount && (
-                  <SuggestionsCount
-                    suggestionsCount={suggestionsCount}
-                  />
-                ),
-                requestsCount && (
-                  <RequestsCount
-                    requestsCount={requestsCount}
-                  />
-                ),
-                lastRequestDate && (
-                  <LastRequestDate
-                    lastRequestDate={lastRequestDate}
-                  />
-                ),
-                channels && <ItemChannels channels={channels} sortMainFirst />,
-              ]}
+}) => (
+  <div className={cx(styles.itemCard, 'workspace-item--card')}>
+    <Card className={cx({ [styles.listItemUnread]: isUnread })}>
+      <div className={styles.checkbox}>
+        { onCheckboxChange && <Checkbox checked={isChecked} onChange={onCheckboxChange} /> }
+      </div>
+      <div className={styles.sharedItemCardLeft}>
+        <ItemThumbnail
+          picture={mediaThumbnail?.media?.picture}
+          maskContent={mediaThumbnail?.show_warning_cover}
+          type={mediaThumbnail?.media?.type}
+          url={mediaThumbnail?.media?.url}
+        />
+      </div>
+      <div className={styles.sharedItemCardMiddle}>
+        <CardHoverContext.Consumer>
+          { isHovered => (
+            <ItemDescription
+              title={title}
+              description={description}
+              factCheckUrl={factCheckUrl}
+              showCollapseButton={isHovered}
             />
-          </div>
+          )}
+        </CardHoverContext.Consumer>
+        <div>
+          <BulletSeparator
+            className={styles.bulletSeparator}
+            compact
+            details={[
+              mediaCount && (
+                <MediaCount
+                  mediaCount={mediaCount}
+                  mediaType={mediaType}
+                />
+              ),
+              suggestionsCount && (
+                <SuggestionsCount
+                  suggestionsCount={suggestionsCount}
+                />
+              ),
+              requestsCount && (
+                <RequestsCount
+                  requestsCount={requestsCount}
+                />
+              ),
+              lastRequestDate && (
+                <LastRequestDate
+                  lastRequestDate={lastRequestDate}
+                />
+              ),
+              channels && <ItemChannels channels={channels} sortMainFirst />,
+            ]}
+          />
         </div>
-        <div className={styles.sharedItemCardRight}>
-          <div className={styles.workspaceItemCardRating}>
-            <ItemRating rating={rating} ratingColor={ratingColor} size="small" />
-            { isPublished && <div className={styles.publishedLabel}><ButtonMain variant="contained" size="small" iconCenter={<FactCheckIcon />} disabled /></div> }
-          </div>
-          { date && <ItemDate date={date} tooltipLabel={<FormattedMessage id="sharedItemCard.lastUpdated" defaultMessage="Last Updated" description="This appears as a label before a date with a colon between them, like 'Last Updated: May 5, 2023'." />} />}
+      </div>
+      <div className={styles.sharedItemCardRight}>
+        <div className={styles.workspaceItemCardRating}>
+          <ItemRating rating={rating} ratingColor={ratingColor} size="small" />
+          { isPublished && <div className={styles.publishedLabel}><ButtonMain variant="contained" size="small" iconCenter={<FactCheckIcon />} disabled /></div> }
         </div>
-      </Card>
-    </div>
-  );
-};
+        { date && <ItemDate date={date} tooltipLabel={<FormattedMessage id="sharedItemCard.lastUpdated" defaultMessage="Last Updated" description="This appears as a label before a date with a colon between them, like 'Last Updated: May 5, 2023'." />} />}
+      </div>
+    </Card>
+  </div>
+);
 
 WorkspaceItemCard.defaultProps = {
   description: null,
   factCheckUrl: null,
+  isChecked: false,
   isUnread: false,
   isPublished: false,
   mediaCount: null,
   mediaThumbnail: null,
   mediaType: null,
+  onCheckboxChange: null,
   rating: null,
   ratingColor: null,
   suggestionsCount: null,
@@ -107,11 +120,12 @@ WorkspaceItemCard.defaultProps = {
 
 
 WorkspaceItemCard.propTypes = {
-  date: PropTypes.string.isRequired,
+  date: PropTypes.instanceOf(Date).isRequired, // Timestamp
   description: PropTypes.string,
   factCheckUrl: PropTypes.string,
   isUnread: PropTypes.bool,
   isPublished: PropTypes.bool,
+  isChecked: PropTypes.bool,
   mediaCount: PropTypes.number,
   mediaThumbnail: PropTypes.exact({
     media: PropTypes.exact({
@@ -122,6 +136,7 @@ WorkspaceItemCard.propTypes = {
     show_warning_cover: PropTypes.bool,
   }),
   mediaType: PropTypes.string,
+  onCheckboxChange: PropTypes.func,
   rating: PropTypes.string,
   ratingColor: PropTypes.string,
   suggestionsCount: PropTypes.number,
