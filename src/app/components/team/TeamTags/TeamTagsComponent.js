meedan/check-web

commit 95ca6ef12f8c3bb96197f80f0c356067c3109d95

Settings styles cleanup continuation CV2-3363 (#1665)
* update duplicate workspace dialog components to match design system CV2-3363

* continue to work on tipline settings area CV2-3363

* fix stray quote mark in translation

* work on consistent section titles CV2-3363

* use more consistent section title and wrapper elements CV2-3363

* clean up layout of bot designer side menu CV2-3363

* Update report settings area to match integrations styles using switches instead of checkbox CV2-3363

* continue to consolidate settings styles CV2-3363, update subtitle font settings to match figma [full ci]

* add back in some tesing ids on switch components for the reports settings area CV2-3363

* consolidate some table styles in the settings area CV2-3363 [full ci]

* update bots page for tests CV2-3363 [full ci]

* Update rules test for change from row click to menu button CV2-3363 [full ci]

* use css var instead of disabling stylelint

* update switch testing target on integrations page CV2-3363 [full ci]

* update old sha

* address some PR comments CV2-3363

* quiet down a couple more testing console warnings

* fix testing typo

src/app/components/team/TeamTags/TeamTagsComponent.js
@@ -6,7 +6,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import ButtonMain from '../../cds/buttons-checkboxes-chips/ButtonMain';
@@ -101,22 +100,26 @@ const TeamTagsComponent = ({
       />
       { totalTags > pageSize && // only display paginator if there are more than pageSize worth of tags overall in the database
         <div className={styles['tags-wrapper']}>
-          <Tooltip title={
-            <FormattedMessage id="search.previousPage" defaultMessage="Previous page" description="Pagination button to go to previous page" />
-          }
+          <Tooltip
+            disableHoverListener={isPaginationLoading || cursor - pageSize < 0}
+            title={
+              <FormattedMessage id="search.previousPage" defaultMessage="Previous page" description="Pagination button to go to previous page" />
+            }
           >
-            <ButtonMain
-              disabled={isPaginationLoading || cursor - pageSize < 0}
-              variant="text"
-              theme="brand"
-              size="default"
-              onClick={() => {
-                if (cursor - pageSize >= 0) {
-                  setCursor(cursor - pageSize);
-                }
-              }}
-              iconCenter={<PrevIcon />}
-            />
+            <span>
+              <ButtonMain
+                disabled={isPaginationLoading || cursor - pageSize < 0}
+                variant="text"
+                theme="brand"
+                size="default"
+                onClick={() => {
+                  if (cursor - pageSize >= 0) {
+                    setCursor(cursor - pageSize);
+                  }
+                }}
+                iconCenter={<PrevIcon />}
+              />
+            </span>
           </Tooltip>
           <span className={cx('typography-button', styles['tags-header-count'])}>
             <FormattedMessage
@@ -130,94 +133,96 @@ const TeamTagsComponent = ({
               }}
             />
           </span>
-          <Tooltip title={
-            <FormattedMessage id="search.nextPage" defaultMessage="Next page" description="Pagination button to go to next page" />
-          }
+          <Tooltip
+            disableHoverListener={isPaginationLoading || cursor + pageSize >= totalCount}
+            title={
+              <FormattedMessage id="search.nextPage" defaultMessage="Next page" description="Pagination button to go to next page" />
+            }
           >
-            <ButtonMain
-              disabled={isPaginationLoading || cursor + pageSize >= totalCount}
-              variant="text"
-              theme="brand"
-              size="default"
-              onClick={() => {
-                if (relay.hasMore() && !relay.isLoading() && (cursor + pageSize >= tags.length)) {
-                  setIsPaginationLoading(true);
-                  relay.loadMore(pageSize, () => {
+            <span>
+              <ButtonMain
+                disabled={isPaginationLoading || cursor + pageSize >= totalCount}
+                variant="text"
+                theme="brand"
+                size="default"
+                onClick={() => {
+                  if (relay.hasMore() && !relay.isLoading() && (cursor + pageSize >= tags.length)) {
+                    setIsPaginationLoading(true);
+                    relay.loadMore(pageSize, () => {
+                      setCursor(cursor + pageSize);
+                      setIsPaginationLoading(false);
+                    });
+                  } else if (cursor + pageSize < tags.length) {
                     setCursor(cursor + pageSize);
-                    setIsPaginationLoading(false);
-                  });
-                } else if (cursor + pageSize < tags.length) {
-                  setCursor(cursor + pageSize);
-                }
-              }}
-              iconCenter={<NextIcon />}
-            />
+                  }
+                }}
+                iconCenter={<NextIcon />}
+              />
+            </span>
           </Tooltip>
         </div>
       }
       <div className={cx(settingsStyles['setting-details-wrapper'])}>
         <div className={settingsStyles['setting-content-container']}>
-          <TableContainer>
-            <Table stickyHeader>
-              <TableHead>
-                <TableRow>
-                  <TableCell className={styles['table-col-head-name']}>
-                    <FormattedMessage
-                      id="teamTagsComponent.tableHeaderName"
-                      defaultMessage="Name"
-                      description="Column header in tags table."
-                    />
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell className={styles['table-col-head-name']}>
+                  <FormattedMessage
+                    id="teamTagsComponent.tableHeaderName"
+                    defaultMessage="Name"
+                    description="Column header in tags table."
+                  />
+                </TableCell>
+                <TableCell className={styles['table-col-head-updated']}>
+                  <FormattedMessage
+                    id="teamTagsComponent.tableHeaderUpdatedAt"
+                    defaultMessage="Updated"
+                    description="Column header in tags table."
+                  />
+                </TableCell>
+                <TableCell className={styles['table-col-head-items']}>
+                  <FormattedMessage
+                    id="teamTagsComponent.tableHeaderTagsCount"
+                    defaultMessage="Items"
+                    description="Column header in tags table."
+                  />
+                </TableCell>
+                <TableCell padding="checkbox" className={styles['table-col-head-action']} />
+              </TableRow>
+            </TableHead>
+            { isPaginationLoading && <MediasLoading size="medium" theme="grey" variant="inline" /> }
+            <TableBody className={isPaginationLoading && styles['tags-hide']}>
+              { tags.slice(cursor, cursor + pageSize).map(tag => (
+                <TableRow key={tag.id} className="team-tags__row">
+                  <TableCell className={classes.teamTagsTableCell}>
+                    {tag.text}
                   </TableCell>
-                  <TableCell className={styles['table-col-head-updated']}>
-                    <FormattedMessage
-                      id="teamTagsComponent.tableHeaderUpdatedAt"
-                      defaultMessage="Updated"
-                      description="Column header in tags table."
-                    />
+                  <TableCell className={classes.teamTagsTableCell}>
+                    <TimeBefore date={tag.updated_at} />
                   </TableCell>
-                  <TableCell className={styles['table-col-head-items']}>
-                    <FormattedMessage
-                      id="teamTagsComponent.tableHeaderTagsCount"
-                      defaultMessage="Items"
-                      description="Column header in tags table."
-                    />
+                  <TableCell className={classes.teamTagsTableCell}>
+                    <a href={`/${teamSlug}/all-items/%7B"tags"%3A%5B"${tag.text}"%5D%7D`} target="_blank" rel="noopener noreferrer">
+                      {tag.tags_count}
+                    </a>
                   </TableCell>
-                  <TableCell padding="checkbox" className={styles['table-col-head-action']} />
+                  <TableCell className={classes.teamTagsTableCell}>
+                    <Can permissions={permissions} permission="create TagText">
+                      <TeamTagsActions
+                        tag={tag}
+                        teamId={teamId}
+                        teamDbid={teamDbid}
+                        rules={rules}
+                        rulesSchema={rulesSchema}
+                        relay={relay}
+                        pageSize={pageSize}
+                      />
+                    </Can>
+                  </TableCell>
                 </TableRow>
-              </TableHead>
-              { isPaginationLoading && <MediasLoading size="medium" theme="grey" variant="inline" /> }
-              <TableBody className={isPaginationLoading && styles['tags-hide']}>
-                { tags.slice(cursor, cursor + pageSize).map(tag => (
-                  <TableRow key={tag.id} className="team-tags__row">
-                    <TableCell className={classes.teamTagsTableCell}>
-                      {tag.text}
-                    </TableCell>
-                    <TableCell className={classes.teamTagsTableCell}>
-                      <TimeBefore date={tag.updated_at} />
-                    </TableCell>
-                    <TableCell className={classes.teamTagsTableCell}>
-                      <a href={`/${teamSlug}/all-items/%7B"tags"%3A%5B"${tag.text}"%5D%7D`} target="_blank" rel="noopener noreferrer">
-                        {tag.tags_count}
-                      </a>
-                    </TableCell>
-                    <TableCell className={classes.teamTagsTableCell}>
-                      <Can permissions={permissions} permission="create TagText">
-                        <TeamTagsActions
-                          tag={tag}
-                          teamId={teamId}
-                          teamDbid={teamDbid}
-                          rules={rules}
-                          rulesSchema={rulesSchema}
-                          relay={relay}
-                          pageSize={pageSize}
-                        />
-                      </Can>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+              ))}
+            </TableBody>
+          </Table>
         </div>
       </div>
       { showCreateTag ?

src/app/components/cds/buttons-checkboxes-chips/ButtonMain.module.css
@@ -15,7 +15,7 @@
 
   .buttonMainLabel {
     display: inline-block;
-    max-width: 150px;
+    max-width: 250px;
     min-width: 0;
     overflow: hidden;
     text-overflow: ellipsis;

src/app/components/cds/settings-pages/TiplineContentTranslation.js
@@ -7,6 +7,7 @@ import {
   Typography,
   TextField,
 } from '@material-ui/core';
+import styles from '../../team/Settings.module.css';
 
 const StyledTextField = withStyles({
   root: {
@@ -41,16 +42,6 @@ const StyledTextField = withStyles({
 })(TextField);
 
 const useStyles = makeStyles(theme => ({
-  // FIXME: Once Typography is implemented according to specs from the design system, this custom style can be removed
-  title: {
-    fontWeight: 600,
-    fontSize: 14,
-  },
-  // FIXME: Once Typography is implemented according to specs from the design system, this custom style can be removed
-  description: {
-    fontWeight: 400,
-    fontSize: 12,
-  },
   defaultString: {
     borderTopLeftRadius: theme.spacing(1),
     borderTopRightRadius: theme.spacing(1),
@@ -99,12 +90,12 @@ const TiplineContentTranslation = ({
     <Box>
 
       {/* Text and description about this field */}
-      <Typography variant="body1" className={classes.title}>
+      <div className={styles['setting-content-container-title']}>
         {title}
-      </Typography>
-      <Typography variant="body1" className={classes.description}>
+      </div>
+      <p>
         {description}
-      </Typography>
+      </p>
 
       <Box mt={1}>
 

src/app/components/team/CreateTeamDialog.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, injectIntl, intlShape, defineMessages } from 'react-intl';
 import { makeStyles } from '@material-ui/core/styles';
 import { graphql, createFragmentContainer, commitMutation } from 'react-relay/compat';
 import { Store } from 'react-relay/classic';
-import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
+import ButtonMain from '../cds/buttons-checkboxes-chips/ButtonMain';
+import TextField from '../cds/inputs/TextField';
 import { getErrorMessage } from '../../helpers';
+
+const messages = defineMessages({
+  duplicating: {
+    id: 'createTeamDialog.duplicating',
+    defaultMessage: 'Duplicating…',
+    description: 'Button label status message while duplicating a workspace is in process',
+  },
+  duplicate: {
+    id: 'createTeamDialog.duplicate',
+    defaultMessage: 'Duplicate',
+    description: 'Button label to start to duplicate a workspace',
+  },
+  creating: {
+    id: 'createTeamDialog.creating',
+    defaultMessage: 'Creating…',
+    description: 'Button label status message while creating a new workspace is in process',
+  },
+  create: {
+    id: 'createTeamDialog.create',
+    defaultMessage: 'Create',
+    description: 'Button label to start to create a new workspace',
+  },
+});
 
 const useStyles = makeStyles(theme => ({
   textField: {
@@ -24,7 +46,7 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const CreateTeamDialog = ({ onDismiss, team }) => {
+const CreateTeamDialog = ({ onDismiss, team, intl }) => {
   const classes = useStyles();
   const [saving, setSaving] = React.useState(false);
   const [name, setName] = React.useState(team ? `Copy of ${team.name}` : '');
@@ -127,6 +149,18 @@ const CreateTeamDialog = ({ onDismiss, team }) => {
   };
 
   const handleSubmit = team ? handleDuplicate : handleCreate;
+  const { formatMessage } = intl;
+
+  let buttonLabel = null;
+  if (team && saving) {
+    buttonLabel = formatMessage(messages.duplicating);
+  } else if (team && !saving) {
+    buttonLabel = formatMessage(messages.duplicate);
+  } else if (!team && saving) {
+    buttonLabel = formatMessage(messages.creating);
+  } else if (!team && !saving) {
+    buttonLabel = formatMessage(messages.create);
+  }
 
   return (
     <Dialog open>
@@ -180,32 +214,35 @@ const CreateTeamDialog = ({ onDismiss, team }) => {
             }
             setSlug(e.target.value);
           }}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                https://checkmedia.org/
-              </InputAdornment>
-            ),
-          }}
+          helpContent={`https://checkmedia.org/${slug || autoSlug}`}
           error={Boolean(errorMessage)}
           helperText={errorMessage}
           {...textFieldProps}
         />
       </DialogContent>
       <DialogActions className={classes.actions}>
-        <Button onClick={onDismiss}>
-          <FormattedMessage
-            id="createTeamDialog.cancel"
-            defaultMessage="Cancel"
-            description="Button label to cancel creating a workspace"
-          />
-        </Button>
-        <Button color="primary" variant="contained" className="create-team-dialog__confirm-button" onClick={handleSubmit} disabled={saving || !name}>
-          { team && saving ? <FormattedMessage id="createTeamDialog.duplicating" defaultMessage="Duplicating…" description="Button label status message while duplicating a workspace is in process" /> : null }
-          { team && !saving ? <FormattedMessage id="createTeamDialog.duplice" defaultMessage="Duplicate" description="Button label to start to duplicate a workspace" /> : null }
-          { !team && saving ? <FormattedMessage id="createTeamDialog.creating" defaultMessage="Creating…" description="Button label status message while creating a new workspace is in process" /> : null }
-          { !team && !saving ? <FormattedMessage id="createTeamDialog.create" defaultMessage="Create" description="Button label to start to create a new workspace" /> : null }
-        </Button>
+        <ButtonMain
+          onClick={onDismiss}
+          theme="text"
+          variant="text"
+          size="default"
+          label={
+            <FormattedMessage
+              id="createTeamDialog.cancel"
+              defaultMessage="Cancel"
+              description="Button label to cancel creating a workspace"
+            />
+          }
+        />
+        <ButtonMain
+          theme="brand"
+          variant="contained"
+          size="default"
+          className="create-team-dialog__confirm-button"
+          onClick={handleSubmit}
+          disabled={saving || !name}
+          label={buttonLabel}
+        />
       </DialogActions>
     </Dialog>
   );
@@ -223,9 +260,10 @@ CreateTeamDialog.propTypes = {
     name: PropTypes.string.isRequired,
     slug: PropTypes.string.isRequired,
   }),
+  intl: intlShape.isRequired,
 };
 
-export default createFragmentContainer(CreateTeamDialog, graphql`
+export default createFragmentContainer(injectIntl(CreateTeamDialog), graphql`
   fragment CreateTeamDialog_team on Team {
     id
     name

src/app/components/team/CreateTeamTask.test.js
@@ -4,7 +4,7 @@ import CreateTeamTask from './CreateTeamTask';
 
 describe('<CreateTeamTask />', () => {
   it('should render create task button', () => {
-    const wrapper = mountWithIntl(<CreateTeamTask team={{}} />);
+    const wrapper = mountWithIntl(<CreateTeamTask team={{}} associatedType="ProjectMedia" />);
     expect(wrapper.find('.create-task__add-button').hostNodes()).toHaveLength(1);
   });
 });

src/app/components/team/Integrations.module.css
@@ -1,13 +1,5 @@
 .integration-bot {
-  margin: 0 0 32px;
-
   .integration-details {
-    margin: 8px 0 0;
-  }
-
-  .integration-bot-switch {
-    align-items: center;
-    display: flex;
     margin: 8px 0 0;
   }
 }

src/app/components/team/Languages/LanguageListItem.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
+import { FormattedHTMLMessage, FormattedMessage, defineMessages, injectIntl } from 'react-intl';
 import { commitMutation, createFragmentContainer, graphql } from 'react-relay/compat';
 import { Store } from 'react-relay/classic';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import cx from 'classnames/bind';
 import IconMoreVert from '../../../icons/more_vert.svg';
 import GenericUnknownErrorMessage from '../../GenericUnknownErrorMessage';
 import { FormattedGlobalMessage } from '../../MappedMessage';
@@ -16,6 +14,7 @@ import ConfirmProceedDialog from '../../layout/ConfirmProceedDialog';
 import { safelyParseJSON, getErrorMessageForRelayModernProblem } from '../../../helpers';
 import { languageLabelFull } from '../../../LanguageRegistry';
 import ButtonMain from '../../cds/buttons-checkboxes-chips/ButtonMain';
+import settingsStyles from '../Settings.module.css';
 
 const messages = defineMessages({
   deleteConfirmationText: {
@@ -176,28 +175,29 @@ const LanguageListItem = ({ code, team, intl }) => {
     });
   };
 
-  const listItemText = (
-    <span className="typography-h6">
-      { languageLabelFull(code) }
-    </span>
-  );
-
   return (
     <React.Fragment>
-      <ListItem>
-        <ListItemText
-          className={isDefault ? `language-list-item__${code}-default` : `language-list-item__${code}`}
-        >
+      <li
+        className={cx(
+          settingsStyles['setting-content-list-language'],
+          {
+            [`language-list-item__${code}-default`]: isDefault,
+            [`language-list-item__${code}`]: !isDefault,
+          })
+        }
+      >
+        <div>
           { isDefault ? (
-            <FormattedMessage
+            <FormattedHTMLMessage
               id="languageListItem.default"
-              defaultMessage="{language} (default)"
+              defaultMessage="<strong>{language}</strong> (default)"
               description="Label to indicate that this language is the default"
-              values={{ language: listItemText }}
+              values={{ language: languageLabelFull(code) }}
             />
-          ) : listItemText }
-        </ListItemText>
-        <ListItemSecondaryAction>
+          ) : <strong>{ languageLabelFull(code) }</strong>
+          }
+        </div>
+        <div className={settingsStyles['setting-content-list-actions']}>
           <ButtonMain iconCenter={<IconMoreVert />} variant="outlined" size="default" theme="text" className="language-actions__menu" onClick={e => setAnchorEl(e.target)} />
           <Menu
             anchorEl={anchorEl}
@@ -211,8 +211,8 @@ const LanguageListItem = ({ code, team, intl }) => {
               <FormattedGlobalMessage messageKey="delete" />
             </MenuItem>
           </Menu>
-        </ListItemSecondaryAction>
-      </ListItem>
+        </div>
+      </li>
       { isTranslationPending ? (
         <ConfirmProceedDialog
           body={(

commit 1ded2a8c5b7e1861da21dd43c354ea1802150ea2

Fix "sticky" tooltip
This was a bug introduced in 95ca6ef12f -- we added `disableHoverListener`, which not only disables hovering, but disables the listener that tells when hovering *finishes*. So if you press "next" until the button is disabled, then mouse away from the button, the tooltip does not get removed, because the hover listener is gone. Removing this entirely, which means the tooltip will appear on the disabled button, but better than having it stick around forever.

src/app/components/team/TeamTags/TeamTagsComponent.js
@@ -108,7 +108,6 @@ const TeamTagsComponent = ({
       { totalTags > pageSize && // only display paginator if there are more than pageSize worth of tags overall in the database
         <div className={styles['tags-wrapper']}>
           <Tooltip
-            disableHoverListener={isPaginationLoading || cursor - pageSize < 0}
             title={
               <FormattedMessage id="search.previousPage" defaultMessage="Previous page" description="Pagination button to go to previous page" />
             }
@@ -141,7 +140,6 @@ const TeamTagsComponent = ({
             />
           </span>
           <Tooltip
-            disableHoverListener={isPaginationLoading || cursor + pageSize >= totalCount}
             title={
               <FormattedMessage id="search.nextPage" defaultMessage="Next page" description="Pagination button to go to next page" />
             }