007a9505c63eafe4773a17c094d2c109a8e8ba1f
Sarah Kehoe <161879347+sarahkeh@users.noreply.github.com>
2024-04-17 09:59:37 -0700

    Cv2 4060 add navigate away dialog to shared feeds (#1892)
    * Added NavigateAwayDialog to SaveFeed, preventing user accidentally losing data when leaving the page.
    
    The unique ways each piece of data gets updated on this page made a single check for any update a little tricky.  I used a useEffect hook that checks for changes to each of the data components to avoid making changes to every onChange. A successful save switches the flag to false, allowing navigation away.  Tested by adding and removing contributors, changing name, removing List.
    
    Adding a check during editing for if the updated value matches the original value felt like overkill, but there is a case where a user undoes their change so it appears nothing is different but the input still went through changes so the popup still appears.
    
    Ticket: CV2-4060
    
    * Change to the NavigateAway solution
    
    Refactored the inputs into a single state object that uses a single update function.  This is a change from each field having its own state and update function.  The reason is to consolidate the updates into a single location making it easy to tell when the form is dirty. Three additional handlers are used for updating values that use external components, to avoid changing any other files.  This also makes it easier to see what data can be changed in the form.
    
    * Updated the Formatted message from Tasks to SaveFeed
    
    * added localization file for task -> saveFeed change
    
    * fix linter errors after merging
    
    * Fix merge error
    
    * Fix SaveFeed not navigating to Feed page
    
    ---------
    
    Co-authored-by: Alexandre Amorim <amoedoamorim@gmail.com>

--- src/app/components/feed/SaveFeed.js
+++ src/app/components/feed/SaveFeed.js
@@ -18,6 +18,7 @@ import Alert from '../cds/alerts-and-prompts/Alert';
 import ButtonMain from '../cds/buttons-checkboxes-chips/ButtonMain';
 import TextArea from '../cds/inputs/TextArea';
 import TextField from '../cds/inputs/TextField';
+import NavigateAwayDialog from '../NavigateAwayDialog';
 
 const createMutation = graphql`
   mutation SaveFeedCreateFeedMutation($input: CreateFeedInput!) {
@@ -153,15 +154,18 @@ const SaveFeed = (props) => {
   const feed = feedTeam?.feed || {}; // Editing a feed or creating a new feed
   const isFeedOwner = feedTeam?.team_id === feed?.team?.dbid;
 
-  const [title, setTitle] = React.useState(feed.name || '');
-  const [description, setDescription] = React.useState(feed.description || '');
-  const [selectedListId, setSelectedListId] = React.useState(isFeedOwner ? feed.saved_search_id : feedTeam.saved_search_id);
   const [createdFeedDbid, setCreatedFeedDbid] = React.useState(null);
-  const [newInvites, setNewInvites] = React.useState([]);
   const [showConfirmationDialog, setShowConfirmationDialog] = React.useState(false);
   const [saving, setSaving] = React.useState(false);
-  const [dataPoints, setDataPoints] = React.useState(feed.data_points || []);
+  const [isEditing, setIsEditing] = React.useState(false);
   const setFlashMessage = React.useContext(FlashMessageSetterContext);
+  const [formData, setFormData] = React.useState({
+    title: (feed.name || ''),
+    description: (feed.description || ''),
+    selectedListId: (isFeedOwner ? feed.saved_search_id : feedTeam.saved_search_id),
+    newInvites: ([]),
+    dataPoints: (feed.data_points || []),
+  });
 
   // tracking pending messages to the API for bulk email invites
   // this is not tracked as state, but rather outside the component lifecycle
@@ -172,6 +176,22 @@ const SaveFeed = (props) => {
   const handleViewFeed = (feedId) => {
     const teamSlug = window.location.pathname.match(/^\/([^/]+)/)[1];
     browserHistory.push(`/${teamSlug}/feed/${feedId}/feed`);
+  };
+
+  const handleFormUpdate = (key, value) => {
+    setFormData({
+      ...formData,
+      [key]: value,
+    });
+    setIsEditing(true);
+  };
+
+  const handleSetDataPoints = (value) => {
+    handleFormUpdate('dataPoints', value);
+  };
+
+  const handleSetNewInvites = (value) => {
+    handleFormUpdate('newInvites', value);
   };
 
   const onInviteSuccess = () => {
@@ -193,9 +213,9 @@ const SaveFeed = (props) => {
     setSaving(true);
 
     // TODO Make createFeedInvitation accept multiple emails
-    pendingMessages = newInvites.length;
+    pendingMessages = formData.newInvites.length;
 
-    newInvites.forEach((email) => {
+    formData.newInvites.forEach((email) => {
       const input = {
         feed_id: dbid,
         email,
@@ -210,33 +230,48 @@ const SaveFeed = (props) => {
   };
 
   React.useEffect(() => {
-    if (createdFeedDbid && newInvites.length) {
+    if (createdFeedDbid && formData.newInvites.length) {
       handleInvite(createdFeedDbid);
     }
   }, [createdFeedDbid]);
+
+
+  // ---- isEditing state changed callback (start) ----
+  // This makes sure we're tring to navigate to the feed only after a save
+  // and the isEditing state is commited back to false and the NavigateAwayDialog
+  // is then unmounted, which prevented, well, navigating away.
+  const prevEditingRef = React.useRef(false);
+
+  React.useEffect(() => {
+    if (createdFeedDbid && prevEditingRef.current === true) { // .current is SO misleading, it actually means previous
+      if (!formData.newInvites.length) {
+        handleViewFeed(createdFeedDbid);
+      }
+    }
+    prevEditingRef.current = isEditing;
+  }, [isEditing]);
+  // ---- isEditing state changed callback (end) ----
 
   const onSuccess = (response) => {
     const dbid = response?.createFeed?.feed?.dbid || feed.dbid;
     setCreatedFeedDbid(dbid);
     setSaving(false);
-    if (!newInvites.length) {
-      handleViewFeed(dbid);
-    }
+    setIsEditing(false);
   };
 
   // Error states that cause the save/edit button to disable
-  const noTitle = title.length === 0;
-  const disableSaveButton = saving || noTitle || dataPoints.length === 0;
+  const noTitle = formData.title.length === 0;
+  const disableSaveButton = saving || noTitle || formData.dataPoints.length === 0;
 
   const handleSave = () => {
     setSaving(true);
     const licenses = [];
     const input = {
-      name: title,
-      description,
-      saved_search_id: selectedListId,
+      name: formData.title,
+      description: formData.description,
+      saved_search_id: formData.selectedListId,
       licenses,
-      dataPoints,
+      dataPoints: formData.dataPoints,
       published: true,
     };
     if (feed.id) {
@@ -257,7 +292,7 @@ const SaveFeed = (props) => {
     setSaving(true);
     const input = {
       id: feedTeam.id,
-      saved_search_id: selectedListId,
+      saved_search_id: formData.selectedListId,
     };
 
     commitMutation(Relay.Store, {
@@ -271,7 +306,7 @@ const SaveFeed = (props) => {
   const handleConfirmOrSave = () => {
     if (feed.id && !isFeedOwner) {
       handleSaveFeedTeam();
-    } else if (feed.id || newInvites.length) {
+    } else if (feed.id || formData.newInvites.length) {
       setShowConfirmationDialog(true);
     } else {
       handleSave();
@@ -422,8 +457,8 @@ const SaveFeed = (props) => {
                   />}
                   error={noTitle}
                   suppressInitialError
-                  value={title}
-                  onChange={e => setTitle(e.target.value)}
+                  value={formData.title}
+                  onChange={e => handleFormUpdate('title', e.target.value)}
                   required
                 />
               )}
@@ -442,8 +477,8 @@ const SaveFeed = (props) => {
                     defaultMessage="Description"
                     description="Label for a field where the user inputs text for a description to a shared feed"
                   />}
-                  value={description}
-                  onChange={e => setDescription(e.target.value)}
+                  value={formData.description}
+                  onChange={e => handleFormUpdate('description', e.target.value)}
                 />
               )}
             </FormattedMessage>
@@ -453,16 +488,16 @@ const SaveFeed = (props) => {
         <div className={styles.saveFeedCard}>
           <FeedDataPoints
             readOnly={Boolean(feed.id)}
-            dataPoints={dataPoints}
-            onChange={setDataPoints}
+            dataPoints={formData.dataPoints}
+            onChange={handleSetDataPoints}
           />
 
-          { dataPoints.length > 0 ?
+          { formData.dataPoints.length > 0 ?
             <FeedContent
-              listId={selectedListId}
-              dataPoints={dataPoints}
-              onChange={e => setSelectedListId(+e.target.value)}
-              onRemove={() => setSelectedListId(null)}
+              listId={formData.selectedListId}
+              dataPoints={formData.dataPoints}
+              onChange={e => handleFormUpdate('selectedListId', +e.target.value)}
+              onRemove={() => handleFormUpdate('selectedListId', null)}
             />
             : null
           }
@@ -507,7 +542,7 @@ const SaveFeed = (props) => {
         <FeedCollaboration
           collaboratorId={feedTeam?.team_id}
           feed={feed}
-          onChange={setNewInvites}
+          onChange={handleSetNewInvites}
           permissions={permissions}
           readOnly={feedTeam?.team_id && feedTeam?.team_id !== feed?.team?.dbid}
         />
@@ -542,7 +577,7 @@ const SaveFeed = (props) => {
                 />
               </p>
             }
-            { newInvites.length ?
+            { formData.newInvites.length ?
               <>
                 <p>
                   <FormattedMessage
@@ -552,7 +587,7 @@ const SaveFeed = (props) => {
                   />
                 </p>
                 <ul>
-                  { newInvites.map(email => (
+                  { formData.newInvites.map(email => (
                     <li key={email} className={styles.invitedEmail}>
                       &bull; {email}
                     </li>
@@ -571,6 +606,27 @@ const SaveFeed = (props) => {
         onCancel={() => { setShowConfirmationDialog(false); }}
         isSaving={saving}
       />
+
+      {
+        isEditing &&
+          <NavigateAwayDialog
+            hasUnsavedChanges
+            title={
+              <FormattedMessage
+                id="saveFeed.confirmLeaveTitle"
+                defaultMessage="Do you want to leave without saving?"
+                description="This is a prompt that appears when a user tries to exit a page before saving their work."
+              />
+            }
+            body={
+              <FormattedMessage
+                id="saveFeed.confirmLeave"
+                defaultMessage="You have unsaved changes to your shared feed. Do you wish to continue to a new page? Your work will not be saved."
+                description="This is a prompt that appears when a user tries to exit a page before saving their work."
+              />
+            }
+          />
+      }
     </div>
   );
 };
