214ca8a7e3ef78e999c7a4d1c98f6e968b874e2a
danielevalverde <danierzevalverde@gmail.com>
2024-04-17 15:12:16 -0300

    Fix sentry errors

---
+++ src/app/components/feed/FeedItemTeams.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallowWithIntl } from '../../../../test/unit/helpers/intl-test';
+import { FeedItemTeams } from './FeedItemTeams';
+
+describe('<FeedItemTeams />', () => {
+  const team = {
+    id: '1',
+    dbid:'1',
+    name: 'teamName',
+    slug: 'slugTeam',
+    members_count: 1,
+    project_groups: { edges: [{ node: { project_group_id: '1', title: 'title' } }] },
+    projects: {
+      edges: [{
+        node: {
+          title: 'title', dbid: 2, project_group_id: '1', id: 'ABJvamVjdC70\n',
+        },
+      }],
+    },
+  };
+
+  const feed = {
+    dbid: 1,
+    name: 'Feed Name',
+  };
+
+  const cluster = {
+    last_request_date: null,
+    center: {
+      title: 'Cluster one center title',
+      media: {
+        url: null,
+        type: 'UploadedImage',
+        picture: 'https://assets.checkmedia.org/image/1.png',
+      },
+    },
+  };
+
+  it('should render teams', () => {
+    const wrapper = shallowWithIntl(<FeedItemTeams team={team} feed={feed} cluster={cluster} />);
+    expect(wrapper.find('#feed-item-page-teams')).toHaveLength(1);
+  });
+
+  it('should render without crashing when center is null', () => {
+    const clusterWithoutCenter = {
+      ...cluster,
+      center: null,
+    };
+    const wrapper = shallowWithIntl(<FeedItemTeams team={team} feed={feed} cluster={clusterWithoutCenter} />);
+    expect(wrapper.find('#feed-item-page-teams')).toHaveLength(1);
+  });
+});
