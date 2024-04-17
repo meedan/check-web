365e65b0bc39db5a93a7e76050e0b4ec80efe8f1
Daniele Valverde <34126648+danielevalverde@users.noreply.github.com>
2024-04-17 15:56:16 -0300

    Fix sentry errors and add unit tests (#1900)
    Reproduced the issues using unit tests and fix by adding self-navigation
    
    Reference: CV2-4510, CV2-4511

--- src/app/components/feed/FeedItemHeader.test.js
+++ src/app/components/feed/FeedItemHeader.test.js
@@ -20,8 +20,32 @@ describe('<FeedItemHeader />', () => {
     },
   };
 
+  const team = {
+    id: '1',
+    name: 'teamName',
+    slug: 'slugTeam',
+    members_count: 1,
+    project_groups: { edges: [{ node: { project_group_id: '1', title: 'title' } }] },
+    projects: {
+      edges: [{
+        node: {
+          title: 'title', dbid: 2, project_group_id: '1', id: 'ABJvamVjdC70\n',
+        },
+      }],
+    },
+  };
+
   it('should render header', () => {
-    const wrapper = shallowWithIntl(<FeedItemHeader teamSlug="test" feed={feed} cluster={cluster} />);
+    const wrapper = shallowWithIntl(<FeedItemHeader team={team} teamSlug="test" feed={feed} cluster={cluster} />);
+    expect(wrapper.find('#feed-item-page-header')).toHaveLength(1);
+  });
+
+  it('should render header when center is null', () => {
+    const clusterWithoutCenter = {
+      ...cluster,
+      center: null,
+    };
+    const wrapper = shallowWithIntl(<FeedItemHeader team={team} teamSlug="test" feed={feed} cluster={clusterWithoutCenter} />);
     expect(wrapper.find('#feed-item-page-header')).toHaveLength(1);
   });
 });
