dbb78be8dd865cb9d97bab1c5d407d9995d552b0
danielevalverde <danierzevalverde@gmail.com>
2024-04-25 12:51:24 -0300

    fix syntax [similarity tests]

--- build.sh
+++ build.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
+echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+echo "TRAVIS_COMMIT_MESSAGE: $TRAVIS_COMMIT_MESSAGE"
+
 # Running only unit tests
 if [[ $TRAVIS_BRANCH != 'develop' && $TRAVIS_BRANCH != 'master' && ! $TRAVIS_COMMIT_MESSAGE =~ \[full\ ci\] && ! $TRAVIS_COMMIT_MESSAGE =~ \[smoke\ tests\] && ! $TRAVIS_COMMIT_MESSAGE =~ \[similarity\ tests\] ]]
 then
+  echo "Running only unit tests"
   docker-compose build web
   docker-compose -f docker-compose.yml -f docker-test.yml up -d web
   until curl --silent -I -f --fail http://localhost:3333; do printf .; sleep 1; done
 # Running all tests
 else
+  echo "ELSEEEEE"
   if [[ $TRAVIS_JOB_NAME == 'integration-and-unit-tests' ]]
   then
+    echo "UP DOCKER COMPOSE"
+    echo "inside integration-and-unit-tests"
     docker-compose build web api api-background pender pender-background
     docker-compose -f docker-compose.yml -f docker-test.yml up -d web api api-background pender pender-background chromedriver
   else
+  echo "inside similarity tests"
     i=0
     NGROK_URL=""
-    ngrok config add-authtoken $NGROK_AUTH
+    # ngrok config add-authtoken $NGROK_AUTH
     while [ -z "$NGROK_URL" -a $i -lt 5 ]; do
       i=$(($i + 1))
       ngrok_output=$(ngrok http 9000 2>&1 &)
-      if [[ $ngrok_output == *"Your account is limited to 1 simultaneous ngrok agent sessions"* ]]; then
-        # echo "Ngrok failed: $ngrok_output"
-        echo "Your account is limited to 1 simultaneous ngrok agent session. Please wait for any other similarity builds to finish before trying again."
-        exit 1
-      fi
       # ngrok http 9000 >/dev/null &
       until curl --silent -I -f --fail http://localhost:4040; do printf .; sleep 1; done
       curl -I -v http://localhost:4040
@@ -35,7 +38,13 @@ else
         kill -9 $(pgrep ngrok)
       fi
       sleep 5
+      echo "while while... $i"
     done
+    if [[ $ngrok_output == *"Your account is limited to 1 simultaneous ngrok agent sessions"* ]]; then
+      # echo "Ngrok failed: $ngrok_output"
+      echo "Your account is limited to 1 simultaneous ngrok agent session. Please wait for any other similarity builds to finish before trying again."
+      exit 1
+    fi
     if [ -z $NGROK_URL ]
     then
       echo "Not able to connect a Ngrok Tunnel. Please try again!"
