de4da5c99c9a4cf7ff3bf365273ebafe5067055f
danielevalverde <danierzevalverde@gmail.com>
2024-04-25 10:14:16 -0300

    breaks build when run simultaneous similarity jobs

--- build.sh
+++ build.sh
@@ -18,7 +18,8 @@ else
     ngrok config add-authtoken $NGROK_AUTH
     while [ -z "$NGROK_URL" -a $i -lt 5 ]; do
       i=$(($i + 1))
-      ngrok http 9000 >/dev/null &
+      ngrok_output=$(ngrok http 9000 2>&1 &)
+      # ngrok http 9000 >/dev/null &
       until curl --silent -I -f --fail http://localhost:4040; do printf .; sleep 1; done
       curl -I -v http://localhost:4040
       curl localhost:4040/api/tunnels > ngrok.json
@@ -30,6 +31,11 @@ else
       fi
       sleep 5
     done
+    if [[ $ngrok_output == *"Your account is limited to 1 simultaneous ngrok agent sessions"* ]]; then
+      # echo "Ngrok failed: $ngrok_output"
+      echo "Your account is limited to 1 simultaneous ngrok agent session. Please wait for any other similarity builds to finish before trying again."
+      exit 1
+    fi
     if [ -z $NGROK_URL ]
     then
       echo "Not able to connect a Ngrok Tunnel. Please try again!"
