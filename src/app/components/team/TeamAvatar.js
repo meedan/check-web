ba6ef78d958e64ca9340cf7835c1465aa2e37216
Brian Fleming <brianfleming@gmail.com>
2024-05-10 12:54:35 -0400

    remove styled components from teamavatar

--- src/app/components/team/TeamAvatar.js
+++ src/app/components/team/TeamAvatar.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const StyledAvatarDiv = styled.div`
-  border: 1px solid var(--grayDisabledBackground);
-  border-radius: 2px;
-  flex: 0 0 auto;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  background-color: var(--otherWhite);
-  width: ${props => (props.size ? props.size : '40px')};
-  height: ${props => (props.size ? props.size : '40px')};
-  border-radius: 5px;
-  border: 2px solid var(--grayBorderMain);
-  position: relative;
-`;
+import styles from './TeamAvatar.module.css';
 
 const TeamAvatar = props => (
-  <StyledAvatarDiv {...props} style={{ backgroundImage: `url(${props.team?.avatar})` }} />
+  <div
+    {...props}
+    className={styles['team-avatar']}
+    style={{
+      backgroundImage: `url(${props.team?.avatar})`,
+      width: props.size ? props.size : null,
+      height: props.size ? props.size : null,
+    }}
+  />
 );
 
 export default TeamAvatar;
