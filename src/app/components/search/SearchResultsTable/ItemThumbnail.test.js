2b1d2d56e68545c94b17f9617282702a6a8bbf3d
Daniele Valverde <34126648+danielevalverde@users.noreply.github.com>
2024-05-31 16:04:12 -0300

    CV2-4685: pass media type to display media icon when there is no picture and improve unit tests (#1962)
    * improve unit tests and pass media type for display media icon
    
    Reference: CV2-4685
    
    * fix media type on unit tests

--- src/app/components/search/SearchResultsTable/ItemThumbnail.test.js
+++ src/app/components/search/SearchResultsTable/ItemThumbnail.test.js
@@ -6,28 +6,24 @@ import ItemThumbnail from './ItemThumbnail';
 import MediaTypeDisplayIcon from '../../media/MediaTypeDisplayIcon';
 
 describe('<ItemThumbnail />', () => {
-  it('should render ItemThumbnail', () => {
+  it('should render picture even when there is no media type', () => {
     const wrapper = mountWithIntl(
       <ItemThumbnail
-        type="image"
         picture="test.jpg"
         maskContent={false}
         url="http://test.com"
       />);
     expect(wrapper.find('img').length).toEqual(1);
     expect(wrapper.find(EmptyMediaIcon).length).toEqual(0);
-    expect(wrapper.find(VisibilityOffIcon).length).toEqual(0);
   });
 
-  it('should render when there is no media type', () => {
+  it('should render MediaTypeDisplayIcon when there is no picture', () => {
     const wrapper = mountWithIntl(
       <ItemThumbnail
-        picture="test.jpg"
         maskContent={false}
-        url="http://test.com"
+        type="UploadedImage"
       />);
-    expect(wrapper.find('img').length).toEqual(1);
-    expect(wrapper.find(EmptyMediaIcon).length).toEqual(0);
+    expect(wrapper.find(MediaTypeDisplayIcon).length).toEqual(1);
   });
 
   it('should render EmptyMediaIcon when there is no media type and no picture', () => {
@@ -47,14 +43,5 @@ describe('<ItemThumbnail />', () => {
         url="http://test.com"
       />);
     expect(wrapper.find(VisibilityOffIcon).length).toEqual(1);
-  });
-
-  it('should render MediaTypeDisplayIcon when there is no picture', () => {
-    const wrapper = mountWithIntl(
-      <ItemThumbnail
-        maskContent={false}
-        type="image"
-      />);
-    expect(wrapper.find(MediaTypeDisplayIcon).length).toEqual(1);
   });
 });
