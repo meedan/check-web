ad396f569e845cc67e4b26502b56b8fc2e35fd71
Brian Fleming <brianfleming@users.noreply.github.com>
2024-06-03 09:49:49 -0400

    Epic 4441 misc updates (#1957)
    * change 404 language to be less cutsey
    
    * add new beige button themes and apply to side navigation new buttons
    
    * update beige button colors
    
    * add missing classname in main button for new beige color
    
    * fix some consistency issues on the side navigation and restyle custom lists to have an easier to read layout
    
    * add missing beige prop type to main button

--- src/app/components/drawer/Projects/FeedsComponent.js
+++ src/app/components/drawer/Projects/FeedsComponent.js
@@ -81,7 +81,7 @@ const FeedsComponent = ({
             iconLeft={<AddIcon />}
             variant="contained"
             size="default"
-            theme="text"
+            theme="lightBeige"
             onClick={(e) => { handleCreateFeed(); e.stopPropagation(); }}
           />
         </div>
