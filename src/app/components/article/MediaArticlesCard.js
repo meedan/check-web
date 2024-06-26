8d3134897b746876fcc28bf1f55732a8ffeba9d0
Caio Almeida <117518+caiosba@users.noreply.github.com>
2024-06-26 14:52:20 -0300

    Add articles to item from item page (#1983)
    See Figma designs in referenced Jira ticket for details. But the main ideas here are:
    
    - Delete the left sidebar from the item page (the one with claim and fact-check)
    - Add a new right sidebar called "Articles"
    - In that sidebar, for items with no articles (so, no explainers and no fact-check), show a button to create new article and list articles to be applied to item
    - Clicking on an article (explainer or fact-check) adds it to the current item
    - The articles in that right sidebar have a hover style
    - When the item has an article, the right sidebar shows only the "New article" button - this case is going to be handled by another ticket
    
    Reference: CV2-4626

---
+++ src/app/components/article/MediaArticlesCard.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createFragmentContainer, graphql } from 'react-relay/compat';
+import { FormattedMessage } from 'react-intl';
+import cx from 'classnames/bind';
+import Tooltip from '../cds/alerts-and-prompts/Tooltip';
+import AddIcon from '../../icons/add.svg';
+import BookIcon from '../../icons/book.svg';
+import FactCheckIcon from '../../icons/fact_check.svg';
+import EllipseIcon from '../../icons/ellipse.svg';
+import { getStatus, getStatusStyle } from '../../helpers';
+import styles from './Articles.module.css';
+
+const MediaArticlesCard = ({ article, team, onAdd }) => {
+  const [isHovered, setIsHovered] = React.useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const handleClick = (e) => {
+    if (onAdd) {
+      let id = null;
+      if (article.nodeType === 'FactCheck') {
+        ({ id } = article.claim_description);
+      } else if (article.nodeType === 'Explainer') {
+        id = article.dbid;
+      }
+      onAdd(article.nodeType, id);
+    }
+    e.stopPropagation();
+  };
+
+  let ratingLabel = null;
+  let ratingColor = null;
+  if (article.rating) {
+    const status = getStatus(team.verification_statuses, article.rating);
+    ratingLabel = status.label;
+    ratingColor = getStatusStyle(status, 'color');
+  }
+
+  return (
+    <Tooltip
+      key={article.id}
+      placement="top"
+      title={
+        <FormattedMessage
+          id="articlesSidebarCard.tooltip"
+          defaultMessage="Add article to this media cluster"
+          description="Tooltip message displayed on explainer cards on item page."
+        />
+      }
+      arrow
+    >
+      <div className={styles.articlesSidebarCard} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick} onKeyDown={handleClick}>
+        <div className={styles.articlesSidebarCardIcon}>
+          { isHovered && <AddIcon /> }
+          { article.nodeType === 'Explainer' && !isHovered && <BookIcon /> }
+          { article.nodeType === 'FactCheck' && !isHovered && <FactCheckIcon /> }
+        </div>
+        <div className={cx('typography-body1', styles.articlesSidebarCardTitle)}>
+          {article.title}
+          { ratingLabel && ratingColor && <div className={cx('typography-caption', styles.articlesSidebarCardCaption)}><EllipseIcon style={{ color: ratingColor }} /> {ratingLabel}</div> }
+        </div>
+      </div>
+    </Tooltip>
+  );
+};
+
+MediaArticlesCard.defaultProps = {
+  onAdd: null,
+};
+
+MediaArticlesCard.propTypes = {
+  team: PropTypes.shape({
+    verification_statuses: PropTypes.object.isRequired,
+  }).isRequired,
+  article: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    dbid: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    rating: PropTypes.string,
+    claim_description: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+    nodeType: PropTypes.oneOf(['FactCheck', 'Explainer']).isRequired,
+  }).isRequired,
+  onAdd: PropTypes.func,
+};
+
+// eslint-disable-next-line import/no-unused-modules
+export { MediaArticlesCard }; // For unit test
+
+export default createFragmentContainer(MediaArticlesCard, graphql`
+  fragment MediaArticlesCard_team on Team {
+    verification_statuses
+  }
+  fragment MediaArticlesCard_article on Node {
+    nodeType: __typename
+    ... on Explainer {
+      id
+      dbid
+      title
+    }
+    ... on FactCheck {
+      id
+      dbid
+      title
+      rating
+      claim_description {
+        id
+      }
+    }
+  }
+`);
