37c1f7648dc1bc15ab164e90985e7ae4b3018c54
Caio Almeida <117518+caiosba@users.noreply.github.com>
2024-06-26 14:52:20 -0300

    Add articles to item from item page (#1983)
    See Figma designs in referenced Jira ticket for details. But the main ideas here are:
    
    - Delete the left sidebar from the item page (the one with claim and fact-check)
    - Add a new right sidebar called "Articles"
    - In that sidebar, for items with no articles (so, no explainers and no fact-check), show a button to create new article and list articles to be applied to item
    - Clicking on an article (explainer or fact-check) adds it to the current item
    - The articles in that right sidebar have a hover style
    - When the item has an article, the right sidebar shows only the "New article" button - this case is going to be handled by another ticket
    
    Reference: CV2-4626

--- src/app/components/article/NewArticleButton.js
+++ src/app/components/article/NewArticleButton.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Menu from '@material-ui/core/Menu';
 import { FormattedMessage } from 'react-intl';
 import MenuItem from '@material-ui/core/MenuItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Tooltip from '../cds/alerts-and-prompts/Tooltip';
 import ButtonMain from '../cds/buttons-checkboxes-chips/ButtonMain';
 import AddIcon from '../../icons/add.svg';
 import styles from './NewArticleButton.module.css';
@@ -12,7 +14,36 @@ import BookIcon from '../../icons/book.svg';
 import ExplainerForm from './ExplainerForm';
 import ClaimFactCheckForm from './ClaimFactCheckForm';
 
-const NewArticleButton = (team) => {
+const NewArticleButtonWrapper = ({ disabled, children }) => {
+  if (disabled) {
+    return (
+      <Tooltip
+        key="create-article-button"
+        placement="top"
+        title={
+          <FormattedMessage
+            id="newArticleButton.tooltip"
+            defaultMessage="You can't add an article here."
+            description="Tooltip message displayed on new article button when it is disabled."
+          />
+        }
+        arrow
+      >
+        <div className="new-article-button__tooltip-children">
+          {children}
+        </div>
+      </Tooltip>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      {children}
+    </React.Fragment>
+  );
+};
+
+const NewArticleButton = ({ team, disabled, buttonMainProps }) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [openExplainer, setOpenExplainer] = React.useState(false);
   const [openFactCheck, setOpenFactCheck] = React.useState(false);
@@ -30,17 +61,25 @@ const NewArticleButton = (team) => {
   };
 
   return (
-    <React.Fragment>
+    <NewArticleButtonWrapper disabled={disabled}>
       <ButtonMain
         variant="contained"
         size="default"
         theme="lightBeige"
         iconLeft={<AddIcon />}
         onClick={e => setAnchorEl(e.currentTarget)}
-        label="New Article"
+        label={
+          <FormattedMessage
+            id="newArticleButton.newArticle"
+            defaultMessage="New Article"
+            description="Label of a button that opens a form to create a new article."
+          />
+        }
+        disabled={disabled}
         buttonProps={{
           id: 'new-article-menu__open-button',
         }}
+        {...buttonMainProps}
       />
       <Menu
         anchorEl={anchorEl}
@@ -77,8 +116,19 @@ const NewArticleButton = (team) => {
       </Menu>
       {openExplainer && <ExplainerForm team={team} onClose={setOpenExplainer} />}
       {openFactCheck && <ClaimFactCheckForm team={team} onClose={setOpenFactCheck} />}
-    </React.Fragment>
+    </NewArticleButtonWrapper>
   );
+};
+
+NewArticleButton.defaultProps = {
+  disabled: false,
+  buttonMainProps: {},
+};
+
+NewArticleButton.propTypes = {
+  team: PropTypes.object.isRequired,
+  disabled: PropTypes.bool,
+  buttonMainProps: PropTypes.object,
 };
 
 export default NewArticleButton;
