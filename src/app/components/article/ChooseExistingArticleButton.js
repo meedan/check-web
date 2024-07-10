ae557932b8214118719c53130272418f0686df7b
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2024-07-10 10:40:32 -0300

    Feature/4628 search for articles to add (#1990)
    * Implement search existing article button and slideout
    
    * add unit test
    
    * add BEM class
    
    * Add empty state for article search
    
    ---------
    
    Co-authored-by: Brian Fleming <brianfleming@gmail.com>

---
+++ src/app/components/article/ChooseExistingArticleButton.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import cx from 'classnames/bind';
+import MediaArticlesTeamArticles from './MediaArticlesTeamArticles';
+import SearchIcon from '../../icons/search.svg';
+import ButtonMain from '../cds/buttons-checkboxes-chips/ButtonMain';
+import Slideout from '../cds/slideout/Slideout';
+import TextField from '../cds/inputs/TextField';
+import styles from './ChooseExistingArticleButton.module.css';
+
+let lastTypedValue = '';
+
+const ChooseExistingArticleButton = ({ teamSlug, onAdd }) => {
+  const [openSlideout, setOpenSlideout] = React.useState(false);
+  const [search, setSearch] = React.useState('');
+
+  const title = (
+    <FormattedMessage
+      id="chooseExistingArticleButton.title"
+      defaultMessage="Choose an existing article"
+      description="Label for the button to find an article."
+    />
+  );
+
+  const handleCloseSlideout = () => {
+    setSearch('');
+    setOpenSlideout(false);
+  };
+
+  const handleType = (value) => {
+    lastTypedValue = value;
+    setTimeout(() => {
+      if (value === lastTypedValue) {
+        setSearch(value);
+      }
+    }, 1500);
+  };
+
+  return (
+    <>
+      <ButtonMain
+        className="choose-existing-article-button__open-slideout"
+        variant="contained"
+        size="small"
+        theme="text"
+        iconLeft={<SearchIcon />}
+        onClick={() => setOpenSlideout(true)}
+        label={title}
+      />
+      { openSlideout && (
+        <Slideout
+          title={title}
+          content={
+            <div className={styles.contentWrapper}>
+              <FormattedMessage
+                id="chooseExistingArticleButton.search"
+                defaultMessage="Search by article title or summary"
+                description="Label for the search field to find an article."
+              >
+                {placeholder => (
+                  <TextField
+                    id="search-articles"
+                    iconLeft={<SearchIcon />}
+                    placeholder={placeholder}
+                    onChange={e => handleType(e.target.value)}
+                  />
+                )}
+              </FormattedMessage>
+              <div>
+                { !search && (
+                  <div className={cx('typography-body2', styles.heading)}>
+                    <FormattedMessage
+                      id="chooseExistingArticleButton.recentArticles"
+                      defaultMessage="Recent Articles"
+                      description="Label for the list of articles."
+                    />
+                  </div>
+                )}
+                <MediaArticlesTeamArticles textSearch={search} teamSlug={teamSlug} onAdd={onAdd} />
+              </div>
+            </div>
+          }
+          onClose={handleCloseSlideout}
+        />
+      )}
+    </>
+  );
+};
+
+ChooseExistingArticleButton.propTypes = {
+  teamSlug: PropTypes.string.isRequired,
+  onAdd: PropTypes.func.isRequired,
+};
+
+export default ChooseExistingArticleButton;
---
+++ src/app/components/article/ChooseExistingArticleButton.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallowWithIntl } from '../../../../test/unit/helpers/intl-test';
+import ChooseExistingArticleButton from './ChooseExistingArticleButton';
+
+describe('<ChooseExistingArticleButton />', () => {
+  it('should open slideout', () => {
+    const wrapper = shallowWithIntl(<ChooseExistingArticleButton teamSlug='meedan' onAdd={() => {}} />);
+    wrapper.find('ButtonMain').simulate('click');
+    expect(wrapper.find('Slideout')).toHaveLength(1);
+  });
+});
