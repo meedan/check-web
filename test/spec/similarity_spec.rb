5fc51ef0203c7350475a8738ae474cbda5f9a326
danielevalverde <danierzevalverde@gmail.com>
2024-07-24 16:33:19 -0300

    fix linter issues

--- test/spec/similarity_spec.rb
+++ test/spec/similarity_spec.rb
@@ -36,7 +36,7 @@ shared_examples 'similarity' do
     @driver.navigate.to "#{@config['self_url']}/#{data[:team].slug}/media/#{pm1.id}"
     wait_for_selector('#media-similarity__add-button')
     wait_for_selector("//span[contains(text(), 'Suggestions')]", :xpath).click
-    @driver.action.move_to(wait_for_selector('.suggested-media__item')).perform #hover element
+    @driver.action.move_to(wait_for_selector('.suggested-media__item')).perform # hover element
     wait_for_selector('.similarity-media-item__accept-relationship').click
     @driver.navigate.refresh
     wait_for_selector('.media__relationship')
@@ -69,9 +69,9 @@ shared_examples 'similarity' do
     wait_for_selector('#media-similarity__add-button')
     expect(@driver.find_elements(:css, '.media__relationship').size).to eq 0
     wait_for_selector("//span[contains(text(), 'Suggestions')]", :xpath).click
-    @driver.action.move_to(wait_for_selector('.suggested-media__item')).perform #hover element
+    @driver.action.move_to(wait_for_selector('.suggested-media__item')).perform # hover element
     wait_for_selector('.similarity-media-item__accept-relationship').click
-    @driver.action.move_to(wait_for_selector_list('.suggested-media__item')[1]).perform #hover element
+    @driver.action.move_to(wait_for_selector_list('.suggested-media__item')[1]).perform # hover element
     wait_for_selector_list('.similarity-media-item__reject-relationship')[1].click
     @driver.navigate.refresh
     wait_for_selector('#media-similarity__add-button')
