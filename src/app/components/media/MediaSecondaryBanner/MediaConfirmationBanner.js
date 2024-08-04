43d6ca85238c8a15d50bd59d5307a3ee716cedab
Caio <117518+caiosba@users.noreply.github.com>
2024-08-04 19:45:37 -0300

    Ticket CV2-5004: Finished design for suggestion box

--- src/app/components/media/MediaSecondaryBanner/MediaConfirmationBanner.js
+++ src/app/components/media/MediaSecondaryBanner/MediaConfirmationBanner.js
@@ -6,28 +6,32 @@ import Alert from '../../cds/alerts-and-prompts/Alert';
 import MediaMainItemPreview from './MediaMainItemPreview';
 
 const MediaConfirmationBanner = ({ projectMedia }) => (
-  <Alert
-    icon
-    variant="success"
-    title={
-      <FormattedMessage
-        id="mediaConfirmationBanner.alertTitle"
-        defaultMessage="Requests and Media have been matched to another item in this workspace."
-        description="Title of the alert message displayed on item page for confirmed matches."
+  <>
+    { projectMedia.is_confirmed && (
+      <Alert
+        icon
+        variant="success"
+        title={
+          <FormattedMessage
+            id="mediaConfirmationBanner.alertTitle"
+            defaultMessage="Requests and Media have been matched to another item in this workspace."
+            description="Title of the alert message displayed on item page for confirmed matches."
+          />
+        }
+        content={
+          <>
+            <FormattedMessage
+              id="mediaConfirmationBanner.alertContent"
+              defaultMessage="All requests for this item will be responded to by the matched item below."
+              description="Content of the alert message displayed on item page for confirmed matches."
+              component="div"
+            />
+            { projectMedia.confirmed_main_item && <MediaMainItemPreview projectMedia={projectMedia.confirmed_main_item} /> }
+          </>
+        }
       />
-    }
-    content={
-      <>
-        <FormattedMessage
-          id="mediaConfirmationBanner.alertContent"
-          defaultMessage="All requests for this item will be responded to by the matched item below."
-          description="Content of the alert message displayed on item page for confirmed matches."
-          component="div"
-        />
-        { projectMedia.confirmed_main_item && <MediaMainItemPreview projectMedia={projectMedia.confirmed_main_item} /> }
-      </>
-    }
-  />
+    )}
+  </>
 );
 
 MediaConfirmationBanner.propTypes = {
@@ -36,6 +40,7 @@ MediaConfirmationBanner.propTypes = {
 
 export default createFragmentContainer(MediaConfirmationBanner, graphql`
   fragment MediaConfirmationBanner_projectMedia on ProjectMedia {
+    is_confirmed
     confirmed_main_item {
       ...MediaMainItemPreview_projectMedia
     }
--- src/app/components/media/MediaSecondaryBanner/MediaSuggestionBanner.js
+++ src/app/components/media/MediaSecondaryBanner/MediaSuggestionBanner.js
@@ -4,25 +4,57 @@ import { FormattedMessage } from 'react-intl';
 import { graphql, createFragmentContainer } from 'react-relay/compat';
 import Alert from '../../cds/alerts-and-prompts/Alert';
 import MediaMainItemPreview from './MediaMainItemPreview';
+import ButtonMain from '../../cds/buttons-checkboxes-chips/ButtonMain.js';
+import ApproveIcon from '../../../icons/done.svg';
+import RejectIcon from '../../../icons/clear.svg';
+import styles from './MediaSecondaryBanner.module.css';
 
-const MediaSuggestionBanner = ({ projectMedia }) => (
-  <Alert
-    icon
-    variant="warning"
-    title={
-      <FormattedMessage
-        id="mediaSuggestionBanner.alertTitle"
-        defaultMessage="Requests and Media appear to be similar as an existing item in your workspace. Are they a good match?"
-        description="Title of the alert message displayed on item page for suggestions."
-      />
-    }
-    content={
-      <>
-        <MediaMainItemPreview projectMedia={projectMedia} />
-      </>
-    }
-  />
-);
+const MediaSuggestionBanner = ({ projectMedia }) => {
+  const handleApprove = () => {};
+
+  const handleReject = () => {};
+
+  return (
+    <>
+      { projectMedia.is_suggested && (
+        <Alert
+          icon
+          variant="warning"
+          title={
+            <FormattedMessage
+              id="mediaSuggestionBanner.alertTitle"
+              defaultMessage="Requests and Media appear to be similar as an existing item in your workspace. Are they a good match?"
+              description="Title of the alert message displayed on item page for suggestions."
+            />
+          }
+          content={
+            <div className={styles.mediaSuggestionsBanner}>
+              { projectMedia.suggested_main_item && <MediaMainItemPreview projectMedia={projectMedia.suggested_main_item} /> }
+              <div className={styles.mediaSuggestionsBannerButtons}>
+                <ButtonMain
+                  size="small"
+                  variant="contained"
+                  theme="validation"
+                  iconLeft={<ApproveIcon />}
+                  label={<FormattedMessage id="mediaSuggestionBanner.approve" defaultMessage="Yes, they are the same" description="Button label to accept similarity suggestion." />}
+                  onClick={handleApprove}
+                />
+                <ButtonMain
+                  size="small"
+                  variant="contained"
+                  theme="error"
+                  iconLeft={<RejectIcon />}
+                  label={<FormattedMessage id="mediaSuggestionBanner.reject" defaultMessage="No, keep them separate" description="Button label to reject similarity suggestion." />}
+                  onClick={handleReject}
+                />
+              </div>
+            </div>
+          }
+        />
+      )}
+    </>
+  );
+};
 
 MediaSuggestionBanner.propTypes = {
   projectMedia: PropTypes.object.isRequired, // See fragment for details
@@ -30,6 +62,9 @@ MediaSuggestionBanner.propTypes = {
 
 export default createFragmentContainer(MediaSuggestionBanner, graphql`
   fragment MediaSuggestionBanner_projectMedia on ProjectMedia {
-    ...MediaMainItemPreview_projectMedia
+    is_suggested
+    suggested_main_item {
+      ...MediaMainItemPreview_projectMedia
+    }
   }
 `);
