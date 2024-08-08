c596a1e4b0c02cfd7b0b4dc694cb2d81cd3a0a34
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2024-08-08 15:59:05 -0300

    Return admin role if check admin (#2046)

--- src/app/components/user/UserUtil.js
+++ src/app/components/user/UserUtil.js
@@ -3,6 +3,9 @@ import { safelyParseJSON } from '../../helpers';
 const UserUtil = {
   myRole: (currentUser, teamSlug) => {
     if (!currentUser) return null;
+
+    if (currentUser.is_admin) return 'admin';
+
     const teams = safelyParseJSON(currentUser.teams);
     return teams[teamSlug] && teams[teamSlug].role;
   },
