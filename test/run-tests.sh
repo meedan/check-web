244522f4428e6366eaf9541599203f30e4e390a8
chinelo-obitube <chinelo@Chinel-Obitubes-Mac.local>
2024-08-26 17:09:22 +0200

    Identify-workflow-for CI-migration

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ $TRAVIS_JOB_NAME == 'integration-and-unit-tests' ]]
+if [[ $GITHUB_JOB_NAME == 'integration-and-unit-tests' ]]
 then
   npm run test:unit || exit 1
   ./../scripts/uncovered-files
   bundle install
   ./parallel-integration-test.sh
-elif [[ $TRAVIS_JOB_NAME == 'unit-tests' ]]
+elif [[ $GITHUB_JOB_NAME == 'unit-tests' ]]
 then
   npm run test:unit || exit 1
   ./../scripts/uncovered-files
