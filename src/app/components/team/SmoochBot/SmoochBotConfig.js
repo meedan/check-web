4d75f12867d79a98f46b537f186f116a10c07ad3
danielevalverde <danierzevalverde@gmail.com>
2024-08-27 12:11:00 -0300

    fix logic and remove condition

--- src/app/components/team/SmoochBot/SmoochBotConfig.js
+++ src/app/components/team/SmoochBot/SmoochBotConfig.js
@@ -27,7 +27,7 @@ const SmoochBotConfig = (props) => {
     value,
   } = props;
   const [currentTab, setCurrentTab] = React.useState('bot');
-  const defaultOption = value.smooch_version === 'smooch_content';
+  const defaultOption = 'smooch_content';
   const [currentOption, setCurrentOption] = React.useState(defaultOption);
   const team = props?.currentUser?.current_team;
   const environment = createEnvironment(props?.currentUser?.token, team.slug);
