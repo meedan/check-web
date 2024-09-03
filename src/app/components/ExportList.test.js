e29d65e5fb0b04ced9ddb9df6b3e6dfe5bdd2be8
Caio <117518+caiosba@users.noreply.github.com>
2024-09-02 21:54:12 -0300

    [WIP] Ticket CV2-4979: Fixing test

--- src/app/components/ExportList.test.js
+++ src/app/components/ExportList.test.js
@@ -4,6 +4,15 @@ import { shallowWithIntl } from '../../../test/unit/helpers/intl-test';
 
 describe('<ExportList />', () => {
   it('should render export list component', () => {
+    global.window = Object.create(window);
+    Object.defineProperty(window, 'Check', {
+      value: {
+        store: {
+          getState: () => ({ app: { context: { currentUser: { email: 'foo@bar.xyz' } } } }),
+        },
+      },
+    });
+
     const wrapper = shallowWithIntl(<ExportList type="media" />);
     expect(wrapper.find('Tooltip')).toHaveLength(1);
   });
