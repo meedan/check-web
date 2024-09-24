2b17601c087498b8fd4a8cf0d03bebda836e8b2a
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2024-09-24 11:57:00 -0300

    VerticalBarChart widget (#2117)
    * WIP: VerticalBarChart widget
    
    * VerticalBarChartWidget
    
    * Remove console, fix warning
    
    * Remove OtherSandbox route
    
    * Cleanup comments
    
    * Unit test and format number

--- src/app/components/cds/Sandbox.js
+++ src/app/components/cds/Sandbox.js
@@ -5,11 +5,12 @@ import cx from 'classnames/bind';
 import * as Sentry from '@sentry/react';
 import Alert from './alerts-and-prompts/Alert';
 import Chip from './buttons-checkboxes-chips/Chip';
+import ListWidget from './charts/ListWidget';
+import NumberWidget from './charts/NumberWidget';
+import VerticalBarChartWidget from './charts/VerticalBarChartWidget';
 import TagList from './menus-lists-dialogs/TagList';
 import TextField from './inputs/TextField';
 import ListSort from './inputs/ListSort';
-import ListWidget from './charts/ListWidget';
-import NumberWidget from './charts/NumberWidget';
 import TextArea from './inputs/TextArea';
 import DatePicker from './inputs/DatePicker';
 import LanguagePickerSelect from './inputs/LanguagePickerSelect';
@@ -324,6 +325,26 @@ const SandboxComponent = ({ admin }) => {
   const [reorderTheme, setReorderTheme] = React.useState('gray');
   const onChangeReorderTheme = (event) => {
     setReorderTheme(event.target.value);
+  };
+
+  const [sampleDataSet, setSampleDataSet] = React.useState('design');
+
+  const verticalBarChartData = {
+    design: [
+      { name: 'Text', value: 6000 },
+      { name: 'Video', value: 5000 },
+      { name: 'Image', value: 4000 },
+      { name: 'Link', value: 3000 },
+      { name: 'Audio', value: 2000 },
+      { name: 'Social Media', value: 1000 },
+    ],
+    statuses: [
+      { name: 'Unstarted', value: 2999, color: '#518FFF' },
+      { name: 'Inconclusive', value: 4987, color: '#9e9e9e' },
+      { name: 'In Progress', value: 1890, color: '#efac51' },
+      { name: 'False', value: 6005, color: '#f04747' },
+      { name: 'Verified', value: 3021, color: '#5cae73' },
+    ],
   };
 
   const generateUncaughtError = () => {
@@ -1905,6 +1926,43 @@ const SandboxComponent = ({ admin }) => {
       }
       { (!selectedCategory || selectedCategory === 'charts') &&
         <section>
+          <h6>Charts</h6>
+          <div className={styles.componentWrapper}>
+            <div className={styles.componentControls}>
+              <div className={cx('typography-subtitle2', [styles.componentName])}>
+                VerticalBarChartWidget
+                <a
+                  className={styles.figmaLink}
+                  href="https://www.figma.com/design/82Go6q0krKApn1L8EQ2joj/Dashboard?node-id=186-5696&node-type=symbol"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  title="Figma Designs"
+                >
+                  <FigmaColorLogo />
+                </a>
+              </div>
+              <ul>
+                <li>
+                  <Select
+                    label="Sample Dataset"
+                    value={sampleDataSet}
+                    onChange={e => setSampleDataSet(e.target.value)}
+                  >
+                    <option value="design">Design</option>
+                    <option value="statuses">Statuses</option>
+                  </Select>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.componentInlineVariants} style={{ backgroundColor: buttonTheme === 'white' ? 'var(--color-gray-15)' : null }}>
+              <VerticalBarChartWidget
+                data={verticalBarChartData[sampleDataSet]}
+                title="Media Received"
+                width="100%"
+              />
+            </div>
+          </div>
+
           <div className={styles.componentWrapper}>
             <div className={cx('typography-subtitle2', [styles.componentName])}>
               Number Widget
@@ -1924,6 +1982,7 @@ const SandboxComponent = ({ admin }) => {
               </div>
             </div>
           </div>
+
           <div className={styles.componentWrapper}>
             <div className={cx('typography-subtitle2', [styles.componentName])}>
               List Widget
