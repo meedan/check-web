ae2a2b92a36a99125f1c2801dfd9dce09a0710ff
Caio <117518+caiosba@users.noreply.github.com>
2024-09-27 22:01:14 -0300

    Debug 15... [similarity tests]

--- test/spec/similarity_spec.rb
+++ test/spec/similarity_spec.rb
@@ -73,8 +73,9 @@ shared_examples 'similarity' do
     api_create_claim(data: data, quote: 'Lorem Ipsum is used to generate dummy texts of the printing and IT industry!')
     puts 'Created second claim...'
     verbose_wait 3
-    # puts 'Opening page...'
-    # @driver.navigate.to "#{@config['self_url']}/#{data[:team].slug}/media/#{pm.id}"
+    puts 'Opening page...'
+    @driver.navigate.to "#{@config['self_url']}/#{data[:team].slug}/media/#{pm.id}"
+    puts 'Page opened!'
     # wait_for_selector('.media__more-medias')
     # expect(@driver.find_elements(:css, '.media__relationship').size).to eq 1
   end
