c94d24ed0130374853ffbef0031c3b8d845c6910
chinelo-obitube <chinelo@ChinelOtubesMac.lan>
2024-09-30 17:25:35 +0100

    [similarity tests]

--- build.sh
+++ build.sh
@@ -17,7 +17,8 @@ else
     i=0
     NGROK_URL=""
     ngrok config add-authtoken $NGROK_AUTH
-    ngrok config upgrade 3
+    ngrok config upgrade 2
+    ngrok start --all
     while [ -z "$NGROK_URL" -a $i -lt 5 ]; do
       i=$(($i + 1))
       ngrok http 9000 >/dev/null &
