543fc05db713d32891e63c72b446af98f1650e4f
Devin Gaffney <itsme@devingaffney.com>
2024-10-03 17:53:07 -0700

    refactor build to only use ngrok against media

--- build.sh
+++ build.sh
@@ -14,38 +14,38 @@ else
     docker-compose build web api api-background pender pender-background
     docker-compose -f docker-compose.yml -f docker-test.yml up -d web api api-background pender pender-background chromedriver
   else
-    i=0
-    NGROK_URL=""
-    ngrok config add-authtoken $NGROK_AUTH
-    ngrok config upgrade 2
-    while [ -z "$NGROK_URL" -a $i -lt 5 ]; do
-      i=$(($i + 1))
-      ngrok http 9000 >/dev/null &
-      until curl --silent -I -f --fail http://localhost:4040; do printf "."; sleep 10; done
-      curl -I -v http://localhost:4040
-      curl localhost:4040/api/tunnels > ngrok.json
-      cat ngrok.json
-      NGROK_URL=$(grep -Po '"public_url": *\K"[^"]*"' ngrok.json | tail -n1 | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/')
-      if [ -z $NGROK_URL ]
-      then
-        kill -9 $(pgrep ngrok)
-      fi
-      sleep 5
-    done
-    if [ -z $NGROK_URL ]
-    then
-      echo "Not able to connect a Ngrok Tunnel. Please try again!"
-      exit 1
-    fi
-    echo "Ngrok tunnel: $NGROK_URL"
-    sed -i "s~similarity_media_file_url_host: ''~similarity_media_file_url_host: '$NGROK_URL'~g" check-api/config/config.yml
-    cat check-api/config/config.yml | grep similarity_media_file_url_host
-    docker-compose build
     if [[ $TRAVIS_JOB_NAME == 'media-similarity-test' ]]
     then
-        docker-compose -f docker-compose.yml -f docker-test.yml up -d web api api-background pender pender-background chromedriver alegre presto-server presto-audio presto-image presto-video
+      i=0
+      NGROK_URL=""
+      ngrok config add-authtoken $NGROK_AUTH
+      ngrok config upgrade 2
+      while [ -z "$NGROK_URL" -a $i -lt 5 ]; do
+        i=$(($i + 1))
+        ngrok http 9000 >/dev/null &
+        until curl --silent -I -f --fail http://localhost:4040; do printf "."; sleep 10; done
+        curl -I -v http://localhost:4040
+        curl localhost:4040/api/tunnels > ngrok.json
+        cat ngrok.json
+        NGROK_URL=$(grep -Po '"public_url": *\K"[^"]*"' ngrok.json | tail -n1 | sed 's/.\(.*\)/\1/' | sed 's/\(.*\)./\1/')
+        if [ -z $NGROK_URL ]
+        then
+          kill -9 $(pgrep ngrok)
+        fi
+        sleep 5
+      done
+      if [ -z $NGROK_URL ]
+      then
+        echo "Not able to connect a Ngrok Tunnel. Please try again!"
+        exit 1
+      fi
+      echo "Ngrok tunnel: $NGROK_URL"
+      sed -i "s~similarity_media_file_url_host: ''~similarity_media_file_url_host: '$NGROK_URL'~g" check-api/config/config.yml
+      cat check-api/config/config.yml | grep similarity_media_file_url_host
+      docker-compose build
+      docker-compose -f docker-compose.yml -f docker-test.yml up -d web api api-background pender pender-background chromedriver alegre presto-server presto-audio presto-image presto-video
     else
-        docker-compose -f docker-compose.yml -f docker-test.yml up -d web api api-background pender pender-background chromedriver alegre presto-server presto-mean-tokens
+      docker-compose -f docker-compose.yml -f docker-test.yml up -d web api api-background pender pender-background chromedriver alegre presto-server presto-mean-tokens
     fi
     until curl --silent -I -f --fail http://localhost:3100; do printf .; sleep 1; done
     until curl --silent -I -f --fail http://localhost:8000/ping; do printf .; sleep 1; done
