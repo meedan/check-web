d2f65bdb657646560bc31b4f57570fc51858e6a4
danielevalverde <danierzevalverde@gmail.com>
2024-10-08 16:36:06 -0300

    update build file [full ci] to only build and start necessary containers

--- build.sh
+++ build.sh
@@ -42,10 +42,10 @@ else
       echo "Ngrok tunnel: $NGROK_URL"
       sed -i "s~similarity_media_file_url_host: ''~similarity_media_file_url_host: '$NGROK_URL'~g" check-api/config/config.yml
       cat check-api/config/config.yml | grep similarity_media_file_url_host
-      docker-compose build
+      docker-compose build web api api-background pender pender-background chromedriver alegre presto-server presto-audio presto-image presto-video
       docker-compose -f docker-compose.yml -f docker-test.yml up -d web api api-background pender pender-background chromedriver alegre presto-server presto-audio presto-image presto-video
     else
-      docker-compose build
+      docker-compose build web api api-background pender pender-background chromedriver alegre presto-server presto-mean-tokens
       docker-compose -f docker-compose.yml -f docker-test.yml up -d web api api-background pender pender-background chromedriver alegre presto-server presto-mean-tokens
     fi
     until curl --silent -I -f --fail http://localhost:3100; do printf .; sleep 1; done
