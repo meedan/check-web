569f0615d1653fef555f98330414243a874aac59
Brian Fleming <brianfleming@gmail.com>
2024-10-11 11:27:19 -0400

    allow webpage titles to wrap when they are unbroken strings [CV2-5460]

--- src/app/components/media/WebPageMediaCard.js
+++ src/app/components/media/WebPageMediaCard.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { graphql, createFragmentContainer } from 'react-relay/compat';
 import { FormattedMessage } from 'react-intl';
 import deepEqual from 'deep-equal';
-import { Box } from '@material-ui/core';
-import MediaCardTitleSummary from './MediaCardTitleSummary';
+import cx from 'classnames/bind';
+import Alert from '../cds/alerts-and-prompts/Alert';
 import AspectRatio from '../layout/AspectRatio';
+import styles from '../media/media.module.css';
 
 class WebPageMediaCard extends Component {
   shouldComponentUpdate(nextProps) {
@@ -35,12 +36,24 @@ class WebPageMediaCard extends Component {
     } = this.props;
 
     return (
-      <article className="web-page-media-card">
-        <MediaCardTitleSummary
-          style={inModal ? { padding: '0 0 16px 0' } : null}
-          summary={inModal ? null : data.description}
-          title={data.title}
-        />
+      <article className={cx('web-page-media-card', styles['webpage-media-card'])}>
+        <div
+          className={cx(
+            styles['webpage-media-card-title-summary'],
+            {
+              [styles['webpage-media-card-title-summary-modal']]: inModal,
+            })
+          }
+        >
+          { data.title ?
+            <div className={cx('media-card-large__title', styles['webpage-media-card-title'])}>
+              {data.title}
+            </div> : null }
+          { data.description ?
+            <p>
+              {inModal ? null : data.description}
+            </p> : null }
+        </div>
         {this.canEmbedHtml() ?
           <div
             dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
@@ -62,16 +75,19 @@ class WebPageMediaCard extends Component {
                 <img alt="" src={projectMedia.picture} onError={(e) => { e.target.onerror = null; e.target.src = '/images/image_placeholder.svg'; }} />
               </AspectRatio> : null
             }
-            { data.error ?
-              <div className="web-page-media-card__error">
-                <Box mt={2}>
+            { data.error &&
+              <Alert
+                className={cx('web-page-media-card__error', styles['webpage-media-card-error'])}
+                contained
+                content={
                   <FormattedMessage
                     defaultMessage="This item could not be identified. It may have been removed, or may only be visible to users who are logged in. Click the link above to navigate to it."
                     description="Error message displayed when link data is unavailable"
                     id="webPageMediaCard.Error"
                   />
-                </Box>
-              </div> : null
+                }
+                variant="error"
+              />
             }
           </>
         }
