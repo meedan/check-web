a0f84fbc588c64b0e0c19712c9fefd29d9ebd6fa
sarahkeh <161879347+sarahkeh@users.noreply.github.com>
2024-10-30 16:31:15 -0700

    first commit, base files

---
+++ src/app/components/cds/menus-lists-dialogs/Tab.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Tab = (
+  active,
+  disabled,
+  iconCenter,
+  iconLeft,
+  iconRight,
+  label,
+  size,
+) => {
+  return (
+    <div>
+      {label}
+    </div>
+  );
+};
+
+export default Tab;
---
+++ src/app/components/cds/menus-lists-dialogs/TabWrapper.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tab from './Tab';
+
+const TabWrapper = (
+  options,
+  variant,
+) => {
+  return (
+    <div>
+      <Tab />
+    </div>
+  );
+};
+
+export default TabWrapper;
