3846b2120bee750b6cb9efba1ff800d792fe0961
Brian Fleming <brianfleming@users.noreply.github.com>
2024-11-12 08:46:07 -0500

    Last uses of makeStyles and withstyles (#2194)
    * remove make styles from MediaPlayerCard
    
    * remove makestyles from MediaTimeline
    
    * remove makestyles from internal SmoochBotSetting
    
    * remove makestyles from ReportDesignerTopBar
    
    * remove makestyles from ReportDesignerPreview
    
    * remove unnecessary use of classname bind
    
    * remove withstyles from ThresholdControl
    
    * remove withstyles from EditTaskDialog
    
    * remove seemingly unnecessary withstyles from api key create test [smoke tests]
    
    * put back reference to withstyles [smoke tests]

--- src/app/components/team/Similarity/ThresholdControl.js
+++ src/app/components/team/Similarity/ThresholdControl.js
@@ -1,28 +1,12 @@
-/* eslint-disable react/sort-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { withStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
 import Slider from '@material-ui/core/Slider';
 import TextField from '../../cds/inputs/TextField';
+import styles from './ThresholdControl.module.css';
 import settingsStyles from '../Settings.module.css';
 
-const styles = theme => ({
-  textFieldRoot: {
-    maxWidth: theme.spacing(10),
-    marginRight: theme.spacing(2),
-  },
-  sliderRoot: {
-    maxWidth: theme.spacing(30),
-  },
-  error: {
-    color: 'var(--color-pink-53)',
-  },
-});
-
 const ThresholdControl = ({
-  classes,
   disabled,
   error,
   label,
@@ -30,30 +14,28 @@ const ThresholdControl = ({
   type,
   value,
 }) => (
-  <Box mb={2} ml={7}>
-    <Box mb={1}>
-      <strong>
-        {label}
-      </strong>
-    </Box>
-    <div>
-      { type === 'matching' ? (
-        <FormattedMessage
-          data-testid="threshold-control__matching-explainer-message"
-          defaultMessage="If the confidence score is above this ratio, items will be matched and reports automatically sent to users."
-          description="Description of the confidence score automatically triggering a report being sent to the user"
-          id="thresholdControl.matchingExplainer"
-        />
-      ) : (
-        <FormattedMessage
-          data-testid="threshold-control__suggestion-explainer-message"
-          defaultMessage="If the confidence score is above this ratio, items will be suggested as similar."
-          description="Description of the confidence score automatically suggesting an item to a system editor"
-          id="thresholdControl.suggestionExplainer"
-        />
-      )}
-    </div>
-    <Box alignItems="center" display="flex" mt={1}>
+  <div className={styles['threshold-control']}>
+    <strong>
+      {label}
+    </strong>
+    { type === 'matching' ? (
+      <FormattedMessage
+        data-testid="threshold-control__matching-explainer-message"
+        defaultMessage="If the confidence score is above this ratio, items will be matched and reports automatically sent to users."
+        description="Description of the confidence score automatically triggering a report being sent to the user"
+        id="thresholdControl.matchingExplainer"
+        tagName="p"
+      />
+    ) : (
+      <FormattedMessage
+        data-testid="threshold-control__suggestion-explainer-message"
+        defaultMessage="If the confidence score is above this ratio, items will be suggested as similar."
+        description="Description of the confidence score automatically suggesting an item to a system editor"
+        id="thresholdControl.suggestionExplainer"
+        tagName="p"
+      />
+    )}
+    <div className={styles['threshold-control-inputs']}>
       <TextField
         className={settingsStyles['similarity-component-input']}
         disabled={disabled}
@@ -64,23 +46,22 @@ const ThresholdControl = ({
         onChange={e => onChange(e.target.value)}
       />
       <Slider
-        classes={{ root: classes.sliderRoot }}
         disabled={disabled}
         value={value}
         onChange={(e, newValue) => onChange(newValue)}
       />
-    </Box>
-    <Box color="var(--color-pink-53)" my={1}>
-      { error ?
+    </div>
+    { error ?
+      <div className={styles['threshold-control-error']}>
         <FormattedMessage
           data-testid="threshold-control__error-message"
           defaultMessage="The suggestion threshold cannot be higher than the matching one."
           description="Error message displayed when suggestion threshold is set higher than value of matching threshold"
           id="thresholdControl.suggestionGreaterThanMatchingError"
-        /> : null
-      }
-    </Box>
-  </Box>
+        />
+      </div> : null
+    }
+  </div>
 );
 
 ThresholdControl.defaultProps = {
@@ -89,14 +70,13 @@ ThresholdControl.defaultProps = {
 };
 
 ThresholdControl.propTypes = {
-  classes: PropTypes.object.isRequired,
-  onChange: PropTypes.func.isRequired,
-  // handleInputChange: PropTypes.func.isRequired,
-  type: PropTypes.string.isRequired,
-  value: PropTypes.number.isRequired,
   disabled: PropTypes.bool,
   error: PropTypes.bool,
+  // handleInputChange: PropTypes.func.isRequired,
   label: PropTypes.node.isRequired,
+  type: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
-export default withStyles(styles)(ThresholdControl);
+export default ThresholdControl;
