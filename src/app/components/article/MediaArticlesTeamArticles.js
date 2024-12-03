368cf7df60bcb7c9f6927c7431a85e814604c781
danielevalverde <danierzevalverde@gmail.com>
2024-12-03 14:45:33 -0300

    update query to include explainers fields

--- src/app/components/article/MediaArticlesTeamArticles.js
+++ src/app/components/article/MediaArticlesTeamArticles.js
@@ -99,6 +99,10 @@ const MediaArticlesTeamArticles = ({
                     id
                     ...MediaArticlesCard_article
                   }
+                  ... on Explainer {
+                    id
+                    ...MediaArticlesCard_article
+                  }
                 }
               }
             }
