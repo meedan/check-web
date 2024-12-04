5e59d82e3c172ecfce98d143777d4027a5b5eb4b
danielevalverde <danierzevalverde@gmail.com>
2024-12-04 15:16:56 -0300

    fix and merge tests [smoke tests]

--- test/spec/source_spec.rb
+++ test/spec/source_spec.rb
@@ -1,43 +1,6 @@
 require_relative 'spec_helper'
 
 shared_examples 'source' do
-  it 'should check, edit and remove source info', bin2: true do
-    api_create_team_and_bot_and_link_and_redirect_to_media_page({ url: 'https://g1.globo.com/' })
-    verbose_wait 1
-    @driver.navigate.refresh
-    wait_for_selector('.test__media')
-    wait_for_selector('.media-tab__articles')
-    wait_for_selector('.media-tab__source').click
-    wait_for_selector('#media__source')
-    expect(@driver.page_source.include?('G1')).to be(true)
-    wait_for_selector('#source__name-input').send_keys('- Edited')
-    @driver.action.send_keys(:enter).perform
-    wait_for_text_change('G1', '.source__name', :css)
-    wait_for_selector("//h6[contains(text(), 'G1- Edited')]", :xpath)
-    expect(@driver.page_source.include?('G1- Edited')).to be(true)
-    # check main link
-    expect(wait_for_selector('#main_source__link').attribute('value') == 'https://g1.globo.com/').to be(true)
-    expect(@driver.find_elements(:css, '.int-clear-input__button--textfield').length == 1).to be(true)
-    # add a secondary link
-    wait_for_selector('.source__add-link-button').click
-    wait_for_selector('#source__link-input-new').send_keys('https://www.bbc.com/news/uk')
-    @driver.action.send_keys(:enter).perform
-    wait_for_selector_list_size('.int-clear-input__button--textfield', 2)
-    # remove main link
-    expect(@driver.find_elements(:css, '.int-clear-input__button--textfield').length == 2).to be(true)
-    wait_for_selector_list('.int-clear-input__button--textfield')[0].click
-    wait_for_selector_none('input[value="https://g1.globo.com/"]', :css, 30)
-    # check that the main link was changed
-    expect(wait_for_selector('#main_source__link').attribute('value') == 'https://www.bbc.com/news/uk').to be(true)
-    expect(@driver.find_elements(:css, '.int-clear-input__button--textfield').length == 1).to be(true)
-    wait_for_selector('.source__add-link-button').click
-    # try to add the same source again
-    wait_for_selector('#source__link-input-new').send_keys('https://www.bbc.com/news/uk')
-    @driver.action.send_keys(:enter).perform
-    wait_for_selector('.int-error__message--textfield')
-    expect(@driver.page_source.include?('Account has already been taken')).to be(true)
-  end
-
   it 'should add a existing source for a media', bin4: true do
     api_create_team_and_bot_and_link_and_redirect_to_media_page({ url: 'https://www.cnnbrasil.com.br/' })
     wait_for_selector('.test__media')
@@ -67,7 +30,7 @@ shared_examples 'source' do
     expect(@driver.page_source.downcase.include?('brasil')).to be(true)
   end
 
-  it 'should add a new source for a media', bin3: true do
+  it 'should add a new source for a media, edit and remove source info', bin3: true do
     api_create_team_and_claim_and_redirect_to_media_page
     wait_for_selector('.media-card-large')
     # create source
@@ -83,5 +46,31 @@ shared_examples 'source' do
     wait_for_selector('.media-tab__source').click
     wait_for_selector('#source__name-input')
     expect(wait_for_selector('.source__name').text == 'BBC').to be(true)
+    wait_for_selector('#source__name-input').send_keys('- Edited')
+    @driver.action.send_keys(:enter).perform
+    wait_for_text_change('BBC', '.source__name', :css)
+    wait_for_selector("//h6[contains(text(), 'BBC- Edited')]", :xpath)
+    expect(@driver.page_source.include?('BBC- Edited')).to be(true)
+    # check main link
+    expect(wait_for_selector('#main_source__link').attribute('value') == 'https://www.bbc.com/portuguese').to be(true)
+    expect(@driver.find_elements(:css, '.int-clear-input__button--textfield').length == 1).to be(true)
+    # add a secondary link
+    wait_for_selector('.source__add-link-button').click
+    wait_for_selector('#source__link-input-new').send_keys('https://www.bbc.com/news/uk')
+    @driver.action.send_keys(:enter).perform
+    wait_for_selector_list_size('.int-clear-input__button--textfield', 2)
+    # remove main link
+    expect(@driver.find_elements(:css, '.int-clear-input__button--textfield').length == 2).to be(true)
+    wait_for_selector_list('.int-clear-input__button--textfield')[0].click
+    wait_for_selector_none('input[value="https://www.bbc.com/portuguese"]', :css, 30)
+    # check that the main link was changed
+    expect(wait_for_selector('#main_source__link').attribute('value') == 'https://www.bbc.com/news/uk').to be(true)
+    expect(@driver.find_elements(:css, '.int-clear-input__button--textfield').length == 1).to be(true)
+    wait_for_selector('.source__add-link-button').click
+    # try to add the same source again
+    wait_for_selector('#source__link-input-new').send_keys('https://www.bbc.com/news/uk')
+    @driver.action.send_keys(:enter).perform
+    wait_for_selector('.int-error__message--textfield')
+    expect(@driver.page_source.include?('Account has already been taken')).to be(true)
   end
 end
