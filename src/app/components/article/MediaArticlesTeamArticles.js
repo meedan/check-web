4fa4e3981b68fdb569390ce9e1ad1458cec0b50b
danielevalverde <danierzevalverde@gmail.com>
2024-12-04 08:58:13 -0300

    remove debug

--- src/app/components/article/MediaArticlesTeamArticles.js
+++ src/app/components/article/MediaArticlesTeamArticles.js
@@ -16,49 +16,44 @@ const MediaArticlesTeamArticlesComponent = ({
   onAdd,
   team,
   textSearch,
-}) => {
-  // eslint-disable-next-line
-  console.log('Articles:', articles);
-
-  return (
-    <>
-      { !hasRelevantArticles && !textSearch && (
-        <div className="typography-subtitle2">
-          <FormattedMessage
-            defaultMessage="Choose a recent article to add to this media:"
-            description="Message displayed on articles sidebar when an item has no articles."
-            id="mediaArticles.chooseRecentArticle"
-          />
-        </div>
-      )}
-      { hasRelevantArticles && !textSearch && (
-        <div className="typography-subtitle2">
-          <FormattedMessage
-            defaultMessage="Choose a relevant article to add to this media:"
-            description="Message displayed on articles sidebar when an item has relevant articles."
-            id="mediaArticles.chooseRelevantArticle"
-          />
-        </div>
-      )}
-      { textSearch && !articles.length ? (
-        <div className={cx('typography-body1', styles.articlesSidebarNoArticle)}>
-          <DescriptionIcon />
-          <FormattedMessage
-            defaultMessage="No results matched your search."
-            description="Message displayed on articles sidebar when search returns no articles."
-            id="mediaArticlesTeamArticles.noResults"
-            tagName="div"
-          />
-        </div>
-      ) : null }
-      <div className={styles.articlesSidebarList} id="articles-sidebar-team-articles">
-        {articles.map(article => (
-          <MediaArticlesCard article={article} key={article.id} team={team} onAdd={onAdd} />
-        ))}
+}) => (
+  <>
+    { !hasRelevantArticles && !textSearch && (
+      <div className="typography-subtitle2">
+        <FormattedMessage
+          defaultMessage="Choose a recent article to add to this media:"
+          description="Message displayed on articles sidebar when an item has no articles."
+          id="mediaArticles.chooseRecentArticle"
+        />
       </div>
-    </>
-  );
-};
+    )}
+    { hasRelevantArticles && !textSearch && (
+      <div className="typography-subtitle2">
+        <FormattedMessage
+          defaultMessage="Choose a relevant article to add to this media:"
+          description="Message displayed on articles sidebar when an item has relevant articles."
+          id="mediaArticles.chooseRelevantArticle"
+        />
+      </div>
+    )}
+    { textSearch && !articles.length ? (
+      <div className={cx('typography-body1', styles.articlesSidebarNoArticle)}>
+        <DescriptionIcon />
+        <FormattedMessage
+          defaultMessage="No results matched your search."
+          description="Message displayed on articles sidebar when search returns no articles."
+          id="mediaArticlesTeamArticles.noResults"
+          tagName="div"
+        />
+      </div>
+    ) : null }
+    <div className={styles.articlesSidebarList} id="articles-sidebar-team-articles">
+      {articles.map(article => (
+        <MediaArticlesCard article={article} key={article.id} team={team} onAdd={onAdd} />
+      ))}
+    </div>
+  </>
+);
 
 MediaArticlesTeamArticlesComponent.defaultProps = {
   articles: [],
@@ -138,22 +133,14 @@ const MediaArticlesTeamArticles = ({
       render={({ error, props }) => {
         if (!error && props) {
           // Merge explainers with fact-checks and re-sort
-          // eslint-disable-next-line
-          console.log("Props:", props);
 
           let articles = [];
           let hasRelevantArticles = false;
           if (props.project_media.relevant_articles_count > 0) {
             hasRelevantArticles = true;
-            // eslint-disable-next-line
-            console.log("Has Relevant Articles:", hasRelevantArticles);
             articles = props.project_media.relevant_articles.edges.map(edge => edge.node);
-            // eslint-disable-next-line
-            console.log("Relevant Articles:", articles);
           } else {
             articles = props.team.factChecks.edges.concat(props.team.explainers.edges).map(edge => edge.node).sort((a, b) => (parseInt(a.created_at, 10) < parseInt(b.created_at, 10)) ? 1 : -1);
-            // eslint-disable-next-line
-            console.log("Recente Articles:", articles);
           }
 
           return (
