cc78d9d8e07048e79c21addea76f236b4ebec570
sarahkeh <161879347+sarahkeh@users.noreply.github.com>
2024-12-10 10:53:11 -0800

    Reduce admin filter to a single option

--- src/app/components/media/SuperAdminControls.js
+++ src/app/components/media/SuperAdminControls.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import ButtonMain from '../cds/buttons-checkboxes-chips/ButtonMain';
@@ -6,26 +6,28 @@ import styles from './SuperAdminControls.module.css';
 
 const SuperAdminControls = ({
   handleSuperAdminMask,
-  handleSuperAdminMaskSession,
 }) => {
-  const superAdminMaskSession = sessionStorage.getItem('superAdminMaskSession');
-  const maskSession = superAdminMaskSession !== 'false';
-  const [superAdminmaskContent, setSuperAdminMaskContent] = React.useState(maskSession);
-  const [superAdminMaskContentSession, setSuperAdminMaskContentSession] = React.useState(maskSession);
+  const [superAdminmaskContent, setSuperAdminMaskContent] = React.useState(true);
+
+  const updateStoredValue = (value) => {
+    window.storage.set('contentMask', value);
+    window.dispatchEvent(new StorageEvent('storage', {
+      key: 'contentMask',
+      newValue: value,
+    }));
+  };
+
+  useEffect(() => {
+    // this will be later updated to pull a stored value in local storage
+    // but at the moment, just reset the local storage value to the default mask value
+    updateStoredValue(true);
+  }, []);
 
   const handleSuperAdminClickPage = () => {
     const newValue = !superAdminmaskContent;
     setSuperAdminMaskContent(newValue);
     handleSuperAdminMask(newValue);
-  };
-
-  const handleSuperAdminClickSession = () => {
-    const newValue = !superAdminMaskContentSession;
-    setSuperAdminMaskContentSession(newValue);
-    handleSuperAdminMaskSession(newValue);
-    // Call methods for apply admin screen on this page to apply session action to current page
-    setSuperAdminMaskContent(newValue);
-    handleSuperAdminMask(newValue);
+    updateStoredValue(newValue);
   };
 
   return (
@@ -63,34 +65,6 @@ const SuperAdminControls = ({
               onClick={handleSuperAdminClickPage}
             />
         }
-        {
-          superAdminMaskContentSession ?
-            <ButtonMain
-              label={
-                <FormattedMessage
-                  defaultMessage="Pause admin screen for this session"
-                  description="A label on a button that pause admin screen for current session."
-                  id="superAdminControls.pause"
-                />}
-              size="small"
-              theme="lightError"
-              variant="contained"
-              onClick={handleSuperAdminClickSession}
-            /> :
-            <ButtonMain
-              label={
-                <FormattedMessage
-                  defaultMessage="Resume admin screen for this session"
-                  description="A label on a button that pause admin screen for current session."
-                  id="superAdminControls.play"
-                />
-              }
-              size="small"
-              theme="lightError"
-              variant="contained"
-              onClick={handleSuperAdminClickSession}
-            />
-        }
       </div>
     </React.Fragment>
   );
@@ -98,7 +72,6 @@ const SuperAdminControls = ({
 
 SuperAdminControls.propTypes = {
   handleSuperAdminMask: PropTypes.func.isRequired,
-  handleSuperAdminMaskSession: PropTypes.func.isRequired,
 };
 
 export default SuperAdminControls;
