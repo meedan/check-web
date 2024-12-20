bf116ab7f0283898530873c0dbd23b7eb130da2a
sarahkeh <161879347+sarahkeh@users.noreply.github.com>
2024-12-19 17:55:02 -0800

    tooltip works on first hover now

--- src/app/components/media/ContentFilterControls.js
+++ src/app/components/media/ContentFilterControls.js
@@ -4,6 +4,7 @@ import ShieldCheckIcon from '../../icons/shield_check.svg';
 import ShieldOffIcon from '../../icons/shield_off.svg';
 import { ToggleButton, ToggleButtonGroup } from '../cds/inputs/ToggleButtonGroup';
 import Tooltip from '../cds/alerts-and-prompts/Tooltip';
+import styles from './ContentFilterControls.module.css';
 
 const ContentFilterControls = () => {
   const [maskContent, setMaskContent] = React.useState(window.storage.getValue('contentMask') ? window.storage.getValue('contentMask') === 'true' : false);
@@ -41,42 +42,46 @@ const ContentFilterControls = () => {
         variant="containedLight"
         onChange={(e, newValue) => handleMaskToggle(newValue)}
       >
-        <Tooltip
-          arrow
-          placement="left"
-          title={
-            <FormattedMessage
-              defaultMessage="Show visual media previews"
-              description="Tooltip for off state of the content filter toggle"
-              id="contentFilterControls.tooltipOff"
-            />
-          }
+        <ToggleButton
+          selected={!maskContent}
+          value="false"
         >
-          <ToggleButton
-            selected={!maskContent}
-            value="false"
+          <Tooltip
+            arrow
+            placement="left"
+            title={
+              <FormattedMessage
+                defaultMessage="Show visual media previews"
+                description="Tooltip for off state of the content filter toggle"
+                id="contentFilterControls.tooltipOff"
+              />
+            }
           >
-            <ShieldOffIcon />
-          </ToggleButton>
-        </Tooltip>
-        <Tooltip
-          arrow
-          placement="left"
-          title={
-            <FormattedMessage
-              defaultMessage="Hide visual media previews"
-              description="Tooltip for on state of the content filter toggle"
-              id="contentFilterControls.tooltipOn"
-            />
-          }
+            <span className={styles.toolTipWrapper}>
+              <ShieldOffIcon />
+            </span>
+          </Tooltip>
+        </ToggleButton>
+        <ToggleButton
+          selected={maskContent}
+          value="true"
         >
-          <ToggleButton
-            selected={maskContent}
-            value="true"
+          <Tooltip
+            arrow
+            placement="left"
+            title={
+              <FormattedMessage
+                defaultMessage="Hide visual media previews"
+                description="Tooltip for on state of the content filter toggle"
+                id="contentFilterControls.tooltipOn"
+              />
+            }
           >
-            <ShieldCheckIcon />
-          </ToggleButton>
-        </Tooltip>
+            <span className={styles.toolTipWrapper}>
+              <ShieldCheckIcon />
+            </span>
+          </Tooltip>
+        </ToggleButton>
       </ToggleButtonGroup>
     </React.Fragment>
   );
