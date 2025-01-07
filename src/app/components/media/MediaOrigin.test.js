5fc81d598b5139e07c1167db1849333e6920e264
danielevalverde <danierzevalverde@gmail.com>
2025-01-07 13:01:47 -0300

    rename unit tests

--- src/app/components/media/MediaOrigin.test.js
+++ src/app/components/media/MediaOrigin.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MediaClusterOriginButton from './MediaClusterOriginButton';
+import MediaOrigin from './MediaOrigin';
 import { mountWithIntl } from '../../../../test/unit/helpers/intl-test';
 import Person from '../../icons/person.svg';
 import PersonAdd from '../../icons/person_add.svg';
@@ -7,33 +7,33 @@ import PersonCheck from '../../icons/person_check.svg';
 import Bolt from '../../icons/bolt.svg';
 import Tipline from '../../icons/question_answer.svg';
 
-describe('<MediaClusterOriginButton />', () => {
+describe('<MediaOrigin />', () => {
   it('should render a button with the correct icon and message for each type', () => {
-    const userMerged = mountWithIntl(<MediaClusterOriginButton type="typeA" />);
+    const userMerged = mountWithIntl(<MediaOrigin type="typeA" />);
     expect(userMerged.find(Person).length).toEqual(1);
     expect(userMerged.html()).toMatch('User Merged');
 
-    const userMatched = mountWithIntl(<MediaClusterOriginButton type="typeB" />);
+    const userMatched = mountWithIntl(<MediaOrigin type="typeB" />);
     expect(userMatched.find(PersonCheck).length).toEqual(1);
     expect(userMatched.html()).toMatch('User Matched');
 
 
-    const userAdded = mountWithIntl(<MediaClusterOriginButton type="typeC" />);
+    const userAdded = mountWithIntl(<MediaOrigin type="typeC" />);
     expect(userAdded.find(PersonAdd).length).toEqual(1);
     expect(userAdded.html()).toMatch('User Added');
 
-    const tiplineSubmitted = mountWithIntl(<MediaClusterOriginButton type="typeD" />);
+    const tiplineSubmitted = mountWithIntl(<MediaOrigin type="typeD" />);
     expect(tiplineSubmitted.find(Tipline).length).toEqual(1);
     expect(tiplineSubmitted.html()).toMatch('Tipline Submitted');
 
-    const autoMatched = mountWithIntl(<MediaClusterOriginButton type="typeE" />);
+    const autoMatched = mountWithIntl(<MediaOrigin type="typeE" />);
     expect(autoMatched.find(Bolt).length).toEqual(1);
     expect(autoMatched.html()).toMatch('Auto Matched');
   });
 
 
   it('should return null for invalid type', () => {
-    const wrapper = mountWithIntl(<MediaClusterOriginButton type="invalidType" />);
+    const wrapper = mountWithIntl(<MediaOrigin type="invalidType" />);
     expect(wrapper.find('FormattedMessage').length).toEqual(0);
   });
 });
