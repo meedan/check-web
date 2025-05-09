0bd80c72d48bc4e041dfc072dcf44699d9a3c466
Sawy <melsawy.be7eri@gmail.com>
2025-05-09 19:58:48 +0300

    CV2-6018: fix creator filter

--- src/app/components/article/ArticleFilters.js
+++ src/app/components/article/ArticleFilters.js
@@ -63,7 +63,13 @@ const ArticleFilters = ({
   };
 
   const handleSubmit = () => {
-    onSubmit(filters);
+    const newFilters = { ...filters };
+    // Creator filter called `users` and mapped to `user_ids` for article filters so no need to keep the `user_ids` when save article filters.
+    if (newFilters.user_ids) {
+      delete newFilters.user_ids;
+    }
+    setFilters(newFilters);
+    onSubmit(newFilters);
   };
 
   const handleReset = () => {
