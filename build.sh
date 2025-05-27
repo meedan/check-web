c3b32b7babebcd86005842e4db2307d0309cbf56
danielevalverde <danierzevalverde@gmail.com>
2025-05-27 14:09:12 -0300

    [smoke tests] debug 5

--- build.sh
+++ build.sh
@@ -17,7 +17,8 @@ else
     docker-compose logs -f api-background &
     docker-compose logs web &
     docker-compose logs -f pender &
-    docker compose -f docker-compose.yml -f docker-test.yml up web api api-background pender pender-background chromedriver
+    docker compose -f docker-compose.yml -f docker-test.yml up web api api-background pender pender-background chromedriver &
+    # docker compose -f docker-compose.yml -f docker-test.yml up web api api-background pender pender-background chromedriver
   else
     if [[ $GITHUB_JOB_NAME == 'media-similarity-tests' ]]
     then
@@ -58,6 +59,7 @@ else
   fi
   until curl --silent -I -f --fail http://localhost:3200; do printf .; sleep 1; done
   until curl --silent -I -f --fail http://localhost:3000; do printf .; sleep 1; done
+  until curl --silent -I -f --fail http://localhost:3333; do printf .; sleep 1; done
   # Uncomment to debug Check API and Alegre. Warning: This can lead to Travis error "The job exceeded the maximum log length, and has been terminated.".
   # tail -f check-api/log/test.log &
   # docker-compose logs -f api &
