682c8802376a495f00b85d354473d1e46714041d
danielevalverde <danierzevalverde@gmail.com>
2025-05-27 14:02:27 -0300

    [smoke tests] debug 4

--- build.sh
+++ build.sh
@@ -11,13 +11,13 @@ then
 else
   if [[ $GITHUB_JOB_NAME == 'integration-and-unit-tests' ]]
   then
-    docker compose build web api api-background
+    docker compose build web api api-background pender pender-background
     tail -f check-api/log/test.log &
     docker-compose logs -f api &
     docker-compose logs -f api-background &
     docker-compose logs web &
     docker-compose logs -f pender &
-    docker compose -f docker-compose.yml -f docker-test.yml up web api api-background
+    docker compose -f docker-compose.yml -f docker-test.yml up web api api-background pender pender-background chromedriver
   else
     if [[ $GITHUB_JOB_NAME == 'media-similarity-tests' ]]
     then
