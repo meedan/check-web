18ad49c143cda5e0a0cc662b4b6b6fac8f5b500e
manu vasconcelos <manu@meedan.com>
2025-05-27 14:48:40 -0300

    move content and title to FeedDataPointsSection
    I think now that:
    - we have the listType prop,
    - we know we are not adding any new types of saved searches beyond article and media,
    - and because the content and title themselves are not dynamic
    
    We can use listType to determine which content and title to display, and
    move the content and title inside the FeedDataPointsSection component.
    
    I think this makes SaveFeed a little bit easier to read.
    I also think it's easier to find the content and title inside the component
    instead of having to find where the prop is being passed from.
    
    But I think this only makes sense because we now know we don't need to
    be as flexible.

--- src/app/components/feed/SaveFeed.js
+++ src/app/components/feed/SaveFeed.js
@@ -555,184 +555,20 @@ const SaveFeed = (props) => {
 
           <div className={styles.saveFeedCard}>
             <FeedDataPointsSection
-              content={
-                <>
-                  <FormattedHTMLMessage
-                    defaultMessage="<strong>Article</strong> data to be shared:"
-                    description="Title of the content section on save feed page where a user can select that fact-checks will be shared in this feed."
-                    id="feedDataPoints.ArticleContentTitle"
-                  />
-                  <ul>
-                    <FormattedMessage
-                      defaultMessage="Title"
-                      description="Media field listed on save feed page, as part of the list of media fields available when the feed is sharing media."
-                      id="feedDataPoints.feedDataPointArticleTitle"
-                      tagName="li"
-                    />
-                    <FormattedMessage
-                      defaultMessage="Summary"
-                      description="Media field listed on save feed page, as part of the list of media fields available when the feed is sharing media."
-                      id="feedDataPoints.feedDataPointArticleSummary"
-                      tagName="li"
-                    />
-                    <FormattedMessage
-                      defaultMessage="URL"
-                      description="Fact-check field listed on save feed page, as part of the list of fact-check fields available when the feed is sharing fact-checks."
-                      id="feedDataPoints.feedDataPointArticleUrl"
-                      tagName="li"
-                    />
-                    <FormattedMessage
-                      defaultMessage="Language"
-                      description="Fact-check field listed on save feed page, as part of the list of fact-check fields available when the feed is sharing fact-checks."
-                      id="feedDataPoints.feedDataPointArticleLanguage"
-                      tagName="li"
-                    />
-                    <FormattedMessage
-                      defaultMessage="Claim*"
-                      description="Media field listed on save feed page, as part of the list of media fields available when the feed is sharing media."
-                      id="feedDataPoints.feedDataPointClaim"
-                      tagName="li"
-                    />
-                    <FormattedMessage
-                      defaultMessage="Rating*"
-                      description="Fact-check field listed on save feed page, as part of the list of fact-check fields available when the feed is sharing fact-checks."
-                      id="feedDataPoints.feedDataPointRating"
-                      tagName="li"
-                    />
-                    <FormattedMessage
-                      defaultMessage="Date published*"
-                      description="Fact-check field listed on save feed page, as part of the list of fact-check fields available when the feed is sharing fact-checks."
-                      id="feedDataPoints.feedDataPointDataPublished"
-                      tagName="li"
-                    />
-                  </ul>
-                  <div className="typography-body2">
-                    <FormattedMessage
-                      defaultMessage="*If article shared is a claim & fact-check"
-                      description="Footer of section on save feed page where a user can select that claim & fact-checks will be shared in this feed."
-                      id="feedDataPoints.factChecksContentFooter"
-                    />
-                  </div>
-                </>
-              }
               enabled={formData.dataPoints.includes(CheckFeedDataPoints.PUBLISHED_FACT_CHECKS)}
               listId={formData.selectedListId}
               listType="article"
               readOnly={Boolean(feed.id)}
-              title={
-                <FormattedMessage
-                  defaultMessage="Articles"
-                  description="Title of section on save feed page where a user can select that fact-checks will be shared in this feed."
-                  id="feedDataPoints.factChecksTitle"
-                />
-              }
               onChange={e => handleFormUpdate('selectedListId', +e.target.value)}
               onRemove={() => handleFormUpdate('selectedListId', null)}
               onToggle={enabled => toggleDataPoint(enabled, CheckFeedDataPoints.PUBLISHED_FACT_CHECKS)}
             />
 
             <FeedDataPointsSection
-              content={
-                <>
-                  <FormattedHTMLMessage
-                    defaultMessage="<strong>Media Cluster</strong> data to be shared:"
-                    description="Title of the content section on save feed page where a user can select that media data will be shared in this feed."
-                    id="feedDataPoints.mediaContentTitle"
-                  />
-                  <ul>
-                    <FormattedMessage
-                      defaultMessage="Type of media (image, video, etc.)"
-                      description="Media field listed on save feed page, as part of the list of media fields available when the feed is sharing media."
-                      id="feedDataPoints.feedDataPointTypeOfMedia"
-                      tagName="li"
-                    />
-                    <FormattedMessage
-                      defaultMessage="Number of requests for each media in the cluster"
-                      description="Media field listed on save feed page, as part of the list of media fields available when the feed is sharing media."
-                      id="feedDataPoints.feedDataPointNumberOfRequests"
-                      tagName="li"
-                    />
-                    <FormattedMessage
-                      defaultMessage="Date last submitted"
-                      description="Media field listed on save feed page, as part of the list of media fields available when the feed is sharing media."
-                      id="feedDataPoints.feedDataPointDateLastSubmitted"
-                      tagName="li"
-                    />
-                    <FormattedMessage
-                      defaultMessage="Language"
-                      description="Media field listed on save feed page, as part of the list of media fields available when the feed is sharing media."
-                      id="feedDataPoints.feedDataPointMediaLanguage"
-                      tagName="li"
-                    />
-                    <FormattedMessage
-                      defaultMessage="Media content when present, such as an image, description, or transcript"
-                      description="Media field listed on save feed page, as part of the list of media fields available when the feed is sharing media."
-                      id="feedDataPoints.feedDataPointMediaContent"
-                      tagName="li"
-                    />
-                  </ul>
-                  <FormattedHTMLMessage
-                    defaultMessage="<strong>Request</strong> data to be shared:"
-                    description="Title of the content section on save feed page where a user can select that request data will be shared in this feed."
-                    id="feedDataPoints.requestContentTitle"
-                  />
-                  <ul>
-                    <FormattedMessage
-                      defaultMessage="Anonymized unique user ID (example: 7299e0)"
-                      description="Request field listed on save feed page, as part of the list of request fields available when the feed is sharing requests. 7299e0 doesn't need to be localized."
-                      id="feedDataPoints.feedDataPointAnonymizedUserId"
-                      tagName="li"
-                    />
-                    <FormattedMessage
-                      defaultMessage="Date"
-                      description="Request field listed on save feed page, as part of the list of request fields available when the feed is sharing requests."
-                      id="feedDataPoints.feedDataPointDate"
-                      tagName="li"
-                    />
-                    <FormattedMessage
-                      defaultMessage="Platform (WhatsApp, Telegram, etc.)"
-                      description="Request field listed on save feed page, as part of the list of request fields available when the feed is sharing requests."
-                      id="feedDataPoints.feedDataPointPlatform"
-                      tagName="li"
-                    />
-                    <FormattedMessage
-                      defaultMessage="Language"
-                      description="Request field listed on save feed page, as part of the list of request fields available when the feed is sharing requests."
-                      id="feedDataPoints.feedDataPointRequestLanguage"
-                      tagName="li"
-                    />
-                  </ul>
-                  <div className="typography-body2">
-                    <FormattedMessage
-                      defaultMessage="Learn more about {mediaDataArticleLink}."
-                      description="Footer of section on save feed page where a user can select that media and requests will be shared in this feed."
-                      id="feedDataPoints.mediaContentFooter"
-                      values={{
-                        mediaDataArticleLink: (
-                          <a href="https://help.checkmedia.org/en/articles/8542417-shared-feed-creation#h_7414a7ab59" rel="noopener noreferrer" target="_blank"> {/* FIXME: Put the correct link here */}
-                            <FormattedMessage
-                              defaultMessage="media & requests data"
-                              description="Text of the link to an article that explains media and requests data. This link is displayed on the media/requests data points section on the save feed page."
-                              id="feedDataPoints.mediaDataArticleLink"
-                            />
-                          </a>
-                        ),
-                      }}
-                    />
-                  </div>
-                </>
-              }
               enabled={formData.dataPoints.includes(CheckFeedDataPoints.MEDIA_CLAIM_REQUESTS)}
               listId={formData.selectedListId}
               listType="media"
               readOnly={Boolean(feed.id)}
-              title={
-                <FormattedMessage
-                  defaultMessage="Media Clusters and Requests"
-                  description="Title of section on save feed page where a user can select that media clusters and requests will be shared in this feed."
-                  id="feedDataPoints.feedDataPointsFactChecksMediaClaimsRequestsTitle"
-                />
-              }
               onChange={e => handleFormUpdate('selectedListId', +e.target.value)}
               onRemove={() => handleFormUpdate('selectedListId', null)}
               onToggle={enabled => toggleDataPoint(enabled, CheckFeedDataPoints.MEDIA_CLAIM_REQUESTS)}
