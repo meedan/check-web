317f6d6b05e27813e80ae85d065e8888018d819d
danielevalverde <danierzevalverde@gmail.com>
2025-06-10 14:36:25 -0300

    update tests and update localization

--- src/app/components/team/WebhookEntry.test.js
+++ src/app/components/team/WebhookEntry.test.js
@@ -18,12 +18,16 @@ describe('<WebhookEntry />', () => {
 
   it('should display the event name correctly', () => {
     const wrapper = shallowWithIntl(<WebhookEntry intl={mockIntl} webhook={webhook} />);
-    expect(wrapper.find('TextField').prop('value')).toEqual('Publish Report');
+    expect(wrapper.find('span').hostNodes().text()).toEqual('Test Webhook');
+    const eventField = wrapper.findWhere(node => node.prop('value') === 'Report Published');
+    expect(eventField.exists()).toBe(true);
   });
 
   it('should not crash if event code has no matching message', () => {
     webhook.events[0].event = 'unknown_event';
     const wrapper = shallowWithIntl(<WebhookEntry intl={mockIntl} webhook={webhook} />);
-    expect(wrapper.find('TextField').prop('value')).toEqual('unknown_event');
+    expect(wrapper.find('span').hostNodes().text()).toEqual('Test Webhook');
+    const eventField = wrapper.findWhere(node => node.prop('value') === 'unknown_event');
+    expect(eventField.exists()).toBe(true);
   });
 });
