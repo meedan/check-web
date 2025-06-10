e7464a4038c6f7e5e0de618e0ee3532105872ffc
Alexandre Amorim <amoedoamorim@gmail.com>
2025-06-10 13:48:27 -0300

    Add unit test to validate fix

--- src/app/components/team/WebhookEntry.test.js
+++ src/app/components/team/WebhookEntry.test.js
@@ -15,4 +15,15 @@ describe('<WebhookEntry />', () => {
     expect(wrapper.find('span').hostNodes().text()).toEqual('Test Webhook');
     expect(wrapper.find('strong').hostNodes().text()).toEqual('https://example.com/webhook');
   });
+
+  it('should display the event name correctly', () => {
+    const wrapper = shallowWithIntl(<WebhookEntry intl={mockIntl} webhook={webhook} />);
+    expect(wrapper.find('TextField').prop('value')).toEqual('Publish Report');
+  });
+
+  it('should not crash if event code has no matching message', () => {
+    webhook.events[0].event = 'unknown_event';
+    const wrapper = shallowWithIntl(<WebhookEntry intl={mockIntl} webhook={webhook} />);
+    expect(wrapper.find('TextField').prop('value')).toEqual('unknown_event');
+  });
 });
