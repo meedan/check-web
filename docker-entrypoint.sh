0c0d8de34be11f242432e613d421dbde6ead795a
danielevalverde <danierzevalverde@gmail.com>
2025-06-17 11:40:20 -0300

    remove debug [smoke tests]

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-echo "[entrypoint] Valor de MODE = '$MODE'"
-
 if [ "$MODE" = "test" ]; then
-  echo "[entrypoint] MODE=test → iniciando nginx"
   service nginx start
-  service nginx status || echo "[entrypoint] ⚠️ nginx failed to start!"
+  service nginx status || echo "[entrypoint] nginx failed to start!"
 else
-  echo "[entrypoint] MODE=$MODE → não iniciando nginx"
-  service nginx status || echo "[entrypoint] nginx não está rodando"
+  service nginx status || echo "[entrypoint] nginx is not running!"
 fi
 
 exec "$@"
-echo "[entrypoint] Executando o comando: $@"
