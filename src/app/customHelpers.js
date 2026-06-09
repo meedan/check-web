08fa4492b399439e016f26eb89a9fd0440fb9e9c
Mohamed El-Sawy <melsawy.be7eri@gmail.com>
2026-06-09 20:56:41 +0300

    CV2-6717: move to .org mails (#2473)

--- src/app/customHelpers.js
+++ src/app/customHelpers.js
@@ -3,11 +3,11 @@ import config from 'config'; // eslint-disable-line require-path-exists/exists
 const customStrings = {
   check: {
     ABOUT_URL: 'https://meedan.com/check',
-    ADMIN_EMAIL: 'check@meedan.com',
+    ADMIN_EMAIL: 'check@meedan.org',
     CONTACT_HUMAN_URL: 'https://meedan.typeform.com/to/Tvf0b8',
     PP_URL: 'https://meedan.com/legal/privacy-policy',
-    SUPPORT_EMAIL: 'check@meedan.com',
-    PRIVACY_EMAIL: 'privacy@meedan.com',
+    SUPPORT_EMAIL: 'check@meedan.org',
+    PRIVACY_EMAIL: 'privacy@meedan.org',
     TOS_URL: 'https://meedan.com/legal/terms-of-service',
     LOGO_URL: '/images/logo/check.svg',
   },
